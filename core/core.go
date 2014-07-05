cd1d4d44552665bc884aca6722f3ece8c675841c
Juan Batiz-Benet <juan@benet.ai>
2014-07-05 15:38:41 -0700

    core block service

--- core/core.go
+++ core/core.go
@@ -3,8 +3,11 @@ package core
 import (
 	"fmt"
 	ds "github.com/jbenet/datastore.go"
+	blocks "github.com/jbenet/go-ipfs/blocks"
 	config "github.com/jbenet/go-ipfs/config"
+	merkledag "github.com/jbenet/go-ipfs/merkledag"
 	peer "github.com/jbenet/go-ipfs/peer"
+	u "github.com/jbenet/go-ipfs/util"
 )
 
 // IPFS Core module. It represents an IPFS instance.
@@ -33,7 +36,7 @@ type IpfsNode struct {
 	// BitSwap *bitswap.BitSwap
 
 	// the block service, get/add blocks.
-	// Blocks *blocks.BlockService
+	Blocks *blocks.BlockService
 
 	// the path resolution system
 	// Resolver *resolver.PathResolver
@@ -52,11 +55,31 @@ func NewIpfsNode(cfg *config.Config) (*IpfsNode, error) {
 		return nil, err
 	}
 
+	bs, err := blocks.NewBlockService(d)
+	if err != nil {
+		return nil, err
+	}
+
 	n := &IpfsNode{
 		Config:    cfg,
 		PeerBook:  &peer.PeerBook{},
 		Datastore: d,
+		Blocks:    bs,
 	}
 
 	return n, nil
+}
+
+func (n *IpfsNode) AddDagNode(nd *merkledag.Node) (u.Key, error) {
+	d, err := nd.Encoded(false)
+	if err != nil {
+		return "", err
+	}
+
+	b, err := blocks.NewBlock(d)
+	if err != nil {
+		return "", err
+	}
+
+	return n.Blocks.AddBlock(b)
 }
