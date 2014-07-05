0d06f2e502ffc9b52c6620e5795e41f253215f73
Juan Batiz-Benet <juan@benet.ai>
2014-07-05 12:45:50 -0700

    refmt

--- cli/ipfs.go
+++ cli/ipfs.go
@@ -1,17 +1,58 @@
 package main
 
 import (
-  "fmt"
-  "os"
+	"fmt"
+	"github.com/gonuts/flag"
+	"github.com/jbenet/commander"
+	u "github.com/jbenet/go-ipfs/util"
+	"os"
 )
 
+var CmdIpfs = &commander.Command{
+	UsageLine: "ipfs [<flags>] <command> [<args>]",
+	Short:     "global versioned p2p merkledag file system",
+	Long: `ipfs - global versioned p2p merkledag file system
+
+Basic commands:
+
+    add <path>    Add an object to ipfs.
+    cat <ref>     Show ipfs object data.
+    ls <ref>      List links from an object.
+    refs <ref>    List link hashes from an object.
+
+Tool commands:
+
+    config      Manage configuration.
+    version     Show ipfs version information.
+    commands    List all available commands.
+
+Advanced Commands:
+
+    mount       Mount a ipfs a read-only mountpoint.
+
+Use "ipfs help <command>" for more information about a command.
+`,
+	Run: ipfsCmd,
+	Subcommands: []*commander.Command{
+		cmdIpfsVersion,
+		// cmdIpfsConfig,
+		cmdIpfsCommands,
+	},
+	Flag: *flag.NewFlagSet("ipfs", flag.ExitOnError),
+}
+
+func ipfsCmd(c *commander.Command, args []string) error {
+	u.POut(c.Long)
+	return nil
+}
+
 func main() {
-  err := CmdIpfs.Dispatch(os.Args[1:])
-  if err != nil {
-    if len(err.Error()) > 0 {
-      fmt.Fprintf(os.Stderr, "%v\n", err)
-    }
-    os.Exit(1)
-  }
-  return
+	err := CmdIpfs.Dispatch(os.Args[1:])
+	if err != nil {
+		if len(err.Error()) > 0 {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+		}
+		os.Exit(1)
+	}
+	return
 }
--- cli/version.go
+++ cli/version.go
@@ -1,31 +1,31 @@
 package main
 
 import (
-  "github.com/jbenet/commander"
-  u "github.com/jbenet/go-ipfs/util"
+	"github.com/jbenet/commander"
+	u "github.com/jbenet/go-ipfs/util"
 )
 
 const Version = "0.1.0"
 
 var cmdIpfsVersion = &commander.Command{
-  UsageLine: "version",
-  Short:     "Show ipfs version information.",
-  Long: `ipfs version - Show ipfs version information.
+	UsageLine: "version",
+	Short:     "Show ipfs version information.",
+	Long: `ipfs version - Show ipfs version information.
 
     Returns the current version of ipfs and exits.
   `,
-  Run:  versionCmd,
+	Run: versionCmd,
 }
 
 func init() {
-  cmdIpfsVersion.Flag.Bool("number", false, "show only the number")
+	cmdIpfsVersion.Flag.Bool("number", false, "show only the number")
 }
 
 func versionCmd(c *commander.Command, _ []string) error {
-  number := c.Flag.Lookup("number").Value.Get().(bool)
-  if !number {
-    u.POut("ipfs version ")
-  }
-  u.POut("%s\n", Version)
-  return nil
+	number := c.Flag.Lookup("number").Value.Get().(bool)
+	if !number {
+		u.POut("ipfs version ")
+	}
+	u.POut("%s\n", Version)
+	return nil
 }
