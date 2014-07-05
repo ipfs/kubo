1d3e72a33b62cbf6b6f55e416ee9301f4e54fbf4
Juan Batiz-Benet <juan@benet.ai>
2014-07-05 15:02:19 -0700

    gofmt

--- util/util_test.go
+++ util/util_test.go
@@ -1,28 +1,28 @@
 package util
 
 import (
-  "bytes"
-  "fmt"
-  mh "github.com/jbenet/go-multihash"
-  "testing"
+	"bytes"
+	"fmt"
+	mh "github.com/jbenet/go-multihash"
+	"testing"
 )
 
 func TestKey(t *testing.T) {
 
-  h1, err := mh.Sum([]byte("beep boop"), mh.SHA2_256, -1)
-  if err != nil {
-    t.Error(err)
-  }
+	h1, err := mh.Sum([]byte("beep boop"), mh.SHA2_256, -1)
+	if err != nil {
+		t.Error(err)
+	}
 
-  k1 := Key(h1)
-  h2 := mh.Multihash(k1)
-  k2 := Key(h2)
+	k1 := Key(h1)
+	h2 := mh.Multihash(k1)
+	k2 := Key(h2)
 
-  if !bytes.Equal(h1, h2) {
-    t.Error("Multihashes not equal.")
-  }
+	if !bytes.Equal(h1, h2) {
+		t.Error("Multihashes not equal.")
+	}
 
-  if k1 != k2 {
-    t.Error("Keys not equal.")
-  }
+	if k1 != k2 {
+		t.Error("Keys not equal.")
+	}
 }
