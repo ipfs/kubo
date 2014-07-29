171f96b794aae13cafe72d81705ba246574fb0aa
Jeromy Johnson <jeromy.johnson@isilon.com>
2014-07-29 14:50:33 -0700

    update messages and add some new code around handling/creating messages

--- routing/dht/dht.go
+++ routing/dht/dht.go
@@ -2,6 +2,7 @@ package dht
 
 import (
 	swarm "github.com/jbenet/go-ipfs/swarm"
+	"sync"
 )
 
 // TODO. SEE https://github.com/jbenet/node-ipfs/blob/master/submodules/ipfs-dht/index.js
@@ -10,13 +11,42 @@ import (
 // IpfsDHT is an implementation of Kademlia with Coral and S/Kademlia modifications.
 // It is used to implement the base IpfsRouting module.
 type IpfsDHT struct {
-  routes RoutingTable
+	routes RoutingTable
 
-  network *swarm.Swarm
+	network *swarm.Swarm
+
+	listeners map[uint64]chan swarm.Message
+	listenLock sync.RWMutex
 }
 
+// Read in all messages from swarm and handle them appropriately
+// NOTE: this function is just a quick sketch
 func (dht *IpfsDHT) handleMessages() {
 	for mes := range dht.network.Chan.Incoming {
+		for {
+			select {
+			case mes := <-dht.network.Chan.Incoming:
+				// Unmarshal message
+				dht.listenLock.RLock()
+				ch, ok := dht.listeners[id]
+				dht.listenLock.RUnlock()
+				if ok {
+					// Send message to waiting goroutine
+					ch <- mes
+				}
 
+			//case closeChan: or something
+			}
+		}
 	}
+}
+
+// Register a handler for a specific message ID, used for getting replies
+// to certain messages (i.e. response to a GET_VALUE message)
+func (dht *IpfsDHT) ListenFor(mesid uint64) <-chan swarm.Message {
+	lchan := make(chan swarm.Message)
+	dht.listenLock.Lock()
+	dht.listeners[mesid] = lchan
+	dht.listenLock.Unlock()
+	return lchan
 }
