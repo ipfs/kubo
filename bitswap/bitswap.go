b784614c2173090d2740b4613732fce0f090bffd
Juan Batiz-Benet <juan@benet.ai>
2014-07-30 01:13:47 -0700

    fixed build errs
    netmux got replaced for the most part by swarm.

--- bitswap/bitswap.go
+++ bitswap/bitswap.go
@@ -1,28 +1,24 @@
 package bitswap
 
 import (
-	"github.com/jbenet/go-ipfs/blocks"
-	mh "github.com/jbenet/go-multihash"
-
-	"time"
+  "time"
+  mh "github.com/jbenet/go-multihash"
+  blocks "github.com/jbenet/go-ipfs/blocks"
+  u "github.com/jbenet/go-ipfs/util"
 )
 
 // aliases
 
 type Ledger struct {
 	Owner mh.Multihash
-
 	Partner mh.Multihash
-
 	BytesSent uint64
-
 	BytesRecv uint64
-
 	Timestamp *time.Time
 }
 
 type BitSwap struct {
-	Ledgers  map[string]*Ledger
-	HaveList map[string]*blocks.Block
-	WantList []*multihash.Multihash
+  Ledgers map[u.Key]*Ledger  // key is peer.ID
+  HaveList map[u.Key]*blocks.Block // key is multihash
+  WantList []*mh.Multihash
 }
