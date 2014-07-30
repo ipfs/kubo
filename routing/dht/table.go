550971fba6d734d8035e7b1ba5d0171df0ebe0d8
Juan Batiz-Benet <juan@benet.ai>
2014-07-29 17:53:31 -0700

    equalize sizes

--- routing/dht/table.go
+++ routing/dht/table.go
@@ -1,61 +1,75 @@
 package dht
 
 import (
-  "container/list"
+	"bytes"
+	"container/list"
 )
-
 
 // ID for IpfsDHT should be a byte slice, to allow for simpler operations
 // (xor). DHT ids are based on the peer.IDs.
 //
-// NOTE: peer.IDs are biased because they are (a) multihashes (first bytes
-// biased), and (b) first bits are zeroes when using the S/Kademlia PoW.
-// Thus, may need to re-hash keys (uniform dist). TODO(jbenet)
+// NOTE: peer.IDs are biased because they are multihashes (first bytes
+// biased). Thus, may need to re-hash keys (uniform dist). TODO(jbenet)
 type ID []byte
 
 // Bucket holds a list of peers.
 type Bucket []*list.List
 
-
 // RoutingTable defines the routing table.
 type RoutingTable struct {
 
-  // kBuckets define all the fingers to other nodes.
-  Buckets []Bucket
+	// kBuckets define all the fingers to other nodes.
+	Buckets []Bucket
 }
 
+func (id ID) Equal(other ID) bool {
+	return bytes.Equal(id, other)
+}
+
+func (id ID) Less(other interface{}) bool {
+	a, b := equalizeSizes(id, other.(ID))
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			return a[i] < b[i]
+		}
+	}
+	return len(a) < len(b)
+}
 
 func (id ID) commonPrefixLen() int {
-  for i := 0; i < len(id); i++ {
-    for j := 0; j < 8; j++ {
-      if (id[i] >> uint8(7 - j)) & 0x1 != 0 {
-        return i * 8 + j;
-      }
-    }
-  }
-  return len(id) * 8 - 1;
+	for i := 0; i < len(id); i++ {
+		for j := 0; j < 8; j++ {
+			if (id[i]>>uint8(7-j))&0x1 != 0 {
+				return i*8 + j
+			}
+		}
+	}
+	return len(id)*8 - 1
 }
 
 func xor(a, b ID) ID {
+	a, b = equalizeSizes(a, b)
 
-  // ids may actually be of different sizes.
-  var ba ID
-  var bb ID
-  if len(a) >= len(b) {
-    ba = a
-    bb = b
-  } else {
-    ba = b
-    bb = a
-  }
+	c := make(ID, len(a))
+	for i := 0; i < len(a); i++ {
+		c[i] = a[i] ^ b[i]
+	}
+	return c
+}
 
-  c := make(ID, len(ba))
-  for i := 0; i < len(ba); i++ {
-    if len(bb) > i {
-      c[i] = ba[i] ^ bb[i]
-    } else {
-      c[i] = ba[i] ^ 0
-    }
-  }
-  return c
+func equalizeSizes(a, b ID) (ID, ID) {
+	la := len(a)
+	lb := len(b)
+
+	if la < lb {
+		na := make([]byte, lb)
+		copy(na, a)
+		a = na
+	} else if lb < la {
+		nb := make([]byte, la)
+		copy(nb, b)
+		b = nb
+	}
+
+	return a, b
 }
