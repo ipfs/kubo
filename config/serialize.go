7618d7830e60df0c1216999b986514a3f0f90962
Peter Borzov <peter@sowingo.com>
2014-07-29 21:45:56 -0400

    ipfs config command with git-style option value getters and setters
    Adds "ipfs config" command designed in git style.
    See specific config's values with:
            $ipfs config datastore.path
            ~/.go-ipfs/datastore
    Assign a new value with:
            $ipfs config datastore.path ~/.go-ipfs/datastore
    
    Open the config file in your default editor(as taken from $EDITOR enviroment variable):
            ipfs config edit

--- config/serialize.go
+++ config/serialize.go
@@ -2,9 +2,13 @@ package config
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
+	u "github.com/jbenet/go-ipfs/util"
 	"io/ioutil"
 	"os"
 	"path"
+	"strings"
 )
 
 // WriteFile writes the given buffer `buf` into file named `filename`.
@@ -35,4 +39,83 @@ func WriteConfigFile(filename string, cfg *Config) error {
 	}
 
 	return WriteFile(filename, buf)
+}
+
+// WriteConfigFile writes the config from `cfg` into `filename`.
+func GetValueInConfigFile(key string) (value string, err error) {
+	// reading config file
+	attrs := strings.Split(key, ".")
+
+	filename, _ := u.TildeExpansion(DefaultConfigFilePath)
+	buf, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return "", err
+	}
+
+	// deserializing json
+	var cfg interface{}
+	var exists bool
+
+	err = json.Unmarshal(buf, &cfg)
+	if err != nil {
+		return "", err
+	}
+
+	for i := range attrs {
+		cfgMap, isMap := cfg.(map[string]interface{})
+		if !isMap {
+			return "", errors.New(fmt.Sprintf("%s has no attributes", strings.Join(attrs[:i], ".")))
+		}
+		cfg, exists = cfgMap[attrs[i]]
+		if !exists {
+			return "", errors.New(fmt.Sprintf("Configuration option key \"%s\" not recognized", strings.Join(attrs[:i+1], ".")))
+		}
+		val, is_string := cfg.(string)
+		if is_string {
+			return val, nil
+		}
+	}
+	return "", errors.New(fmt.Sprintf("%s is not a string", key))
+}
+
+// WriteConfigFile writes the config from `cfg` into `filename`.
+func SetValueInConfigFile(key string, values []string) error {
+	assignee := strings.Join(values, " ")
+	attrs := strings.Split(key, ".")
+
+	filename, _ := u.TildeExpansion(DefaultConfigFilePath)
+	buf, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+
+	// deserializing json
+	var cfg, orig interface{}
+	var exists, isMap bool
+	cfgMap := make(map[string]interface{})
+
+	err = json.Unmarshal(buf, &orig)
+	cfg = orig
+	if err != nil {
+		return err
+	}
+
+	for i := 0; i < len(attrs); i++ {
+		cfgMap, isMap = cfg.(map[string]interface{})
+		// curs = append(curs, cfgMap)
+		if !isMap {
+			return errors.New(fmt.Sprintf("%s has no attributes", strings.Join(attrs[:i], ".")))
+		}
+		cfg, exists = cfgMap[attrs[i]]
+		if !exists {
+			return errors.New(fmt.Sprintf("Configuration option key \"%s\" not recognized", strings.Join(attrs[:i+1], ".")))
+		}
+	}
+	cfgMap[attrs[len(attrs)-1]] = assignee
+	buf, err = json.MarshalIndent(orig, "", "  ")
+	if err != nil {
+		return err
+	}
+	WriteFile(filename, buf)
+	return nil
 }
