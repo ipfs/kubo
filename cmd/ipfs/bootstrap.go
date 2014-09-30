0c41c5758d837bf17f3748fcf8d61c99fd3a5ab1
Siraj Ravel <jason.ravel@cbsinteractive.com>
2014-09-29 20:01:21 -0700

    pulling peerID and address wonderfully

--- cmd/ipfs/bootstrap.go
+++ cmd/ipfs/bootstrap.go
@@ -6,6 +6,10 @@ import (
 	"github.com/jbenet/go-ipfs/Godeps/_workspace/src/github.com/jbenet/commander"
 	//"github.com/jbenet/go-ipfs/core/commands"
 	"fmt"
+    "io/ioutil"
+    "encoding/json"
+	
+	
 )
 
 
@@ -21,10 +25,61 @@ var cmdIpfsBootstrap = &commander.Command{
 	Flag: *flag.NewFlagSet("ipfs-bootstrap", flag.ExitOnError),
 }
 
+
+type Peer struct {
+	PeerID     string
+	Address string
+}
+
+type Config struct {
+	Bootstrap []Peer
+}
+
+var in = `{
+    "peers": [
+        {
+            "pid": 1,
+            "address": "127.0.0.1:17001"
+        },
+        {
+            "pid": 2,
+            "address": "127.0.0.1:17002"
+        }
+    ]
+}`
+
+
+
 func bootstrapCmd(c *commander.Command, inp []string) error {
 	
-	fmt.Printf("\n Test \n")
 	
+    dat, _ := ioutil.ReadFile("$HOME/.go-ipfs/config")
+    var configText = string(dat)
+	
+	
+	 var conf Config
+ 	 err := json.Unmarshal([]byte(configText), &conf)
+
+	 if err != nil {
+		fmt.Print("Error:", err)
+	 }
+
+ 	fmt.Printf("%#v\n", conf)
+	fmt.Printf("%#v\n", conf.Bootstrap[0].PeerID)
 	return nil
 
-}
+ }
+//
+// func (q *JSONData) FromJSON(file string) error {
+//
+//     //Reading JSON file
+//     J, err := ioutil.ReadFile(file)
+//     if err != nil {
+// 		panic(err)
+// 	}
+//
+//
+//     var data = &q
+//     //Umarshalling JSON into struct
+//     return json.Unmarshal(J, data)
+// }
