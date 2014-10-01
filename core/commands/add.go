mappum/go-ipfs

fixes to make interface more usable

core/commands/add.go
@@ -62,6 +62,15 @@ func addFile(n *core.IpfsNode, fpath string, depth int) (*dag.Node, error) {
 		return nil, err
 	}
 
+	k, err := root.Key()
+	if err != nil {
+		return nil, err
+	}
+	fmt.Printf("Adding file: %s = %s\n", fpath, k.Pretty())
+	for _, l := range root.Links {
+		fmt.Printf("SubBlock: %s\n", l.Hash.B58String())
+	}
+
 	return root, addNode(n, root, fpath)
 }
 

ipfs/kubo

core/commands/add.go
@@ -103,6 +103,16 @@ func addFile(n *core.IpfsNode, fpath string, depth int) (*dag.Node, error) {
 		return nil, err
 	}
 
+	k, err := root.Key()
+	if err != nil {
+		return nil, err
+	}
+
+	log.Info("Adding file: %s = %s\n", fpath, k.Pretty())
+	for _, l := range root.Links {
+		log.Info("SubBlock: %s\n", l.Hash.B58String())
+	}
+
 	return root, addNode(n, root, fpath)
 }
 