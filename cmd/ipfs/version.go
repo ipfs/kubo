bd9142f042c30a6d442173da9d63caae86ba03c5
Peter Borzov <peter@sowingo.com>
2014-10-06 17:26:52 -0400

    Turn on update checker for local nodes

--- cmd/ipfs/version.go
+++ cmd/ipfs/version.go
@@ -13,7 +13,7 @@ import (
 
 // The IPFS version.
 const (
-	Version                   = "0.2.0"
+	Version                   = "0.1.0"
 	EndpointURLLatestReleases = "https://api.github.com/repos/jbenet/go-ipfs/tags"
 	VersionErrorShort         = `Warning: You are running version %s of go-ipfs. The latest version is %s.`
 	VersionErrorLong          = `
