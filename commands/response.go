a3a843759b4ed6db41ac8b2c316bc27938b0e11d
Matt Bell <mappum@gmail.com>
2014-10-10 13:33:47 -0700

    commands: Added marshalling to Response

--- commands/response.go
+++ commands/response.go
@@ -1,5 +1,12 @@
 package commands
 
+import (
+  "fmt"
+  "strings"
+  "encoding/json"
+  "encoding/xml"
+)
+
 type ErrorType uint
 const (
   Normal ErrorType = iota // general errors
@@ -11,6 +18,19 @@ const (
 type Error struct {
   message string
   code ErrorType
+}
+
+type EncodingType string
+const (
+  Json = "json"
+  Xml = "xml"
+  // TODO: support more encoding types
+)
+
+type Marshaller func(v interface{})([]byte, error)
+var marshallers = map[EncodingType]Marshaller{
+  Json: json.Marshal,
+  Xml: xml.Marshal,
 }
 
 type Response struct {
@@ -29,6 +49,17 @@ func (r *Response) FormatError() Error {
   return Error{ r.Error.Error(), r.ErrorType }
 }
 
-/*func (r *Response) Encode() ([]byte, error) {
+func (r *Response) Marshal() ([]byte, error) {
+  enc := r.req.Option("enc")
+  if enc == nil {
+    return nil, fmt.Errorf("No encoding type was specified")
+  }
+  encType := EncodingType(strings.ToLower(enc.(string)))
 
-}*/
+  marshaller, ok := marshallers[encType]
+  if !ok {
+    return nil, fmt.Errorf("No marshaller found for encoding type '%s'", enc)
+  }
+
+  return marshaller(r.Value)
+}
