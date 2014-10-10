df034c9c0be29c4e0dc875a06b63133140c4448d
Matt Bell <mappum@gmail.com>
2014-10-10 12:37:11 -0700

    commands: Updated Command tests for new Response API

--- commands/command_test.go
+++ commands/command_test.go
@@ -8,60 +8,58 @@ func TestOptionValidation(t *testing.T) {
       Option{ []string{ "b", "beep" }, Int },
       Option{ []string{ "B", "boop" }, String },
     },
-    f: func(req *Request) (interface{}, error) {
-      return nil, nil
-    },
+    f: func(req *Request, res *Response) {},
   }
 
   req := NewRequest()
   req.options["foo"] = 5
-  _, err := cmd.Call(nil, req)
-  if err == nil {
+  res := cmd.Call(nil, req)
+  if res.Error == nil {
     t.Error("Should have failed (unrecognized command)")
   }
 
   req = NewRequest()
   req.options["beep"] = 5
   req.options["b"] = 10
-  _, err = cmd.Call(nil, req)
-  if err == nil {
+  res = cmd.Call(nil, req)
+  if res.Error == nil {
     t.Error("Should have failed (duplicate options)")
   }
 
   req = NewRequest()
   req.options["beep"] = "foo"
-  _, err = cmd.Call(nil, req)
-  if err == nil {
+  res = cmd.Call(nil, req)
+  if res.Error == nil {
     t.Error("Should have failed (incorrect type)")
   }
 
   req = NewRequest()
   req.options["beep"] = 5
-  _, err = cmd.Call(nil, req)
-  if err != nil {
+  res = cmd.Call(nil, req)
+  if res.Error != nil {
     t.Error("Should have passed")
   }
 
   req = NewRequest()
   req.options["beep"] = 5
   req.options["boop"] = "test"
-  _, err = cmd.Call(nil, req)
-  if err != nil {
+  res = cmd.Call(nil, req)
+  if res.Error != nil {
     t.Error("Should have passed")
   }
 
   req = NewRequest()
   req.options["b"] = 5
   req.options["B"] = "test"
-  _, err = cmd.Call(nil, req)
-  if err != nil {
+  res = cmd.Call(nil, req)
+  if res.Error != nil {
     t.Error("Should have passed")
   }
 
   req = NewRequest()
   req.options["enc"] = "json"
-  _, err = cmd.Call(nil, req)
-  if err != nil {
+  res = cmd.Call(nil, req)
+  if res.Error != nil {
     t.Error("Should have passed")
   }
 }
@@ -72,45 +70,35 @@ func TestRegistration(t *testing.T) {
       Options: []Option{
         Option{ []string{ "beep" }, Int },
       },
-      f: func(req *Request) (interface{}, error) {
-        return nil, nil
-      },
+      f: func(req *Request, res *Response) {},
     },
 
     &Command{
       Options: []Option{
         Option{ []string{ "boop" }, Int },
       },
-      f: func(req *Request) (interface{}, error) {
-        return nil, nil
-      },
+      f: func(req *Request, res *Response) {},
     },
 
     &Command{
       Options: []Option{
         Option{ []string{ "boop" }, String },
       },
-      f: func(req *Request) (interface{}, error) {
-        return nil, nil
-      },
+      f: func(req *Request, res *Response) {},
     },
 
     &Command{
       Options: []Option{
         Option{ []string{ "bop" }, String },
       },
-      f: func(req *Request) (interface{}, error) {
-        return nil, nil
-      },
+      f: func(req *Request, res *Response) {},
     },
 
     &Command{
       Options: []Option{
         Option{ []string{ "enc" }, String },
       },
-      f: func(req *Request) (interface{}, error) {
-        return nil, nil
-      },
+      f: func(req *Request, res *Response) {},
     },
   }
 
