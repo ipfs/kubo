d1595ce34cb388db250ab69a28078a31505f0a70
Matt Bell <mappum@gmail.com>
2014-10-10 12:38:03 -0700

    commands: Added basic methods to Request

--- commands/request.go
+++ commands/request.go
@@ -3,18 +3,20 @@ package commands
 // Request represents a call to a command from a consumer
 type Request struct {
   options map[string]interface{}
+  arguments []string
 }
 
-/*func (r *Request) Option(name string) interface{} {
-
+func (r *Request) Option(name string) interface{} {
+  return r.options[name]
 }
 
-func (r *Request) Arguments() interface{} {
-
-}*/
+func (r *Request) Arguments() []string {
+  return r.arguments
+}
 
 func NewRequest() *Request {
   return &Request{
     make(map[string]interface{}),
+    make([]string, 0),
   }
 }
