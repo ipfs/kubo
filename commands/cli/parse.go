12a6a87b2c8a23ebace8034f4345ff65034d52d2
Matt Bell <mappum@gmail.com>
2014-10-21 18:27:05 -0700

    commands/cli: Made Parse return a Request (again)

--- commands/cli/parse.go
+++ commands/cli/parse.go
@@ -9,14 +9,14 @@ import (
 
 // Parse parses the input commandline string (cmd, flags, and args).
 // returns the corresponding command Request object.
-func Parse(input []string, root *commands.Command) ([]string, map[string]interface{}, []string, error) {
+func Parse(input []string, root *commands.Command) (commands.Request, error) {
 	path, input := parsePath(input, root)
 	opts, args, err := parseOptions(input)
 	if err != nil {
-		return nil, nil, nil, err
+		return nil, err
 	}
 
-	return path, opts, args, nil
+	return commands.NewRequest(path, opts, args, nil), nil
 }
 
 // parsePath gets the command path from the command line input
