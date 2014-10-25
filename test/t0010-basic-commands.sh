12490cb477786c3bce9e16eb37027b60c5b382ff
Christian Couder <chriscool@tuxfamily.org>
2014-10-25 09:58:41 +0200

    Add t0010-basic-commands.sh
    This checks a little bit the installation and some
    basic commands.
    
    You can run it like that:
    
    $ cd test
    $ ./t0010-basic-commands.sh
     ok 1 - current dir is writable
     ok 2 - ipfs version succeeds
     ok 3 - ipfs version output looks good
     ok 4 - ipfs help succeeds
     ok 5 - ipfs help output looks good
     # passed all 5 test(s)
     1..5

---
+++ test/t0010-basic-commands.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+# Copyright (c) 2014 Christian Couder
+# MIT Licensed; see the LICENSE file in this repository.
+#
+
+test_description="Test installation and some basic commands"
+
+. ./test-lib.sh
+
+test_expect_success "current dir is writable" '
+	echo "It works!" >test.txt
+'
+
+test_expect_success "ipfs version succeeds" '
+	ipfs version >version.txt
+'
+
+test_expect_success "ipfs version output looks good" '
+	cat version.txt | egrep "^ipfs version [0-9]+\.[0-9]+\.[0-9]"
+'
+
+test_expect_success "ipfs help succeeds" '
+	ipfs help >help.txt
+'
+
+test_expect_success "ipfs help output looks good" '
+	cat help.txt | egrep "^Usage: +ipfs"
+'
+
+test_done
+
