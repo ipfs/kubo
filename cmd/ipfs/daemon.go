cb89f302e7e1c4b7978b28805533ad0e16e602eb
Matt Bell <mappum@gmail.com>
2014-10-27 16:20:48 -0700

    cmd/ipfs: Obtain lock when starting daemon

--- cmd/ipfs/daemon.go
+++ cmd/ipfs/daemon.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/jbenet/go-ipfs/Godeps/_workspace/src/github.com/camlistore/lock"
 
 	cmds "github.com/jbenet/go-ipfs/commands"
 	cmdsHttp "github.com/jbenet/go-ipfs/commands/http"
+	"github.com/jbenet/go-ipfs/config"
 )
+
+// DaemonLockFile is the filename of the daemon lock, relative to config dir
+const DaemonLockFile = "daemon.lock"
 
 var Daemon = &cmds.Command{
 	Options:     []cmds.Option{},
@@ -15,13 +22,33 @@ var Daemon = &cmds.Command{
 }
 
 func daemonFunc(req cmds.Request, res cmds.Response) {
+	configPath, err := getConfigPath(req)
+	if err != nil {
+		res.SetError(err, cmds.ErrNormal)
+		return
+	}
+
+	lockPath, err := config.Path(configPath, DaemonLockFile)
+	if err != nil {
+		res.SetError(err, cmds.ErrNormal)
+		return
+	}
+
+	lk, err := lock.Lock(lockPath)
+	if err != nil {
+		res.SetError(fmt.Errorf("Couldn't obtain lock. Is another daemon already running?"), cmds.ErrNormal)
+		return
+	}
+	defer lk.Close()
+
 	handler := cmdsHttp.Handler{}
 	http.Handle(cmdsHttp.ApiPath+"/", handler)
 	// TODO: load listen address/port from config/options
-	err := http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		res.SetError(err, cmds.ErrNormal)
 		return
 	}
 	// TODO: log to indicate that we are now listening
+
 }
