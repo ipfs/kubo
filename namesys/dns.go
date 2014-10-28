c534daef8668f85e272b8155bcb1c87005e9b1eb
Brian Tiger Chow <brian.holderchow@gmail.com>
2014-10-28 02:22:32 -0700

    refactor(namesys) use one-off error

--- namesys/dns.go
+++ namesys/dns.go
@@ -1,13 +1,12 @@
 package namesys
 
 import (
+	"fmt"
 	"net"
 
 	b58 "github.com/jbenet/go-ipfs/Godeps/_workspace/src/github.com/jbenet/go-base58"
 	isd "github.com/jbenet/go-ipfs/Godeps/_workspace/src/github.com/jbenet/go-is-domain"
 	mh "github.com/jbenet/go-ipfs/Godeps/_workspace/src/github.com/jbenet/go-multihash"
-
-	u "github.com/jbenet/go-ipfs/util"
 )
 
 // DNSResolver implements a Resolver on DNS domains
@@ -44,5 +43,5 @@ func (r *DNSResolver) Resolve(name string) (string, error) {
 		return t, nil
 	}
 
-	return "", u.ErrNotFound
+	return "", fmt.Errorf("namesys: %v not found", name)
 }
