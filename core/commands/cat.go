a7819867aefabcf7e776df722109c02ff275af38
Matt Bell <mappum@gmail.com>
2014-10-28 23:15:55 -0700

    core/commands: Added 'cat' command

--- core/commands/cat.go
+++ core/commands/cat.go
@@ -4,26 +4,34 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/jbenet/go-ipfs/core"
+	cmds "github.com/jbenet/go-ipfs/commands"
 	uio "github.com/jbenet/go-ipfs/unixfs/io"
 )
 
-func Cat(n *core.IpfsNode, args []string, opts map[string]interface{}, out io.Writer) error {
-	for _, fn := range args {
-		dagnode, err := n.Resolver.ResolvePath(fn)
-		if err != nil {
-			return fmt.Errorf("catFile error: %v", err)
+var cat = &cmds.Command{
+	Help: "TODO",
+	Run: func(req cmds.Request, res cmds.Response) {
+		node := req.Context().Node
+		fmt.Println(node.Resolver)
+		readers := make([]io.Reader, 0, len(req.Arguments()))
+
+		for _, path := range req.Arguments() {
+			dagnode, err := node.Resolver.ResolvePath(path)
+			if err != nil {
+				res.SetError(err, cmds.ErrNormal)
+				return
+			}
+
+			read, err := uio.NewDagReader(dagnode, node.DAG)
+			if err != nil {
+				res.SetError(err, cmds.ErrNormal)
+				return
+			}
+
+			readers = append(readers, read)
 		}
 
-		read, err := uio.NewDagReader(dagnode, n.DAG)
-		if err != nil {
-			return fmt.Errorf("cat error: %v", err)
-		}
-
-		_, err = io.Copy(out, read)
-		if err != nil {
-			return fmt.Errorf("cat error: %v", err)
-		}
-	}
-	return nil
+		reader := io.MultiReader(readers...)
+		res.SetValue(reader)
+	},
 }
