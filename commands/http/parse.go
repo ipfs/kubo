81f29257a143eee542a575625397a25688c1f40d
Matt Bell <mappum@gmail.com>
2014-10-28 19:27:07 -0700

    commands: Gave Requests a reference to the command they are being called on

--- commands/http/parse.go
+++ commands/http/parse.go
@@ -10,18 +10,24 @@ import (
 
 // Parse parses the data in a http.Request and returns a command Request object
 func Parse(r *http.Request) (cmds.Request, error) {
+	// TODO: take root cmd as a param, like the commands/cli Parse
+
 	path := strings.Split(r.URL.Path, "/")[3:]
 	args := make([]string, 0)
 
-	if cmd, err := commands.Root.Get(path[:len(path)-1]); err != nil {
+	cmd, err := commands.Root.Get(path[:len(path)-1])
+	if err != nil {
 		// 404 if there is no command at that path
 		return nil, ErrNotFound
 
-	} else if cmd.Subcommand(path[len(path)-1]) == nil {
+	} else if sub := cmd.Subcommand(path[len(path)-1]); sub == nil {
 		// if the last string in the path isn't a subcommand, use it as an argument
 		// e.g. /objects/Qabc12345 (we are passing "Qabc12345" to the "objects" command)
 		args = append(args, path[len(path)-1])
 		path = path[:len(path)-1]
+
+	} else {
+		cmd = sub
 	}
 
 	opts, args2 := parseOptions(r)
@@ -33,7 +39,7 @@ func Parse(r *http.Request) (cmds.Request, error) {
 	// (r.Body will be nil if there is no request body, like in GET requests)
 	in := r.Body
 
-	return cmds.NewRequest(path, opts, args, in), nil
+	return cmds.NewRequest(path, opts, args, in, cmd), nil
 }
 
 func parseOptions(r *http.Request) (map[string]interface{}, []string) {
