4e2763a40a794134f28fb3dbf625526ac6dc2039
Christian Couder <chriscool@tuxfamily.org>
2014-11-01 10:52:09 +0100

    Refactor mount related tests
    This adds test_launch_ipfs_mount() and test_kill_ipfs_mount()
    to avoid duplicating tests to launch "ipfs mount" and to
    kill it.
    
    License: MIT
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- test/t0040-add-and-cat.sh
+++ test/t0040-add-and-cat.sh
@@ -8,28 +8,7 @@ test_description="Test add and cat commands"
 
 . ./test-lib.sh
 
-test_expect_success "ipfs init succeeds" '
-	export IPFS_DIR="$(pwd)/.go-ipfs" &&
-	ipfs init -b=2048
-'
-
-test_expect_success "prepare config" '
-	mkdir mountdir ipfs ipns &&
-	ipfs config Mounts.IPFS "$(pwd)/ipfs" &&
-	ipfs config Mounts.IPNS "$(pwd)/ipns"
-'
-
-test_expect_success "ipfs mount succeeds" '
-	ipfs mount mountdir >actual &
-'
-
-test_expect_success "ipfs mount output looks good" '
-	IPFS_PID=$! &&
-	sleep 5 &&
-	echo "mounting ipfs at $(pwd)/ipfs" >expected &&
-	echo "mounting ipns at $(pwd)/ipns" >>expected &&
-	test_cmp expected actual
-'
+test_launch_ipfs_mount
 
 test_expect_success "ipfs add succeeds" '
 	echo "Hello Worlds!" >mountdir/hello.txt &&
@@ -59,14 +38,6 @@ test_expect_success "cat ipfs/stuff looks good" '
 	test_cmp expected actual
 '
 
-test_expect_success "ipfs mount is still running" '
-	kill -0 $IPFS_PID
-'
-
-test_expect_success "ipfs mount can be killed" '
-	kill $IPFS_PID &&
-	sleep 1 &&
-	! kill -0 $IPFS_PID 2>/dev/null
-'
+test_kill_ipfs_mount
 
 test_done
--- test/test-lib.sh
+++ test/test-lib.sh
@@ -16,3 +16,41 @@
 
 # Please put go-ipfs specific shell functions below
 
+test_launch_ipfs_mount() {
+
+	test_expect_success "ipfs init succeeds" '
+		export IPFS_DIR="$(pwd)/.go-ipfs" &&
+		ipfs init -b=2048
+	'
+
+	test_expect_success "prepare config" '
+		mkdir mountdir ipfs ipns &&
+		ipfs config Mounts.IPFS "$(pwd)/ipfs" &&
+		ipfs config Mounts.IPNS "$(pwd)/ipns"
+	'
+
+	test_expect_success "ipfs mount succeeds" '
+		ipfs mount mountdir >actual &
+	'
+
+	test_expect_success "ipfs mount output looks good" '
+		IPFS_PID=$! &&
+		sleep 5 &&
+		echo "mounting ipfs at $(pwd)/ipfs" >expected &&
+		echo "mounting ipns at $(pwd)/ipns" >>expected &&
+		test_cmp expected actual
+	'
+}
+
+test_kill_ipfs_mount() {
+
+	test_expect_success "ipfs mount is still running" '
+		kill -0 $IPFS_PID
+	'
+
+	test_expect_success "ipfs mount can be killed" '
+		kill $IPFS_PID &&
+		sleep 1 &&
+		! kill -0 $IPFS_PID 2>/dev/null
+	'
+}
