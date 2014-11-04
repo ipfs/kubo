9a8f1d0be74614a44f30893330eefab9667ceeda
Brian Tiger Chow <brian.holderchow@gmail.com>
2014-11-04 02:57:34 -0800

    docs(commands2/cat) help text

--- core/commands2/cat.go
+++ core/commands2/cat.go
@@ -13,7 +13,11 @@ var catCmd = &cmds.Command{
 	Arguments: []cmds.Argument{
 		cmds.Argument{"object", cmds.ArgString, false, true},
 	},
-	Help: "TODO",
+	Help: `ipfs cat <ipfs-path> - Show ipfs object data.
+
+	Retrieves the object named by <ipfs-path> and displays the Data
+	it contains.
+	`,
 	Run: func(res cmds.Response, req cmds.Request) {
 		node := req.Context().Node
 		paths := make([]string, 0, len(req.Arguments()))
