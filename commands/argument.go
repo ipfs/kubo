40e96a1fa6cd59ab3de290d4451359da8b5e0a10
Matt Bell <mappum@gmail.com>
2014-11-11 15:54:53 -0800

    commands: Added Argument helper constructors

--- commands/argument.go
+++ commands/argument.go
@@ -14,3 +14,23 @@ type Argument struct {
 	Variadic    bool
 	Description string
 }
+
+func StringArg(name string, required, variadic bool, description string) Argument {
+	return Argument{
+		Name:        name,
+		Type:        ArgString,
+		Required:    required,
+		Variadic:    variadic,
+		Description: description,
+	}
+}
+
+func FileArg(name string, required, variadic bool, description string) Argument {
+	return Argument{
+		Name:        name,
+		Type:        ArgFile,
+		Required:    required,
+		Variadic:    variadic,
+		Description: description,
+	}
+}
