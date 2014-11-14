476aab955260c5f25d0e67d2851f2652d01cea96
Brian Tiger Chow <brian.holderchow@gmail.com>
2014-11-13 21:09:08 -0800

    rm unused tests
    License: MIT
    Signed-off-by: Brian Tiger Chow <brian@perfmode.com>

--- cmd/ipfs2/tour_test.go
+++ cmd/ipfs2/tour_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"errors"
 	"testing"
 
 	"github.com/jbenet/go-ipfs/tour"
@@ -23,49 +22,4 @@ This is where we test our beautiful command line interfaces
 		t.Fatal(err)
 	}
 	t.Log(buf.String())
-}
-
-func TestRenderTourOutputList(t *testing.T) {
-
-	t.Log(`Ensure we can successfully print the tour output when there's an
-	error and list of tour topics`)
-	listOutput := &tourOutput{
-		Error: errors.New("Topic 42 does not exist"),
-		Topics: []tour.Topic{
-			tour.Topic{
-				ID:    "41",
-				Title: "Being one shy of the mark",
-				Text:  "Poor thing.",
-			},
-			tour.Topic{
-				ID:    "44",
-				Title: "Two shy of the mark",
-				Text:  "Oh no.",
-			},
-		},
-	}
-
-	var list bytes.Buffer
-	if err := printTourOutput(&list, listOutput); err != nil {
-		t.Fatal(err)
-	}
-	t.Log(list.String())
-}
-
-func TestRenderTourOutputSingle(t *testing.T) {
-	t.Log(`
-	When there's just a single topic in the output, ensure we can render the
-	template`)
-	singleOutput := &tourOutput{
-		Topic: &tour.Topic{
-			ID:    "42",
-			Title: "Informative!",
-			Text:  "Compelling!",
-		},
-	}
-	var single bytes.Buffer
-	if err := printTourOutput(&single, singleOutput); err != nil {
-		t.Fatal(err)
-	}
-	t.Log(single.String())
 }
