356a2d7faafbcbf4461f74020b490813c58f5c86
Brian Tiger Chow <brian.holderchow@gmail.com>
2014-11-16 10:32:08 -0800

    fix(daemon) if lock file does not exist, then the lock is not acquired
    @mappum @jbenet LGTU?
    
    License: MIT
    Signed-off-by: Brian Tiger Chow <brian@perfmode.com>

--- daemon2/daemon.go
+++ daemon2/daemon.go
@@ -5,6 +5,7 @@ import (
 	"path"
 
 	lock "github.com/jbenet/go-ipfs/Godeps/_workspace/src/github.com/camlistore/lock"
+	"github.com/jbenet/go-ipfs/util"
 )
 
 // LockFile is the filename of the daemon lock, relative to config dir
@@ -15,6 +16,9 @@ func Lock(confdir string) (io.Closer, error) {
 }
 
 func Locked(confdir string) bool {
+	if !util.FileExists(path.Join(confdir, LockFile)) {
+		return false
+	}
 	if lk, err := Lock(confdir); err != nil {
 		return true
 
