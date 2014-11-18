9ae420abb80538d09e9777827ee7bcf75d810ec3
Juan Batiz-Benet <juan@benet.ai>
2014-11-18 05:48:04 -0800

    SizeSplitter fix: keep-reading until chunk full
    if the underlying reader is buffered with a smaller buffer
    it would force the chunk sizes to come out smaller than
    intended.
    
    cc @whyrusleeping @mappum

--- importer/chunk/splitting_test.go
+++ importer/chunk/splitting_test.go
@@ -3,6 +3,7 @@ package chunk
 import (
 	"bytes"
 	"crypto/rand"
+	"io"
 	"testing"
 )
 
@@ -53,4 +54,52 @@ func TestSizeSplitterIsDeterministic(t *testing.T) {
 	for run := 0; run < 1; run++ { // crank this up to satisfy yourself.
 		test()
 	}
+}
+
+func TestSizeSplitterFillsChunks(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
+	}
+
+	max := 10000000
+	b := randBuf(t, max)
+	r := &clipReader{r: bytes.NewReader(b), size: 4000}
+	s := SizeSplitter{Size: 1024 * 256}
+	c := s.Split(r)
+
+	sofar := 0
+	whole := make([]byte, max)
+	for chunk := range c {
+
+		bc := b[sofar : sofar+len(chunk)]
+		if !bytes.Equal(bc, chunk) {
+			t.Fatalf("chunk not correct: (sofar: %d) %d != %d, %v != %v", sofar, len(bc), len(chunk), bc[:100], chunk[:100])
+		}
+
+		copy(whole[sofar:], chunk)
+
+		sofar += len(chunk)
+		if sofar != max && len(chunk) < s.Size {
+			t.Fatal("sizesplitter split at a smaller size")
+		}
+	}
+
+	if !bytes.Equal(b, whole) {
+		t.Fatal("splitter did not split right")
+	}
+}
+
+type clipReader struct {
+	size int
+	r    io.Reader
+}
+
+func (s *clipReader) Read(buf []byte) (int, error) {
+
+	// clip the incoming buffer to produce smaller chunks
+	if len(buf) > s.size {
+		buf = buf[:s.size]
+	}
+
+	return s.r.Read(buf)
 }
