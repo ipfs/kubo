438ffa1dd7a211c9b327f6759bc364406ba36a27
Brian Tiger Chow <brian.holderchow@gmail.com>
2014-11-21 13:09:44 -0800

    feat(util) ForwardNBlocks
    License: MIT
    Signed-off-by: Brian Tiger Chow <brian@perfmode.com>

---
+++ util/async/forward.go
@@ -0,0 +1,30 @@
+package async
+
+import (
+	context "github.com/jbenet/go-ipfs/Godeps/_workspace/src/code.google.com/p/go.net/context"
+	"github.com/jbenet/go-ipfs/blocks"
+)
+
+// ForwardN forwards up to |num| blocks to the returned channel.
+func ForwardN(ctx context.Context, in <-chan *blocks.Block, num int) <-chan *blocks.Block {
+	out := make(chan *blocks.Block)
+	go func() {
+		defer close(out)
+		for i := 0; i < num; i++ {
+			select {
+			case block, ok := <-in:
+				if !ok {
+					return
+				}
+				select {
+				case out <- block:
+				case <-ctx.Done():
+					return
+				}
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+	return out
+}
