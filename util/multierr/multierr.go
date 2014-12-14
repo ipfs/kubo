55d1e794c75406cce6bcfc8913de61c4368b9249
Juan Batiz-Benet <juan@benet.ai>
2014-12-14 11:35:57 -0800

    multierr

---
+++ util/multierr/multierr.go
@@ -0,0 +1,30 @@
+package multierr
+
+import (
+	"fmt"
+)
+
+// Error contains a set of errors. Used to return multiple errors, as in listen.
+type Error struct {
+	Errors []error
+}
+
+func (e *Error) Error() string {
+	if e == nil {
+		return "<nil error>"
+	}
+	var out string
+	for i, v := range e.Errors {
+		if v != nil {
+			out += fmt.Sprintf("%d: %s\n", i, v)
+		}
+	}
+	return out
+}
+
+func New(errs ...error) *Error {
+	if len(errs) == 0 {
+		return nil
+	}
+	return &Error{errs}
+}
