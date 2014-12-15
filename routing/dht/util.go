0142fd79fe1be39b14e70adaac514b23e23a00cf
Jeromy <jeromyj@gmail.com>
2014-12-15 01:33:04 +0000

    rewrite sendWantlistToProviders

--- routing/dht/util.go
+++ routing/dht/util.go
@@ -2,8 +2,6 @@ package dht
 
 import (
 	"sync"
-
-	peer "github.com/jbenet/go-ipfs/peer"
 )
 
 // Pool size is the number of nodes used for group find/set RPC calls
@@ -38,46 +36,4 @@ func (c *counter) Size() (s int) {
 	s = c.n
 	c.mut.Unlock()
 	return
-}
-
-// peerSet is a threadsafe set of peers
-type peerSet struct {
-	ps map[string]bool
-	lk sync.RWMutex
-}
-
-func newPeerSet() *peerSet {
-	ps := new(peerSet)
-	ps.ps = make(map[string]bool)
-	return ps
-}
-
-func (ps *peerSet) Add(p peer.Peer) {
-	ps.lk.Lock()
-	ps.ps[string(p.ID())] = true
-	ps.lk.Unlock()
-}
-
-func (ps *peerSet) Contains(p peer.Peer) bool {
-	ps.lk.RLock()
-	_, ok := ps.ps[string(p.ID())]
-	ps.lk.RUnlock()
-	return ok
-}
-
-func (ps *peerSet) Size() int {
-	ps.lk.RLock()
-	defer ps.lk.RUnlock()
-	return len(ps.ps)
-}
-
-func (ps *peerSet) AddIfSmallerThan(p peer.Peer, maxsize int) bool {
-	var success bool
-	ps.lk.Lock()
-	if _, ok := ps.ps[string(p.ID())]; !ok && len(ps.ps) < maxsize {
-		success = true
-		ps.ps[string(p.ID())] = true
-	}
-	ps.lk.Unlock()
-	return success
 }
