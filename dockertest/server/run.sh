9946fc83e0b0822883cf15f01f86b59337a31f45
Brian Tiger Chow <brian.holderchow@gmail.com>
2014-12-15 11:26:20 -0800

    fix(dockertest): mv file into place
    as @whyrusleeping points out, file is created before hash is written
    
    License: MIT
    Signed-off-by: Brian Tiger Chow <brian@perfmode.com>

--- dockertest/server/run.sh
+++ dockertest/server/run.sh
@@ -8,10 +8,12 @@ sleep 3
 # TODO instead of bootrapping: ipfs swarm connect /ip4/$BOOTSTRAP_PORT_4011_TCP_ADDR/tcp/$BOOTSTRAP_PORT_4011_TCP_PORT/QmNXuBh8HFsWq68Fid8dMbGNQTh7eG6hV9rr1fQyfmfomE
 
 # must mount this volume from data container
-ipfs add -q /data/filetiny > /data/idtiny
+ipfs add -q /data/filetiny > tmptiny
+mv tmptiny /data/idtiny
 echo added tiny file. hash is $(cat /data/idtiny)
 
-ipfs add -q /data/filerand > /data/idrand
+ipfs add -q /data/filerand > tmprand
+mv tmprand /data/idrand
 echo added rand file. hash is $(cat /data/idrand)
 
 # allow ample time for the client to pull the data
