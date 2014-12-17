0545c4d15d4c680a01e6207c45a08480740fade9
Brian Tiger Chow <brian.holderchow@gmail.com>
2014-12-17 00:24:59 -0800

    refactor: *Entry -> Entry
    in many places, entries are assigned from one slice to another and in
    different goroutines. In one place, entries were modified (in the
    queue). To avoid shared mutable state, probably best to handle entries
    by value.
    
    License: MIT
    Signed-off-by: Brian Tiger Chow <brian@perfmode.com>

--- exchange/bitswap/message/message.go
+++ exchange/bitswap/message/message.go
@@ -19,7 +19,7 @@ import (
 type BitSwapMessage interface {
 	// Wantlist returns a slice of unique keys that represent data wanted by
 	// the sender.
-	Wantlist() []*Entry
+	Wantlist() []Entry
 
 	// Blocks returns a slice of unique blocks
 	Blocks() []*blocks.Block
@@ -48,7 +48,7 @@ type Exportable interface {
 
 type impl struct {
 	full     bool
-	wantlist map[u.Key]*Entry
+	wantlist map[u.Key]Entry
 	blocks   map[u.Key]*blocks.Block // map to detect duplicates
 }
 
@@ -59,7 +59,7 @@ func New() BitSwapMessage {
 func newMsg() *impl {
 	return &impl{
 		blocks:   make(map[u.Key]*blocks.Block),
-		wantlist: make(map[u.Key]*Entry),
+		wantlist: make(map[u.Key]Entry),
 		full:     true,
 	}
 }
@@ -90,8 +90,8 @@ func (m *impl) Full() bool {
 	return m.full
 }
 
-func (m *impl) Wantlist() []*Entry {
-	var out []*Entry
+func (m *impl) Wantlist() []Entry {
+	var out []Entry
 	for _, e := range m.wantlist {
 		out = append(out, e)
 	}
@@ -120,7 +120,7 @@ func (m *impl) addEntry(k u.Key, priority int, cancel bool) {
 		e.Priority = priority
 		e.Cancel = cancel
 	} else {
-		m.wantlist[k] = &Entry{
+		m.wantlist[k] = Entry{
 			Entry: wantlist.Entry{
 				Key:      k,
 				Priority: priority,
