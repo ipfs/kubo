2798d9bcaff294ed853986e555ac750d231b04bb
Brian Tiger Chow <brian.holderchow@gmail.com>
2014-12-16 17:34:18 -0800

    maybe this.
    i don't really have an opinion either way
    
    License: MIT
    Signed-off-by: Brian Tiger Chow <brian@perfmode.com>

--- util/metrics/metrics.go
+++ util/metrics/metrics.go
@@ -4,29 +4,20 @@ import "github.com/jbenet/go-ipfs/Godeps/_workspace/src/github.com/rcrowley/go-m
 
 // TODO add a metrics registry exposing functionality of the form M.RegisterBWCounter("system", &bwc)
 
-type BandwidthCounter interface {
-	IncIn(i int64)
-	IncOut(i int64)
-	InOut() (int64, int64)
+type Counter interface {
+	Count() int64
+	Inc(int64)
 }
 
-func NewBandwidthCounter() BandwidthCounter {
-	return &bwc{}
-}
-
-type bwc struct {
+type BandwidthCounter struct {
 	in  metrics.StandardCounter
 	out metrics.StandardCounter
 }
 
-func (c *bwc) IncIn(i int64) {
-	c.in.Inc(i)
+func (c *BandwidthCounter) In() Counter {
+	return &c.in
 }
 
-func (c *bwc) IncOut(i int64) {
-	c.out.Inc(i)
-}
-
-func (c *bwc) InOut() (int64, int64) {
-	return c.in.Count(), c.out.Count()
+func (c *BandwidthCounter) Out() Counter {
+	return &c.out
 }
