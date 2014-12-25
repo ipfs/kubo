abd971d3931454551c73e09d1c42ed79eda3c16e
Brian Tiger Chow <brian.holderchow@gmail.com>
2014-12-25 01:37:15 -0500

    extract initialization
    refactor(epictest) Core
    
    refactor: extract repo
    
    fix
    
    move core

--- epictest/addcat_test.go
+++ epictest/addcat_test.go
@@ -10,27 +10,9 @@ import (
 	"time"
 
 	context "github.com/jbenet/go-ipfs/Godeps/_workspace/src/code.google.com/p/go.net/context"
-	datastore "github.com/jbenet/go-ipfs/Godeps/_workspace/src/github.com/jbenet/go-datastore"
-	sync "github.com/jbenet/go-ipfs/Godeps/_workspace/src/github.com/jbenet/go-datastore/sync"
 	random "github.com/jbenet/go-ipfs/Godeps/_workspace/src/github.com/jbenet/go-random"
-	blockstore "github.com/jbenet/go-ipfs/blocks/blockstore"
-	blockservice "github.com/jbenet/go-ipfs/blockservice"
-	exchange "github.com/jbenet/go-ipfs/exchange"
-	bitswap "github.com/jbenet/go-ipfs/exchange/bitswap"
-	bsnet "github.com/jbenet/go-ipfs/exchange/bitswap/network"
-	importer "github.com/jbenet/go-ipfs/importer"
-	chunk "github.com/jbenet/go-ipfs/importer/chunk"
-	merkledag "github.com/jbenet/go-ipfs/merkledag"
-	net "github.com/jbenet/go-ipfs/net"
 	mocknet "github.com/jbenet/go-ipfs/net/mock"
-	path "github.com/jbenet/go-ipfs/path"
-	peer "github.com/jbenet/go-ipfs/peer"
-	dht "github.com/jbenet/go-ipfs/routing/dht"
-	uio "github.com/jbenet/go-ipfs/unixfs/io"
-	util "github.com/jbenet/go-ipfs/util"
-	"github.com/jbenet/go-ipfs/util/datastore2"
 	errors "github.com/jbenet/go-ipfs/util/debugerror"
-	delay "github.com/jbenet/go-ipfs/util/delay"
 )
 
 const kSeed = 1
@@ -42,7 +24,7 @@ func Test1KBInstantaneous(t *testing.T) {
 		BlockstoreLatency: 0,
 	}
 
-	if err := AddCatBytes(RandomBytes(100*MB), conf); err != nil {
+	if err := AddCatBytes(RandomBytes(1*KB), conf); err != nil {
 		t.Fatal(err)
 	}
 }
@@ -96,23 +78,10 @@ func RandomBytes(n int64) []byte {
 	return data.Bytes()
 }
 
-type instance struct {
-	ID             peer.ID
-	Network        net.Network
-	Blockstore     blockstore.Blockstore
-	Datastore      datastore.ThreadSafeDatastore
-	DHT            *dht.IpfsDHT
-	Exchange       exchange.Interface
-	BitSwapNetwork bsnet.BitSwapNetwork
-
-	datastoreDelay delay.D
-}
-
 func AddCatBytes(data []byte, conf Config) error {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	const numPeers = 2
-	instances := make(map[peer.ID]*instance, numPeers)
 
 	// create network
 	mn, err := mocknet.FullMeshLinked(ctx, numPeers)
@@ -124,57 +93,28 @@ func AddCatBytes(data []byte, conf Config) error {
 		// TODO add to conf. This is tricky because we want 0 values to be functional.
 		Bandwidth: math.MaxInt32,
 	})
-	for _, p := range mn.Peers() {
-		instances[p] = &instance{
-			ID:      p,
-			Network: mn.Net(p),
-		}
-	}
 
-	// create dht network
-	for _, p := range mn.Peers() {
-		dsDelay := delay.Fixed(conf.BlockstoreLatency)
-		instances[p].Datastore = sync.MutexWrap(datastore2.WithDelay(datastore.NewMapDatastore(), dsDelay))
-		instances[p].datastoreDelay = dsDelay
+	if len(mn.Peers()) < numPeers {
+		return errors.New("test initialization error")
 	}
-	for _, p := range mn.Peers() {
-		instances[p].DHT = dht.NewDHT(ctx, p, instances[p].Network, instances[p].Datastore)
-	}
-	// create two bitswap network clients
-	for _, p := range mn.Peers() {
-		instances[p].BitSwapNetwork = bsnet.NewFromIpfsNetwork(instances[p].Network, instances[p].DHT)
-	}
-	for _, p := range mn.Peers() {
-		const kWriteCacheElems = 100
-		const alwaysSendToPeer = true
-		adapter := instances[p].BitSwapNetwork
-		dstore := instances[p].Datastore
-		instances[p].Blockstore, err = blockstore.WriteCached(blockstore.NewBlockstore(dstore), kWriteCacheElems)
-		if err != nil {
-			return err
-		}
-		instances[p].Exchange = bitswap.New(ctx, p, adapter, instances[p].Blockstore, alwaysSendToPeer)
-	}
-	var peers []peer.ID
-	for _, p := range mn.Peers() {
-		peers = append(peers, p)
-	}
-
-	adder := instances[peers[0]]
-	catter := instances[peers[1]]
-
-	// bootstrap the DHTs
-	adder.DHT.Connect(ctx, catter.ID)
-	catter.DHT.Connect(ctx, adder.ID)
-
-	adder.datastoreDelay.Set(0) // disable blockstore latency during add operation
-	keyAdded, err := add(adder, bytes.NewReader(data))
+	adder, err := makeCore(ctx, MocknetTestRepo(mn.Peers()[0], mn.Net(mn.Peers()[0]), conf))
 	if err != nil {
 		return err
 	}
-	adder.datastoreDelay.Set(conf.BlockstoreLatency) // add some blockstore delay to make the catter wait
+	catter, err := makeCore(ctx, MocknetTestRepo(mn.Peers()[1], mn.Net(mn.Peers()[1]), conf))
+	if err != nil {
+		return err
+	}
 
-	readerCatted, err := cat(catter, keyAdded)
+	adder.Bootstrap(ctx, catter.ID())
+	catter.Bootstrap(ctx, adder.ID())
+
+	keyAdded, err := adder.Add(bytes.NewReader(data))
+	if err != nil {
+		return err
+	}
+
+	readerCatted, err := catter.Cat(keyAdded)
 	if err != nil {
 		return err
 	}
@@ -186,28 +126,6 @@ func AddCatBytes(data []byte, conf Config) error {
 		return errors.New("catted data does not match added data")
 	}
 	return nil
-}
-
-func cat(catter *instance, k util.Key) (io.Reader, error) {
-	catterdag := merkledag.NewDAGService(&blockservice.BlockService{catter.Blockstore, catter.Exchange})
-	nodeCatted, err := (&path.Resolver{catterdag}).ResolvePath(k.String())
-	if err != nil {
-		return nil, err
-	}
-	return uio.NewDagReader(nodeCatted, catterdag)
-}
-
-func add(adder *instance, r io.Reader) (util.Key, error) {
-	nodeAdded, err := importer.BuildDagFromReader(
-		r,
-		merkledag.NewDAGService(&blockservice.BlockService{adder.Blockstore, adder.Exchange}),
-		nil,
-		chunk.DefaultSplitter,
-	)
-	if err != nil {
-		return "", err
-	}
-	return nodeAdded.Key()
 }
 
 func SkipUnlessEpic(t *testing.T) {
