0da8af1684173677fd5964283699eb87b963207d
Brian Tiger Chow <brian.holderchow@gmail.com>
2014-12-25 06:27:49 -0500

    add repo to repo package

---
+++ repo/repo.go
@@ -0,0 +1,83 @@
+package repo
+
+import (
+	context "github.com/jbenet/go-ipfs/Godeps/_workspace/src/code.google.com/p/go.net/context"
+	datastore "github.com/jbenet/go-ipfs/Godeps/_workspace/src/github.com/jbenet/go-datastore"
+	sync "github.com/jbenet/go-ipfs/Godeps/_workspace/src/github.com/jbenet/go-datastore/sync"
+	blockstore "github.com/jbenet/go-ipfs/blocks/blockstore"
+	epictest "github.com/jbenet/go-ipfs/epictest"
+	exchange "github.com/jbenet/go-ipfs/exchange"
+	bitswap "github.com/jbenet/go-ipfs/exchange/bitswap"
+	bsnet "github.com/jbenet/go-ipfs/exchange/bitswap/network"
+	net "github.com/jbenet/go-ipfs/net"
+	peer "github.com/jbenet/go-ipfs/peer"
+	dht "github.com/jbenet/go-ipfs/routing/dht"
+	datastore2 "github.com/jbenet/go-ipfs/util/datastore2"
+	delay "github.com/jbenet/go-ipfs/util/delay"
+)
+
+type RepoConfig func(ctx context.Context) (Repo, error)
+
+type Repo interface {
+	ID() peer.ID
+	Blockstore() blockstore.Blockstore
+	Exchange() exchange.Interface
+
+	Bootstrap(ctx context.Context, peer peer.ID) error
+}
+
+func MocknetTestRepo(p peer.ID, n net.Network, conf epictest.Config) RepoConfig {
+	return func(ctx context.Context) (Repo, error) {
+		const kWriteCacheElems = 100
+		const alwaysSendToPeer = true
+		dsDelay := delay.Fixed(conf.BlockstoreLatency)
+		ds := sync.MutexWrap(datastore2.WithDelay(datastore.NewMapDatastore(), dsDelay))
+		dhtt := dht.NewDHT(ctx, p, n, ds)
+		bsn := bsnet.NewFromIpfsNetwork(n, dhtt)
+		bstore, err := blockstore.WriteCached(blockstore.NewBlockstore(ds), kWriteCacheElems)
+		if err != nil {
+			return nil, err
+		}
+		exch := bitswap.New(ctx, p, bsn, bstore, alwaysSendToPeer)
+		return &repo{
+			bitSwapNetwork: bsn,
+			blockstore:     bstore,
+			exchange:       exch,
+			datastore:      ds,
+			network:        n,
+			dht:            dhtt,
+			id:             p,
+		}, nil
+	}
+}
+
+type repo struct {
+	// DHT, Exchange, Network,Datastore
+	bitSwapNetwork bsnet.BitSwapNetwork
+	blockstore     blockstore.Blockstore
+	exchange       exchange.Interface
+	datastore      datastore.ThreadSafeDatastore
+	network        net.Network
+	dht            *dht.IpfsDHT
+	id             peer.ID
+}
+
+func (r *repo) ID() peer.ID {
+	return r.id
+}
+
+func (c *repo) Bootstrap(ctx context.Context, p peer.ID) error {
+	return c.dht.Connect(ctx, p)
+}
+
+func (r *repo) Datastore() datastore.ThreadSafeDatastore {
+	return r.datastore
+}
+
+func (r *repo) Blockstore() blockstore.Blockstore {
+	return r.blockstore
+}
+
+func (r *repo) Exchange() exchange.Interface {
+	return r.exchange
+}
