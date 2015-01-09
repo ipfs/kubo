136ef70f00dcc54dc5842f6aa4c09a6d52ebd9ff
Juan Batiz-Benet <juan@benet.ai>
2015-01-08 16:18:45 -0800

    calc_test for picking link block size

--- importer/importer.go
+++ importer/importer.go
@@ -20,7 +20,24 @@ var log = util.Logger("importer")
 // BlockSizeLimit specifies the maximum size an imported block can have.
 var BlockSizeLimit = 1048576 // 1 MB
 
-var DefaultLinksPerBlock = 8192
+// rough estimates on expected sizes
+var roughDataBlockSize = chunk.DefaultBlockSize
+var roughLinkBlockSize = 1 << 13 // 8KB
+var roughLinkSize = 258 + 8 + 5  // sha256 multihash + size + no name + protobuf framing
+
+// DefaultLinksPerBlock governs how the importer decides how many links there
+// will be per block. This calculation is based on expected distributions of:
+//  * the expected distribution of block sizes
+//  * the expected distribution of link sizes
+//  * desired access speed
+// For now, we use:
+//
+//   var roughLinkBlockSize = 1 << 13 // 8KB
+//   var roughLinkSize = 288          // sha256 + framing + name
+//   var DefaultLinksPerBlock = (roughLinkBlockSize / roughLinkSize)
+//
+// See calc_test.go
+var DefaultLinksPerBlock = (roughLinkBlockSize / roughLinkSize)
 
 // ErrSizeLimitExceeded signals that a block is larger than BlockSizeLimit.
 var ErrSizeLimitExceeded = fmt.Errorf("object size limit exceeded")
