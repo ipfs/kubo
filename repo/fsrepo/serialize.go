9700d2f94bf3f2ae6e0e031d282fc7ad6068a367
Brian Tiger Chow <brian.holderchow@gmail.com>
2015-01-13 06:27:24 -0800

    use atomicfile for safer writes
    for now, allow daemon and client to both hit config

--- repo/fsrepo/serialize.go
+++ repo/fsrepo/serialize.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/jbenet/go-ipfs/Godeps/_workspace/src/github.com/facebookgo/atomicfile"
 	"github.com/jbenet/go-ipfs/repo/config"
 	"github.com/jbenet/go-ipfs/util"
 	"github.com/jbenet/go-ipfs/util/debugerror"
@@ -34,7 +35,7 @@ func writeConfigFile(filename string, cfg interface{}) error {
 		return err
 	}
 
-	f, err := os.Create(filename)
+	f, err := atomicfile.New(filename, 0775)
 	if err != nil {
 		return err
 	}
