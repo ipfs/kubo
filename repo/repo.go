30a5aa9b0497ee8cde7d40497292752f421284b3
Brian Tiger Chow <brian.holderchow@gmail.com>
2015-01-14 09:26:13 -0800

    feat(repo): expose the Datastore() in repo.Repo interface

--- repo/repo.go
+++ repo/repo.go
@@ -1,6 +1,7 @@
 package repo
 
 import (
+	datastore "github.com/jbenet/go-ipfs/Godeps/_workspace/src/github.com/jbenet/go-datastore"
 	config "github.com/jbenet/go-ipfs/repo/config"
 	util "github.com/jbenet/go-ipfs/util"
 )
@@ -11,6 +12,8 @@ type Repo interface {
 
 	SetConfigKey(key string, value interface{}) error
 	GetConfigKey(key string) (interface{}, error)
+
+	Datastore() datastore.ThreadSafeDatastore
 }
 
 // IsInitialized returns true if the path is home to an initialized IPFS
