9acc02461ebe739cb0e73a7b8e819404840d81fd
Brian Tiger Chow <brian.holderchow@gmail.com>
2015-01-14 05:42:35 -0800

    test(fsrepo) harden tests

--- repo/fsrepo/fsrepo_test.go
+++ repo/fsrepo/fsrepo_test.go
@@ -30,14 +30,18 @@ func TestCanManageReposIndependently(t *testing.T) {
 	pathB := testRepoPath("b", t)
 
 	t.Log("initialize two repos")
-	AssertNil(Init(pathA, &config.Config{}), t, "should initialize successfully")
-	AssertNil(Init(pathB, &config.Config{}), t, "should initialize successfully")
+	AssertNil(Init(pathA, &config.Config{}), t, "a", "should initialize successfully")
+	AssertNil(Init(pathB, &config.Config{}), t, "b", "should initialize successfully")
+
+	t.Log("ensure repos initialized")
+	Assert(IsInitialized(pathA), t, "a should be initialized")
+	Assert(IsInitialized(pathB), t, "b should be initialized")
 
 	t.Log("open the two repos")
 	repoA := At(pathA)
 	repoB := At(pathB)
-	AssertNil(repoA.Open(), t)
-	AssertNil(repoB.Open(), t)
+	AssertNil(repoA.Open(), t, "a")
+	AssertNil(repoB.Open(), t, "b")
 
 	t.Log("close and remove b while a is open")
 	AssertNil(repoB.Close(), t, "close b")
@@ -50,12 +54,18 @@ func TestCanManageReposIndependently(t *testing.T) {
 
 func AssertNil(err error, t *testing.T, msgs ...string) {
 	if err != nil {
-		t.Error(msgs, "error:", err)
+		t.Fatal(msgs, "error:", err)
+	}
+}
+
+func Assert(v bool, t *testing.T, msgs ...string) {
+	if !v {
+		t.Fatal(msgs)
 	}
 }
 
 func AssertErr(err error, t *testing.T, msgs ...string) {
 	if err == nil {
-		t.Error(msgs, "error:", err)
+		t.Fatal(msgs, "error:", err)
 	}
 }
