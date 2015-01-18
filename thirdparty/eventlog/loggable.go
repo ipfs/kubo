c89b5ce8244807a267280d72c550fd8937c5a7c4
Brian Tiger Chow <brian.holderchow@gmail.com>
2015-01-18 15:09:14 -0800

    wip Loggable func

--- thirdparty/eventlog/loggable.go
+++ thirdparty/eventlog/loggable.go
@@ -10,3 +10,19 @@ type LoggableMap map[string]interface{}
 func (l LoggableMap) Loggable() map[string]interface{} {
 	return l
 }
+
+// Loggable converts a func into a Loggable
+type LoggableF func() map[string]interface{}
+
+func (l LoggableF) Loggable() map[string]interface{} {
+	return l()
+}
+
+func Deferred(key string, f func() string) Loggable {
+	function := func() map[string]interface{} {
+		return map[string]interface{}{
+			key: f(),
+		}
+	}
+	return LoggableF(function)
+}
