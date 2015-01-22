038ec6a5b3a4dc6732b7a5522afe07d4d520bf1e
Brian Tiger Chow <brian.holderchow@gmail.com>
2015-01-21 22:45:06 -0800

    feat: IPFSWatch

---
+++ cmd/ipfswatch/main.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/jbenet/go-ipfs/Godeps/_workspace/src/code.google.com/p/go.net/context"
+)
+import (
+	"github.com/jbenet/go-ipfs/Godeps/_workspace/src/github.com/mitchellh/go-homedir"
+	"github.com/jbenet/go-ipfs/Godeps/_workspace/src/gopkg.in/fsnotify.v1"
+	"github.com/jbenet/go-ipfs/core"
+	"github.com/jbenet/go-ipfs/core/coreunix"
+	"github.com/jbenet/go-ipfs/repo/config"
+	"github.com/jbenet/go-ipfs/repo/fsrepo"
+)
+
+var repoPath = flag.String("repo", os.Getenv("IPFS_PATH"), "IPFS_PATH to use")
+var watchPath = flag.String("path", ".", "the path to watch")
+
+func main() {
+	flag.Parse()
+
+	// precedence
+	// 1. --repo flag
+	// 2. IPFS_PATH environment variable
+	// 3. default repo path
+	ipfsPath := config.DefaultPathRoot
+	if *repoPath != "" {
+		ipfsPath = *repoPath
+	}
+
+	if err := run(ipfsPath, *watchPath); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run(ipfsPath, watchPath string) error {
+	log.Printf("running IPFSWatch on %s using repo at %s...", watchPath, ipfsPath)
+
+	ipfsPath, err := homedir.Expand(ipfsPath)
+	if err != nil {
+		return err
+	}
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return err
+	}
+	defer watcher.Close()
+
+	if err := addTree(watcher, watchPath); err != nil {
+		return err
+	}
+
+	r := fsrepo.At(ipfsPath)
+	if err := r.Open(); err != nil {
+		// TODO handle case: daemon running
+		// TODO handle case: repo doesn't exist or isn't initialized
+		return err
+	}
+	node, err := core.NewIPFSNode(context.Background(), core.Online(r))
+	if err != nil {
+		return err
+	}
+	defer node.Close()
+
+	for {
+		select {
+		case e := <-watcher.Events:
+			log.Printf("received event: %s", e)
+			isDir, err := IsDirectory(e.Name)
+			if err != nil {
+				continue
+			}
+			switch e.Op {
+			case fsnotify.Remove:
+				if isDir {
+					if err := watcher.Remove(e.Name); err != nil {
+						return err
+					}
+				}
+			default:
+				// all events except for Remove result in an IPFS.Add, but only
+				// directory creation triggers a new watch
+				switch e.Op {
+				case fsnotify.Create:
+					if isDir {
+						addTree(watcher, e.Name)
+					}
+				}
+				func() {
+					file, err := os.Open(e.Name)
+					if err != nil {
+						log.Println(err)
+					}
+					defer file.Close()
+					k, err := coreunix.Add(node, file)
+					if err != nil {
+						log.Println(err)
+					}
+					log.Printf("added %s... key: %s", e.Name, k)
+				}()
+			}
+		case err := <-watcher.Errors:
+			log.Println(err)
+		}
+	}
+	return nil
+}
+
+func addTree(w *fsnotify.Watcher, root string) error {
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		isDir, err := IsDirectory(path)
+		if err != nil {
+			log.Println(err)
+			return nil
+		}
+		switch {
+		case isDir && IsHidden(path):
+			log.Println(path)
+			return filepath.SkipDir
+		case isDir:
+			log.Println(path)
+			if err := w.Add(path); err != nil {
+				return err
+			}
+		default:
+			return nil
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func IsDirectory(path string) (bool, error) {
+	fileInfo, err := os.Stat(path)
+	return fileInfo.IsDir(), err
+}
+
+func IsHidden(path string) bool {
+	path = filepath.Base(path)
+	if path == "." || path == "" {
+		return false
+	}
+	if rune(path[0]) == rune('.') {
+		return true
+	}
+	return false
+}
