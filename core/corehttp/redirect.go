e2be5c2039f87ca2517d6a43ac217f23682315c2
Matt Bell <mappum@gmail.com>
2015-01-26 23:03:05 -0800

    core/corehttp: Added RedirectOption

---
+++ core/corehttp/redirect.go
@@ -0,0 +1,23 @@
+package corehttp
+
+import (
+	"net/http"
+
+	core "github.com/jbenet/go-ipfs/core"
+)
+
+func RedirectOption(path string, redirect string) ServeOption {
+	handler := &redirectHandler{redirect}
+	return func(n *core.IpfsNode, mux *http.ServeMux) error {
+		mux.Handle("/"+path, handler)
+		return nil
+	}
+}
+
+type redirectHandler struct {
+	path string
+}
+
+func (i *redirectHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, i.path, 302)
+}
