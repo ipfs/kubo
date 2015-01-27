45ed27e27333c22dbca0cead5cdeffc92d52f6a8
Matt Bell <mappum@gmail.com>
2015-01-26 17:00:40 -0800

    test/sharness: get: Quote variables

--- test/sharness/t0090-get.sh
+++ test/sharness/t0090-get.sh
@@ -23,56 +23,56 @@ test_expect_success "'ipfs get --help' output looks good" '
 test_expect_success "ipfs get succeeds" '
   echo "Hello Worlds!" > data &&
   HASH=`ipfs add -q data` &&
-  ipfs get $HASH > actual
+  ipfs get "$HASH" > actual
 '
 
 test_expect_success "ipfs get output looks good" '
-  echo "Saving file(s) to $HASH
+  echo "Saving file(s) to "$HASH"
 " > expected &&
   test_cmp expected actual
 '
 
 test_expect_success "ipfs get file output looks good" '
-  test_cmp $HASH data
+  test_cmp "$HASH" data
 '
 
 test_expect_success "ipfs get errors when trying to overwrite a file" '
-  test_must_fail ipfs get $HASH > actual &&
-  rm $HASH
+  test_must_fail ipfs get "$HASH" > actual &&
+  rm "$HASH"
 '
 
 test_expect_success "ipfs get -a succeeds" '
-  ipfs get $HASH -a > actual
+  ipfs get "$HASH" -a > actual
 '
 
 test_expect_success "ipfs get -a output looks good" '
-  echo "Saving archive to $HASH.tar
+  echo "Saving archive to "$HASH".tar
 " > expected &&
   test_cmp expected actual
 '
 
 test_expect_success "ipfs get -a archive output is valid" '
   tar -xf "$HASH".tar &&
-  test_cmp $HASH data &&
+  test_cmp "$HASH" data &&
   rm "$HASH".tar &&
-  rm $HASH
+  rm "$HASH"
 '
 
 test_expect_success "ipfs get -a -C succeeds" '
-  ipfs get $HASH -a -C > actual
+  ipfs get "$HASH" -a -C > actual
 '
 
 test_expect_success "ipfs get -a -C output looks good" '
-  echo "Saving archive to $HASH.tar.gz
+  echo "Saving archive to "$HASH".tar.gz
 " > expected &&
   test_cmp expected actual
 '
 
 test_expect_failure "gzipped tar archive output is valid" '
-  tar -zxf $HASH.tar.gz &&
-  test_cmp $HASH data &&
+  tar -zxf "$HASH".tar.gz &&
+  test_cmp "$HASH" data &&
   rm "$HASH".tar.gz &&
-  rm $HASH
+  rm "$HASH"
 '
 
 test_expect_success "ipfs get succeeds (directory)" '
@@ -81,11 +81,11 @@ test_expect_success "ipfs get succeeds (directory)" '
   mkdir dir/b &&
   echo "Hello, Worlds!" > dir/b/c &&
   HASH2=`ipfs add -r -q dir | tail -n 1` &&
-  ipfs get $HASH2 > actual
+  ipfs get "$HASH2" > actual
 '
 
 test_expect_success "ipfs get output looks good (directory)" '
-  echo "Saving file(s) to $HASH2
+  echo "Saving file(s) to "$HASH2"
 " > expected &&
   test_cmp expected actual
 '
@@ -93,11 +93,11 @@ test_expect_success "ipfs get output looks good (directory)" '
 test_expect_success "ipfs get output is valid (directory)" '
   test_cmp dir/a "$HASH2"/a &&
   test_cmp dir/b/c "$HASH2"/b/c &&
-  rm -r $HASH2
+  rm -r "$HASH2"
 '
 
 test_expect_success "ipfs get -a -C succeeds (directory)" '
-  ipfs get $HASH2 -a -C > actual
+  ipfs get "$HASH2" -a -C > actual
 '
 
 test_expect_success "ipfs get -a -C output looks good (directory)" '
@@ -110,7 +110,7 @@ test_expect_success "gzipped tar archive output is valid (directory)" '
   tar -zxf "$HASH2".tar.gz &&
   test_cmp dir/a "$HASH2"/a &&
   test_cmp dir/b/c "$HASH2"/b/c &&
-  rm -r $HASH2
+  rm -r "$HASH2"
 '
 
 test_kill_ipfs_daemon
