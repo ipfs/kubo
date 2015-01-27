9dac5bb7d313cda105f47064caf65766f2651a60
Juan Batiz-Benet <juan@benet.ai>
2015-01-27 01:22:48 -0800

    p2p/net: dial log -> events
    This commit turns all dial logs into log.Events.
    
    Everything's great except for one problem:
    The LoggableMap I'm using does not print out things
    correctly. I gave it peer.IDs, and Multiaddrs
    and both got logged as nothing `{}` (didn't even call
    their String() methods!) So, for now, this function
    encodes it when called... This is wrong and should be
    fixed before being merged in. Otherwise we  will be
    constantly encoding peer.IDs and Multiaddrs without
    needing to.
    
    @briantigerchow how do you suggest doing this?
    I don't know my way around your Loggable.

--- util/eventlog/loggables/loggables.go
+++ util/eventlog/loggables/loggables.go
@@ -9,7 +9,11 @@ package loggables
 import (
 	"net"
 
+	ma "github.com/jbenet/go-ipfs/Godeps/_workspace/src/github.com/jbenet/go-multiaddr"
+
 	log "github.com/jbenet/go-ipfs/thirdparty/eventlog"
+
+	peer "github.com/jbenet/go-ipfs/p2p/peer"
 )
 
 // NetConn returns an eventlog.Metadata with the conn addresses
@@ -25,4 +29,22 @@ func Error(e error) log.Loggable {
 	return log.Metadata{
 		"error": e.Error(),
 	}
+}
+
+// Dial metadata is metadata for dial events
+func Dial(sys string, lid, rid peer.ID, laddr, raddr ma.Multiaddr) log.LoggableMap {
+	m := log.Metadata{"subsystem": sys}
+	if lid != "" {
+		m["localPeer"] = lid.Pretty()
+	}
+	if laddr != nil {
+		m["localAddr"] = laddr.String()
+	}
+	if rid != "" {
+		m["remotePeer"] = rid.Pretty()
+	}
+	if raddr != nil {
+		m["remoteAddr"] = raddr.String()
+	}
+	return log.LoggableMap(m)
 }
