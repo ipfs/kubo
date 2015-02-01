pombredanne/go-ipfs

bootstrap: use ipfsaddr for boostrap peers :warning:
:warning: this commit makes your current configs unusable, as the
default bootstrap peers. You may need to edit your config.

Go from:

```js
Bootstrap: [
  {
    "Address": "/ip4/104.131.131.82/tcp/4001",
    "PeerID": "QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ"
  }
]
```

To:
```js
Bootstrap: [
  "/ip4/104.131.131.82/tcp/4001/ipfs/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ"
]
```

core/core.go
@@ -305,10 +305,9 @@ func (n *IpfsNode) Bootstrap(cfg BootstrapConfig) error {
 	// freshest bootstrap peers from config. this responds to live changes.
 	if cfg.BootstrapPeers == nil {
 		cfg.BootstrapPeers = func() []peer.PeerInfo {
-			bpeers := n.Repo.Config().Bootstrap
-			ps, err := toPeerInfos(bpeers)
+			ps, err := n.loadBootstrapPeers()
 			if err != nil {
-				log.Error("failed to parse bootstrap peers from config: %s", bpeers)
+				log.Error("failed to parse bootstrap peers from config: %s", n.Repo.Config().Bootstrap)
 				return nil
 			}
 			return ps
@@ -355,6 +354,14 @@ func (n *IpfsNode) loadPrivateKey() error {
 	n.Peerstore.AddPrivKey(n.Identity, n.PrivateKey)
 	n.Peerstore.AddPubKey(n.Identity, sk.GetPublic())
 	return nil
+}
+
+func (n *IpfsNode) loadBootstrapPeers() ([]peer.PeerInfo, error) {
+	parsed, err := n.Repo.Config().BootstrapPeers()
+	if err != nil {
+		return nil, err
+	}
+	return toPeerInfos(parsed), nil
 }
 
 // SetupOfflineRouting loads the local nodes private key and

ipfs/kubo

core/core.go
@@ -318,10 +318,9 @@ func (n *IpfsNode) Bootstrap(cfg BootstrapConfig) error {
 	// freshest bootstrap peers from config. this responds to live changes.
 	if cfg.BootstrapPeers == nil {
 		cfg.BootstrapPeers = func() []peer.PeerInfo {
-			bpeers := n.Repo.Config().Bootstrap
-			ps, err := toPeerInfos(bpeers)
+			ps, err := n.loadBootstrapPeers()
 			if err != nil {
-				log.Warningf("failed to parse bootstrap peers from config: %s", bpeers)
+				log.Warningf("failed to parse bootstrap peers from config: %s", n.Repo.Config().Bootstrap)
 				return nil
 			}
 			return ps
@@ -368,6 +367,14 @@ func (n *IpfsNode) loadPrivateKey() error {
 	n.Peerstore.AddPrivKey(n.Identity, n.PrivateKey)
 	n.Peerstore.AddPubKey(n.Identity, sk.GetPublic())
 	return nil
+}
+
+func (n *IpfsNode) loadBootstrapPeers() ([]peer.PeerInfo, error) {
+	parsed, err := n.Repo.Config().BootstrapPeers()
+	if err != nil {
+		return nil, err
+	}
+	return toPeerInfos(parsed), nil
 }
 
 // SetupOfflineRouting loads the local nodes private key and