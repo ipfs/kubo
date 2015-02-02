0676a9b10b7850739eb38e04c70a308b6359ecf9
Brian Tiger Chow <brian.holderchow@gmail.com>
2015-02-02 06:09:21 -0800

    fix(gcr/s) defer log event

--- routing/grandcentral/server.go
+++ routing/grandcentral/server.go
@@ -42,7 +42,7 @@ func (s *Server) HandleRequest(ctx context.Context, p peer.ID, req *dhtpb.Messag
 func (s *Server) handleMessage(
 	ctx context.Context, p peer.ID, req *dhtpb.Message) (peer.ID, *dhtpb.Message) {
 
-	log.EventBegin(ctx, "routingMessageReceived", req, p, s.local).Done() // TODO may need to differentiate between local and remote
+	defer log.EventBegin(ctx, "routingMessageReceived", req, p, s.local).Done() // TODO may need to differentiate between local and remote
 
 	var response = dhtpb.NewMessage(req.GetType(), req.GetKey(), req.GetClusterLevel())
 	switch req.GetType() {
