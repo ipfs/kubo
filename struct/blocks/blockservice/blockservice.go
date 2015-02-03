sysbot/go-ipfs

moved things around

struct/blocks/blockservice/blockservice.go
@@ -1,0 +1,149 @@
+// package blockservice implements a BlockService interface that provides
+// a single GetBlock/AddBlock interface that seamlessly retrieves data either
+// locally or from a remote peer through the exchange.
+package blockservice
+
+import (
+	"errors"
+	"fmt"
+
+	context "github.com/jbenet/go-ipfs/Godeps/_workspace/src/code.google.com/p/go.net/context"
+
+	exchange "github.com/jbenet/go-ipfs/exchange"
+	blocks "github.com/jbenet/go-ipfs/struct/blocks"
+	worker "github.com/jbenet/go-ipfs/struct/blocks/blockservice/worker"
+	"github.com/jbenet/go-ipfs/struct/blocks/blockstore"
+	u "github.com/jbenet/go-ipfs/util"
+)
+
+var wc = worker.Config{
+	// When running on a single core, NumWorkers has a harsh negative effect on
+	// throughput. (-80% when < 25)
+	// Running a lot more workers appears to have very little effect on both
+	// single and multicore configurations.
+	NumWorkers: 25,
+
+	// These have no effect on when running on multiple cores, but harsh
+	// negative effect on throughput when running on a single core
+	// On multicore configurations these buffers have little effect on
+	// throughput.
+	// On single core configurations, larger buffers have severe adverse
+	// effects on throughput.
+	ClientBufferSize: 0,
+	WorkerBufferSize: 0,
+}
+
+var log = u.Logger("blockservice")
+var ErrNotFound = errors.New("blockservice: key not found")
+
+// BlockService is a hybrid block datastore. It stores data in a local
+// datastore and may retrieve data from a remote Exchange.
+// It uses an internal `datastore.Datastore` instance to store values.
+type BlockService struct {
+	// TODO don't expose underlying impl details
+	Blockstore blockstore.Blockstore
+	Exchange   exchange.Interface
+
+	worker *worker.Worker
+}
+
+// NewBlockService creates a BlockService with given datastore instance.
+func New(bs blockstore.Blockstore, rem exchange.Interface) (*BlockService, error) {
+	if bs == nil {
+		return nil, fmt.Errorf("BlockService requires valid blockstore")
+	}
+	if rem == nil {
+		log.Warning("blockservice running in local (offline) mode.")
+	}
+
+	return &BlockService{
+		Blockstore: bs,
+		Exchange:   rem,
+		worker:     worker.NewWorker(rem, wc),
+	}, nil
+}
+
+// AddBlock adds a particular block to the service, Putting it into the datastore.
+// TODO pass a context into this if the remote.HasBlock is going to remain here.
+func (s *BlockService) AddBlock(b *blocks.Block) (u.Key, error) {
+	k := b.Key()
+	err := s.Blockstore.Put(b)
+	if err != nil {
+		return k, err
+	}
+	if err := s.worker.HasBlock(b); err != nil {
+		return "", errors.New("blockservice is closed")
+	}
+	return k, nil
+}
+
+// GetBlock retrieves a particular block from the service,
+// Getting it from the datastore using the key (hash).
+func (s *BlockService) GetBlock(ctx context.Context, k u.Key) (*blocks.Block, error) {
+	log.Debugf("BlockService GetBlock: '%s'", k)
+	block, err := s.Blockstore.Get(k)
+	if err == nil {
+		return block, nil
+		// TODO be careful checking ErrNotFound. If the underlying
+		// implementation changes, this will break.
+	} else if err == blockstore.ErrNotFound && s.Exchange != nil {
+		log.Debug("Blockservice: Searching bitswap.")
+		blk, err := s.Exchange.GetBlock(ctx, k)
+		if err != nil {
+			return nil, err
+		}
+		return blk, nil
+	} else {
+		log.Debug("Blockservice GetBlock: Not found.")
+		return nil, ErrNotFound
+	}
+}
+
+// GetBlocks gets a list of blocks asynchronously and returns through
+// the returned channel.
+// NB: No guarantees are made about order.
+func (s *BlockService) GetBlocks(ctx context.Context, ks []u.Key) <-chan *blocks.Block {
+	out := make(chan *blocks.Block, 0)
+	go func() {
+		defer close(out)
+		var misses []u.Key
+		for _, k := range ks {
+			hit, err := s.Blockstore.Get(k)
+			if err != nil {
+				misses = append(misses, k)
+				continue
+			}
+			log.Debug("Blockservice: Got data in datastore.")
+			select {
+			case out <- hit:
+			case <-ctx.Done():
+				return
+			}
+		}
+
+		rblocks, err := s.Exchange.GetBlocks(ctx, misses)
+		if err != nil {
+			log.Errorf("Error with GetBlocks: %s", err)
+			return
+		}
+
+		for b := range rblocks {
+			select {
+			case out <- b:
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+	return out
+}
+
+// DeleteBlock deletes a block in the blockservice from the datastore
+func (s *BlockService) DeleteBlock(k u.Key) error {
+	return s.Blockstore.DeleteBlock(k)
+}
+
+func (s *BlockService) Close() error {
+	log.Debug("blockservice is shutting down...")
+	return s.worker.Close()
+}

struct/blocks/set/dbset.go
@@ -1,0 +1,48 @@
+package set
+
+import (
+	ds "github.com/jbenet/go-ipfs/Godeps/_workspace/src/github.com/jbenet/go-datastore"
+	"github.com/jbenet/go-ipfs/struct/blocks/bloom"
+	"github.com/jbenet/go-ipfs/util"
+)
+
+type datastoreBlockSet struct {
+	dstore ds.Datastore
+	bset   BlockSet
+}
+
+// NewDBWrapperSet returns a new blockset wrapping a given datastore
+func NewDBWrapperSet(d ds.Datastore, bset BlockSet) BlockSet {
+	return &datastoreBlockSet{
+		dstore: d,
+		bset:   bset,
+	}
+}
+
+func (d *datastoreBlockSet) AddBlock(k util.Key) {
+	err := d.dstore.Put(k.DsKey(), []byte{})
+	if err != nil {
+		log.Errorf("blockset put error: %s", err)
+	}
+
+	d.bset.AddBlock(k)
+}
+
+func (d *datastoreBlockSet) RemoveBlock(k util.Key) {
+	d.bset.RemoveBlock(k)
+	if !d.bset.HasKey(k) {
+		d.dstore.Delete(k.DsKey())
+	}
+}
+
+func (d *datastoreBlockSet) HasKey(k util.Key) bool {
+	return d.bset.HasKey(k)
+}
+
+func (d *datastoreBlockSet) GetBloomFilter() bloom.Filter {
+	return d.bset.GetBloomFilter()
+}
+
+func (d *datastoreBlockSet) GetKeys() []util.Key {
+	return d.bset.GetKeys()
+}

struct/merkledag/merkledag.go
@@ -1,0 +1,251 @@
+// package merkledag implements the ipfs Merkle DAG datastructures.
+package merkledag
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/jbenet/go-ipfs/Godeps/_workspace/src/code.google.com/p/go.net/context"
+
+	blocks "github.com/jbenet/go-ipfs/struct/blocks"
+	bserv "github.com/jbenet/go-ipfs/struct/blocks/blockservice"
+	u "github.com/jbenet/go-ipfs/util"
+)
+
+var log = u.Logger("merkledag")
+var ErrNotFound = fmt.Errorf("merkledag: not found")
+
+// DAGService is an IPFS Merkle DAG service.
+type DAGService interface {
+	Add(*Node) (u.Key, error)
+	AddRecursive(*Node) error
+	Get(u.Key) (*Node, error)
+	Remove(*Node) error
+
+	// GetDAG returns, in order, all the single leve child
+	// nodes of the passed in node.
+	GetDAG(context.Context, *Node) []NodeGetter
+	GetNodes(context.Context, []u.Key) []NodeGetter
+}
+
+func NewDAGService(bs *bserv.BlockService) DAGService {
+	return &dagService{bs}
+}
+
+// dagService is an IPFS Merkle DAG service.
+// - the root is virtual (like a forest)
+// - stores nodes' data in a BlockService
+// TODO: should cache Nodes that are in memory, and be
+//       able to free some of them when vm pressure is high
+type dagService struct {
+	Blocks *bserv.BlockService
+}
+
+// Add adds a node to the dagService, storing the block in the BlockService
+func (n *dagService) Add(nd *Node) (u.Key, error) {
+	if n == nil { // FIXME remove this assertion. protect with constructor invariant
+		return "", fmt.Errorf("dagService is nil")
+	}
+
+	d, err := nd.Encoded(false)
+	if err != nil {
+		return "", err
+	}
+
+	b := new(blocks.Block)
+	b.Data = d
+	b.Multihash, err = nd.Multihash()
+	if err != nil {
+		return "", err
+	}
+
+	return n.Blocks.AddBlock(b)
+}
+
+// AddRecursive adds the given node and all child nodes to the BlockService
+func (n *dagService) AddRecursive(nd *Node) error {
+	_, err := n.Add(nd)
+	if err != nil {
+		log.Info("AddRecursive Error: %s\n", err)
+		return err
+	}
+
+	for _, link := range nd.Links {
+		if link.Node != nil {
+			err := n.AddRecursive(link.Node)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+// Get retrieves a node from the dagService, fetching the block in the BlockService
+func (n *dagService) Get(k u.Key) (*Node, error) {
+	if n == nil {
+		return nil, fmt.Errorf("dagService is nil")
+	}
+
+	ctx, _ := context.WithTimeout(context.TODO(), time.Minute)
+	// we shouldn't use an arbitrary timeout here.
+	// since Get doesnt take in a context yet, we give a large upper bound.
+	// think of an http request. we want it to go on as long as the client requests it.
+
+	b, err := n.Blocks.GetBlock(ctx, k)
+	if err != nil {
+		return nil, err
+	}
+
+	return Decoded(b.Data)
+}
+
+// Remove deletes the given node and all of its children from the BlockService
+func (n *dagService) Remove(nd *Node) error {
+	for _, l := range nd.Links {
+		if l.Node != nil {
+			n.Remove(l.Node)
+		}
+	}
+	k, err := nd.Key()
+	if err != nil {
+		return err
+	}
+	return n.Blocks.DeleteBlock(k)
+}
+
+// FetchGraph asynchronously fetches all nodes that are children of the given
+// node, and returns a channel that may be waited upon for the fetch to complete
+func FetchGraph(ctx context.Context, root *Node, serv DAGService) chan struct{} {
+	log.Warning("Untested.")
+	var wg sync.WaitGroup
+	done := make(chan struct{})
+
+	for _, l := range root.Links {
+		wg.Add(1)
+		go func(lnk *Link) {
+
+			// Signal child is done on way out
+			defer wg.Done()
+			select {
+			case <-ctx.Done():
+				return
+			}
+
+			nd, err := lnk.GetNode(serv)
+			if err != nil {
+				log.Error(err)
+				return
+			}
+
+			// Wait for children to finish
+			<-FetchGraph(ctx, nd, serv)
+		}(l)
+	}
+
+	go func() {
+		wg.Wait()
+		done <- struct{}{}
+	}()
+
+	return done
+}
+
+// FindLinks searches this nodes links for the given key,
+// returns the indexes of any links pointing to it
+func FindLinks(links []u.Key, k u.Key, start int) []int {
+	var out []int
+	for i, lnk_k := range links[start:] {
+		if k == lnk_k {
+			out = append(out, i+start)
+		}
+	}
+	return out
+}
+
+// GetDAG will fill out all of the links of the given Node.
+// It returns a channel of nodes, which the caller can receive
+// all the child nodes of 'root' on, in proper order.
+func (ds *dagService) GetDAG(ctx context.Context, root *Node) []NodeGetter {
+	var keys []u.Key
+	for _, lnk := range root.Links {
+		keys = append(keys, u.Key(lnk.Hash))
+	}
+
+	return ds.GetNodes(ctx, keys)
+}
+
+// GetNodes returns an array of 'NodeGetter' promises, with each corresponding
+// to the key with the same index as the passed in keys
+func (ds *dagService) GetNodes(ctx context.Context, keys []u.Key) []NodeGetter {
+	promises := make([]NodeGetter, len(keys))
+	sendChans := make([]chan<- *Node, len(keys))
+	for i, _ := range keys {
+		promises[i], sendChans[i] = newNodePromise(ctx)
+	}
+
+	go func() {
+		blkchan := ds.Blocks.GetBlocks(ctx, keys)
+
+		for {
+			select {
+			case blk, ok := <-blkchan:
+				if !ok {
+					return
+				}
+
+				nd, err := Decoded(blk.Data)
+				if err != nil {
+					// NB: can happen with improperly formatted input data
+					log.Error("Got back bad block!")
+					return
+				}
+				is := FindLinks(keys, blk.Key(), 0)
+				for _, i := range is {
+					sendChans[i] <- nd
+				}
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+	return promises
+}
+
+func newNodePromise(ctx context.Context) (NodeGetter, chan<- *Node) {
+	ch := make(chan *Node, 1)
+	return &nodePromise{
+		recv: ch,
+		ctx:  ctx,
+	}, ch
+}
+
+type nodePromise struct {
+	cache *Node
+	recv  <-chan *Node
+	ctx   context.Context
+}
+
+// NodeGetter provides a promise like interface for a dag Node
+// the first call to Get will block until the Node is received
+// from its internal channels, subsequent calls will return the
+// cached node.
+type NodeGetter interface {
+	Get() (*Node, error)
+}
+
+func (np *nodePromise) Get() (*Node, error) {
+	if np.cache != nil {
+		return np.cache, nil
+	}
+
+	select {
+	case blk := <-np.recv:
+		np.cache = blk
+	case <-np.ctx.Done():
+		return nil, np.ctx.Err()
+	}
+	return np.cache, nil
+}

ipfs/kubo

struct/blocks/blockservice/blockservice.go
@@ -1,0 +1,149 @@
+// package blockservice implements a BlockService interface that provides
+// a single GetBlock/AddBlock interface that seamlessly retrieves data either
+// locally or from a remote peer through the exchange.
+package blockservice
+
+import (
+	"errors"
+	"fmt"
+
+	context "github.com/jbenet/go-ipfs/Godeps/_workspace/src/code.google.com/p/go.net/context"
+
+	exchange "github.com/jbenet/go-ipfs/exchange"
+	blocks "github.com/jbenet/go-ipfs/struct/blocks"
+	worker "github.com/jbenet/go-ipfs/struct/blocks/blockservice/worker"
+	"github.com/jbenet/go-ipfs/struct/blocks/blockstore"
+	u "github.com/jbenet/go-ipfs/util"
+)
+
+var wc = worker.Config{
+	// When running on a single core, NumWorkers has a harsh negative effect on
+	// throughput. (-80% when < 25)
+	// Running a lot more workers appears to have very little effect on both
+	// single and multicore configurations.
+	NumWorkers: 25,
+
+	// These have no effect on when running on multiple cores, but harsh
+	// negative effect on throughput when running on a single core
+	// On multicore configurations these buffers have little effect on
+	// throughput.
+	// On single core configurations, larger buffers have severe adverse
+	// effects on throughput.
+	ClientBufferSize: 0,
+	WorkerBufferSize: 0,
+}
+
+var log = u.Logger("blockservice")
+var ErrNotFound = errors.New("blockservice: key not found")
+
+// BlockService is a hybrid block datastore. It stores data in a local
+// datastore and may retrieve data from a remote Exchange.
+// It uses an internal `datastore.Datastore` instance to store values.
+type BlockService struct {
+	// TODO don't expose underlying impl details
+	Blockstore blockstore.Blockstore
+	Exchange   exchange.Interface
+
+	worker *worker.Worker
+}
+
+// NewBlockService creates a BlockService with given datastore instance.
+func New(bs blockstore.Blockstore, rem exchange.Interface) (*BlockService, error) {
+	if bs == nil {
+		return nil, fmt.Errorf("BlockService requires valid blockstore")
+	}
+	if rem == nil {
+		log.Warning("blockservice running in local (offline) mode.")
+	}
+
+	return &BlockService{
+		Blockstore: bs,
+		Exchange:   rem,
+		worker:     worker.NewWorker(rem, wc),
+	}, nil
+}
+
+// AddBlock adds a particular block to the service, Putting it into the datastore.
+// TODO pass a context into this if the remote.HasBlock is going to remain here.
+func (s *BlockService) AddBlock(b *blocks.Block) (u.Key, error) {
+	k := b.Key()
+	err := s.Blockstore.Put(b)
+	if err != nil {
+		return k, err
+	}
+	if err := s.worker.HasBlock(b); err != nil {
+		return "", errors.New("blockservice is closed")
+	}
+	return k, nil
+}
+
+// GetBlock retrieves a particular block from the service,
+// Getting it from the datastore using the key (hash).
+func (s *BlockService) GetBlock(ctx context.Context, k u.Key) (*blocks.Block, error) {
+	log.Debugf("BlockService GetBlock: '%s'", k)
+	block, err := s.Blockstore.Get(k)
+	if err == nil {
+		return block, nil
+		// TODO be careful checking ErrNotFound. If the underlying
+		// implementation changes, this will break.
+	} else if err == blockstore.ErrNotFound && s.Exchange != nil {
+		log.Debug("Blockservice: Searching bitswap.")
+		blk, err := s.Exchange.GetBlock(ctx, k)
+		if err != nil {
+			return nil, err
+		}
+		return blk, nil
+	} else {
+		log.Debug("Blockservice GetBlock: Not found.")
+		return nil, ErrNotFound
+	}
+}
+
+// GetBlocks gets a list of blocks asynchronously and returns through
+// the returned channel.
+// NB: No guarantees are made about order.
+func (s *BlockService) GetBlocks(ctx context.Context, ks []u.Key) <-chan *blocks.Block {
+	out := make(chan *blocks.Block, 0)
+	go func() {
+		defer close(out)
+		var misses []u.Key
+		for _, k := range ks {
+			hit, err := s.Blockstore.Get(k)
+			if err != nil {
+				misses = append(misses, k)
+				continue
+			}
+			log.Debug("Blockservice: Got data in datastore.")
+			select {
+			case out <- hit:
+			case <-ctx.Done():
+				return
+			}
+		}
+
+		rblocks, err := s.Exchange.GetBlocks(ctx, misses)
+		if err != nil {
+			log.Debugf("Error with GetBlocks: %s", err)
+			return
+		}
+
+		for b := range rblocks {
+			select {
+			case out <- b:
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+	return out
+}
+
+// DeleteBlock deletes a block in the blockservice from the datastore
+func (s *BlockService) DeleteBlock(k u.Key) error {
+	return s.Blockstore.DeleteBlock(k)
+}
+
+func (s *BlockService) Close() error {
+	log.Debug("blockservice is shutting down...")
+	return s.worker.Close()
+}

struct/blocks/set/dbset.go
@@ -1,0 +1,48 @@
+package set
+
+import (
+	ds "github.com/jbenet/go-ipfs/Godeps/_workspace/src/github.com/jbenet/go-datastore"
+	"github.com/jbenet/go-ipfs/struct/blocks/bloom"
+	"github.com/jbenet/go-ipfs/util"
+)
+
+type datastoreBlockSet struct {
+	dstore ds.Datastore
+	bset   BlockSet
+}
+
+// NewDBWrapperSet returns a new blockset wrapping a given datastore
+func NewDBWrapperSet(d ds.Datastore, bset BlockSet) BlockSet {
+	return &datastoreBlockSet{
+		dstore: d,
+		bset:   bset,
+	}
+}
+
+func (d *datastoreBlockSet) AddBlock(k util.Key) {
+	err := d.dstore.Put(k.DsKey(), []byte{})
+	if err != nil {
+		log.Debugf("blockset put error: %s", err)
+	}
+
+	d.bset.AddBlock(k)
+}
+
+func (d *datastoreBlockSet) RemoveBlock(k util.Key) {
+	d.bset.RemoveBlock(k)
+	if !d.bset.HasKey(k) {
+		d.dstore.Delete(k.DsKey())
+	}
+}
+
+func (d *datastoreBlockSet) HasKey(k util.Key) bool {
+	return d.bset.HasKey(k)
+}
+
+func (d *datastoreBlockSet) GetBloomFilter() bloom.Filter {
+	return d.bset.GetBloomFilter()
+}
+
+func (d *datastoreBlockSet) GetKeys() []util.Key {
+	return d.bset.GetKeys()
+}

struct/merkledag/merkledag.go
@@ -1,0 +1,251 @@
+// package merkledag implements the ipfs Merkle DAG datastructures.
+package merkledag
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/jbenet/go-ipfs/Godeps/_workspace/src/code.google.com/p/go.net/context"
+
+	blocks "github.com/jbenet/go-ipfs/struct/blocks"
+	bserv "github.com/jbenet/go-ipfs/struct/blocks/blockservice"
+	u "github.com/jbenet/go-ipfs/util"
+)
+
+var log = u.Logger("merkledag")
+var ErrNotFound = fmt.Errorf("merkledag: not found")
+
+// DAGService is an IPFS Merkle DAG service.
+type DAGService interface {
+	Add(*Node) (u.Key, error)
+	AddRecursive(*Node) error
+	Get(u.Key) (*Node, error)
+	Remove(*Node) error
+
+	// GetDAG returns, in order, all the single leve child
+	// nodes of the passed in node.
+	GetDAG(context.Context, *Node) []NodeGetter
+	GetNodes(context.Context, []u.Key) []NodeGetter
+}
+
+func NewDAGService(bs *bserv.BlockService) DAGService {
+	return &dagService{bs}
+}
+
+// dagService is an IPFS Merkle DAG service.
+// - the root is virtual (like a forest)
+// - stores nodes' data in a BlockService
+// TODO: should cache Nodes that are in memory, and be
+//       able to free some of them when vm pressure is high
+type dagService struct {
+	Blocks *bserv.BlockService
+}
+
+// Add adds a node to the dagService, storing the block in the BlockService
+func (n *dagService) Add(nd *Node) (u.Key, error) {
+	if n == nil { // FIXME remove this assertion. protect with constructor invariant
+		return "", fmt.Errorf("dagService is nil")
+	}
+
+	d, err := nd.Encoded(false)
+	if err != nil {
+		return "", err
+	}
+
+	b := new(blocks.Block)
+	b.Data = d
+	b.Multihash, err = nd.Multihash()
+	if err != nil {
+		return "", err
+	}
+
+	return n.Blocks.AddBlock(b)
+}
+
+// AddRecursive adds the given node and all child nodes to the BlockService
+func (n *dagService) AddRecursive(nd *Node) error {
+	_, err := n.Add(nd)
+	if err != nil {
+		log.Info("AddRecursive Error: %s\n", err)
+		return err
+	}
+
+	for _, link := range nd.Links {
+		if link.Node != nil {
+			err := n.AddRecursive(link.Node)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+// Get retrieves a node from the dagService, fetching the block in the BlockService
+func (n *dagService) Get(k u.Key) (*Node, error) {
+	if n == nil {
+		return nil, fmt.Errorf("dagService is nil")
+	}
+
+	ctx, _ := context.WithTimeout(context.TODO(), time.Minute)
+	// we shouldn't use an arbitrary timeout here.
+	// since Get doesnt take in a context yet, we give a large upper bound.
+	// think of an http request. we want it to go on as long as the client requests it.
+
+	b, err := n.Blocks.GetBlock(ctx, k)
+	if err != nil {
+		return nil, err
+	}
+
+	return Decoded(b.Data)
+}
+
+// Remove deletes the given node and all of its children from the BlockService
+func (n *dagService) Remove(nd *Node) error {
+	for _, l := range nd.Links {
+		if l.Node != nil {
+			n.Remove(l.Node)
+		}
+	}
+	k, err := nd.Key()
+	if err != nil {
+		return err
+	}
+	return n.Blocks.DeleteBlock(k)
+}
+
+// FetchGraph asynchronously fetches all nodes that are children of the given
+// node, and returns a channel that may be waited upon for the fetch to complete
+func FetchGraph(ctx context.Context, root *Node, serv DAGService) chan struct{} {
+	log.Warning("Untested.")
+	var wg sync.WaitGroup
+	done := make(chan struct{})
+
+	for _, l := range root.Links {
+		wg.Add(1)
+		go func(lnk *Link) {
+
+			// Signal child is done on way out
+			defer wg.Done()
+			select {
+			case <-ctx.Done():
+				return
+			}
+
+			nd, err := lnk.GetNode(serv)
+			if err != nil {
+				log.Debug(err)
+				return
+			}
+
+			// Wait for children to finish
+			<-FetchGraph(ctx, nd, serv)
+		}(l)
+	}
+
+	go func() {
+		wg.Wait()
+		done <- struct{}{}
+	}()
+
+	return done
+}
+
+// FindLinks searches this nodes links for the given key,
+// returns the indexes of any links pointing to it
+func FindLinks(links []u.Key, k u.Key, start int) []int {
+	var out []int
+	for i, lnk_k := range links[start:] {
+		if k == lnk_k {
+			out = append(out, i+start)
+		}
+	}
+	return out
+}
+
+// GetDAG will fill out all of the links of the given Node.
+// It returns a channel of nodes, which the caller can receive
+// all the child nodes of 'root' on, in proper order.
+func (ds *dagService) GetDAG(ctx context.Context, root *Node) []NodeGetter {
+	var keys []u.Key
+	for _, lnk := range root.Links {
+		keys = append(keys, u.Key(lnk.Hash))
+	}
+
+	return ds.GetNodes(ctx, keys)
+}
+
+// GetNodes returns an array of 'NodeGetter' promises, with each corresponding
+// to the key with the same index as the passed in keys
+func (ds *dagService) GetNodes(ctx context.Context, keys []u.Key) []NodeGetter {
+	promises := make([]NodeGetter, len(keys))
+	sendChans := make([]chan<- *Node, len(keys))
+	for i, _ := range keys {
+		promises[i], sendChans[i] = newNodePromise(ctx)
+	}
+
+	go func() {
+		blkchan := ds.Blocks.GetBlocks(ctx, keys)
+
+		for {
+			select {
+			case blk, ok := <-blkchan:
+				if !ok {
+					return
+				}
+
+				nd, err := Decoded(blk.Data)
+				if err != nil {
+					// NB: can happen with improperly formatted input data
+					log.Debug("Got back bad block!")
+					return
+				}
+				is := FindLinks(keys, blk.Key(), 0)
+				for _, i := range is {
+					sendChans[i] <- nd
+				}
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+	return promises
+}
+
+func newNodePromise(ctx context.Context) (NodeGetter, chan<- *Node) {
+	ch := make(chan *Node, 1)
+	return &nodePromise{
+		recv: ch,
+		ctx:  ctx,
+	}, ch
+}
+
+type nodePromise struct {
+	cache *Node
+	recv  <-chan *Node
+	ctx   context.Context
+}
+
+// NodeGetter provides a promise like interface for a dag Node
+// the first call to Get will block until the Node is received
+// from its internal channels, subsequent calls will return the
+// cached node.
+type NodeGetter interface {
+	Get() (*Node, error)
+}
+
+func (np *nodePromise) Get() (*Node, error) {
+	if np.cache != nil {
+		return np.cache, nil
+	}
+
+	select {
+	case blk := <-np.recv:
+		np.cache = blk
+	case <-np.ctx.Done():
+		return nil, np.ctx.Err()
+	}
+	return np.cache, nil
+}