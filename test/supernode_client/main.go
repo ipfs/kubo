bbcd89fbf2a0e2745894b833fd257ed7b8fb94b0
Brian Tiger Chow <brian.holderchow@gmail.com>
2015-02-15 06:27:06 -0800

    fix(test/snr_client) use config.DefaultSNRServers

--- test/supernode_client/main.go
+++ test/supernode_client/main.go
@@ -41,16 +41,6 @@ var (
 	nBitsForKeypair = flag.Int("b", 1024, "number of bits for keypair (if repo is uninitialized)")
 )
 
-var bootstrapAddresses = []string{
-	"/ip4/104.236.70.34/tcp/4001/ipfs/QmaWJw5mcWkCThPtC7hVq28e3WbwLHnWF8HbMNJrRDybE4",
-	"/ip4/128.199.72.111/tcp/4001/ipfs/Qmd2cSiZUt7vhiuTmqBB7XWbkuFR8KMLiEuQssjyNXyaZT",
-	"/ip4/162.243.251.152/tcp/4001/ipfs/QmeaDCMmFWDuF4baxhMuvQH8APoymtMSJwhyZqUp9ux3SN",
-	"/ip4/178.62.8.190/tcp/4001/ipfs/QmdXzZ25cyzSF99csCQmmPZ1NTbWTe8qtKFaZKpZQPdTFB",
-	"/ip4/188.166.8.195/tcp/4001/ipfs/QmNU1Vpryj5hfSmybSYHnS497ttgy9aNJ3T2B8wY2uMso4",
-	"/ip4/188.226.225.73/tcp/4001/ipfs/QmYHGLxLfHFd9veUt159LMhLD8LqhDjBUS9ZehLkPap6od",
-	"/ip4/192.241.209.121/tcp/4001/ipfs/QmNPM851fxWb3qjLV66a8Nqtw7fBnMw2UrdFQidSdBQnvS",
-}
-
 func main() {
 	flag.Parse()
 	if err := run(); err != nil {
@@ -60,8 +50,9 @@ func main() {
 }
 
 func run() error {
+	servers := config.DefaultGCRServers
 	fmt.Println("using gcr remotes:")
-	for _, p := range bootstrapAddresses {
+	for _, p := range servers {
 		fmt.Println("\t", p)
 	}
 
@@ -80,13 +71,13 @@ func run() error {
 		return err
 	}
 	cfg := repo.Config()
-	cfg.Bootstrap = bootstrapAddresses
+	cfg.Bootstrap = servers
 	if err := repo.SetConfig(cfg); err != nil {
 		return err
 	}
 
 	var addrs []ipfsaddr.IPFSAddr
-	for _, info := range bootstrapAddresses {
+	for _, info := range servers {
 		addr, err := ipfsaddr.ParseString(info)
 		if err != nil {
 			return err
