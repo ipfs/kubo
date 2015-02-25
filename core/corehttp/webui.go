8fb6241bc5470c095ab5afb469f8c175ed2bf7d3
David Hamp-Gonsalves <davidhampgonsalves@gmail.com>
2015-02-25 08:14:54 -0800

    updating webui to the current version

--- core/corehttp/webui.go
+++ core/corehttp/webui.go
@@ -1,11 +1,12 @@
 package corehttp
 
 // TODO: move to IPNS
-const WebUIPath = "/ipfs/QmXdu7HWdV6CUaUabd9q2ZeA4iHZLVyDRj3Gi4dsJsWjbr"
+const WebUIPath = "QmRjkUt5A3UHjTm5uG9yZG1bRxCv4cveviQHeVTzkpXTFt"
 
 // this is a list of all past webUI paths.
 var WebUIPaths = []string{
 	WebUIPath,
+  "/ipfs/QmXdu7HWdV6CUaUabd9q2ZeA4iHZLVyDRj3Gi4dsJsWjbr",
 	"/ipfs/QmaaqrHyAQm7gALkRW8DcfGX3u8q9rWKnxEMmf7m9z515w",
 	"/ipfs/QmSHDxWsMPuJQKWmVA1rB5a3NX2Eme5fPqNb63qwaqiqSp",
 	"/ipfs/QmctngrQAt9fjpQUZr7Bx3BsXUcif52eZGTizWhvcShsjz",
