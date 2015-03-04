e55aaf3f832bb42e8b943470e6653b67f664dd3f
Henry <cryptix@riseup.net>
2015-03-05 00:45:38 +0100

    nofuse: better help description for 'ipfs mount'

--- core/commands/mount_nofuse.go
+++ core/commands/mount_nofuse.go
@@ -11,9 +11,14 @@ import (
 
 var MountCmd = &cmds.Command{
 	Helptext: cmds.HelpText{
-		Tagline: "Mounts IPFS to the filesystem (read-only) - !!Disabled!!",
+		Tagline: "Mounts IPFS to the filesystem (disabled)",
 		ShortDescription: `
-Caution! Your version ipfs is compiled _without_ fuse support.
+This version of ipfs is compiled without fuse support, which is required
+for mounting. If you'd like to be able to mount, please use a version of
+ipfs compiled with fuse.
+
+For the latest instructions, please check the project's repository:
+  http://github.com/jbenet/go-ipfs
 `,
 	},
 }
