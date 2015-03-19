ff7a333b5c82ea3925f60d7d3e2a5862ed8cec3b
Konstantin Koroviev <kkoroviev@gmail.com>
2015-03-19 13:56:58 +0200

    t0030-mount.sh: 2 known breakages vanished

--- test/sharness/t0030-mount.sh
+++ test/sharness/t0030-mount.sh
@@ -20,11 +20,11 @@ test_launch_ipfs_daemon
 
 # run this mount failure before mounting properly.
 
-test_expect_failure "'ipfs mount' fails when no mount dir (issue #341)" '
+test_expect_success "'ipfs mount' fails when no mount dir (issue #341)" '
 	test_must_fail ipfs mount -f=not_ipfs -n=not_ipns >actual
 '
 
-test_expect_failure "'ipfs mount' looks good when it fails (issue #341)" '
+test_expect_success "'ipfs mount' looks bad when it fails (issue #341)" '
 	! grep "IPFS mounted at: $(pwd)/ipfs" actual >/dev/null &&
 	! grep "IPNS mounted at: $(pwd)/ipns" actual >/dev/null ||
 	test_fsh cat actual
