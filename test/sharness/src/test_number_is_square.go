35803e43887fff15b4caa762fbc582163e7989ee
Konstantin Koroviev <kkoroviev@gmail.com>
2015-03-24 23:36:49 +0200

    Add examples of Sharness test helpers

---
+++ test/sharness/src/test_number_is_square.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Printf("%s: 1 argument required\n", os.Args[0])
+		os.Exit(2)
+	}
+	n, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Printf("%s: %v\n", os.Args[0], err)
+		os.Exit(2)
+	}
+	for i := 1; ; i++ {
+		switch {
+		case i*i == n:
+			os.Exit(0)
+		case i*i > n:
+			os.Exit(1)
+		}
+
+	}
+}
