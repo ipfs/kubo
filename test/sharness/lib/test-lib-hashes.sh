5a455485e5f0ca2d73481a19a3845285f175f091
Ho-Sheng Hsiao <talktohosh@gmail.com>
2015-03-30 22:54:18 -0700

    [TEST] Added welcome doc hashes, updated contact doc

--- test/sharness/lib/test-lib-hashes.sh
+++ test/sharness/lib/test-lib-hashes.sh
@@ -1,6 +1,5 @@
 # this file defines several useful hashes used across the test codebase.
 # thus they can be defined + changed in one place
 
-#HASH_WELCOME_DOCS="QmV9E5oNhFDWeRV8NQ91G7vp3pvff2S4UYkn89T5koSzqo"
 HASH_WELCOME_DOCS="Qmc8ZcdAUduQfC32oSXnLsUyDb7GNZ9rX4VjrtB4s212v4"
 HASH_EMPTY_DIR="QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn"
