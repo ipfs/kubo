000de0300060df2fd624ef4d6c39bf512282fe81
theswitch <theswitch@users.noreply.github.com>
2015-04-06 12:39:06 +1000

    Correct port in quick-start examples

--- test/sharness/lib/test-lib-hashes.sh
+++ test/sharness/lib/test-lib-hashes.sh
@@ -1,5 +1,5 @@
 # this file defines several useful hashes used across the test codebase.
 # thus they can be defined + changed in one place
 
-HASH_WELCOME_DOCS="Qmc8ZcdAUduQfC32oSXnLsUyDb7GNZ9rX4VjrtB4s212v4"
+HASH_WELCOME_DOCS="Qmcqtw8FfrVSBaRmbWwHxt3AuySBhJLcvmFYi3Lbc4xnwj"
 HASH_EMPTY_DIR="QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn"
