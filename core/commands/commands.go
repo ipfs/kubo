95ccdca9ec35db370653c48044ce18490759159e
W. Trevor King <wking@tremily.us>
2015-05-02 08:21:47 -0700

    core/commands/commands: Add a package comment

--- core/commands/commands.go
+++ core/commands/commands.go
@@ -1,3 +1,10 @@
+/*
+Package commands implements the IPFS command interface
+
+Using github.com/ipfs/go-ipfs/commands to define the command line and
+HTTP APIs.  This is the interface available to folks consuming IPFS
+from outside of the Go language.
+*/
 package commands
 
 import (
