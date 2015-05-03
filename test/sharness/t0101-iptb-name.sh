9cf8c5cbc9c4c4679daf1f8b42739eb0e663c50b
Jeromy <jeromyj@gmail.com>
2015-05-03 16:39:41 -0700

    update iptb dependency and use different ports for each iptb cluster
    update iptb dependency again, and pick different ports for each iptb cluster
    
    try and fix godeps crap

--- test/sharness/t0101-iptb-name.sh
+++ test/sharness/t0101-iptb-name.sh
@@ -11,7 +11,7 @@ test_description="Test ipfs repo operations"
 export IPTB_ROOT="`pwd`/.iptb"
 
 test_expect_success "set up an iptb cluster" '
-	iptb -n=4 init &&
+	iptb -n=4 -p=9000 init &&
 	iptb -wait start
 '
 
