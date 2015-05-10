e67c2a7b14b1e6cf25000dc63feb0c4f75d56d96
Juan Batiz-Benet <juan@benet.ai>
2015-05-10 09:23:45 -0400

    version bump to 0.3.4
    (I'll add the changelog later today)

--- repo/config/version.go
+++ repo/config/version.go
@@ -8,7 +8,7 @@ import (
 )
 
 // CurrentVersionNumber is the current application's version literal
-const CurrentVersionNumber = "0.3.3"
+const CurrentVersionNumber = "0.3.4"
 
 // Version regulates checking if the most recent version is run
 type Version struct {
