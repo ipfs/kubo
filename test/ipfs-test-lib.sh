9bb1b55afb33a6a1eeda24a1bbc0b65b04de14cd
Tommi Virtanen <tv@eagain.net>
2015-05-11 10:36:17 -0700

    sharness: Don't assume we know all things that can create garbage

--- test/ipfs-test-lib.sh
+++ test/ipfs-test-lib.sh
@@ -23,6 +23,28 @@ test_sort_cmp() {
 	test_cmp "$1_sorted" "$2_sorted"
 }
 
+# Test whether all the expected lines are included in a file. The file
+# can have extra lines.
+#
+# $1 - Path to file with expected lines.
+# $2 - Path to file with actual output.
+#
+# Examples
+#
+#   test_expect_success 'foo says hello' '
+#       echo hello >expected &&
+#       foo >actual &&
+#       test_cmp expected actual
+#   '
+#
+# Returns the exit code of the command set by TEST_CMP.
+test_includes_lines() {
+	sort "$1" >"$1_sorted" &&
+	sort "$2" >"$2_sorted" &&
+	comm -2 -3 "$1_sorted" "$2_sorted" >"$2_missing" &&
+	[ ! -s "$2_missing" ] || test_fsh comm -2 -3 "$1_sorted" "$2_sorted"
+}
+
 # Depending on GNU seq availability is not nice.
 # Git also has test_seq but it uses Perl.
 test_seq() {
