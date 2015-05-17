ff9cb9ecf4a41e1e1cc52102145372cda5a7af5c
Christian Couder <chriscool@tuxfamily.org>
2015-05-17 20:34:20 +0200

    parse_test: test multiline stdin
    License: MIT
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- commands/cli/parse_test.go
+++ commands/cli/parse_test.go
@@ -224,4 +224,10 @@ func TestArgumentParsing(t *testing.T) {
 	test([]string{"stdinenabled"}, fstdin, []string{"stdin1"})
 	test([]string{"stdinenabled", "value1"}, fstdin, []string{"stdin1", "value1"})
 	test([]string{"stdinenabled", "value1", "value2"}, fstdin, []string{"stdin1", "value1", "value2"})
+
+	fstdin = fileToSimulateStdin(t, "stdin1\nstdin2")
+	test([]string{"stdinenabled"}, fstdin, []string{"stdin1", "stdin2"})
+
+	fstdin = fileToSimulateStdin(t, "stdin1\nstdin2\nstdin3")
+	test([]string{"stdinenabled"}, fstdin, []string{"stdin1", "stdin2", "stdin3"})
 }
