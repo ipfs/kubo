b515b05574aa2199948d612e3ae50e64e606ff5a
Tommi Virtanen <tv@eagain.net>
2015-05-19 16:05:57 -0700

    fsrepo: Detect uninitialized repo by missing config file
    Earlier, it also checked checked the leveldb directory. That part
    added no crash safety to the application, and just hardcoded
    assumptions about the datastore.
    
    If anything, this should rely on the absolute last item created by
    fsrepo.Init, and there should be fsync guarantees about ordering.
    
    License: MIT
    Signed-off-by: Tommi Virtanen <tv@eagain.net>

--- repo/fsrepo/fsrepo.go
+++ repo/fsrepo/fsrepo.go
@@ -620,8 +620,5 @@ func isInitializedUnsynced(repoPath string) bool {
 	if !configIsInitialized(repoPath) {
 		return false
 	}
-	if !util.FileExists(path.Join(repoPath, leveldbDirectory)) {
-		return false
-	}
 	return true
 }
