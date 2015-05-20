84e45b46738eb0d599d327b9fdb5f8a11f49a6d0
Tommi Virtanen <tv@eagain.net>
2015-05-20 08:50:36 -0700

    fsrepo: Refactor to extract datastore internals
    License: MIT
    Signed-off-by: Tommi Virtanen <tv@eagain.net>

--- repo/repo.go
+++ repo/repo.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 	"io"
 
-	datastore "github.com/ipfs/go-ipfs/Godeps/_workspace/src/github.com/jbenet/go-datastore"
+	ds "github.com/ipfs/go-ipfs/Godeps/_workspace/src/github.com/jbenet/go-datastore"
 
 	config "github.com/ipfs/go-ipfs/repo/config"
 )
@@ -20,11 +20,18 @@ type Repo interface {
 	SetConfigKey(key string, value interface{}) error
 	GetConfigKey(key string) (interface{}, error)
 
-	Datastore() datastore.ThreadSafeDatastore
+	Datastore() Datastore
 	GetStorageUsage() (uint64, error)
 
 	// SetAPIAddr sets the API address in the repo.
 	SetAPIAddr(addr string) error
 
+	io.Closer
+}
+
+// Datastore is the interface required from a datastore to be
+// acceptable to FSRepo.
+type Datastore interface {
+	ds.Datastore // should be threadsafe, just be careful
 	io.Closer
 }
