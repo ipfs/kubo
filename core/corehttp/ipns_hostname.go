373260d373c4066fa89ae60209f80e1fac92800c
Knut Ahlers <knut@ahlers.me>
2015-05-21 08:44:17 +0200

    Fix: dnslink domain resolving was broken; Add: no caching for those
    fixes #1234
    fixes #1267

--- core/corehttp/ipns_hostname.go
+++ core/corehttp/ipns_hostname.go
@@ -5,6 +5,7 @@ import (
 	"path"
 	"strings"
 
+	isd "github.com/ipfs/go-ipfs/Godeps/_workspace/src/github.com/jbenet/go-is-domain"
 	"github.com/ipfs/go-ipfs/Godeps/_workspace/src/golang.org/x/net/context"
 	"github.com/ipfs/go-ipfs/core"
 )
@@ -20,8 +21,11 @@ func IPNSHostnameOption() ServeOption {
 			defer cancel()
 
 			host := strings.SplitN(r.Host, ":", 2)[0]
-			if p, err := n.Namesys.Resolve(ctx, host); err == nil {
-				r.URL.Path = path.Join(p.String(), r.URL.Path)
+			if len(host) > 0 && isd.IsDomain(host) {
+				name := "/ipns/" + host
+				if _, err := n.Namesys.Resolve(ctx, name); err == nil {
+					r.URL.Path = path.Join("/ipns/", host) + r.URL.Path
+				}
 			}
 			childMux.ServeHTTP(w, r)
 		})
