d96246c6223021f55049a8714bda70466f47bd24
Travis Person <travis.person@gmail.com>
2015-05-20 21:56:20 -0700

    Fixed tests to actually test for the error we are seeking
    Cleaned the tests, and actually test for the error.

--- core/pathresolver_test.go
+++ core/pathresolver_test.go
@@ -3,39 +3,19 @@ package core
 import (
 	"testing"
 
-	context "github.com/ipfs/go-ipfs/Godeps/_workspace/src/golang.org/x/net/context"
-	config "github.com/ipfs/go-ipfs/repo/config"
-	"github.com/ipfs/go-ipfs/util/testutil"
-	"github.com/ipfs/go-ipfs/repo"
 	path "github.com/ipfs/go-ipfs/path"
+	"strings"
 )
 
 func TestResolveInvalidPath(t *testing.T) {
-	ctx := context.TODO()
-	id := testIdentity
-
-	r := &repo.Mock{
-		C: config.Config{
-			Identity: id,
-			Datastore: config.Datastore{
-				Type: "memory",
-			},
-			Addresses: config.Addresses{
-				Swarm: []string{"/ip4/0.0.0.0/tcp/4001"},
-				API:   "/ip4/127.0.0.1/tcp/8000",
-			},
-		},
-		D: testutil.ThreadSafeCloserMapDatastore(),
-	}
-
-	n, err := NewIPFSNode(ctx, Standard(r, false))
+	n, err := NewMockNode()
 	if n == nil || err != nil {
-		t.Error("Should have constructed.", err)
+		t.Fatal("Should have constructed.", err)
 	}
 
-	_, err = Resolve(ctx, n, path.Path("/ipfs/"))
-	if err == nil {
-		t.Error("Should get invalid path")
+	_, err = Resolve(n.Context(), n, path.Path("/ipfs/"))
+	if !strings.HasPrefix(err.Error(), "invalid path") {
+		t.Fatal("Should get invalid path.", err)
 	}
 
 }
