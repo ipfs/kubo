624c6e8fe597f02d6e11c0e919b525f47e8df065
Henry <cryptix@riseup.net>
2015-07-03 10:57:02 +0200

    assets: fix typo
    closes #1343
    
    License: MIT
    Signed-off-by: Henry <cryptix@riseup.net>

--- test/sharness/lib/test-lib-hashes.sh
+++ test/sharness/lib/test-lib-hashes.sh
@@ -1,6 +1,6 @@
 # this file defines several useful hashes used across the test codebase.
 # thus they can be defined + changed in one place
 
-HASH_WELCOME_DOCS="QmUaZRPqxSpkeQ5QvtYWYzRQA9K4zDPfQkkYjVgLx2XWrd"
+HASH_WELCOME_DOCS="QmVtU7ths96fMgZ8YSZAbKghyieq7AjxNdcqyVzxTt3qVe"
 HASH_HELP_PAGE="QmY5heUM5qgRubMDD1og9fhCPA6QdkMp3QCwd4s7gJsyE7"
 HASH_EMPTY_DIR="QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn"
