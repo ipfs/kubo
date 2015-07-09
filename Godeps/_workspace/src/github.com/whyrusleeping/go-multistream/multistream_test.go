f2aa058142f1ef43feedc1c2c0d3a1a20abc04ce
Jeromy <jeromyj@gmail.com>
2015-07-09 14:42:41 -0700

    allow multistream to have zero rtt stream opening
    License: MIT
    Signed-off-by: Jeromy <jeromyj@gmail.com>

--- Godeps/_workspace/src/github.com/whyrusleeping/go-multistream/multistream_test.go
+++ Godeps/_workspace/src/github.com/whyrusleeping/go-multistream/multistream_test.go
@@ -118,6 +118,112 @@ func TestSelectOneAndWrite(t *testing.T) {
 	verifyPipe(t, a, b)
 }
 
+func TestLazyConns(t *testing.T) {
+	a, b := net.Pipe()
+
+	mux := NewMultistreamMuxer()
+	mux.AddHandler("/a", nil)
+	mux.AddHandler("/b", nil)
+	mux.AddHandler("/c", nil)
+
+	la := NewLazyHandshakeConn(a, "/c")
+	lb := NewLazyHandshakeConn(b, "/c")
+
+	verifyPipe(t, la, lb)
+}
+
+func TestLazyAndMux(t *testing.T) {
+	a, b := net.Pipe()
+
+	mux := NewMultistreamMuxer()
+	mux.AddHandler("/a", nil)
+	mux.AddHandler("/b", nil)
+	mux.AddHandler("/c", nil)
+
+	done := make(chan struct{})
+	go func() {
+		selected, _, err := mux.Negotiate(a)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if selected != "/c" {
+			t.Fatal("incorrect protocol selected")
+		}
+
+		msg := make([]byte, 5)
+		_, err = a.Read(msg)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		close(done)
+	}()
+
+	lb := NewLazyHandshakeConn(b, "/c")
+
+	// do a write to push the handshake through
+	_, err := lb.Write([]byte("hello"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	select {
+	case <-time.After(time.Second):
+		t.Fatal("failed to complete in time")
+	case <-done:
+	}
+
+	verifyPipe(t, a, lb)
+}
+
+func TestLazyAndMuxWrite(t *testing.T) {
+	a, b := net.Pipe()
+
+	mux := NewMultistreamMuxer()
+	mux.AddHandler("/a", nil)
+	mux.AddHandler("/b", nil)
+	mux.AddHandler("/c", nil)
+
+	done := make(chan struct{})
+	go func() {
+		selected, _, err := mux.Negotiate(a)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if selected != "/c" {
+			t.Fatal("incorrect protocol selected")
+		}
+
+		_, err = a.Write([]byte("hello"))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		close(done)
+	}()
+
+	lb := NewLazyHandshakeConn(b, "/c")
+
+	// do a write to push the handshake through
+	msg := make([]byte, 5)
+	_, err := lb.Read(msg)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(msg) != "hello" {
+		t.Fatal("wrong!")
+	}
+
+	select {
+	case <-time.After(time.Second):
+		t.Fatal("failed to complete in time")
+	case <-done:
+	}
+
+	verifyPipe(t, a, lb)
+}
+
 func verifyPipe(t *testing.T, a, b io.ReadWriter) {
 	mes := make([]byte, 1024)
 	rand.Read(mes)
