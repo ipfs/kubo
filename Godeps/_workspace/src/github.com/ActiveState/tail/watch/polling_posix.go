64960110f6435d9b23dd9dbdd4838051d40f6a28
Jeromy <jeromyj@gmail.com>
2015-07-29 09:46:31 -0700

    clean up unused pacakges
    License: MIT
    Signed-off-by: Jeromy <jeromyj@gmail.com>

--- Godeps/_workspace/src/github.com/ActiveState/tail/watch/polling_posix.go
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2013 ActiveState Software Inc. All rights reserved.
-// +build linux darwin freebsd
-
-package watch
-
-func permissionErrorRetry(err error, retry *int) bool {
-	// No need for this on linux, don't retry
-	return false
-}
