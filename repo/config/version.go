d50def3f5cb42a724de6f96c333b11281975add1
Jeromy <jeromyj@gmail.com>
2015-07-30 10:56:50 -0700

    go-ipfs version 0.3.6 (config version bump)
    License: MIT
    Signed-off-by: Jeromy <jeromyj@gmail.com>

--- repo/config/version.go
+++ repo/config/version.go
@@ -8,7 +8,7 @@ import (
 )
 
 // CurrentVersionNumber is the current application's version literal
-const CurrentVersionNumber = "0.3.5"
+const CurrentVersionNumber = "0.3.6"
 
 // Version regulates checking if the most recent version is run
 type Version struct {
