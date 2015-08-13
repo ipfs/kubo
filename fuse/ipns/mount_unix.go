a97639a78a6166eb81e55eec7903587187901dc0
Jeromy <jeromyj@gmail.com>
2015-08-13 15:16:58 -0700

    dont create ipnsfs filesystem until its needed
    License: MIT
    Signed-off-by: Jeromy <jeromyj@gmail.com>

--- fuse/ipns/mount_unix.go
+++ fuse/ipns/mount_unix.go
@@ -6,12 +6,21 @@ package ipns
 import (
 	core "github.com/ipfs/go-ipfs/core"
 	mount "github.com/ipfs/go-ipfs/fuse/mount"
+	ipnsfs "github.com/ipfs/go-ipfs/ipnsfs"
 )
 
 // Mount mounts ipns at a given location, and returns a mount.Mount instance.
 func Mount(ipfs *core.IpfsNode, ipnsmp, ipfsmp string) (mount.Mount, error) {
 	cfg := ipfs.Repo.Config()
 	allow_other := cfg.Mounts.FuseAllowOther
+
+	if ipfs.IpnsFs == nil {
+		fs, err := ipnsfs.NewFilesystem(ipfs.Context(), ipfs.DAG, ipfs.Namesys, ipfs.Pinning, ipfs.PrivateKey)
+		if err != nil {
+			return nil, err
+		}
+		ipfs.IpnsFs = fs
+	}
 
 	fsys, err := NewFileSystem(ipfs, ipfs.PrivateKey, ipfsmp, ipnsmp)
 	if err != nil {
