e9bedb1aef3641c8ad94f0f03c62e12b4acef900
Jeromy <jeromyj@gmail.com>
2015-08-18 15:45:14 -0700

    bump development version
    License: MIT
    Signed-off-by: Jeromy <jeromyj@gmail.com>

--- repo/config/version.go
+++ repo/config/version.go
@@ -8,7 +8,7 @@ import (
 )
 
 // CurrentVersionNumber is the current application's version literal
-const CurrentVersionNumber = "0.3.7"
+const CurrentVersionNumber = "0.3.8-dev"
 
 // Version regulates checking if the most recent version is run
 type Version struct {
