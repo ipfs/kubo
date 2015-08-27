be96d8d7d5027716e415ebef08a381e6ceb21a7f
rht <rhtbot@gmail.com>
2015-08-27 15:41:52 +0700

    Fix test cases for ipfs api check
    License: MIT
    Signed-off-by: rht <rhtbot@gmail.com>

--- test/sharness/t0062-daemon-api.sh
+++ test/sharness/t0062-daemon-api.sh
@@ -9,8 +9,23 @@ test_description="Test daemon command"
 
 test_init_ipfs
 
-test_expect_success "client should work when there is no api file" '
-  ipfs --api "$differentapi" id
+differentport=$((PORT_API + 1))
+differentapi="/ip4/127.0.0.1/tcp/$differentport"
+peerid=$(ipfs config Identity.PeerID)
+
+test_client() {
+    args="$@"
+    printf $peerid >expected
+    ipfs $args id -f="<id>" >actual
+    test_cmp expected actual
+}
+
+test_expect_success "client should work when there is no api file and no --api is specified" '
+  test_client
+'
+
+test_expect_success "client should err when there is no api file and with --api is specified" '
+  test_must_fail test_client --api "$differentapi"
 '
 
 test_launch_ipfs_daemon
@@ -19,8 +34,10 @@ test_expect_success "'ipfs daemon' creates api file" '
   test -f ".ipfs/api"
 '
 
-differentport=$((PORT_API + 1))
-differentapi="/ip4/127.0.0.1/tcp/$differentport"
+test_expect_success "api file looks good" '
+  printf "$ADDR_API" >expected &&
+  test_cmp expected .ipfs/api
+'
 
 test_expect_success "client should err if client api != api file while daemon is on" '
   echo "Error: api not running" >expected &&
@@ -42,12 +59,16 @@ ADDR_API=$differentapi
 test_launch_ipfs_daemon --api "$ADDR_API"
 
 test_expect_success "'ipfs daemon' api option works" '
-  printf "$differentapi" > expected &&
+  printf "$differentapi" >expected &&
   test_cmp expected .ipfs/api
 '
 
 test_expect_success "client should work if client api == api file, != cfg api while daemon is on" '
-  ipfs --api "$differentapi" id
+  test_client --api "$differentapi"
+'
+
+test_expect_success "client should read the api file while daemon is on" '
+  test_client
 '
 
 test_kill_ipfs_daemon
