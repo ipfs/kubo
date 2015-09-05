davidar/go-ipfs

add-only-hash no longer stores entirety of everything in memory
License: MIT
Signed-off-by: Jeromy <jeromyj@gmail.com>

merkledag/utils/utils_test.go
@@ -23,7 +23,7 @@ func TestAddLink(t *testing.T) {
 	}
 
 	nd := new(dag.Node)
-	nnode, err := addLink(context.Background(), ds, nd, "fish", fk)
+	nnode, err := addLink(context.Background(), ds, nd, "fish", fishnode)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -104,7 +104,7 @@ func testInsert(t *testing.T, e *Editor, path, data string, create bool, experr 
 		}
 	}
 
-	err = e.InsertNodeAtPath(context.TODO(), path, ck, c)
+	err = e.InsertNodeAtPath(context.TODO(), path, child, c)
 	if experr != "" {
 		var got string
 		if err != nil {

ipfs/kubo

merkledag/utils/utils_test.go
@@ -23,7 +23,7 @@ func TestAddLink(t *testing.T) {
 	}
 
 	nd := new(dag.Node)
-	nnode, err := addLink(context.Background(), ds, nd, "fish", fk)
+	nnode, err := addLink(context.Background(), ds, nd, "fish", fishnode)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -104,7 +104,7 @@ func testInsert(t *testing.T, e *Editor, path, data string, create bool, experr 
 		}
 	}
 
-	err = e.InsertNodeAtPath(context.Background(), path, ck, c)
+	err = e.InsertNodeAtPath(context.Background(), path, child, c)
 	if experr != "" {
 		var got string
 		if err != nil {