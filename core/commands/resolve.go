8d370a39462d79c80df6573782520bd862cb2b4c
Matt Bell <mappum@gmail.com>
2015-09-06 12:55:37 -0700

    Added help text to 'resolve' command to show DAG path resolution.
    License: MIT
    Signed-off-by: Matt Bell <mappum@gmail.com>

--- core/commands/resolve.go
+++ core/commands/resolve.go
@@ -47,6 +47,11 @@ Resolve the value of another name recursively:
   > ipfs resolve -r /ipns/QmbCMUZw6JFeZ7Wp9jkzbye3Fzp2GGcPgC3nmeUjfVF87n
   /ipfs/Qmcqtw8FfrVSBaRmbWwHxt3AuySBhJLcvmFYi3Lbc4xnwj
 
+Resolve the value of an IPFS DAG path:
+
+  > ipfs resolve /ipfs/QmeZy1fGbwgVSrqbfh9fKQrAWgeyRnj7h8fsHS1oy3k99x/beep/boop
+  /ipfs/QmYRMjyvAiHKN9UTi8Bzt1HUspmSRD8T8DwxfSMzLgBon1
+
 `,
 	},
 
