0f97747e1762b8e47ad7165306d838553f929e84
Daniel Meyer <daniel.meyer@essendi.de>
2015-09-24 14:50:27 +0200

    x-robots-header to prevent duplicate content on public gateways
    License: MIT
    Signed-off-by: Daniel Meyer <daniel.meyer@essendi.de>

--- core/corehttp/gateway_handler.go
+++ core/corehttp/gateway_handler.go
@@ -125,7 +125,7 @@ func (i *gatewayHandler) getOrHeadHandler(w http.ResponseWriter, r *http.Request
 	i.addUserHeaders(w) // ok, _now_ write user's headers.
 	w.Header().Set("X-IPFS-Path", urlPath)
 
-	//robots-header to prevent search-engines from indexing the gateway because of the duplicate-content problematic
+	//robots-header to prevent search-engines from indexing the gateway because of the duplicate-content problematic when makeing the gateway public
 	w.Header().Set("X-Robots-Tag", "noindex")
 
 
