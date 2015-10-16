05d0b4eb1601e487ace09ae1b6ad1178306d9db1
Christian Couder <chriscool@tuxfamily.org>
2015-10-16 16:18:22 +0200

    coverage_helper: refactor egreps
    License: MIT
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- test/sharness_test_coverage_helper.sh
+++ test/sharness_test_coverage_helper.sh
@@ -48,53 +48,46 @@ CMD_RAW="$TMPDIR/ipfs_cmd_raw.txt"
 git grep -n -E '\Wipfs\W' -- sharness/t*-*.sh >"$CMD_RAW" ||
 die "Could not grep ipfs in the sharness tests"
 
-log "Remove test_expect_{success,failure} lines"
-CMD_EXPECT="$TMPDIR/ipfs_cmd_expect.txt"
-egrep -v 'test_expect_.*ipfs' "$CMD_RAW" >"$CMD_EXPECT" ||
-die "Could not remove test_expect_{success,failure} lines"
+grep_out() {
+    pattern="$1"
+    src="$TMPDIR/ipfs_cmd_${2}.txt"
+    dst="$TMPDIR/ipfs_cmd_${3}.txt"
+    desc="$4"
 
-log "Remove comments"
-CMD_COMMENT="$TMPDIR/ipfs_cmd_comment.txt"
-egrep -v '^[^:]+:[^:]+:\s*#' "$CMD_EXPECT" >"$CMD_COMMENT" ||
-die "Could not remove comments"
+    log "Remove $desc"
+    egrep -v "$pattern" "$src" >"$dst" || die "Could not remove $desc"
+}
 
-log "Remove test_description lines"
-CMD_DESC="$TMPDIR/ipfs_cmd_description.txt"
-egrep -v 'test_description=' "$CMD_COMMENT" >"$CMD_DESC" ||
-die "Could not remove test_description lines"
+grep_out 'test_expect_.*ipfs' raw expect "test_expect_{success,failure} lines"
+grep_out '^[^:]+:[^:]+:\s*#' expect comment "comments"
+grep_out 'test_description=' comment desc "test_description lines"
+grep_out '^[^:]+:[^:]+:\s*e?grep\W[^|]*\Wipfs' desc grep "grep lines"
+grep_out '^[^:]+:[^:]+:\s*echo\W[^|]*\Wipfs' grep echo "echo lines"
 
-log "Remove grep lines"
-CMD_GREP="$TMPDIR/ipfs_cmd_grep.txt"
-egrep -v '^[^:]+:[^:]+:\s*e?grep\W[^|]*\Wipfs' "$CMD_DESC" >"$CMD_GREP" ||
-die "Could not remove grep lines"
+grep_in() {
+    pattern="$1"
+    src="$TMPDIR/ipfs_cmd_${2}.txt"
+    dst="$TMPDIR/ipfs_cmd_${3}.txt"
+    desc="$4"
 
-log "Remove echo lines"
-CMD_ECHO="$TMPDIR/ipfs_cmd_echo.txt"
-egrep -v '^[^:]+:[^:]+:\s*echo\W[^|]*\Wipfs' "$CMD_GREP" >"$CMD_ECHO" ||
-die "Could not remove echo lines"
+    log "Keep $desc"
+    egrep "$pattern" "$src" >"$dst"
+}
 
+grep_in '\Wipfs\W.*/ipfs/' echo slash_in1 "ipfs.*/ipfs/"
+grep_in '/ipfs/.*\Wipfs\W' echo slash_in2 "/ipfs/.*ipfs"
 
+grep_out '/ipfs/' echo slash "/ipfs/"
 
-log "Keep ipfs.*/ipfs/"
-CMD_SLASH_OK="$TMPDIR/ipfs_cmd_slash_ok.txt"
-egrep '\Wipfs\W.*/ipfs/' "$CMD_ECHO" >"$CMD_SLASH_OK"
+grep_in '\Wipfs\W.*\.ipfs' slash dot_in1 "ipfs.*\.ipfs"
+grep_in '\.ipfs.*\Wipfs\W' slash dot_in2 "\.ipfs.*ipfs"
 
-log "Keep ipfs.*\.ipfs and \.ipfs.*ipfs"
-CMD_DOT_OK="$TMPDIR/ipfs_cmd_dot_ok.txt"
-egrep -e '\Wipfs\W.*\.ipfs' -e '\.ipfs.*\Wipfs\W' "$CMD_ECHO" >"$CMD_DOT_OK"
-
-log "Remove /ipfs/"
-CMD_SLASH="$TMPDIR/ipfs_cmd_slash.txt"
-egrep -v '/ipfs/' "$CMD_ECHO" >"$CMD_SLASH" ||
-die "Could not remove /ipfs/"
-
-log "Remove .ipfs"
-CMD_DOT="$TMPDIR/ipfs_cmd_dot.txt"
-egrep -v '\.ipfs' "$CMD_SLASH" >"$CMD_DOT" ||
-die "Could not remove .ipfs"
-
+grep_out '\.ipfs' slash dot ".ipfs"
 
 log "Print result"
-cat "$CMD_DOT" "$CMD_SLASH_OK" "$CMD_DOT_OK" | sort
+for f in dot slash_in1 slash_in2 dot_in1 dot_in2
+do
+    cat "$TMPDIR/ipfs_cmd_${f}.txt"
+done | sort | uniq
 
 # Remove temp directory...
