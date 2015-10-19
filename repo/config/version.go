6976ca7480c2e95960fb14aa58eeb82d480f2cc2
Jeromy <jeromyj@gmail.com>
2015-10-19 15:39:03 -0700

    fix makefile failing when not in a git repo
    License: MIT
    Signed-off-by: Jeromy <jeromyj@gmail.com>

--- repo/config/version.go
+++ repo/config/version.go
@@ -8,7 +8,7 @@ import (
 )
 
 // CurrentVersionNumber is the current application's version literal
-const CurrentVersionNumber = "0.3.8"
+const CurrentVersionNumber = "0.3.9-dev"
 
 // CurrentCommit is the current git commit, this is set as a ldflag in the Makefile
 var CurrentCommit string
