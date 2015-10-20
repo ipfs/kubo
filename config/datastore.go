14cf564b3a1a7d7956677c60448b724c45a3b2f7
rht <rhtbot@gmail.com>
2015-10-20 10:56:41 +0700

    go-ipfs-config: Add fixed period repo GC + test
    License: MIT
    Signed-off-by: rht <rhtbot@gmail.com>

--- config/datastore.go
+++ config/datastore.go
@@ -5,8 +5,11 @@ const DefaultDataStoreDirectory = "datastore"
 
 // Datastore tracks the configuration of the datastore.
 type Datastore struct {
-	Type string
-	Path string
+	Type               string
+	Path               string
+	StorageMax         string // in B, kB, kiB, MB, ...
+	StorageGCWatermark int64  // in percentage to multiply on StorageMax
+	GCPeriod           string // in ns, us, ms, s, m, h
 }
 
 // DataStorePath returns the default data store path given a configuration root
