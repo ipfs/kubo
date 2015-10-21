f3489070e4556999b1e548476b397dbb2302f5aa
Jeromy <jeromyj@gmail.com>
2015-10-20 20:15:22 -0700

    force godeps to save windows import
    License: MIT
    Signed-off-by: Jeromy <jeromyj@gmail.com>

--- util/sadhack/godep.go
+++ util/sadhack/godep.go
@@ -7,3 +7,7 @@ import _ "github.com/ipfs/go-ipfs/Godeps/_workspace/src/github.com/dustin/go-hum
 // imported by chegga/pb on windows, this is here so running godeps on non-windows doesnt
 // drop it from our vendoring
 import _ "github.com/ipfs/go-ipfs/Godeps/_workspace/src/github.com/olekukonko/ts"
+
+// these two are for diagnostics on windows systems
+import _ "github.com/ipfs/go-ipfs/Godeps/_workspace/src/github.com/StackExchange/wmi"
+import _ "github.com/ipfs/go-ipfs/Godeps/_workspace/src/github.com/go-ole/go-ole"
