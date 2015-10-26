268258dba5e7c933fcd5bbba2baee925b282aa35
Mildred Ki'Lya <mildred-pub.git@mildred.fr>
2015-10-26 17:13:44 +0100

    Remove usage of merkledag.Link.Node pointer outside of merkledag
    This prepares for inclusion of IPLD where the Node pointer won't be there.
    
    License: MIT
    Signed-off-by: Mildred Ki'Lya <mildred-pub.git@mildred.fr>

--- core/commands/ls.go
+++ core/commands/ls.go
@@ -78,12 +78,13 @@ it contains, with the following format:
 				Links: make([]LsLink, len(dagnode.Links)),
 			}
 			for j, link := range dagnode.Links {
-				link.Node, err = link.GetNode(req.Context(), node.DAG)
+				var linkNode *merkledag.Node
+				linkNode, err = link.GetNode(req.Context(), node.DAG)
 				if err != nil {
 					res.SetError(err, cmds.ErrNormal)
 					return
 				}
-				d, err := unixfs.FromBytes(link.Node.Data)
+				d, err := unixfs.FromBytes(linkNode.Data)
 				if err != nil {
 					res.SetError(err, cmds.ErrNormal)
 					return
