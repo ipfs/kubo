3d55feb3b1f2fae240323ce60d8bcf387116e458
gamemanj <gamemanj@hotmail.co.uk>
2015-11-03 14:29:34 +0000

    core/commands/publish + test/sharness/t0100-name.sh: Add "privkey" parameter to ipfs name publish
    See PR #1907 for more details.
    This is a rebase to the latest go-ipfs master. It also adheres better to guidelines.
    
    License: MIT
    Signed-off-by: Matthew Woodford-Hall <gamemanj@hotmail.co.uk>

--- core/commands/publish.go
+++ core/commands/publish.go
@@ -24,12 +24,12 @@ var PublishCmd = &cmds.Command{
 		ShortDescription: `
 IPNS is a PKI namespace, where names are the hashes of public keys, and
 the private key enables publishing new (signed) values. In publish, the
-default value of <name> is your own identity public key.
+default value of <privkey> is your own identity private key.
 `,
 		LongDescription: `
 IPNS is a PKI namespace, where names are the hashes of public keys, and
 the private key enables publishing new (signed) values. In publish, the
-default value of <name> is your own identity public key.
+default value of <privkey> is your own identity private key.
 
 Examples:
 
@@ -38,19 +38,29 @@ Publish an <ipfs-path> to your identity name:
   > ipfs name publish /ipfs/QmatmE9msSfkKxoffpHwNLNKgwZG8eT9Bud6YoPab52vpy
   Published to QmbCMUZw6JFeZ7Wp9jkzbye3Fzp2GGcPgC3nmeUjfVF87n: /ipfs/QmatmE9msSfkKxoffpHwNLNKgwZG8eT9Bud6YoPab52vpy
 
-Publish an <ipfs-path> to another public key (not implemented):
+Publish an <ipfs-path> to a <privkey>:
 
-  > ipfs name publish /ipfs/QmatmE9msSfkKxoffpHwNLNKgwZG8eT9Bud6YoPab52vpy QmbCMUZw6JFeZ7Wp9jkzbye3Fzp2GGcPgC3nmeUjfVF87n
-  Published to QmbCMUZw6JFeZ7Wp9jkzbye3Fzp2GGcPgC3nmeUjfVF87n: /ipfs/QmatmE9msSfkKxoffpHwNLNKgwZG8eT9Bud6YoPab52vpy
+  > ipfs name publish /ipfs/QmatmE9msSfkKxoffpHwNLNKgwZG8eT9Bud6YoPab52vpy CAAS4AQwggJcAgEAAoGBALksWw8SwEX0DJZOkzbB597OVHkzFb8zW5LoX5Vx658ZYR9R1T5qRXa4wLegD1BPBnnfqkcbEN+Wweheib0w1i7u7zYKMyz9xwRcCPHh4eL8oCg2r3DX8rWUYvZne34bnFqnbObVIRx4pmPJXC5rNsdqTG4Dg9P8+/UXuBev9aorAgMBAAECgYEAoYVtUHKcwOgmap3Tj7oIVbNIwAeteoCD6ltDtQoP61GqBDXPeogcW3jAseuuL/EexwQwdaHIUCAiuFxubVbCG+Z3tK5fS/zHvl4mQp5THjbN36dqmgu2gpZNU+P+K45+x+1Mj+1VW0pRUFsm3FbPQQIYalz7gkc9P52AG27pubECQQDXc5uob7wbgG3H5Ca3do3g/3k2CMbVyQ0R6qIWP0v+q6z85rvftbj4UFzFDM0XuWINah6CXeaue3anmL0hKjKHAkEA3AXybn/fWfhLn49vB2Re9dTVonJeyE+fel1qI1nzaluGqSbNxuvUSArjro5q23DAGAMZCNZ4J2jTzD1xZqNgPQJAF27jhzZf5z3Yst0FuP6T/9zJei8KMUZkvYYfivvncBOMBRWzaWmCbL+Q133E8Meg+oSIPPWpmWCkTyY1q93DEQJAUSeMZT+bNYdE9YSlUleuQwSPDA0dcssTqsG7/XAXPZqmz8t1STMBKNWDZ4Y2Wdx7rh+uYzkgNoEO5h2fr1kBjQJARpRKE5vWnChCt8s6mibpjeuZM7ZbILl+b82Cq2N2KqKflBA3Al4XnZDfg2XCM7CcPMJSdNWbbm44qt0Q3z0DAA==
+  Published to QmdeadvG5GdPWiea2h7jAsZdVyxawY714KDWSDBAaRgkAD: /ipfs/QmatmE9msSfkKxoffpHwNLNKgwZG8eT9Bud6YoPab52vpy
 
+TODO: Publish an <ipfs-path> to a public key (in the <privkey> arg) that has a private key in the keystore:
+
+  > ipfs name publish /ipfs/QmatmE9msSfkKxoffpHwNLNKgwZG8eT9Bud6YoPab52vpy QmdeadvG5GdPWiea2h7jAsZdVyxawY714KDWSDBAaRgkAD --keystore
+  Published to QmdeadvG5GdPWiea2h7jAsZdVyxawY714KDWSDBAaRgkAD: /ipfs/QmatmE9msSfkKxoffpHwNLNKgwZG8eT9Bud6YoPab52vpy
 `,
 	},
 
 	Arguments: []cmds.Argument{
-		cmds.StringArg("ipfs-path", true, false, "IPFS path of the obejct to be published").EnableStdin(),
+		cmds.StringArg("ipfs-path", true, false, "IPFS path of the object to be published").EnableStdin(),
+		cmds.StringArg("privkey", false, false, "The private key to publish (default is your own key)"),
 	},
 	Options: []cmds.Option{
 		cmds.BoolOption("resolve", "resolve given path before publishing (default=true)"),
+		// example: ipfs name publish /ipfs/QmatmE9msSfkKxoffpHwNLNKgwZG8eT9Bud6YoPab52vpy QmdeadvG5GdPWiea2h7jAsZdVyxawY714KDWSDBAaRgkAD --keystore
+		// Or, privkey could be turned into a string option, mutually exclusive with --keystore as a string option.
+		// cmds.BoolOption("keystore", "Treat privkey as a public key, getting the real private key from the keystore."),
+		// Another option is for it to try parsing it as a multihash first (checking length to be sure) and looking for it in the keystore.
+		// That way both options could work transparently.
 		cmds.StringOption("lifetime", "t", "time duration that the record will be valid for (default: 24hrs)"),
 		cmds.StringOption("ttl", "time duration this record should be cached for (caution: experimental)"),
 	},
@@ -71,10 +81,25 @@ Publish an <ipfs-path> to another public key (not implemented):
 		}
 
 		pstr := req.Arguments()[0]
-
+		
 		if n.Identity == "" {
 			res.SetError(errors.New("Identity not loaded!"), cmds.ErrNormal)
 			return
+		}
+		
+		privkey := n.PrivateKey
+
+		if len(req.Arguments()) != 1 {
+			data, err := crypto.ConfigDecodeKey(req.Arguments()[1])
+			if err != nil {
+				res.SetError(fmt.Errorf("error decoding privkey: %s", err), cmds.ErrNormal)
+				return
+			}
+			privkey, err = crypto.UnmarshalPrivateKey(data)
+			if err != nil {
+				res.SetError(fmt.Errorf("error unmarshalling privkey: %s", err), cmds.ErrNormal)
+				return
+			}
 		}
 
 		popts := &publishOpts{
@@ -108,7 +133,7 @@ Publish an <ipfs-path> to another public key (not implemented):
 			ctx = context.WithValue(ctx, "ipns-publish-ttl", d)
 		}
 
-		output, err := publish(ctx, n, n.PrivateKey, path.Path(pstr), popts)
+		output, err := publish(ctx, n, privkey, path.Path(pstr), popts)
 		if err != nil {
 			res.SetError(err, cmds.ErrNormal)
 			return
--- test/sharness/t0100-name.sh
+++ test/sharness/t0100-name.sh
@@ -60,18 +60,29 @@ test_expect_success "ipfs cat on published content succeeds" '
     test_cmp expected actual
 '
 
-# publish with an explicit node ID
+# publish with a specific private key (DEADKP_PK)
 
-test_expect_failure "'ipfs name publish <local-id> <hash>' succeeds" '
-	PEERID=`ipfs id --format="<id>"` &&
-	test_check_peerid "${PEERID}" &&
-	echo ipfs name publish "${PEERID}" "/ipfs/$HASH_WELCOME_DOCS" &&
-	ipfs name publish "${PEERID}" "/ipfs/$HASH_WELCOME_DOCS" >actual_node_id_publish
+# Create some non-useless content
+# (Someone should pin this.)
+HASH_DEADKP_DOC=`echo 'Public Key: $HASH_DEADKP_ID
+Private Key: $HASH_DEADKP_PK
+
+This keypair is for documentation purposes.
+If you have a need for a keypair in IPFS documentation,
+feel free to use this keypair.' | ipfs add | grep -o Qm[a-zA-Z0-9]* | tail -n 1`
+
+test_expect_success "'ipfs name publish <hash> <privkey>' succeeds" '
+	echo ipfs name publish "/ipfs/$HASH_DEADKP_DOC" $HASH_DEADKP_PK &&
+	ipfs name publish "/ipfs/$HASH_DEADKP_DOC" $HASH_DEADKP_PK >actual_privkey_publish
 '
 
-test_expect_failure "publish with our explicit node ID looks good" '
-	echo "Published to ${PEERID}: /ipfs/$HASH_WELCOME_DOCS" >expected_node_id_publish &&
-	test_cmp expected_node_id_publish actual_node_id_publish
+test_expect_success "publish with our explicit privkey looks good" '
+	echo "Published to ${HASH_DEADKP_ID}: /ipfs/$HASH_DEADKP_DOC" >expected_privkey_publish &&
+	test_cmp expected_privkey_publish actual_privkey_publish
 '
+
+# Note that we can't resolve this because the DEADKP is a public keypair (by nature of it being used in tests),
+# so it would be perfectly possible for someone to perform a bit of mischief to make the tests fail.
+# And since the content's going to likely remain the same, it's a useless check, really.
 
 test_done
