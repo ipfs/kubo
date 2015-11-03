488ebd670aa6d52482825fdaec806d9317172582
David Dias <daviddias.p@gmail.com>
2015-11-03 16:19:17 +0000

    update version
    License: MIT
    Signed-off-by: David Dias <daviddias.p@gmail.com>

--- repo/config/version.go
+++ repo/config/version.go
@@ -8,7 +8,7 @@ import (
 )
 
 // CurrentVersionNumber is the current application's version literal
-const CurrentVersionNumber = "0.3.8-dev"
+const CurrentVersionNumber = "0.4.0-dev"
 
 // Version regulates checking if the most recent version is run
 type Version struct {
