d594d85ce440240de7c4886a2ae8b1b786cbc720
Etienne Laurin <etienne@atnnn.com>
2015-11-26 03:08:12 +0000

    use ServeContent for index.html
    One advantage is that it sets the Content-Type header correctly.
    
    License: MIT
    Signed-off-by: Etienne Laurin <etienne@atnnn.com>

--- core/corehttp/gateway_handler.go
+++ core/corehttp/gateway_handler.go
@@ -204,9 +204,7 @@ func (i *gatewayHandler) getOrHeadHandler(w http.ResponseWriter, r *http.Request
 			defer dr.Close()
 
 			// write to request
-			if r.Method != "HEAD" {
-				io.Copy(w, dr)
-			}
+			http.ServeContent(w, r, "index.html", modtime, dr)
 			break
 		}
 
