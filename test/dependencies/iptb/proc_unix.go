3f176e2cb26f2da2da731558e55529eba498d5ab
dignifiedquire <dignifiedquire@gmail.com>
2015-12-06 20:49:15 +0100

    Update iptb dep
    License: MIT
    Signed-off-by: dignifiedquire <dignifiedquire@gmail.com>

---
+++ test/dependencies/iptb/proc_unix.go
@@ -0,0 +1,13 @@
+// +build !windows
+package main
+
+import (
+	"os/exec"
+	"syscall"
+)
+
+func init() {
+	setupOpt = func(cmd *exec.Cmd) {
+		cmd.SysProcAttr = &syscall.SysProcAttr{Setsid: true}
+	}
+}
