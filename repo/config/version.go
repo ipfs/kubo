8e891d8b5022348e5e26b4e7786914c94151c59a
Jeromy <jeromyj@gmail.com>
2015-12-07 17:31:28 -0800

    ipfs 0.3.11-dev version bump
    License: MIT
    Signed-off-by: Jeromy <jeromyj@gmail.com>

--- repo/config/version.go
+++ repo/config/version.go
@@ -8,7 +8,7 @@ import (
 )
 
 // CurrentVersionNumber is the current application's version literal
-const CurrentVersionNumber = "0.3.10"
+const CurrentVersionNumber = "0.3.11-dev"
 
 // CurrentCommit is the current git commit, this is set as a ldflag in the Makefile
 var CurrentCommit string
