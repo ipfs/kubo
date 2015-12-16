5ed2322772c361cc8e5f902d43f8afa08a50751f
Jonathan Dahan <jonathan@jonathan.is>
2015-12-16 11:31:55 -0500

    Have install.sh use the full path to ipfs binary if detected
    License: MIT
    Signed-off-by: Jonathan Dahan <jonathan@jonathan.is>

--- misc/launchd/install.sh
+++ misc/launchd/install.sh
@@ -6,9 +6,13 @@ dest_dir="$HOME/Library/LaunchAgents"
 IPFS_PATH="${IPFS_PATH:-$HOME/.ipfs}"
 escaped_ipfs_path=$(echo $IPFS_PATH|sed 's/\//\\\//g')
 
+IPFS_BIN=$(which ipfs || echo ipfs)
+escaped_ipfs_bin=$(echo $IPFS_BIN|sed 's/\//\\\//g')
+
 mkdir -p "$dest_dir"
 
-sed 's/{{IPFS_PATH}}/'"$escaped_ipfs_path"'/g' \
+sed -e 's/{{IPFS_PATH}}/'"$escaped_ipfs_path"'/g' \
+  -e 's/{{IPFS_BIN}}/'"$escaped_ipfs_bin"'/g' \
   "$src_dir/$plist" \
   > "$dest_dir/$plist"
 
