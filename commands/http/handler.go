981b389a04fa0fbe69f47c3912adfad305261f7d
Jakub Sztandera <kubuxu@gmail.com>
2015-12-31 14:24:31 +0100

    Add Server field in HTTP API
    Resolves #625
    Included in tests.
    
    License: MIT
    Signed-off-by: Jakub (Kubuxu) Sztandera <kubuxu@gmail.com>

--- commands/http/handler.go
+++ commands/http/handler.go
@@ -199,6 +199,10 @@ func guessMimeType(res cmds.Response) (string, error) {
 }
 
 func sendResponse(w http.ResponseWriter, r *http.Request, res cmds.Response, req cmds.Request) {
+	h := w.Header()
+	// Expose our agent to allow identification
+	h.Set("Server", "go-ipfs/" + config.CurrentVersionNumber)
+	
 	mime, err := guessMimeType(res)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -221,8 +225,6 @@ func sendResponse(w http.ResponseWriter, r *http.Request, res cmds.Response, req
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
-	h := w.Header()
 
 	// Set up our potential trailer
 	h.Set("Trailer", StreamErrHeader)
