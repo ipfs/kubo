389545c7508b308856223751b31e4653c729a355
Jeromy <jeromyj@gmail.com>
2016-01-28 10:07:26 -0800

    go-ipfs-config: correct go-log dep
    License: MIT
    Signed-off-by: Jeromy <jeromyj@gmail.com>

--- config/config.go
+++ config/config.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 
 	u "github.com/ipfs/go-ipfs/util"
-	logging "gx/ipfs/QmaPaGNE2GqnfJjRRpQuQuFHuJn4FZvsrGxdik4kgxCkBi/go-log"
+	logging "gx/ipfs/Qmazh5oNUVsDZTs2g59rq8aYQqwpss8tcUWQzor5sCCEuH/go-log"
 )
 
 var log = logging.Logger("config")
