8cce0aa1f725eac905c0bff2e7369a82c40ebe6c
Richard Littauer <richard.littauer@gmail.com>
2016-03-01 15:17:02 -0500

    Add default to dns resolve
    See https://github.com/ipfs/http-api-spec/pull/41#discussion_r53164737
    
    License: MIT
    Signed-off-by: Richard Littauer <richard.littauer@gmail.com>

--- core/commands/dns.go
+++ core/commands/dns.go
@@ -50,7 +50,7 @@ The resolver will give:
 		cmds.StringArg("domain-name", true, false, "The domain-name name to resolve.").EnableStdin(),
 	},
 	Options: []cmds.Option{
-		cmds.BoolOption("recursive", "r", "Resolve until the result is not a DNS link."),
+		cmds.BoolOption("recursive", "r", "Resolve until the result is not a DNS link. Default: false."),
 	},
 	Run: func(req cmds.Request, res cmds.Response) {
 
