9e3b5c3dfe6b21dd5505f111a14227bde676b1e8
Jeromy <why@ipfs.io>
2016-04-25 12:08:20 -0700

    ipfs version 0.4.1
    License: MIT
    Signed-off-by: Jeromy <why@ipfs.io>

--- repo/config/version.go
+++ repo/config/version.go
@@ -4,6 +4,6 @@ package config
 var CurrentCommit string
 
 // CurrentVersionNumber is the current application's version literal
-const CurrentVersionNumber = "0.4.1-dev"
+const CurrentVersionNumber = "0.4.1"
 
 const ApiVersion = "/go-ipfs/" + CurrentVersionNumber + "/"
