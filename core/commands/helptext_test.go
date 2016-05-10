e7e22597ce2c9a17b850a938f1801895df05a6e4
Jeromy <why@ipfs.io>
2016-05-10 09:01:12 -0700

    add test to enforce helptext on commands
    License: MIT
    Signed-off-by: Jeromy <why@ipfs.io>

---
+++ core/commands/helptext_test.go
@@ -0,0 +1,33 @@
+package commands
+
+import (
+	cmds "github.com/ipfs/go-ipfs/commands"
+	"strings"
+	"testing"
+)
+
+func checkHelptextRecursive(t *testing.T, name []string, c *cmds.Command) {
+	if c.Helptext.Tagline == "" {
+		t.Errorf("%s has no tagline!", strings.Join(name, " "))
+	}
+
+	if c.Helptext.LongDescription == "" {
+		t.Errorf("%s has no long description!", strings.Join(name, " "))
+	}
+
+	if c.Helptext.ShortDescription == "" {
+		t.Errorf("%s has no short description!", strings.Join(name, " "))
+	}
+
+	if c.Helptext.Synopsis == "" {
+		t.Errorf("%s has no synopsis!", strings.Join(name, " "))
+	}
+
+	for subname, sub := range c.Subcommands {
+		checkHelptextRecursive(t, append(name, subname), sub)
+	}
+}
+
+func TestHelptexts(t *testing.T) {
+	checkHelptextRecursive(t, []string{"ipfs"}, Root)
+}
