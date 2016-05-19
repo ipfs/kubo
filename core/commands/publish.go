70342314ae9019d0b27b890440cfccaad59fda30
Jakub Sztandera <kubuxu@protonmail.ch>
2016-05-19 18:54:37 +0200

    Allow to specify where to put default options in option desciption
    License: MIT
    Signed-off-by: Jakub Sztandera <kubuxu@protonmail.ch>

--- core/commands/publish.go
+++ core/commands/publish.go
@@ -51,11 +51,10 @@ Publish an <ipfs-path> to another public key (not implemented):
 	},
 	Options: []cmds.Option{
 		cmds.BoolOption("resolve", "Resolve given path before publishing.").Default(true),
-		cmds.StringOption("lifetime", "t", `Time duration that the record will be valid for.
-
+		cmds.StringOption("lifetime", "t",
+			`Time duration that the record will be valid for. <default>
     This accepts durations such as "300s", "1.5h" or "2h45m". Valid time units are
-    "ns", "us" (or "µs"), "ms", "s", "m", "h".
-		`).Default("24h"),
+    "ns", "us" (or "µs"), "ms", "s", "m", "h".`).Default("24h"),
 		cmds.StringOption("ttl", "Time duration this record should be cached for (caution: experimental)."),
 	},
 	Run: func(req cmds.Request, res cmds.Response) {
