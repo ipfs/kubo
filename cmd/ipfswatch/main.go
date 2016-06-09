fb0aa6358774e469cf74ca93e1d560994699213a
Jakub Sztandera <kubuxu@protonmail.ch>
2016-06-09 11:11:29 +0200

    Move fsnotify to gx
    License: MIT
    Signed-off-by: Jakub Sztandera <kubuxu@protonmail.ch>

--- cmd/ipfswatch/main.go
+++ cmd/ipfswatch/main.go
@@ -8,7 +8,6 @@ import (
 	"path/filepath"
 
 	homedir "github.com/ipfs/go-ipfs/Godeps/_workspace/src/github.com/mitchellh/go-homedir"
-	fsnotify "github.com/ipfs/go-ipfs/Godeps/_workspace/src/gopkg.in/fsnotify.v1"
 	commands "github.com/ipfs/go-ipfs/commands"
 	core "github.com/ipfs/go-ipfs/core"
 	corehttp "github.com/ipfs/go-ipfs/core/corehttp"
@@ -17,6 +16,7 @@ import (
 	fsrepo "github.com/ipfs/go-ipfs/repo/fsrepo"
 	process "gx/ipfs/QmQopLATEYMNg7dVqZRNDfeE2S1yKy8zrRh5xnYiuqeZBn/goprocess"
 	context "gx/ipfs/QmZy2y8t9zQH2a1b8q2ZSLKp17ATuJoCNxxyMFG5qFExpt/go-net/context"
+	fsnotify "gx/ipfs/QmczzCMvJ3HV57WBKDy8b4ucp7quT325JjDbixYRS5Pwvv/fsnotify.v1"
 )
 
 var http = flag.Bool("http", false, "expose IPFS HTTP API")
