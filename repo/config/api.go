e976430c95e21240b175edd549c30a8c13460eb1
Kevin Atkinson <k@kevina.org>
2016-06-13 18:45:25 -0400

    Diff noise cleanup.
    License: MIT
    Signed-off-by: Kevin Atkinson <k@kevina.org>

--- repo/config/api.go
+++ repo/config/api.go
@@ -1,5 +1,5 @@
 package config
 
 type API struct {
-	HTTPHeaders    map[string][]string // HTTP headers to return with the API.
+	HTTPHeaders map[string][]string // HTTP headers to return with the API.
 }
