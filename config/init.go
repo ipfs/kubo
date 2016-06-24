c36294a933e693ec2297ee6558670033a01e9139
Jakub Sztandera <kubuxu@protonmail.ch>
2016-06-24 18:38:07 +0200

    go-ipfs-config: Update go-log in whole dependency tree (#2898)
    * Update golog in go-ipfs
    
    License: MIT
    Signed-off-by: Jakub Sztandera <kubuxu@protonmail.ch>
    
    * Update go-libp2p for go-log
    
    License: MIT
    Signed-off-by: Jakub Sztandera <kubuxu@protonmail.ch>
    
    * Update go-libp2p-secio for go-log
    
    License: MIT
    Signed-off-by: Jakub Sztandera <kubuxu@protonmail.ch>
    
    * Update go-libp2p-crypto for go-log
    
    License: MIT
    Signed-off-by: Jakub Sztandera <kubuxu@protonmail.ch>
    
    * Update go-libp2p-peer for go-log
    
    License: MIT
    Signed-off-by: Jakub Sztandera <kubuxu@protonmail.ch>
    
    * Import peersore, it wasn't imported
    
    License: MIT
    Signed-off-by: Jakub Sztandera <kubuxu@protonmail.ch>
    
    * Update peerstore
    
    License: MIT
    Signed-off-by: Jakub Sztandera <kubuxu@protonmail.ch>
    
    * Update peer
    
    License: MIT
    Signed-off-by: Jakub Sztandera <kubuxu@protonmail.ch>
    
    * Update secio
    
    License: MIT
    Signed-off-by: Jakub Sztandera <kubuxu@protonmail.ch>
    
    * Update go-libp2p
    
    License: MIT
    Signed-off-by: Jakub Sztandera <kubuxu@protonmail.ch>

--- config/init.go
+++ config/init.go
@@ -6,8 +6,8 @@ import (
 	"fmt"
 	"io"
 
-	peer "gx/ipfs/QmQGwpJy9P4yXZySmqkZEXCmbBpJUb8xntCv8Ca4taZwDC/go-libp2p-peer"
-	ci "gx/ipfs/QmUEUu1CM8bxBJxc3ZLojAi8evhTr4byQogWstABet79oY/go-libp2p-crypto"
+	peer "gx/ipfs/QmRBqJF7hb8ZSpRcMwUt8hNhydWcxGEhtk81HKq6oUwKvs/go-libp2p-peer"
+	ci "gx/ipfs/QmUWER4r4qMvaCnX5zREcfyiWN7cXN9g3a7fkRqNz8qWPP/go-libp2p-crypto"
 )
 
 func Init(out io.Writer, nBitsForKeypair int) (*Config, error) {
