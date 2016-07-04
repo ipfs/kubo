016d3d97ef5ef7963b320278fea19c593b4d6cf4
Jakub Sztandera <kubuxu@protonmail.ch>
2016-07-04 23:02:29 +0200

    blocks/blockstore: introduce context passing to blockstore
    License: MIT
    Signed-off-by: Jakub Sztandera <kubuxu@protonmail.ch>

--- blocks/blockstore/caching.go
+++ blocks/blockstore/caching.go
@@ -1,5 +1,11 @@
 package blockstore
 
+import (
+	"errors"
+
+	context "gx/ipfs/QmZy2y8t9zQH2a1b8q2ZSLKp17ATuJoCNxxyMFG5qFExpt/go-net/context"
+)
+
 // Next to each option is it aproximate memory usage per unit
 type CacheOpts struct {
 	HasBloomFilterSize   int // 1 bit
@@ -13,4 +19,24 @@ func DefaultCacheOpts() CacheOpts {
 		HasBloomFilterHashes: 7,
 		HasARCCacheSize:      64 * 1024,
 	}
+}
+
+func CachedBlockstore(bs GCBlockstore,
+	ctx context.Context, opts CacheOpts) (cbs GCBlockstore, err error) {
+	if ctx == nil {
+		ctx = context.TODO() // For tests
+	}
+
+	if opts.HasBloomFilterSize < 0 || opts.HasBloomFilterHashes < 0 ||
+		opts.HasARCCacheSize < 0 {
+		return nil, errors.New("all options for cache need to be greater than zero")
+	}
+
+	if opts.HasBloomFilterSize != 0 && opts.HasBloomFilterHashes == 0 {
+		return nil, errors.New("bloom filter hash count can't be 0 when there is size set")
+	}
+	cbs, err = bloomCached(bs, ctx, opts.HasBloomFilterSize, opts.HasBloomFilterHashes,
+		opts.HasARCCacheSize)
+
+	return cbs, err
 }
--- core/builder.go
+++ core/builder.go
@@ -131,7 +131,7 @@ func setupNode(ctx context.Context, n *IpfsNode, cfg *BuildCfg) error {
 
 	var err error
 	bs := bstore.NewBlockstore(n.Repo.Datastore())
-	n.Blockstore, err = bstore.BloomCached(bs, 256*1024, kSizeBlockstoreWriteCache)
+	n.Blockstore, err = bstore.CachedBlockstore(bs, ctx, bstore.DefaultCacheOpts())
 	if err != nil {
 		return err
 	}
