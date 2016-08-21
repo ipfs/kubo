8bcc42900581af1b560baae89c20e4ee1b5d2989
Kevin Atkinson <k@kevina.org>
2016-08-21 09:39:49 -0400

    Pinner: Providing Pinned.String() method and use it in "block rm"
    License: MIT
    Signed-off-by: Kevin Atkinson <k@kevina.org>

--- core/commands/block.go
+++ core/commands/block.go
@@ -305,19 +305,13 @@ func checkIfPinned(pins pin.Pinner, keys []key.Key, out chan<- interface{}) ([]k
 		return nil, err
 	}
 	for _, r := range res {
-		switch r.Mode {
-		case pin.NotPinned:
+		if !r.Pinned() {
 			stillOkay = append(stillOkay, r.Key)
-		case pin.Indirect:
+		} else {
 			out <- &RemovedBlock{
 				Hash:  r.Key.String(),
-				Error: fmt.Sprintf("pinned via %s", r.Via)}
-		default:
-			modeStr, _ := pin.PinModeToString(r.Mode)
-			out <- &RemovedBlock{
-				Hash:  r.Key.String(),
-				Error: fmt.Sprintf("pinned: %s", modeStr)}
-
+				Error: r.String(),
+			}
 		}
 	}
 	return stillOkay, nil
