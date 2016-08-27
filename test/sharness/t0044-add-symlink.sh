88e4b110badef2445f7336556a5cf6fe34f9a578
Kevin Atkinson <k@kevina.org>
2016-08-27 02:29:15 -0400

    Revert "Resolve symlink if it is directly referenced in cli (#2897)"
    This reverts commit fe7b01f14e2b8db6e856b567dbd7db4d00f6e366.
    
    Conflicts:
    	commands/cli/parse.go
    
    Revert "Merge pull request #3023 from ipfs/feature/eval-symlink-windows"
    
    This reverts commit 16c5a89dd44ea787f645e1fdfaf2e5b8ebb2945e, reversing
    changes made to 8c77ff81886da887bc33f1c775a4f8881d1281b5.
    
    Conflicts:
    	commands/cli/parse.go
    
    License: MIT
    Signed-off-by: Kevin Atkinson <k@kevina.org>

--- test/sharness/t0044-add-symlink.sh
+++ test/sharness/t0044-add-symlink.sh
@@ -11,13 +11,9 @@ test_description="Test add -w"
 test_expect_success "creating files succeeds" '
 	mkdir -p files/foo &&
 	mkdir -p files/bar &&
-	mkdir -p files/badin
 	echo "some text" > files/foo/baz &&
-	ln -s ../foo/baz files/bar/baz &&
-	ln -s files/does/not/exist files/badin/bad &&
-	mkdir -p files2/a/b/c &&
-	echo "some other text" > files2/a/b/c/foo &&
-	ln -s b files2/a/d
+	ln -s files/foo/baz files/bar/baz &&
+	ln -s files/does/not/exist files/bad
 '
 
 test_add_symlinks() {
@@ -27,33 +23,26 @@ test_add_symlinks() {
 	'
 
 	test_expect_success "output looks good" '
-		echo QmQRgZT6xVFKJLVVpJDu3WcPkw2iqQ1jqK1F9jmdeq9zAv > filehash_exp &&
+		echo QmWdiHKoeSW8G1u7ATCgpx4yMoUhYaJBQGkyPLkS9goYZ8 > filehash_exp &&
 		test_cmp filehash_exp filehash_out
 	'
 
-	test_expect_success "adding a symlink adds the file itself" '
+	test_expect_success "adding a symlink adds the link itself" '
 		ipfs add -q files/bar/baz > goodlink_out
 	'
 
 	test_expect_success "output looks good" '
-		echo QmcPNXE5zjkWkM24xQ7Bi3VAm8fRxiaNp88jFsij7kSQF1 > goodlink_exp &&
+		echo "QmdocmZeF7qwPT9Z8SiVhMSyKA2KKoA2J7jToW6z6WBmxR" > goodlink_exp &&
 		test_cmp goodlink_exp goodlink_out
 	'
 
 	test_expect_success "adding a broken symlink works" '
-		ipfs add -qr files/badin | head -1 > badlink_out
+		ipfs add -q files/bad > badlink_out
 	'
 
 	test_expect_success "output looks good" '
 		echo "QmWYN8SEXCgNT2PSjB6BnxAx6NJQtazWoBkTRH9GRfPFFQ" > badlink_exp &&
 		test_cmp badlink_exp badlink_out
-	'
-
-	test_expect_success "adding with symlink in middle of path is same as\
-adding with no symlink" '
-		ipfs add -rq files2/a/b/c > no_sym &&
-		ipfs add -rq files2/a/d/c > sym &&
-		test_cmp no_sym sym
 	'
 }
 
