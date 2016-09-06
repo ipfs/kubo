fc20fe870d8402e2505a7adbabb248232e38279e
Stephan Kulla <git.mail@kulla.me>
2016-09-06 15:46:35 +0200

    install.sh: Add error message at the end.
    License: MIT
    Signed-off-by: Stephan Kulla <git.mail@kulla.me>

--- cmd/ipfs/dist/install.sh
+++ cmd/ipfs/dist/install.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
 bin=ipfs
+binpaths="/usr/local/bin /usr/bin"
 
 # this script is currently brain dead.
 # it merely tries two locations.
 # in the future maybe use value of $PATH.
 
-for binpath in /usr/local/bin /usr/bin; do
+for binpath in $binpaths; do
   if [ -d "$binpath" ]; then
     mv "$bin" "$binpath/$bin"
     echo "installed $binpath/$bin"
     exit 0
   fi
 done
+
+echo "cannot install $bin in one of the directories $binpaths"
+exit 1
