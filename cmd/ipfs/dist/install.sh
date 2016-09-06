1f81933fb685e5a47a0e2c1c2a08f166af865151
Stephan Kulla <git.mail@kulla.me>
2016-09-06 15:41:27 +0200

    install.sh: Add for loop to shorten script.
    License: MIT
    Signed-off-by: Stephan Kulla <git.mail@kulla.me>

--- cmd/ipfs/dist/install.sh
+++ cmd/ipfs/dist/install.sh
@@ -6,16 +6,10 @@ bin=ipfs
 # it merely tries two locations.
 # in the future maybe use value of $PATH.
 
-binpath=/usr/local/bin
-if [ -d "$binpath" ]; then
-  mv "$bin" "$binpath/$bin"
-  echo "installed $binpath/$bin"
-  exit 0
-fi
-
-binpath=/usr/bin
-if [ -d "$binpath" ]; then
-  mv "$bin" "$binpath/$bin"
-  echo "installed $binpath/$bin"
-  exit 0
-fi
+for binpath in /usr/local/bin /usr/bin; do
+  if [ -d "$binpath" ]; then
+    mv "$bin" "$binpath/$bin"
+    echo "installed $binpath/$bin"
+    exit 0
+  fi
+done
