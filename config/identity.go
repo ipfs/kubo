3e6db54da249a0c99a20c14bc2f4ef2439697500
Jeromy <why@ipfs.io>
2016-10-05 15:49:08 -0700

    go-ipfs-config: update to libp2p 4.0.1 and propogate other changes
    License: MIT
    Signed-off-by: Jeromy <why@ipfs.io>

--- config/identity.go
+++ config/identity.go
@@ -2,7 +2,7 @@ package config
 
 import (
 	"encoding/base64"
-	ic "gx/ipfs/QmVoi5es8D5fNHZDqoW6DgDAEPEV5hQp8GBz161vZXiwpQ/go-libp2p-crypto"
+	ic "gx/ipfs/QmfWDLQjGjVe4fr5CoztYW2DYYjRysMJrFe1RCsXLPTf46/go-libp2p-crypto"
 )
 
 const IdentityTag = "Identity"
