a54ab23371a9475fb03bad10f7fcb400ce8fc60c
Lars Gierth <lgierth@users.noreply.github.com>
2017-01-25 02:51:41 +0100

    migrations: update dist.ipfs.io path
    License: MIT
    Signed-off-by: Lars Gierth <larsg@systemli.org>

--- repo/fsrepo/migrations/migrations.go
+++ repo/fsrepo/migrations/migrations.go
@@ -15,7 +15,7 @@ import (
 	"strings"
 )
 
-var DistPath = "https://ipfs.io/ipfs/QmUnvqDuRyfe7HJuiMMHv77AMUFnjGyAU28LFPeTYwGmFF"
+var DistPath = "https://ipfs.io/ipfs/QmUgY7wRGB7bEKREE5BNrSwjzrRZddkRV4bu64bP2qgv7f"
 
 func init() {
 	if dist := os.Getenv("IPFS_DIST_PATH"); dist != "" {
