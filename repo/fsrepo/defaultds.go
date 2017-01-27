e895ff647551271e9207f2cfddfa316df538c7e1
Jeromy <jeromyj@gmail.com>
2017-01-26 23:39:28 -0800

    update flatfs to v1.1.1, path -> filepath fixes
    License: MIT
    Signed-off-by: Jeromy <jeromyj@gmail.com>

--- repo/fsrepo/defaultds.go
+++ repo/fsrepo/defaultds.go
@@ -8,12 +8,12 @@ import (
 	config "github.com/ipfs/go-ipfs/repo/config"
 	"github.com/ipfs/go-ipfs/thirdparty/dir"
 
+	"gx/ipfs/QmQktQvV8bRSZK3RJz9LW7m7uGFWZjdigzJxgfSrteY1Bd/go-ds-flatfs"
 	ds "gx/ipfs/QmRWDav6mzWseLWeYfVd5fvUKiVe9xNH29YfMF438fG364/go-datastore"
 	mount "gx/ipfs/QmRWDav6mzWseLWeYfVd5fvUKiVe9xNH29YfMF438fG364/go-datastore/syncmount"
 	levelds "gx/ipfs/QmaHHmfEozrrotyhyN44omJouyuEtx6ahddqV6W5yRaUSQ/go-ds-leveldb"
 	ldbopts "gx/ipfs/QmbBhyDKsY4mbY6xsKt3qu9Y7FPvMJ6qbD8AMjYYvPRw1g/goleveldb/leveldb/opt"
 	measure "gx/ipfs/QmbUSMTQtK9GRrUbD4ngqJwSzHsquUc8nyDubRWp4vPybH/go-ds-measure"
-	"gx/ipfs/Qmcdc2Str4Dkc8yVCzzUFpnxmkS9AZXnpfo6jbhjjtXXkB/go-ds-flatfs"
 )
 
 const (
