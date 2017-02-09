dc128cb470a327d911c52e070a81c1863ad2153f
Jakub Sztandera <kubuxu@protonmail.ch>
2017-02-09 17:45:12 +0100

    Make gx less verbose
    License: MIT
    Signed-off-by: Jakub Sztandera <kubuxu@protonmail.ch>

--- mk/gx.mk
+++ mk/gx.mk
@@ -1,7 +1,7 @@
 gx-path = gx/ipfs/$(shell gx deps find $(1))/$(1)
 
-gx-deps: 
-	gx --verbose install --global > /dev/null 2>&1
+gx-deps:
+	gx install --global
 .PHONY: gx-deps
 
 ifneq ($(IPFS_GX_USE_GLOBAL),1)
