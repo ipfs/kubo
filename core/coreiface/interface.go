1c53ef3f984b664538ada9d9adf27cdc4ea3c2b6
Lars Gierth <larsg@systemli.org>
2017-03-17 03:47:59 +0100

    coreapi: make the interfaces path centric
    The new coreiface.Path maps a path to the cid.Cid
    resulting from a full path resolution.
    
    The path is internally represented as a go-ipfs/path.Path,
    but that doesn't matter to the outside.
    
    Apart from the path-to-CID mapping, it also aims to hold all
    resolved segment CIDs of the path. Right now it only exposes
    Root(), and only for flat paths a la /ipfs/Qmfoo. In other cases,
    the root is nil.
    
    In the future, resolution will internally use
    go-ipfs/path.Resolver.ResolvePathComponents and thus always return
    the proper resolved segments, via Root(), or a future Segments() func.
    
    - Add coreiface.Path with Cid() and Root().
    - Add CoreAPI.ResolvePath() for getting a coreiface.Path.
    - All functions now expect and return coreiface.Path.
    - Add ParsePath() and ParseCid() for constructing a coreiface.Path.
    - Add coreiface.Node and Link which are simply go-ipld-node.Node and Link.
    - Add CoreAPI.ResolveNode() for getting a Node from a Path.
    
    License: MIT
    Signed-off-by: Lars Gierth <larsg@systemli.org>
    
    
    This commit was moved from ipfs/interface-go-ipfs-core@66af039105d5e4ebc82a178143d8643ad3fed91d
    
    This commit was moved from ipfs/boxo@61e07c830c0222fbd796118e7784e24ecf1c7e24

--- core/coreiface/interface.go
+++ core/coreiface/interface.go
@@ -9,6 +9,16 @@ import (
 	ipld "gx/ipfs/QmYDscK7dmdo2GZ9aumS8s5auUUAH5mR1jvj5pYhWusfK7/go-ipld-node"
 )
 
+type Path interface {
+	String() string
+	Cid() *cid.Cid
+	Root() *cid.Cid
+	Resolved() bool
+}
+
+// TODO: should we really copy these?
+//       if we didn't, godoc would generate nice links straight to go-ipld-node
+type Node ipld.Node
 type Link ipld.Link
 
 type Reader interface {
@@ -18,12 +28,14 @@ type Reader interface {
 
 type CoreAPI interface {
 	Unixfs() UnixfsAPI
+	ResolvePath(context.Context, Path) (Path, error)
+	ResolveNode(context.Context, Path) (Node, error)
 }
 
 type UnixfsAPI interface {
-	Add(context.Context, io.Reader) (*cid.Cid, error)
-	Cat(context.Context, string) (Reader, error)
-	Ls(context.Context, string) ([]*Link, error)
+	Add(context.Context, io.Reader) (Path, error)
+	Cat(context.Context, Path) (Reader, error)
+	Ls(context.Context, Path) ([]*Link, error)
 }
 
 // type ObjectAPI interface {
@@ -49,5 +61,4 @@ type UnixfsAPI interface {
 // }
 
 var ErrIsDir = errors.New("object is a directory")
-var ErrIsNonDag = errors.New("not a merkledag object")
 var ErrOffline = errors.New("can't resolve, ipfs node is offline")
