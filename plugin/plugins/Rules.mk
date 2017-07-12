04b26fe08983428ae0afa3900275e93d6aa43eb8
Jakub Sztandera <kubuxu@protonmail.ch>
2017-07-12 22:47:01 +0200

    make: fix buildmode=plugin and pkgdir problems
    License: MIT
    Signed-off-by: Jakub Sztandera <kubuxu@protonmail.ch>

--- plugin/plugins/Rules.mk
+++ plugin/plugins/Rules.mk
@@ -4,7 +4,7 @@ $(d)_plugins:=$(d)/git
 $(d)_plugins_so:=$(addsuffix .so,$($(d)_plugins))
 
 $($(d)_plugins_so): $$(DEPS_GO) ALWAYS
-	go build -buildmode=plugin -i $(go-flags-with-tags) -o "$@" "$(call go-pkg-name,$(basename $@))"
+	go build -buildmode=plugin -i -pkgdir "$$GOPATH/pkg/linux_amd64_dynlink" $(go-flags-with-tags) -o "$@" "$(call go-pkg-name,$(basename $@))"
 
 CLEAN += $($(d)_plugins_so)
 
