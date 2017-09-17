0c92a778b1b7bd3e608bd820673cc89fbacdad88
Łukasz Magiera <magik6k@gmail.com>
2017-09-17 23:07:43 +0200

    plugin: support system-wide plugins
    License: MIT
    Signed-off-by: Łukasz Magiera <magik6k@gmail.com>

--- cmd/ipfs/Rules.mk
+++ cmd/ipfs/Rules.mk
@@ -13,7 +13,10 @@ PATH := $(realpath $(d)):$(PATH)
 # DEPS_OO_$(d) += merkledag/pb/merkledag.pb.go namesys/pb/namesys.pb.go
 # DEPS_OO_$(d) += pin/internal/pb/header.pb.go unixfs/pb/unixfs.pb.go
 
-$(d)_flags =-ldflags="-X "github.com/ipfs/go-ipfs/repo/config".CurrentCommit=$(shell git rev-parse --short HEAD)" 
+CONFIG_COMMIT ?= github.com/ipfs/go-ipfs/repo/config.CurrentCommit=$(shell git rev-parse --short HEAD)
+CONFIG_PLUGIN_PATH ?= github.com/ipfs/go-ipfs/repo/config.SystemPluginPath=${PLUGIN_PATH}
+
+$(d)_flags =-ldflags="-X ${CONFIG_COMMIT} -X ${CONFIG_PLUGIN_PATH}"
 
 $(d)-try-build $(IPFS_BIN_$(d)): GOFLAGS += $(cmd/ipfs_flags)
 
--- repo/config/version.go
+++ repo/config/version.go
@@ -3,6 +3,9 @@ package config
 // CurrentCommit is the current git commit, this is set as a ldflag in the Makefile
 var CurrentCommit string
 
+// SystemPluginPath is the a system-global plugin path, this is set as a ldflag in the Makefile
+var SystemPluginPath string
+
 // CurrentVersionNumber is the current application's version literal
 const CurrentVersionNumber = "0.4.14-dev"
 
