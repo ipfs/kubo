demoso/go-ipfs

coreapi: functional options for DagAPI
License: MIT
Signed-off-by: Łukasz Magiera <magik6k@gmail.com>

core/coreapi/dag_test.go
@@ -7,6 +7,8 @@ import (
 	"testing"
 
 	coreapi "github.com/ipfs/go-ipfs/core/coreapi"
+
+	mh "gx/ipfs/QmU9a9NV9RdPNwZQDYd5uKsm6N6LJLSvLbywDDYFbaaC6P/go-multihash"
 )
 
 var (
@@ -26,12 +28,29 @@ func TestPut(t *testing.T) {
 		t.Error(err)
 	}
 
-	res, err := api.Dag().Put(ctx, strings.NewReader(`"Hello"`), "json", nil)
+	res, err := api.Dag().Put(ctx, strings.NewReader(`"Hello"`))
 	if err != nil {
 		t.Error(err)
 	}
 
 	if res[0].Cid().String() != "zdpuAqckYF3ToF3gcJNxPZXmnmGuXd3gxHCXhq81HGxBejEvv" {
+		t.Errorf("got wrong cid: %s", res[0].Cid().String())
+	}
+}
+
+func TestPutWithHash(t *testing.T) {
+	ctx := context.Background()
+	_, api, err := makeAPI(ctx)
+	if err != nil {
+		t.Error(err)
+	}
+
+	res, err := api.Dag().Put(ctx, strings.NewReader(`"Hello"`), api.Dag().WithHash(mh.ID, -1))
+	if err != nil {
+		t.Error(err)
+	}
+
+	if res[0].Cid().String() != "zdpVa1FHqbK7AV6dCZoipskbXsY49RbK3HUCRkmyKCG1BZo3u" {
 		t.Errorf("got wrong cid: %s", res[0].Cid().String())
 	}
 }
@@ -43,12 +62,12 @@ func TestPath(t *testing.T) {
 		t.Error(err)
 	}
 
-	sub, err := api.Dag().Put(ctx, strings.NewReader(`"foo"`), "json", nil)
+	sub, err := api.Dag().Put(ctx, strings.NewReader(`"foo"`))
 	if err != nil {
 		t.Error(err)
 	}
 
-	res, err := api.Dag().Put(ctx, strings.NewReader(`{"lnk": {"/": "`+sub[0].Cid().String()+`"}}`), "json", nil)
+	res, err := api.Dag().Put(ctx, strings.NewReader(`{"lnk": {"/": "`+sub[0].Cid().String()+`"}}`))
 	if err != nil {
 		t.Error(err)
 	}
@@ -75,7 +94,7 @@ func TestTree(t *testing.T) {
 		t.Error(err)
 	}
 
-	res, err := api.Dag().Put(ctx, strings.NewReader(`{"a": 123, "b": "foo", "c": {"d": 321, "e": 111}}`), "json", nil)
+	res, err := api.Dag().Put(ctx, strings.NewReader(`{"a": 123, "b": "foo", "c": {"d": 321, "e": 111}}`))
 	if err != nil {
 		t.Error(err)
 	}

core/coreapi/interface/options/dag.go
@@ -1,0 +1,83 @@
+package options
+
+import (
+	"math"
+
+	cid "gx/ipfs/QmNp85zy9RLrQ5oQD4hPyS39ezrrXpcaa7R4Y9kxdWQLLQ/go-cid"
+)
+
+type DagPutSettings struct {
+	InputEnc string
+	Codec    uint64
+	MhType   uint64
+	MhLength int
+}
+
+type DagTreeSettings struct {
+	Depth int
+}
+
+type DagPutOption func(*DagPutSettings) error
+type DagTreeOption func(*DagTreeSettings) error
+
+func DagPutOptions(opts ...DagPutOption) (*DagPutSettings, error) {
+	options := &DagPutSettings{
+		InputEnc: "json",
+		Codec:    cid.DagCBOR,
+		MhType:   math.MaxUint64,
+		MhLength: -1,
+	}
+
+	for _, opt := range opts {
+		err := opt(options)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return options, nil
+}
+
+func DagTreeOptions(opts ...DagTreeOption) (*DagTreeSettings, error) {
+	options := &DagTreeSettings{
+		Depth: -1,
+	}
+
+	for _, opt := range opts {
+		err := opt(options)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return options, nil
+}
+
+type DagOptions struct{}
+
+func (api *DagOptions) WithInputEnc(enc string) DagPutOption {
+	return func(settings *DagPutSettings) error {
+		settings.InputEnc = enc
+		return nil
+	}
+}
+
+func (api *DagOptions) WithCodec(codec uint64) DagPutOption {
+	return func(settings *DagPutSettings) error {
+		settings.Codec = codec
+		return nil
+	}
+}
+
+func (api *DagOptions) WithHash(mhType uint64, mhLen int) DagPutOption {
+	return func(settings *DagPutSettings) error {
+		settings.MhType = mhType
+		settings.MhLength = mhLen
+		return nil
+	}
+}
+
+func (api *DagOptions) WithDepth(depth int) DagTreeOption {
+	return func(settings *DagTreeSettings) error {
+		settings.Depth = depth
+		return nil
+	}
+}

ipfs/kubo

core/coreapi/dag_test.go
@@ -7,6 +7,8 @@ import (
 	"testing"
 
 	coreapi "github.com/ipfs/go-ipfs/core/coreapi"
+
+	mh "gx/ipfs/QmYeKnKpubCMRiq3PGZcTREErthbb5Q9cXsCoSkD9bjEBd/go-multihash"
 )
 
 var (
@@ -26,12 +28,29 @@ func TestPut(t *testing.T) {
 		t.Error(err)
 	}
 
-	res, err := api.Dag().Put(ctx, strings.NewReader(`"Hello"`), "json", nil)
+	res, err := api.Dag().Put(ctx, strings.NewReader(`"Hello"`))
 	if err != nil {
 		t.Error(err)
 	}
 
 	if res[0].Cid().String() != "zdpuAqckYF3ToF3gcJNxPZXmnmGuXd3gxHCXhq81HGxBejEvv" {
+		t.Errorf("got wrong cid: %s", res[0].Cid().String())
+	}
+}
+
+func TestPutWithHash(t *testing.T) {
+	ctx := context.Background()
+	_, api, err := makeAPI(ctx)
+	if err != nil {
+		t.Error(err)
+	}
+
+	res, err := api.Dag().Put(ctx, strings.NewReader(`"Hello"`), api.Dag().WithHash(mh.ID, -1))
+	if err != nil {
+		t.Error(err)
+	}
+
+	if res[0].Cid().String() != "z5hRLNd2sv4z1c" {
 		t.Errorf("got wrong cid: %s", res[0].Cid().String())
 	}
 }
@@ -43,12 +62,12 @@ func TestPath(t *testing.T) {
 		t.Error(err)
 	}
 
-	sub, err := api.Dag().Put(ctx, strings.NewReader(`"foo"`), "json", nil)
+	sub, err := api.Dag().Put(ctx, strings.NewReader(`"foo"`))
 	if err != nil {
 		t.Error(err)
 	}
 
-	res, err := api.Dag().Put(ctx, strings.NewReader(`{"lnk": {"/": "`+sub[0].Cid().String()+`"}}`), "json", nil)
+	res, err := api.Dag().Put(ctx, strings.NewReader(`{"lnk": {"/": "`+sub[0].Cid().String()+`"}}`))
 	if err != nil {
 		t.Error(err)
 	}
@@ -75,7 +94,7 @@ func TestTree(t *testing.T) {
 		t.Error(err)
 	}
 
-	res, err := api.Dag().Put(ctx, strings.NewReader(`{"a": 123, "b": "foo", "c": {"d": 321, "e": 111}}`), "json", nil)
+	res, err := api.Dag().Put(ctx, strings.NewReader(`{"a": 123, "b": "foo", "c": {"d": 321, "e": 111}}`))
 	if err != nil {
 		t.Error(err)
 	}

core/coreapi/interface/options/dag.go
@@ -1,0 +1,83 @@
+package options
+
+import (
+	"math"
+
+	cid "gx/ipfs/QmeSrf6pzut73u6zLQkRFQ3ygt3k6XFT2kjdYP8Tnkwwyg/go-cid"
+)
+
+type DagPutSettings struct {
+	InputEnc string
+	Codec    uint64
+	MhType   uint64
+	MhLength int
+}
+
+type DagTreeSettings struct {
+	Depth int
+}
+
+type DagPutOption func(*DagPutSettings) error
+type DagTreeOption func(*DagTreeSettings) error
+
+func DagPutOptions(opts ...DagPutOption) (*DagPutSettings, error) {
+	options := &DagPutSettings{
+		InputEnc: "json",
+		Codec:    cid.DagCBOR,
+		MhType:   math.MaxUint64,
+		MhLength: -1,
+	}
+
+	for _, opt := range opts {
+		err := opt(options)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return options, nil
+}
+
+func DagTreeOptions(opts ...DagTreeOption) (*DagTreeSettings, error) {
+	options := &DagTreeSettings{
+		Depth: -1,
+	}
+
+	for _, opt := range opts {
+		err := opt(options)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return options, nil
+}
+
+type DagOptions struct{}
+
+func (api *DagOptions) WithInputEnc(enc string) DagPutOption {
+	return func(settings *DagPutSettings) error {
+		settings.InputEnc = enc
+		return nil
+	}
+}
+
+func (api *DagOptions) WithCodec(codec uint64) DagPutOption {
+	return func(settings *DagPutSettings) error {
+		settings.Codec = codec
+		return nil
+	}
+}
+
+func (api *DagOptions) WithHash(mhType uint64, mhLen int) DagPutOption {
+	return func(settings *DagPutSettings) error {
+		settings.MhType = mhType
+		settings.MhLength = mhLen
+		return nil
+	}
+}
+
+func (api *DagOptions) WithDepth(depth int) DagTreeOption {
+	return func(settings *DagTreeSettings) error {
+		settings.Depth = depth
+		return nil
+	}
+}