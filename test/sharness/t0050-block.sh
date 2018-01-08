8c176d26ebdad5d0a225f83a467c7efcd2285295
Łukasz Magiera <magik6k@gmail.com>
2018-01-08 14:22:53 +0100

    commands/block: use CIDv1 with custom mhtype
    License: MIT
    Signed-off-by: Łukasz Magiera <magik6k@gmail.com>

--- test/sharness/t0050-block.sh
+++ test/sharness/t0050-block.sh
@@ -209,4 +209,16 @@ test_expect_success "no panic in output" '
   test_expect_code 1 grep "panic" stat_out
 '
 
+test_expect_success "can set multihash type and length on block put without format" '
+  HASH=$(echo "foooo" | ipfs block put --mhtype=sha3 --mhlen=16)
+'
+
+test_expect_success "output looks good" '
+  test "z2APJNN6rqZTWPpv7gYFHzh7ZEDX" = "$HASH"
+'
+
+test_expect_success "put with sha3 and cidv0 fails" '
+  echo "foooo" | test_must_fail ipfs block put --mhtype=sha3 --mhlen=16 --format=v0
+'
+
 test_done
