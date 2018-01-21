e0e856aa9371cd2104e8c05429be21f187b33f00
Jeromy <jeromyj@gmail.com>
2018-01-21 11:46:03 -0800

    data storage interfaces
    License: MIT
    Signed-off-by: Jeromy <jeromyj@gmail.com>

--- repo/repo.go
+++ repo/repo.go
@@ -16,24 +16,41 @@ var (
 	ErrApiNotRunning = errors.New("api not running")
 )
 
+// Repo represents all persistent data of a given ipfs node.
 type Repo interface {
+	// Config returns the ipfs configuration file from the repo. Changes made
+	// to the returned config are not automatically persisted.
 	Config() (*config.Config, error)
+
+	// BackupConfig creates a backup of the current configuration file using
+	// the given prefix for naming.
 	BackupConfig(prefix string) (string, error)
+
+	// SetConfig persists the given configuration struct to storage.
 	SetConfig(*config.Config) error
 
+	// SetConfigKey sets the given key-value pair within the config and persists it to storage.
 	SetConfigKey(key string, value interface{}) error
+
+	// GetConfigKey reads the value for the given key from the configuration in storage.
 	GetConfigKey(key string) (interface{}, error)
 
+	// Datastore returns a reference to the configured data storage backend.
 	Datastore() Datastore
+
+	// GetStorageUsage returns the number of bytes stored.
 	GetStorageUsage() (uint64, error)
 
+	// Keystore returns a reference to the key management interface.
 	Keystore() keystore.Keystore
 
+	// FileManager returns a reference to the filestore file manager.
 	FileManager() *filestore.FileManager
 
 	// SetAPIAddr sets the API address in the repo.
 	SetAPIAddr(addr ma.Multiaddr) error
 
+	// SwarmKey returns the configured shared symmetric key for the private networks feature.
 	SwarmKey() ([]byte, error)
 
 	io.Closer
