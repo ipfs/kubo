161503c26f9fdc99045fc10d731bb8036769e390
Łukasz Magiera <magik6k@gmail.com>
2018-02-08 17:39:05 +0100

    coreapi: separate path into two types
    License: MIT
    Signed-off-by: Łukasz Magiera <magik6k@gmail.com>
    
    
    This commit was moved from ipfs/interface-go-ipfs-core@79875b01300b77c8437ec0ca1b22d497edb79cd9
    
    This commit was moved from ipfs/boxo@5a61226d830993d09c7d76d970b1c5a9de94f834

--- core/coreiface/dag.go
+++ core/coreiface/dag.go
@@ -14,7 +14,7 @@ type DagAPI interface {
 	// Put inserts data using specified format and input encoding.
 	// Unless used with WithCodec or WithHash, the defaults "dag-cbor" and
 	// "sha256" are used.
-	Put(ctx context.Context, src io.Reader, opts ...options.DagPutOption) (Path, error)
+	Put(ctx context.Context, src io.Reader, opts ...options.DagPutOption) (ResolvedPath, error)
 
 	// Get attempts to resolve and get the node specified by the path
 	Get(ctx context.Context, path Path) (ipld.Node, error)
