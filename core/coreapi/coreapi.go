2d2a9791f1ffccf49a2f1d557259b5ea356381ac
Łukasz Magiera <magik6k@gmail.com>
2018-03-05 14:57:46 +0100

    coreapi: minor doc fixes
    License: MIT
    Signed-off-by: Łukasz Magiera <magik6k@gmail.com>

--- core/coreapi/coreapi.go
+++ core/coreapi/coreapi.go
@@ -24,35 +24,37 @@ func NewCoreAPI(n *core.IpfsNode) coreiface.CoreAPI {
 	return api
 }
 
-// Unixfs returns the UnixfsAPI interface backed by the go-ipfs node
+// Unixfs returns the UnixfsAPI interface implementation backed by the go-ipfs node
 func (api *CoreAPI) Unixfs() coreiface.UnixfsAPI {
 	return (*UnixfsAPI)(api)
 }
 
+// Block returns the BlockAPI interface implementation backed by the go-ipfs node
 func (api *CoreAPI) Block() coreiface.BlockAPI {
 	return &BlockAPI{api, nil}
 }
 
-// Dag returns the DagAPI interface backed by the go-ipfs node
+// Dag returns the DagAPI interface implementation backed by the go-ipfs node
 func (api *CoreAPI) Dag() coreiface.DagAPI {
 	return &DagAPI{api, nil}
 }
 
-// Name returns the NameAPI interface backed by the go-ipfs node
+// Name returns the NameAPI interface implementation backed by the go-ipfs node
 func (api *CoreAPI) Name() coreiface.NameAPI {
 	return &NameAPI{api, nil}
 }
 
-// Key returns the KeyAPI interface backed by the go-ipfs node
+// Key returns the KeyAPI interface implementation backed by the go-ipfs node
 func (api *CoreAPI) Key() coreiface.KeyAPI {
 	return &KeyAPI{api, nil}
 }
 
-//Object returns the ObjectAPI interface backed by the go-ipfs node
+// Object returns the ObjectAPI interface implementation backed by the go-ipfs node
 func (api *CoreAPI) Object() coreiface.ObjectAPI {
 	return &ObjectAPI{api, nil}
 }
 
+// Pin returns the PinAPI interface implementation backed by the go-ipfs node
 func (api *CoreAPI) Pin() coreiface.PinAPI {
 	return &PinAPI{api, nil}
 }
