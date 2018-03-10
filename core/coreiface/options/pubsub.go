f9a93470069c116015b79f33f105275896f9917a
Łukasz Magiera <magik6k@gmail.com>
2018-03-10 19:23:38 +0100

    coreapi: pubsub interface
    License: MIT
    Signed-off-by: Łukasz Magiera <magik6k@gmail.com>
    
    
    This commit was moved from ipfs/interface-go-ipfs-core@ccaec46f3deaf46e20f9c265d5920f68251e4da4
    
    This commit was moved from ipfs/boxo@f06c01e06b04ec7a27b0738f984e0f30b005c711

---
+++ core/coreiface/options/pubsub.go
@@ -0,0 +1,56 @@
+package options
+
+type PubSubPeersSettings struct {
+	Topic string
+}
+
+type PubSubSubscribeSettings struct {
+	Discover bool
+}
+
+type PubSubPeersOption func(*PubSubPeersSettings) error
+type PubSubSubscribeOption func(*PubSubSubscribeSettings) error
+
+func PubSubPeersOptions(opts ...PubSubPeersOption) (*PubSubPeersSettings, error) {
+	options := &PubSubPeersSettings{
+		Topic: "",
+	}
+
+	for _, opt := range opts {
+		err := opt(options)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return options, nil
+}
+
+func PubSubSubscribeOptions(opts ...PubSubSubscribeOption) (*PubSubSubscribeSettings, error) {
+	options := &PubSubSubscribeSettings{
+		Discover: false,
+	}
+
+	for _, opt := range opts {
+		err := opt(options)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return options, nil
+}
+
+type PubSubOptions struct{}
+
+func (api *PubSubOptions) WithTopic(topic string) PubSubPeersOption {
+	return func(settings *PubSubPeersSettings) error {
+		settings.Topic = topic
+		return nil
+	}
+}
+
+func (api *PubSubOptions) WithDiscover(discover bool) PubSubSubscribeOption {
+	return func(settings *PubSubSubscribeSettings) error {
+		settings.Discover = discover
+		return nil
+	}
+}
