f3162d6ec66cecaf16eb7cd2f7db03035741db37
Łukasz Magiera <magik6k@gmail.com>
2018-03-10 22:17:27 +0100

    coreapi: var block for errors
    License: MIT
    Signed-off-by: Łukasz Magiera <magik6k@gmail.com>
    
    
    This commit was moved from ipfs/interface-go-ipfs-core@60fc569676d93341f657737a221de2228a178183
    
    This commit was moved from ipfs/boxo@5de1a8368e0d7b6abe9226f7e789ab8a521fb886

--- core/coreiface/errors.go
+++ core/coreiface/errors.go
@@ -2,5 +2,7 @@ package iface
 
 import "errors"
 
-var ErrIsDir = errors.New("object is a directory")
-var ErrOffline = errors.New("can't resolve, ipfs node is offline")
+var (
+	ErrIsDir   = errors.New("object is a directory")
+	ErrOffline = errors.New("can't resolve, ipfs node is offline")
+)
