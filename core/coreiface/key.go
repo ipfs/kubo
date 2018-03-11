5e488491670bdb45a3fa957b624f44a068cb40a5
Łukasz Magiera <magik6k@gmail.com>
2018-03-11 18:55:35 +0100

    coreapi: remove options from interfaces
    License: MIT
    Signed-off-by: Łukasz Magiera <magik6k@gmail.com>
    
    
    This commit was moved from ipfs/interface-go-ipfs-core@38ccf0555876033759418de2c43b5d9c727b2c19
    
    This commit was moved from ipfs/boxo@595afb8260f48d77dfff80a7d4a191f185adfb87

--- core/coreiface/key.go
+++ core/coreiface/key.go
@@ -20,28 +20,9 @@ type KeyAPI interface {
 	// name and returns a base58 encoded multihash of it's public key
 	Generate(ctx context.Context, name string, opts ...options.KeyGenerateOption) (Key, error)
 
-	// WithType is an option for Generate which specifies which algorithm
-	// should be used for the key. Default is options.RSAKey
-	//
-	// Supported key types:
-	// * options.RSAKey
-	// * options.Ed25519Key
-	WithType(algorithm string) options.KeyGenerateOption
-
-	// WithSize is an option for Generate which specifies the size of the key to
-	// generated. Default is -1
-	//
-	// value of -1 means 'use default size for key type':
-	//  * 2048 for RSA
-	WithSize(size int) options.KeyGenerateOption
-
 	// Rename renames oldName key to newName. Returns the key and whether another
 	// key was overwritten, or an error
 	Rename(ctx context.Context, oldName string, newName string, opts ...options.KeyRenameOption) (Key, bool, error)
-
-	// WithForce is an option for Rename which specifies whether to allow to
-	// replace existing keys.
-	WithForce(force bool) options.KeyRenameOption
 
 	// List lists keys stored in keystore
 	List(ctx context.Context) ([]Key, error)
