06f1d1a19a8863b41d9e9d7095ba00333be49598
Łukasz Magiera <magik6k@gmail.com>
2018-04-20 13:56:33 +0200

    coreapi: add more docs for path
    License: MIT
    Signed-off-by: Łukasz Magiera <magik6k@gmail.com>
    
    
    This commit was moved from ipfs/interface-go-ipfs-core@5abaad9e8573557e0bd7f3b1b45838053a85ffd7
    
    This commit was moved from ipfs/boxo@d1d67409e744a2a697a3c05de0eb34b0bfbed175

--- core/coreiface/path.go
+++ core/coreiface/path.go
@@ -17,7 +17,9 @@ type Path interface {
 	// String returns the path as a string.
 	String() string
 
-	// Namespace returns the first component of the path
+	// Namespace returns the first component of the path.
+	//
+	// For example path "/ipfs/QmHash", calling Namespace() will return "ipfs"
 	Namespace() string
 
 	// Mutable returns false if the data pointed to by this path in guaranteed
@@ -29,13 +31,29 @@ type Path interface {
 
 // ResolvedPath is a resolved Path
 type ResolvedPath interface {
-	// Cid returns the CID referred to by path
+	// Cid returns the CID of the object referenced by the path.
+	//
+	// Example:
+	// If you have 3 linked objects: QmRoot -> A -> B, and resolve path
+	// "/ipfs/QmRoot/A/B", the Cid method will return the CID of object B
 	Cid() *cid.Cid
 
-	// Root returns the CID of root path
+	// Root returns the CID of the root object of the path
+	//
+	// Example:
+	// If you have 3 linked objects: QmRoot -> A -> B, and resolve path
+	// "/ipfs/QmRoot/A/B", the Root method will return the CID of object QmRoot
 	Root() *cid.Cid
 
 	// Remainder returns unresolved part of the path
+	//
+	// Example:
+	// If you have 2 linked objects: QmRoot -> A, where A is a CBOR node
+	// containing the following data:
+	//
+	// {"foo": {"bar": 42}}
+	//
+	// When resolving "/ipld/QmRoot/A/foo/bar", Remainder will return "foo/bar"
 	Remainder() string
 
 	Path
