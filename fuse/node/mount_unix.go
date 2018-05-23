56eb2b00d5d24f45de2e12db8c4327fd550cc68a
Dominic Della Valle <ddvpublic@gmail.com>
2018-05-23 10:49:58 -0400

    remove nofuse build constraints
    License: MIT
    Signed-off-by: Dominic Della Valle <ddvpublic@gmail.com>

--- fuse/node/mount_unix.go
+++ fuse/node/mount_unix.go
@@ -1,5 +1,4 @@
-// +build linux darwin freebsd netbsd openbsd
-// +build !nofuse
+// +build !windows,!nofuse
 
 package node
 
@@ -9,11 +8,12 @@ import (
 	"strings"
 	"sync"
 
+	logging "gx/ipfs/QmRb5jh8z2E8hMGN2tkvs1yHynUanqnZ3UeKwgN1i9P1F8/go-log"
+
 	core "github.com/ipfs/go-ipfs/core"
 	ipns "github.com/ipfs/go-ipfs/fuse/ipns"
 	mount "github.com/ipfs/go-ipfs/fuse/mount"
 	rofs "github.com/ipfs/go-ipfs/fuse/readonly"
-	logging "gx/ipfs/QmRb5jh8z2E8hMGN2tkvs1yHynUanqnZ3UeKwgN1i9P1F8/go-log"
 )
 
 var log = logging.Logger("node")
