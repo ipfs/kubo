c2c49c88c6f1184be563917be5d2f3a008fa0c31
Steven Allen <steven@stebalien.com>
2018-05-31 20:03:22 -0700

    bump repo version and update migration dist path
    License: MIT
    Signed-off-by: Steven Allen <steven@stebalien.com>

--- repo/fsrepo/fsrepo.go
+++ repo/fsrepo/fsrepo.go
@@ -36,7 +36,7 @@ const LockFile = "repo.lock"
 var log = logging.Logger("fsrepo")
 
 // version number that we are currently expecting to see
-var RepoVersion = 6
+var RepoVersion = 7
 
 var migrationInstructions = `See https://github.com/ipfs/fs-repo-migrations/blob/master/run.md
 Sorry for the inconvenience. In the future, these will run automatically.`
