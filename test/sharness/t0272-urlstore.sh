1358b28204d2c8f077d78d61fdd32c9abf9294e4
Steven Allen <steven@stebalien.com>
2018-07-16 15:03:10 -0700

    add sharness for trickle importer
    License: MIT
    Signed-off-by: Steven Allen <steven@stebalien.com>

--- test/sharness/t0272-urlstore.sh
+++ test/sharness/t0272-urlstore.sh
@@ -124,6 +124,12 @@ test_expect_success "get large file via urlstore" '
   test_cmp file3 file3.actual
 '
 
+test_expect_success "check that the trickle option works" '
+  HASHat=$(ipfs add -q --cid-version=1 --raw-leaves=true -n --trickle file3) &&
+  HASHut=$(ipfs urlstore add --trickle http://127.0.0.1:$GWAY_PORT/ipfs/$HASH3a) &&
+  test $HASHat = $HASHut
+'
+
 test_kill_ipfs_daemon
 
 test_expect_success "files can not be retrieved via the urlstore" '
