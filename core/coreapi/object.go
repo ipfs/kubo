allchain/go-ipfs

commands: switch object commands to CoreAPI
License: MIT
Signed-off-by: Łukasz Magiera <magik6k@gmail.com>

core/coreapi/object.go
@@ -319,10 +319,16 @@ func (api *ObjectAPI) Diff(ctx context.Context, before coreiface.Path, after cor
 	out := make([]coreiface.ObjectChange, len(changes))
 	for i, change := range changes {
 		out[i] = coreiface.ObjectChange{
-			Type:   change.Type,
-			Path:   change.Path,
-			Before: api.ParseCid(change.Before),
-			After:  api.ParseCid(change.After),
+			Type: change.Type,
+			Path: change.Path,
+		}
+
+		if change.Before != nil {
+			out[i].Before = api.ParseCid(change.Before)
+		}
+
+		if change.After != nil {
+			out[i].After = api.ParseCid(change.After)
 		}
 	}
 

ipfs/kubo

core/coreapi/object.go
@@ -316,10 +316,16 @@ func (api *ObjectAPI) Diff(ctx context.Context, before coreiface.Path, after cor
 	out := make([]coreiface.ObjectChange, len(changes))
 	for i, change := range changes {
 		out[i] = coreiface.ObjectChange{
-			Type:   change.Type,
-			Path:   change.Path,
-			Before: coreiface.IpfsPath(change.Before),
-			After:  coreiface.IpfsPath(change.After),
+			Type: change.Type,
+			Path: change.Path,
+		}
+
+		if change.Before != nil {
+			out[i].Before = coreiface.IpfsPath(change.Before)
+		}
+
+		if change.After != nil {
+			out[i].After = coreiface.IpfsPath(change.After)
 		}
 	}
 