04c548b1d2e75aa0f31755a90b1d059ff9210dc6
Łukasz Magiera <magik6k@gmail.com>
2018-08-03 18:06:57 +0200

    coreapi: dag: Batching interface
    License: MIT
    Signed-off-by: Łukasz Magiera <magik6k@gmail.com>
    
    
    This commit was moved from ipfs/interface-go-ipfs-core@f4b74679d1eb5597e430a52f4cca826d511898c3
    
    This commit was moved from ipfs/boxo@83081a0c1b91bb9554d56ea074ef28ca02b38ed9

--- core/coreiface/dag.go
+++ core/coreiface/dag.go
@@ -4,21 +4,36 @@ import (
 	"context"
 	"io"
 
-	options "github.com/ipfs/go-ipfs/core/coreapi/interface/options"
+	"github.com/ipfs/go-ipfs/core/coreapi/interface/options"
 
 	ipld "gx/ipfs/QmZtNq8dArGfnpCZfx2pUNY7UcjGhVp5qqwQ4hH6mpTMRQ/go-ipld-format"
 )
 
-// DagAPI specifies the interface to IPLD
-type DagAPI interface {
+// DagOps groups operations that can be batched together
+type DagOps interface {
 	// Put inserts data using specified format and input encoding.
 	// Unless used with WithCodec or WithHash, the defaults "dag-cbor" and
 	// "sha256" are used.
 	Put(ctx context.Context, src io.Reader, opts ...options.DagPutOption) (ResolvedPath, error)
+}
+
+// DagBatch is the batching version of DagAPI. All implementations of DagBatch
+// should be threadsafe
+type DagBatch interface {
+	DagOps
+
+	Commit(ctx context.Context) error
+}
+
+// DagAPI specifies the interface to IPLD
+type DagAPI interface {
+	DagOps
 
 	// Get attempts to resolve and get the node specified by the path
 	Get(ctx context.Context, path Path) (ipld.Node, error)
 
 	// Tree returns list of paths within a node specified by the path.
 	Tree(ctx context.Context, path Path, opts ...options.DagTreeOption) ([]Path, error)
+
+	Batch(ctx context.Context) DagBatch
 }
