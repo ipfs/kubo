b0f90e3e848380344dbe8d456784114b6a977356
Kevin Atkinson <k@kevina.org>
2018-08-21 22:48:00 -0400

    Address code review.
    License: MIT
    Signed-off-by: Kevin Atkinson <k@kevina.org>

--- core/commands/add.go
+++ core/commands/add.go
@@ -46,7 +46,7 @@ const (
 	cidVersionOptionName  = "cid-version"
 	hashOptionName        = "hash"
 	inlineOptionName      = "inline"
-	idHashLimitOptionName = "id-hash-limit"
+	inlineLimitOptionName = "inline-limit"
 )
 
 const adderOutChanSize = 8
@@ -123,8 +123,8 @@ You can now check what blocks have been created by:
 		cmdkit.BoolOption(fstoreCacheOptionName, "Check the filestore for pre-existing blocks. (experimental)"),
 		cmdkit.IntOption(cidVersionOptionName, "CID version. Defaults to 0 unless an option that depends on CIDv1 is passed. (experimental)"),
 		cmdkit.StringOption(hashOptionName, "Hash function to use. Implies CIDv1 if not sha2-256. (experimental)").WithDefault("sha2-256"),
-		cmdkit.BoolOption(inlineOptionName, "Inline small objects using identity hash. (experimental)"),
-		cmdkit.IntOption(idHashLimitOptionName, "Identity hash maxium size. (experimental)").WithDefault(64),
+		cmdkit.BoolOption(inlineOptionName, "Inline small blocks into CIDs. (experimental)"),
+		cmdkit.IntOption(inlineLimitOptionName, "Maximum block size to inline. (experimental)").WithDefault(64),
 	},
 	PreRun: func(req *cmds.Request, env cmds.Environment) error {
 		quiet, _ := req.Options[quietOptionName].(bool)
@@ -179,7 +179,7 @@ You can now check what blocks have been created by:
 		cidVer, cidVerSet := req.Options[cidVersionOptionName].(int)
 		hashFunStr, _ := req.Options[hashOptionName].(string)
 		inline, _ := req.Options[inlineOptionName].(bool)
-		idHashLimit, _ := req.Options[idHashLimitOptionName].(int)
+		inlineLimit, _ := req.Options[inlineLimitOptionName].(int)
 
 		// The arguments are subject to the following constraints.
 		//
@@ -291,7 +291,8 @@ You can now check what blocks have been created by:
 		if inline {
 			fileAdder.CidBuilder = cidutil.InlineBuilder{
 				Builder: fileAdder.CidBuilder,
-				Limit:   idHashLimit}
+				Limit:   inlineLimit,
+			}
 		}
 
 		if hash {
