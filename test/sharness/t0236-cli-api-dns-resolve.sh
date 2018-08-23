c89102a01c6cc0c8fef0317062770e5075a5fc7e
Steven Allen <steven@stebalien.com>
2018-08-23 01:13:41 +0000

    dns test: switch to localhost
    License: MIT
    Signed-off-by: Steven Allen <steven@stebalien.com>

--- test/sharness/t0236-cli-api-dns-resolve.sh
+++ test/sharness/t0236-cli-api-dns-resolve.sh
@@ -10,14 +10,11 @@ test_description="test dns resolution of api endpoint by cli"
 
 test_init_ipfs
 
-# this test uses the localtest.me domain which resolves to 127.0.0.1
-# see http://readme.localtest.me/
-# in case if failure, check A record of that domain
 test_expect_success "can make http request against dns resolved nc server" '
   nc -ld 5005 > nc_out &
   NCPID=$!
   go-sleep 0.5s && kill "$NCPID" &
-  ipfs cat /ipfs/Qmabcdef --api /dns4/localtest.me/tcp/5005 || true
+  ipfs cat /ipfs/Qmabcdef --api /dns4/localhost/tcp/5005 || true
 '
 
 test_expect_success "request was received by local nc server" '
