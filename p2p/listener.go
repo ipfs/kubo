zhuliting/go-ipfs

p2p: Optimize registry, move stream stuff around
License: MIT
Signed-off-by: Łukasz Magiera <magik6k@gmail.com>

p2p/listener.go
@@ -1,0 +1,39 @@
+package p2p
+
+import (
+	p2phost "gx/ipfs/QmNmJZL7FQySMtE2BQuLMuZg2EB2CLEunJJUSVSc9YnnbV/go-libp2p-host"
+	pstore "gx/ipfs/QmXauCuJzmzapetmC6W4TuDJLL1yFFrVzSHoWv8YdbmnxH/go-libp2p-peerstore"
+	peer "gx/ipfs/QmZoWKhxUmZ2seW4BzX6fJkNR8hh9PsGModr7q171yq2SS/go-libp2p-peer"
+)
+
+type Listener interface {
+	Protocol() string
+	Address() string
+
+	// Close closes the listener. Does not affect child streams
+	Close() error
+}
+
+// NewP2P creates new P2P struct
+func NewP2P(identity peer.ID, peerHost p2phost.Host, peerstore pstore.Peerstore) *P2P {
+	return &P2P{
+		identity:  identity,
+		peerHost:  peerHost,
+		peerstore: peerstore,
+	}
+}
+
+// ListenerRegistry is a collection of local application proto listeners.
+type ListenerRegistry struct {
+	Listeners map[string]Listener
+}
+
+// Register registers listenerInfo2 in this registry
+func (c *ListenerRegistry) Register(listenerInfo Listener) {
+	c.Listeners[listenerInfo.Protocol()] = listenerInfo
+}
+
+// Deregister removes p2p listener from this registry
+func (c *ListenerRegistry) Deregister(proto string) {
+	delete(c.Listeners, proto)
+}

p2p/stream.go
@@ -1,0 +1,83 @@
+package p2p
+
+import (
+	"io"
+
+	manet "gx/ipfs/QmRK2LxanhK2gZq6k6R7vk5ZoYZk8ULSSTB7FzDsMUX6CB/go-multiaddr-net"
+	ma "gx/ipfs/QmWWQ2Txc2c6tqjsBpzg5Ar652cHPGNsQQp2SejkNmkUMb/go-multiaddr"
+	net "gx/ipfs/QmXfkENeeBvh3zYA51MaSdGUdBjhQ99cP5WQe8zgr6wchG/go-libp2p-net"
+	peer "gx/ipfs/QmZoWKhxUmZ2seW4BzX6fJkNR8hh9PsGModr7q171yq2SS/go-libp2p-peer"
+)
+
+// Stream holds information on active incoming and outgoing p2p streams.
+type Stream struct {
+	Id uint64
+
+	Protocol string
+
+	LocalPeer peer.ID
+	LocalAddr ma.Multiaddr
+
+	RemotePeer peer.ID
+	RemoteAddr ma.Multiaddr
+
+	Local  manet.Conn
+	Remote net.Stream
+
+	Registry *StreamRegistry
+}
+
+// Close closes stream endpoints and deregisters it
+func (s *Stream) Close() error {
+	s.Local.Close()
+	s.Remote.Close()
+	s.Registry.Deregister(s.Id)
+	return nil
+}
+
+// Reset closes stream endpoints and deregisters it
+func (s *Stream) Reset() error {
+	s.Local.Close()
+	s.Remote.Reset()
+	s.Registry.Deregister(s.Id)
+	return nil
+}
+
+func (s *Stream) startStreaming() {
+	go func() {
+		_, err := io.Copy(s.Local, s.Remote)
+		if err != nil {
+			s.Reset()
+		} else {
+			s.Close()
+		}
+	}()
+
+	go func() {
+		_, err := io.Copy(s.Remote, s.Local)
+		if err != nil {
+			s.Reset()
+		} else {
+			s.Close()
+		}
+	}()
+}
+
+// StreamRegistry is a collection of active incoming and outgoing proto app streams.
+type StreamRegistry struct {
+	Streams map[uint64]*Stream
+
+	nextId uint64
+}
+
+// Register registers a stream to the registry
+func (c *StreamRegistry) Register(streamInfo *Stream) {
+	streamInfo.Id = c.nextId
+	c.Streams[c.nextId] = streamInfo
+	c.nextId++
+}
+
+// Deregister deregisters stream from the registry
+func (c *StreamRegistry) Deregister(streamId uint64) {
+	delete(c.Streams, streamId)
+}

ipfs/kubo

p2p/listener.go
@@ -1,0 +1,39 @@
+package p2p
+
+import (
+	pstore "gx/ipfs/QmZb7hAgQEhW9dBbzBudU39gCeD4zbe6xafD52LUuF4cUN/go-libp2p-peerstore"
+	peer "gx/ipfs/QmcJukH2sAFjY3HdBKq35WDzWoL3UUu2gt9wdfqZTUyM74/go-libp2p-peer"
+	p2phost "gx/ipfs/QmdHyfNVTZ5VtUx4Xz23z8wtnioSrFQ28XSfpVkdhQBkGA/go-libp2p-host"
+)
+
+type Listener interface {
+	Protocol() string
+	Address() string
+
+	// Close closes the listener. Does not affect child streams
+	Close() error
+}
+
+// NewP2P creates new P2P struct
+func NewP2P(identity peer.ID, peerHost p2phost.Host, peerstore pstore.Peerstore) *P2P {
+	return &P2P{
+		identity:  identity,
+		peerHost:  peerHost,
+		peerstore: peerstore,
+	}
+}
+
+// ListenerRegistry is a collection of local application proto listeners.
+type ListenerRegistry struct {
+	Listeners map[string]Listener
+}
+
+// Register registers listenerInfo2 in this registry
+func (c *ListenerRegistry) Register(listenerInfo Listener) {
+	c.Listeners[listenerInfo.Protocol()] = listenerInfo
+}
+
+// Deregister removes p2p listener from this registry
+func (c *ListenerRegistry) Deregister(proto string) {
+	delete(c.Listeners, proto)
+}

p2p/stream.go
@@ -1,0 +1,83 @@
+package p2p
+
+import (
+	"io"
+
+	ma "gx/ipfs/QmWWQ2Txc2c6tqjsBpzg5Ar652cHPGNsQQp2SejkNmkUMb/go-multiaddr"
+	net "gx/ipfs/QmYj8wdn5sZEHX2XMDWGBvcXJNdzVbaVpHmXvhHBVZepen/go-libp2p-net"
+	manet "gx/ipfs/QmcGXGdw9BWDysPJQHxJinjGHha3eEg4vzFETre4woNwcX/go-multiaddr-net"
+	peer "gx/ipfs/QmcJukH2sAFjY3HdBKq35WDzWoL3UUu2gt9wdfqZTUyM74/go-libp2p-peer"
+)
+
+// Stream holds information on active incoming and outgoing p2p streams.
+type Stream struct {
+	Id uint64
+
+	Protocol string
+
+	LocalPeer peer.ID
+	LocalAddr ma.Multiaddr
+
+	RemotePeer peer.ID
+	RemoteAddr ma.Multiaddr
+
+	Local  manet.Conn
+	Remote net.Stream
+
+	Registry *StreamRegistry
+}
+
+// Close closes stream endpoints and deregisters it
+func (s *Stream) Close() error {
+	s.Local.Close()
+	s.Remote.Close()
+	s.Registry.Deregister(s.Id)
+	return nil
+}
+
+// Reset closes stream endpoints and deregisters it
+func (s *Stream) Reset() error {
+	s.Local.Close()
+	s.Remote.Reset()
+	s.Registry.Deregister(s.Id)
+	return nil
+}
+
+func (s *Stream) startStreaming() {
+	go func() {
+		_, err := io.Copy(s.Local, s.Remote)
+		if err != nil {
+			s.Reset()
+		} else {
+			s.Close()
+		}
+	}()
+
+	go func() {
+		_, err := io.Copy(s.Remote, s.Local)
+		if err != nil {
+			s.Reset()
+		} else {
+			s.Close()
+		}
+	}()
+}
+
+// StreamRegistry is a collection of active incoming and outgoing proto app streams.
+type StreamRegistry struct {
+	Streams map[uint64]*Stream
+
+	nextId uint64
+}
+
+// Register registers a stream to the registry
+func (c *StreamRegistry) Register(streamInfo *Stream) {
+	streamInfo.Id = c.nextId
+	c.Streams[c.nextId] = streamInfo
+	c.nextId++
+}
+
+// Deregister deregisters stream from the registry
+func (c *StreamRegistry) Deregister(streamId uint64) {
+	delete(c.Streams, streamId)
+}