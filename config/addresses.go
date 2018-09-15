6a618df199319152a78fdf343f3bbb0d1720f201
Steven Allen <steven@stebalien.com>
2018-09-15 13:56:12 -0700

    go-ipfs-config: allow multiple API/Gateway addresses
    Alternative to #6 that doesn't require a migration.

--- config/addresses.go
+++ config/addresses.go
@@ -5,6 +5,6 @@ type Addresses struct {
 	Swarm      []string // addresses for the swarm to listen on
 	Announce   []string // swarm addresses to announce to the network
 	NoAnnounce []string // swarm addresses not to announce to the network
-	API        string   // address for the local API (RPC)
-	Gateway    string   // address to listen on for IPFS HTTP object gateway
+	API        Strings  // address for the local API (RPC)
+	Gateway    Strings  // address to listen on for IPFS HTTP object gateway
 }
