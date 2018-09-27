reeditalo/go-ipfs

Add --cid-base & --upgrade-cidv0 global opt. and enable them on select cmds
License: MIT
Signed-off-by: Kevin Atkinson <k@kevina.org>

core/coreunix/add.go
@@ -41,16 +41,16 @@ type Link struct {
 }
 
 type Object struct {
-	Hash  string
+	Hash  core.APICid
 	Links []Link
 	Size  string
 }
 
 type AddedObject struct {
 	Name  string
-	Hash  string `json:",omitempty"`
-	Bytes int64  `json:",omitempty"`
-	Size  string `json:",omitempty"`
+	Hash  core.APICid `json:",omitempty"`
+	Bytes int64       `json:",omitempty"`
+	Size  string      `json:",omitempty"`
 }
 
 // NewAdder Returns a new Adder used for a file add operation.
@@ -564,7 +564,7 @@ func getOutput(dagnode ipld.Node) (*Object, error) {
 	}
 
 	output := &Object{
-		Hash:  c.String(),
+		Hash:  core.FromCid(c),
 		Size:  strconv.FormatUint(s, 10),
 		Links: make([]Link, len(dagnode.Links())),
 	}

core/coreunix/add_test.go
@@ -93,7 +93,7 @@ func TestAddGCLive(t *testing.T) {
 
 	}()
 
-	addedHashes := make(map[string]struct{})
+	addedHashes := make(map[core.APICid]struct{})
 	select {
 	case o := <-out:
 		addedHashes[o.(*AddedObject).Hash] = struct{}{}
@@ -132,7 +132,7 @@ func TestAddGCLive(t *testing.T) {
 		if r.Error != nil {
 			t.Fatal(err)
 		}
-		if _, ok := addedHashes[r.KeyRemoved.String()]; ok {
+		if _, ok := addedHashes[core.FromCid(r.KeyRemoved)]; ok {
 			t.Fatal("gc'ed a hash we just added")
 		}
 	}
@@ -140,7 +140,7 @@ func TestAddGCLive(t *testing.T) {
 	var last cid.Cid
 	for a := range out {
 		// wait for it to finish
-		c, err := cid.Decode(a.(*AddedObject).Hash)
+		c, err := a.(*AddedObject).Hash.Cid()
 		if err != nil {
 			t.Fatal(err)
 		}

ipfs/kubo

core/coreunix/add.go
@@ -25,6 +25,7 @@ import (
 	logging "gx/ipfs/QmZChCsSt8DctjceaL56Eibc29CVQq4dGKRXC5JRZ6Ppae/go-log"
 	mfs "gx/ipfs/QmahrY1adY4wvtYEtoGjpZ2GUohTyukrkMkwUR9ytRjTG2/go-mfs"
 	ipld "gx/ipfs/QmdDXJs4axxefSPgK6Y1QhpJWKuDPnGJiqgq4uncb4rFHL/go-ipld-format"
+	apicid "gx/ipfs/QmdPF1WZQHFNfLdwhaShiR3e4KvFviAM58TrxVxPMhukic/go-cidutil/apicid"
 	bstore "gx/ipfs/QmdriVJgKx4JADRgh3cYPXqXmsa1A45SvFki1nDWHhQNtC/go-ipfs-blockstore"
 )
 
@@ -41,16 +42,16 @@ type Link struct {
 }
 
 type Object struct {
-	Hash  string
+	Hash  apicid.Hash
 	Links []Link
 	Size  string
 }
 
 type AddedObject struct {
 	Name  string
-	Hash  string `json:",omitempty"`
-	Bytes int64  `json:",omitempty"`
-	Size  string `json:",omitempty"`
+	Hash  apicid.Hash `json:",omitempty"`
+	Bytes int64       `json:",omitempty"`
+	Size  string      `json:",omitempty"`
 }
 
 // NewAdder Returns a new Adder used for a file add operation.
@@ -564,7 +565,7 @@ func getOutput(dagnode ipld.Node) (*Object, error) {
 	}
 
 	output := &Object{
-		Hash:  c.String(),
+		Hash:  apicid.FromCid(c),
 		Size:  strconv.FormatUint(s, 10),
 		Links: make([]Link, len(dagnode.Links())),
 	}

core/coreunix/add_test.go
@@ -23,6 +23,7 @@ import (
 	config "gx/ipfs/QmVBUpxsHh53rNcufqxMpLAmz37eGyLJUaexDy1W9YkiNk/go-ipfs-config"
 	dag "gx/ipfs/QmcBoNcAP6qDjgRBew7yjvCqHq7p5jMstE44jPUBWBxzsV/go-merkledag"
 	"gx/ipfs/QmcRecCZWM2NZfCQrCe97Ch3Givv8KKEP82tGUDntzdLFe/go-blockservice"
+	apicid "gx/ipfs/QmdPF1WZQHFNfLdwhaShiR3e4KvFviAM58TrxVxPMhukic/go-cidutil/apicid"
 	blockstore "gx/ipfs/QmdriVJgKx4JADRgh3cYPXqXmsa1A45SvFki1nDWHhQNtC/go-ipfs-blockstore"
 )
 
@@ -93,7 +94,7 @@ func TestAddGCLive(t *testing.T) {
 
 	}()
 
-	addedHashes := make(map[string]struct{})
+	addedHashes := make(map[apicid.Hash]struct{})
 	select {
 	case o := <-out:
 		addedHashes[o.(*AddedObject).Hash] = struct{}{}
@@ -132,7 +133,7 @@ func TestAddGCLive(t *testing.T) {
 		if r.Error != nil {
 			t.Fatal(err)
 		}
-		if _, ok := addedHashes[r.KeyRemoved.String()]; ok {
+		if _, ok := addedHashes[apicid.FromCid(r.KeyRemoved)]; ok {
 			t.Fatal("gc'ed a hash we just added")
 		}
 	}
@@ -140,7 +141,7 @@ func TestAddGCLive(t *testing.T) {
 	var last cid.Cid
 	for a := range out {
 		// wait for it to finish
-		c, err := cid.Decode(a.(*AddedObject).Hash)
+		c, err := a.(*AddedObject).Hash.Cid()
 		if err != nil {
 			t.Fatal(err)
 		}