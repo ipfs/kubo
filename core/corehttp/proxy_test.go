91c919a47e5d7c9bf2ac066f07035a0648b788ae
Hector Sanjuan <hector@protocol.ai>
2018-10-05 02:58:05 +0200

    Fix test
    License: MIT
    Signed-off-by: Hector Sanjuan <hector@protocol.ai>

--- core/corehttp/proxy_test.go
+++ core/corehttp/proxy_test.go
@@ -1,10 +1,11 @@
 package corehttp
 
 import (
-	"github.com/ipfs/go-ipfs/thirdparty/assert"
 	"net/http"
 	"strings"
 	"testing"
+
+	"github.com/ipfs/go-ipfs/thirdparty/assert"
 )
 
 func TestParseRequest(t *testing.T) {
@@ -15,9 +16,9 @@ func TestParseRequest(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	assert.True(parsed.httpPath == "path/to/index.txt", t, "proxy request path")
+	assert.True(parsed.httpPath == "/path/to/index.txt", t, "proxy request path")
 	assert.True(parsed.name == "test-name", t, "proxy request name")
-	assert.True(parsed.target.Pretty() == "QmT8JtU54XSmC38xSb1XHFSMm775VuTeajg7LWWWTAwzxT", t, "proxy request peer-id")
+	assert.True(parsed.target == "QmT8JtU54XSmC38xSb1XHFSMm775VuTeajg7LWWWTAwzxT", t, "proxy request peer-id")
 }
 
 func TestParseRequestInvalidPath(t *testing.T) {
