7cecbb9dce8b6db19d0c976248f1f0d2d6ca85f5
Steven Allen <steven@stebalien.com>
2018-10-25 10:15:54 -0700

    go-ipfs-config: add pubsub message signing options to config

--- config/pubsub.go
+++ config/pubsub.go
@@ -1,5 +1,18 @@
 package config
 
 type PubsubConfig struct {
+	// Router can be either floodsub (legacy) or gossipsub (new and
+	// backwards compatible).
 	Router string
+
+	// DisableSigning disables message signing. Message signing is *enabled*
+	// by default.
+	DisableSigning bool
+
+	// StrictSignatureVerification enables strict signature verification.
+	// When enabled, unsigned messages will be rejected. Eventually, this
+	// will be made the default and this option will disappear. Once this
+	// happens, networks will either need to completely disable or
+	// completely enable message signing.
+	StrictSignatureVerification bool
 }
