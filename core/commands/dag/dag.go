64a517afed3bce08320715702836bfe93f21a8ff
Steven Allen <steven@stebalien.com>
2018-10-26 12:57:32 -0700

    simplify dag add
    License: MIT
    Signed-off-by: Steven Allen <steven@stebalien.com>

--- core/commands/dag/dag.go
+++ core/commands/dag/dag.go
@@ -87,81 +87,58 @@ into an object of the specified format.
 			}
 		}
 
-		outChan := make(chan interface{}, 8)
+		cids := cid.NewSet()
+		b := ipld.NewBatch(req.Context, nd.DAG)
 
-		addAllAndPin := func(f files.File) error {
-			cids := cid.NewSet()
-			b := ipld.NewBatch(req.Context, nd.DAG)
-
-			for {
-				file, err := f.NextFile()
-				if err == io.EOF {
-					// Finished the list of files.
-					break
-				} else if err != nil {
-					return err
-				}
-
-				nds, err := coredag.ParseInputs(ienc, format, file, mhType, -1)
-				if err != nil {
-					return err
-				}
-				if len(nds) == 0 {
-					return fmt.Errorf("no node returned from ParseInputs")
-				}
-
-				for _, nd := range nds {
-					err := b.Add(nd)
-					if err != nil {
-						return err
-					}
-				}
-
-				cid := nds[0].Cid()
-				cids.Add(cid)
-
-				select {
-				case outChan <- &OutputObject{Cid: cid}:
-				case <-req.Context.Done():
-					return nil
-				}
-			}
-
-			if err := b.Commit(); err != nil {
+		for {
+			file, err := req.Files.NextFile()
+			if err == io.EOF {
+				// Finished the list of files.
+				break
+			} else if err != nil {
 				return err
 			}
 
-			if dopin {
-				defer nd.Blockstore.PinLock().Unlock()
+			nds, err := coredag.ParseInputs(ienc, format, file, mhType, -1)
+			if err != nil {
+				return err
+			}
+			if len(nds) == 0 {
+				return fmt.Errorf("no node returned from ParseInputs")
+			}
 
-				cids.ForEach(func(c cid.Cid) error {
-					nd.Pinning.PinWithMode(c, pin.Recursive)
-					return nil
-				})
-
-				err := nd.Pinning.Flush()
+			for _, nd := range nds {
+				err := b.Add(nd)
 				if err != nil {
 					return err
 				}
 			}
 
-			return nil
+			cid := nds[0].Cid()
+			cids.Add(cid)
+			if err := res.Emit(&OutputObject{Cid: cid}); err != nil {
+				return err
+			}
 		}
 
-		errC := make(chan error)
-		go func() {
-			var err error
-			defer func() { errC <- err }()
-			defer close(outChan)
-			err = addAllAndPin(req.Files)
-		}()
-
-		err = res.Emit(outChan)
-		if err != nil {
+		if err := b.Commit(); err != nil {
 			return err
 		}
 
-		return <-errC
+		if dopin {
+			defer nd.Blockstore.PinLock().Unlock()
+
+			cids.ForEach(func(c cid.Cid) error {
+				nd.Pinning.PinWithMode(c, pin.Recursive)
+				return nil
+			})
+
+			err := nd.Pinning.Flush()
+			if err != nil {
+				return err
+			}
+		}
+		return nil
 	},
 	Type: OutputObject{},
 	Encoders: cmds.EncoderMap{
