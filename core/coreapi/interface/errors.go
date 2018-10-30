e37b252c0c7459af31a54fc6e0be46f011a1d856
Steven Allen <steven@stebalien.com>
2018-10-30 08:48:49 -0700

    coreapi: fix errisdir
    JavaScript expects this to be "this dag node is a directory". I'm almost of a
    mind to say "don't parse errors" but, well, we don't give any better
    alternatives.
    
    License: MIT
    Signed-off-by: Steven Allen <steven@stebalien.com>

--- core/coreapi/interface/errors.go
+++ core/coreapi/interface/errors.go
@@ -3,6 +3,6 @@ package iface
 import "errors"
 
 var (
-	ErrIsDir   = errors.New("object is a directory")
+	ErrIsDir   = errors.New("this dag node is a directory")
 	ErrOffline = errors.New("this action must be run in online mode, try running 'ipfs daemon' first")
 )
