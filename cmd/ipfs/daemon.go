11c0fff4d6f1d0cb793091a1fda392ba3066955d
Daniel Grossmann-Kavanagh <me@danielgk.com>
2018-11-02 19:24:00 -0700

    add Web UI link to daemon startup

--- cmd/ipfs/daemon.go
+++ cmd/ipfs/daemon.go
@@ -435,7 +435,7 @@ func serveHTTPApi(req *cmds.Request, cctx *oldcmds.Context) (<-chan error, error
 		// we might have listened to /tcp/0 - lets see what we are listing on
 		apiMaddr = apiLis.Multiaddr()
 		fmt.Printf("API server listening on %s\n", apiMaddr)
-
+		fmt.Printf("WebUI: http://%s%s\n", apiLis.Addr(), corehttp.WebUIPath)
 		listeners = append(listeners, apiLis)
 	}
 
