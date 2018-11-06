e4282bdb7ab3f69d892b1d4e0798b3823e181555
Łukasz Magiera <magik6k@gmail.com>
2018-11-06 15:39:53 +0100

    go-ipfs-config: Add one more test for config.Clone

--- config/config_test.go
+++ config/config_test.go
@@ -7,7 +7,7 @@ import (
 func TestClone(t *testing.T) {
 	c := new(Config)
 	c.Identity.PeerID = "faketest"
-	c.API.HTTPHeaders = map[string][]string{"foo": []string{"bar"}}
+	c.API.HTTPHeaders = map[string][]string{"foo": {"bar"}}
 
 	newCfg, err := c.Clone()
 	if err != nil {
@@ -16,6 +16,12 @@ func TestClone(t *testing.T) {
 	if newCfg.Identity.PeerID != c.Identity.PeerID {
 		t.Fatal("peer ID not preserved")
 	}
+
+	c.API.HTTPHeaders["foo"] = []string{"baz"}
+	if newCfg.API.HTTPHeaders["foo"][0] != "bar" {
+		t.Fatal("HTTP headers not preserved")
+	}
+
 	delete(c.API.HTTPHeaders, "foo")
 	if newCfg.API.HTTPHeaders["foo"][0] != "bar" {
 		t.Fatal("HTTP headers not preserved")
