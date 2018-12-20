whitecandylee/go-ipfs

files2.0: Use new type helpers
License: MIT
Signed-off-by: Łukasz Magiera <magik6k@gmail.com>

cmd/ipfs/init.go
@@ -16,6 +16,7 @@ import (
 	namesys "github.com/ipfs/go-ipfs/namesys"
 	fsrepo "github.com/ipfs/go-ipfs/repo/fsrepo"
 
+	"gx/ipfs/QmPhx9B9cuaXc4vuw62567BF5NxfpsdD1AVE9HbTn7t1Y6/go-ipfs-files"
 	"gx/ipfs/QmXctaABKwgzmQgNM4bucMJf7zJnxxvhmPM1Pw95dxUfB5/go-ipfs-config"
 	"gx/ipfs/Qmb7ARH13wPpsyZSXEDeF8aUqSf1ixmWDuD9eNoHUFM5bW/go-ipfs-cmds"
 	"gx/ipfs/Qmde5VP1qUkyQXKCfmEUA7bP64V2HAptbJ7phuPp7jXWwg/go-ipfs-cmdkit"
@@ -89,12 +90,12 @@ environment variable:
 			if !it.Next() && it.Err() != nil {
 				return it.Err()
 			}
-			if it.File() == nil {
+			if files.FileFrom(it) == nil {
 				return fmt.Errorf("expected a regular file")
 			}
 
 			conf = &config.Config{}
-			if err := json.NewDecoder(it.File()).Decode(conf); err != nil {
+			if err := json.NewDecoder(files.FileFrom(it)).Decode(conf); err != nil {
 				return err
 			}
 		}

core/commands/block.go
@@ -11,6 +11,7 @@ import (
 	coreiface "github.com/ipfs/go-ipfs/core/coreapi/interface"
 	"github.com/ipfs/go-ipfs/core/coreapi/interface/options"
 
+	files "gx/ipfs/QmPhx9B9cuaXc4vuw62567BF5NxfpsdD1AVE9HbTn7t1Y6/go-ipfs-files"
 	cmds "gx/ipfs/Qmb7ARH13wPpsyZSXEDeF8aUqSf1ixmWDuD9eNoHUFM5bW/go-ipfs-cmds"
 	cmdkit "gx/ipfs/Qmde5VP1qUkyQXKCfmEUA7bP64V2HAptbJ7phuPp7jXWwg/go-ipfs-cmdkit"
 	mh "gx/ipfs/QmerPMzPk1mJVowm8KgmoknWa4yCYvvugMPsgWmDNUvDLW/go-multihash"
@@ -157,7 +158,7 @@ than 'sha2-256' or format to anything other than 'v0' will result in CIDv1.
 		if !it.Next() && it.Err() != nil {
 			return it.Err()
 		}
-		if it.File() == nil {
+		if files.FileFrom(it) == nil {
 			return fmt.Errorf("expected a regular file")
 		}
 
@@ -181,7 +182,7 @@ than 'sha2-256' or format to anything other than 'v0' will result in CIDv1.
 			}
 		}
 
-		p, err := api.Block().Put(req.Context, it.File(), options.Block.Hash(mhtval, mhlen), options.Block.Format(format))
+		p, err := api.Block().Put(req.Context, files.FileFrom(it), options.Block.Hash(mhtval, mhlen), options.Block.Format(format))
 		if err != nil {
 			return err
 		}

core/commands/dag/dag.go
@@ -9,6 +9,7 @@ import (
 	"github.com/ipfs/go-ipfs/core/coredag"
 	"github.com/ipfs/go-ipfs/pin"
 
+	files "gx/ipfs/QmPhx9B9cuaXc4vuw62567BF5NxfpsdD1AVE9HbTn7t1Y6/go-ipfs-files"
 	path "gx/ipfs/QmQtg7N4XjAk2ZYpBjjv8B6gQprsRekabHBCnF6i46JYKJ/go-path"
 	cid "gx/ipfs/QmR8BauakNcBa3RbE4nbQu76PDiJgoQgz8AJdhJuiU4TAw/go-cid"
 	cmds "gx/ipfs/Qmb7ARH13wPpsyZSXEDeF8aUqSf1ixmWDuD9eNoHUFM5bW/go-ipfs-cmds"
@@ -94,10 +95,10 @@ into an object of the specified format.
 
 		it := req.Files.Entries()
 		for it.Next() {
-			if it.File() == nil {
+			if files.FileFrom(it) == nil {
 				return fmt.Errorf("expected a regular file")
 			}
-			nds, err := coredag.ParseInputs(ienc, format, it.File(), mhType, -1)
+			nds, err := coredag.ParseInputs(ienc, format, files.FileFrom(it), mhType, -1)
 			if err != nil {
 				return err
 			}

core/commands/files.go
@@ -16,6 +16,7 @@ import (
 
 	ft "gx/ipfs/QmPBbzSbX3pMRnN3fHLWDiLkCAtuZkZJhbctMNwVSMzN7m/go-unixfs"
 	humanize "gx/ipfs/QmPSBJL4momYnE7DcUyk2DVhD6rH488ZmHBGLbxNdhU44K/go-humanize"
+	files "gx/ipfs/QmPhx9B9cuaXc4vuw62567BF5NxfpsdD1AVE9HbTn7t1Y6/go-ipfs-files"
 	cid "gx/ipfs/QmR8BauakNcBa3RbE4nbQu76PDiJgoQgz8AJdhJuiU4TAw/go-cid"
 	bservice "gx/ipfs/QmVDTbzzTwnuBwNbJdhW3u7LoBQp46bezm9yp4z1RoEepM/go-blockservice"
 	mfs "gx/ipfs/QmXaDT2octH295k1hZDXAz1Yx9WiD5E1QiyixQko7UYP94/go-mfs"
@@ -773,11 +774,11 @@ stat' on the file or any of its ancestors.
 		if !it.Next() && it.Err() != nil {
 			return it.Err()
 		}
-		if it.File() == nil {
+		if files.FileFrom(it) == nil {
 			return fmt.Errorf("expected a regular file")
 		}
 
-		var r io.Reader = it.File()
+		var r io.Reader = files.FileFrom(it)
 		if countfound {
 			r = io.LimitReader(r, int64(count))
 		}

core/commands/object/object.go
@@ -12,6 +12,7 @@ import (
 	coreiface "github.com/ipfs/go-ipfs/core/coreapi/interface"
 	"github.com/ipfs/go-ipfs/core/coreapi/interface/options"
 
+	files "gx/ipfs/QmPhx9B9cuaXc4vuw62567BF5NxfpsdD1AVE9HbTn7t1Y6/go-ipfs-files"
 	cid "gx/ipfs/QmR8BauakNcBa3RbE4nbQu76PDiJgoQgz8AJdhJuiU4TAw/go-cid"
 	cmds "gx/ipfs/Qmb7ARH13wPpsyZSXEDeF8aUqSf1ixmWDuD9eNoHUFM5bW/go-ipfs-cmds"
 	ipld "gx/ipfs/QmcKKBwfz6FyQdHR2jsXrrF6XeSBXYL86anmWNewpFpoF5/go-ipld-format"
@@ -395,7 +396,7 @@ And then run:
 		if !it.Next() && it.Err() != nil {
 			return it.Err()
 		}
-		if it.File() == nil {
+		if files.FileFrom(it) == nil {
 			return fmt.Errorf("expected a regular file")
 		}
 
@@ -414,7 +415,7 @@ And then run:
 			return err
 		}
 
-		p, err := api.Object().Put(req.Context, it.File(),
+		p, err := api.Object().Put(req.Context, files.FileFrom(it),
 			options.Object.DataType(datafieldenc),
 			options.Object.InputEnc(inputenc),
 			options.Object.Pin(dopin))

core/commands/object/patch.go
@@ -8,6 +8,7 @@ import (
 	coreiface "github.com/ipfs/go-ipfs/core/coreapi/interface"
 	"github.com/ipfs/go-ipfs/core/coreapi/interface/options"
 
+	"gx/ipfs/QmPhx9B9cuaXc4vuw62567BF5NxfpsdD1AVE9HbTn7t1Y6/go-ipfs-files"
 	"gx/ipfs/Qmb7ARH13wPpsyZSXEDeF8aUqSf1ixmWDuD9eNoHUFM5bW/go-ipfs-cmds"
 	"gx/ipfs/Qmde5VP1qUkyQXKCfmEUA7bP64V2HAptbJ7phuPp7jXWwg/go-ipfs-cmdkit"
 )
@@ -64,11 +65,11 @@ the limit will not be respected by the network.
 		if !it.Next() && it.Err() != nil {
 			return it.Err()
 		}
-		if it.File() == nil {
+		if files.FileFrom(it) == nil {
 			return fmt.Errorf("expected a regular file")
 		}
 
-		p, err := api.Object().AppendData(req.Context, root, it.File())
+		p, err := api.Object().AppendData(req.Context, root, files.FileFrom(it))
 		if err != nil {
 			return err
 		}
@@ -114,11 +115,11 @@ Example:
 		if !it.Next() && it.Err() != nil {
 			return it.Err()
 		}
-		if it.File() == nil {
+		if files.FileFrom(it) == nil {
 			return fmt.Errorf("expected a regular file")
 		}
 
-		p, err := api.Object().SetData(req.Context, root, it.File())
+		p, err := api.Object().SetData(req.Context, root, files.FileFrom(it))
 		if err != nil {
 			return err
 		}

core/commands/tar.go
@@ -9,6 +9,7 @@ import (
 	coreiface "github.com/ipfs/go-ipfs/core/coreapi/interface"
 	tar "github.com/ipfs/go-ipfs/tar"
 
+	files "gx/ipfs/QmPhx9B9cuaXc4vuw62567BF5NxfpsdD1AVE9HbTn7t1Y6/go-ipfs-files"
 	"gx/ipfs/QmQtg7N4XjAk2ZYpBjjv8B6gQprsRekabHBCnF6i46JYKJ/go-path"
 	cmds "gx/ipfs/Qmb7ARH13wPpsyZSXEDeF8aUqSf1ixmWDuD9eNoHUFM5bW/go-ipfs-cmds"
 	dag "gx/ipfs/QmdURv6Sbob8TVW2tFFve9vcEWrSUgwPqeqnXyvYhLrkyd/go-merkledag"
@@ -48,11 +49,11 @@ represent it.
 		if !it.Next() && it.Err() != nil {
 			return it.Err()
 		}
-		if it.File() == nil {
+		if files.FileFrom(it) == nil {
 			return fmt.Errorf("expected a regular file")
 		}
 
-		node, err := tar.ImportTar(req.Context, it.File(), nd.DAG)
+		node, err := tar.ImportTar(req.Context, files.FileFrom(it), nd.DAG)
 		if err != nil {
 			return err
 		}

core/coreunix/add_test.go
@@ -71,20 +71,18 @@ func TestAddGCLive(t *testing.T) {
 	}
 	adder.Out = out
 
-	dataa := ioutil.NopCloser(bytes.NewBufferString("testfileA"))
-	rfa := files.NewReaderFile(dataa, nil)
+	rfa := files.FileFrom([]byte("testfileA"))
 
 	// make two files with pipes so we can 'pause' the add for timing of the test
 	piper, pipew := io.Pipe()
-	hangfile := files.NewReaderFile(piper, nil)
+	hangfile := files.FileFrom(piper)
 
-	datad := ioutil.NopCloser(bytes.NewBufferString("testfileD"))
-	rfd := files.NewReaderFile(datad, nil)
+	rfd := files.FileFrom([]byte("testfileD"))
 
-	slf := files.NewSliceFile([]files.DirEntry{
-		files.FileEntry("a", rfa),
-		files.FileEntry("b", hangfile),
-		files.FileEntry("d", rfd),
+	slf := files.FileFrom(map[string]files.Node{
+		"a": rfa,
+		"b": hangfile,
+		"d": rfd,
 	})
 
 	addDone := make(chan struct{})

ipfs/kubo

cmd/ipfs/init.go
@@ -19,6 +19,7 @@ import (
 	"gx/ipfs/QmYyzmMnhNTtoXx5ttgUaRdHHckYnQWjPL98hgLAR2QLDD/go-ipfs-config"
 	"gx/ipfs/QmaAP56JAwdjwisPTu4yx17whcjTr6y5JCSCF77Y1rahWV/go-ipfs-cmds"
 	"gx/ipfs/Qmde5VP1qUkyQXKCfmEUA7bP64V2HAptbJ7phuPp7jXWwg/go-ipfs-cmdkit"
+	"gx/ipfs/QmeaQRmnRog7NxLEWHP9zSTkics4cbgwBVa7q49LmBowDr/go-ipfs-files"
 )
 
 const (
@@ -89,12 +90,12 @@ environment variable:
 			if !it.Next() && it.Err() != nil {
 				return it.Err()
 			}
-			if it.File() == nil {
+			if files.FileFrom(it) == nil {
 				return fmt.Errorf("expected a regular file")
 			}
 
 			conf = &config.Config{}
-			if err := json.NewDecoder(it.File()).Decode(conf); err != nil {
+			if err := json.NewDecoder(files.FileFrom(it)).Decode(conf); err != nil {
 				return err
 			}
 		}

core/commands/block.go
@@ -13,6 +13,7 @@ import (
 
 	cmds "gx/ipfs/QmaAP56JAwdjwisPTu4yx17whcjTr6y5JCSCF77Y1rahWV/go-ipfs-cmds"
 	cmdkit "gx/ipfs/Qmde5VP1qUkyQXKCfmEUA7bP64V2HAptbJ7phuPp7jXWwg/go-ipfs-cmdkit"
+	files "gx/ipfs/QmeaQRmnRog7NxLEWHP9zSTkics4cbgwBVa7q49LmBowDr/go-ipfs-files"
 	mh "gx/ipfs/QmerPMzPk1mJVowm8KgmoknWa4yCYvvugMPsgWmDNUvDLW/go-multihash"
 )
 
@@ -157,7 +158,7 @@ than 'sha2-256' or format to anything other than 'v0' will result in CIDv1.
 		if !it.Next() && it.Err() != nil {
 			return it.Err()
 		}
-		if it.File() == nil {
+		if files.FileFrom(it) == nil {
 			return fmt.Errorf("expected a regular file")
 		}
 
@@ -181,7 +182,7 @@ than 'sha2-256' or format to anything other than 'v0' will result in CIDv1.
 			}
 		}
 
-		p, err := api.Block().Put(req.Context, it.File(), options.Block.Hash(mhtval, mhlen), options.Block.Format(format))
+		p, err := api.Block().Put(req.Context, files.FileFrom(it), options.Block.Hash(mhtval, mhlen), options.Block.Format(format))
 		if err != nil {
 			return err
 		}

core/commands/dag/dag.go
@@ -14,6 +14,7 @@ import (
 	cmds "gx/ipfs/QmaAP56JAwdjwisPTu4yx17whcjTr6y5JCSCF77Y1rahWV/go-ipfs-cmds"
 	ipld "gx/ipfs/QmcKKBwfz6FyQdHR2jsXrrF6XeSBXYL86anmWNewpFpoF5/go-ipld-format"
 	cmdkit "gx/ipfs/Qmde5VP1qUkyQXKCfmEUA7bP64V2HAptbJ7phuPp7jXWwg/go-ipfs-cmdkit"
+	files "gx/ipfs/QmeaQRmnRog7NxLEWHP9zSTkics4cbgwBVa7q49LmBowDr/go-ipfs-files"
 	mh "gx/ipfs/QmerPMzPk1mJVowm8KgmoknWa4yCYvvugMPsgWmDNUvDLW/go-multihash"
 )
 
@@ -94,10 +95,10 @@ into an object of the specified format.
 
 		it := req.Files.Entries()
 		for it.Next() {
-			if it.File() == nil {
+			if files.FileFrom(it) == nil {
 				return fmt.Errorf("expected a regular file")
 			}
-			nds, err := coredag.ParseInputs(ienc, format, it.File(), mhType, -1)
+			nds, err := coredag.ParseInputs(ienc, format, files.FileFrom(it), mhType, -1)
 			if err != nil {
 				return err
 			}

core/commands/files.go
@@ -25,6 +25,7 @@ import (
 	logging "gx/ipfs/QmcuXC5cxs79ro2cUuHs4HQ2bkDLJUYokwL8aivcX6HW3C/go-log"
 	dag "gx/ipfs/QmdV35UHnL1FM52baPkeUo6u7Fxm2CRUkPTLRPxeF8a4Ap/go-merkledag"
 	cmdkit "gx/ipfs/Qmde5VP1qUkyQXKCfmEUA7bP64V2HAptbJ7phuPp7jXWwg/go-ipfs-cmdkit"
+	files "gx/ipfs/QmeaQRmnRog7NxLEWHP9zSTkics4cbgwBVa7q49LmBowDr/go-ipfs-files"
 	mh "gx/ipfs/QmerPMzPk1mJVowm8KgmoknWa4yCYvvugMPsgWmDNUvDLW/go-multihash"
 )
 
@@ -773,11 +774,11 @@ stat' on the file or any of its ancestors.
 		if !it.Next() && it.Err() != nil {
 			return it.Err()
 		}
-		if it.File() == nil {
+		if files.FileFrom(it) == nil {
 			return fmt.Errorf("expected a regular file")
 		}
 
-		var r io.Reader = it.File()
+		var r io.Reader = files.FileFrom(it)
 		if countfound {
 			r = io.LimitReader(r, int64(count))
 		}

core/commands/object/object.go
@@ -17,6 +17,7 @@ import (
 	ipld "gx/ipfs/QmcKKBwfz6FyQdHR2jsXrrF6XeSBXYL86anmWNewpFpoF5/go-ipld-format"
 	dag "gx/ipfs/QmdV35UHnL1FM52baPkeUo6u7Fxm2CRUkPTLRPxeF8a4Ap/go-merkledag"
 	cmdkit "gx/ipfs/Qmde5VP1qUkyQXKCfmEUA7bP64V2HAptbJ7phuPp7jXWwg/go-ipfs-cmdkit"
+	files "gx/ipfs/QmeaQRmnRog7NxLEWHP9zSTkics4cbgwBVa7q49LmBowDr/go-ipfs-files"
 )
 
 type Node struct {
@@ -395,7 +396,7 @@ And then run:
 		if !it.Next() && it.Err() != nil {
 			return it.Err()
 		}
-		if it.File() == nil {
+		if files.FileFrom(it) == nil {
 			return fmt.Errorf("expected a regular file")
 		}
 
@@ -414,7 +415,7 @@ And then run:
 			return err
 		}
 
-		p, err := api.Object().Put(req.Context, it.File(),
+		p, err := api.Object().Put(req.Context, files.FileFrom(it),
 			options.Object.DataType(datafieldenc),
 			options.Object.InputEnc(inputenc),
 			options.Object.Pin(dopin))

core/commands/object/patch.go
@@ -10,6 +10,7 @@ import (
 
 	"gx/ipfs/QmaAP56JAwdjwisPTu4yx17whcjTr6y5JCSCF77Y1rahWV/go-ipfs-cmds"
 	"gx/ipfs/Qmde5VP1qUkyQXKCfmEUA7bP64V2HAptbJ7phuPp7jXWwg/go-ipfs-cmdkit"
+	"gx/ipfs/QmeaQRmnRog7NxLEWHP9zSTkics4cbgwBVa7q49LmBowDr/go-ipfs-files"
 )
 
 var ObjectPatchCmd = &cmds.Command{
@@ -64,11 +65,11 @@ the limit will not be respected by the network.
 		if !it.Next() && it.Err() != nil {
 			return it.Err()
 		}
-		if it.File() == nil {
+		if files.FileFrom(it) == nil {
 			return fmt.Errorf("expected a regular file")
 		}
 
-		p, err := api.Object().AppendData(req.Context, root, it.File())
+		p, err := api.Object().AppendData(req.Context, root, files.FileFrom(it))
 		if err != nil {
 			return err
 		}
@@ -114,11 +115,11 @@ Example:
 		if !it.Next() && it.Err() != nil {
 			return it.Err()
 		}
-		if it.File() == nil {
+		if files.FileFrom(it) == nil {
 			return fmt.Errorf("expected a regular file")
 		}
 
-		p, err := api.Object().SetData(req.Context, root, it.File())
+		p, err := api.Object().SetData(req.Context, root, files.FileFrom(it))
 		if err != nil {
 			return err
 		}

core/commands/tar.go
@@ -13,6 +13,7 @@ import (
 	cmds "gx/ipfs/QmaAP56JAwdjwisPTu4yx17whcjTr6y5JCSCF77Y1rahWV/go-ipfs-cmds"
 	dag "gx/ipfs/QmdV35UHnL1FM52baPkeUo6u7Fxm2CRUkPTLRPxeF8a4Ap/go-merkledag"
 	cmdkit "gx/ipfs/Qmde5VP1qUkyQXKCfmEUA7bP64V2HAptbJ7phuPp7jXWwg/go-ipfs-cmdkit"
+	files "gx/ipfs/QmeaQRmnRog7NxLEWHP9zSTkics4cbgwBVa7q49LmBowDr/go-ipfs-files"
 )
 
 var TarCmd = &cmds.Command{
@@ -48,11 +49,11 @@ represent it.
 		if !it.Next() && it.Err() != nil {
 			return it.Err()
 		}
-		if it.File() == nil {
+		if files.FileFrom(it) == nil {
 			return fmt.Errorf("expected a regular file")
 		}
 
-		node, err := tar.ImportTar(req.Context, it.File(), nd.DAG)
+		node, err := tar.ImportTar(req.Context, files.FileFrom(it), nd.DAG)
 		if err != nil {
 			return err
 		}

core/coreunix/add_test.go
@@ -71,20 +71,18 @@ func TestAddGCLive(t *testing.T) {
 	}
 	adder.Out = out
 
-	dataa := ioutil.NopCloser(bytes.NewBufferString("testfileA"))
-	rfa := files.NewReaderFile(dataa, nil)
+	rfa := files.FileFrom([]byte("testfileA"))
 
 	// make two files with pipes so we can 'pause' the add for timing of the test
 	piper, pipew := io.Pipe()
-	hangfile := files.NewReaderFile(piper, nil)
+	hangfile := files.FileFrom(piper)
 
-	datad := ioutil.NopCloser(bytes.NewBufferString("testfileD"))
-	rfd := files.NewReaderFile(datad, nil)
+	rfd := files.FileFrom([]byte("testfileD"))
 
-	slf := files.NewSliceFile([]files.DirEntry{
-		files.FileEntry("a", rfa),
-		files.FileEntry("b", hangfile),
-		files.FileEntry("d", rfd),
+	slf := files.DirFrom(map[string]files.Node{
+		"a": rfa,
+		"b": hangfile,
+		"d": rfd,
 	})
 
 	addDone := make(chan struct{})