abcdsxg/go-ipfs

coreapi: Untangle from core.IpfsNode
License: MIT
Signed-off-by: Łukasz Magiera <magik6k@gmail.com>

core/coreapi/coreapi.go
@@ -15,11 +15,26 @@ package coreapi
 
 import (
 	"context"
+	"errors"
 
 	core "github.com/ipfs/go-ipfs/core"
 	coreiface "github.com/ipfs/go-ipfs/core/coreapi/interface"
+	options "github.com/ipfs/go-ipfs/core/coreapi/interface/options"
+	namesys "github.com/ipfs/go-ipfs/namesys"
+	pin "github.com/ipfs/go-ipfs/pin"
+	repo "github.com/ipfs/go-ipfs/repo"
 
+	ci "gx/ipfs/QmNiJiXwWE3kRhZrC5ej3kSjWHm337pYfhjLGSCDNKJP2s/go-libp2p-crypto"
+	exchange "gx/ipfs/QmP2g3VxmC7g7fyRJDj1VJ72KHZbJ9UW24YjSWEj1XTb4H/go-ipfs-exchange-interface"
+	pstore "gx/ipfs/QmQAGG1zxfePqj2t7bLxyN8AFccZ889DDR9Gn8kVLDrGZo/go-libp2p-peerstore"
+	blockstore "gx/ipfs/QmS2aqUZLJp8kF1ihE5rvDGE5LvmKDPnx32w9Z1BW9xLV5/go-ipfs-blockstore"
+	record "gx/ipfs/QmSoeYGNm8v8jAF49hX7UwHwkXjoeobSrn9sya5NPPsxXP/go-libp2p-record"
+	bserv "gx/ipfs/QmVDTbzzTwnuBwNbJdhW3u7LoBQp46bezm9yp4z1RoEepM/go-blockservice"
+	routing "gx/ipfs/QmZBH87CAPFHcc7cYmBqeSQ98zQ3SX9KUxiYgzPmLWNVKz/go-libp2p-routing"
+	p2phost "gx/ipfs/QmahxMNoNuSsgQefo9rkpcfRFmQrMN6Q99aztKXf63K7YJ/go-libp2p-host"
+	pubsub "gx/ipfs/Qmc3BYVGtLs8y3p4uVpARWyo3Xk2oCBFF1AhYUVMPWgwUK/go-libp2p-pubsub"
 	ipld "gx/ipfs/QmcKKBwfz6FyQdHR2jsXrrF6XeSBXYL86anmWNewpFpoF5/go-ipld-format"
+	peer "gx/ipfs/QmcqU6QUDSXprb1518vYDGczrTJTyGwLG9eUa5iNX4xUtS/go-libp2p-peer"
 	logging "gx/ipfs/QmcuXC5cxs79ro2cUuHs4HQ2bkDLJUYokwL8aivcX6HW3C/go-log"
 	dag "gx/ipfs/QmdURv6Sbob8TVW2tFFve9vcEWrSUgwPqeqnXyvYhLrkyd/go-merkledag"
 )
@@ -27,13 +42,76 @@ import (
 var log = logging.Logger("core/coreapi")
 
 type CoreAPI struct {
-	node *core.IpfsNode
-	dag  ipld.DAGService
+	nctx context.Context
+
+	identity   peer.ID //TODO: check mutable structs
+	privateKey ci.PrivKey
+
+	repo       repo.Repo
+	blockstore blockstore.GCBlockstore
+	baseBlocks blockstore.Blockstore
+	blocks     bserv.BlockService
+	dag        ipld.DAGService
+	pinning    pin.Pinner
+
+	peerstore       pstore.Peerstore
+	peerHost        p2phost.Host
+	namesys         namesys.NameSystem
+	recordValidator record.Validator
+	exchange        exchange.Interface
+
+	routing routing.IpfsRouting
+	pubSub  *pubsub.PubSub
+
+	checkRouting func(bool) error
+
+	// TODO: this can be generalized to all functions when we implement some
+	// api based security mechanism
+	isPublishAllowed func() error
 }
 
 // NewCoreAPI creates new instance of IPFS CoreAPI backed by go-ipfs Node.
-func NewCoreAPI(n *core.IpfsNode) coreiface.CoreAPI {
-	api := &CoreAPI{n, n.DAG}
+func NewCoreAPI(n *core.IpfsNode, opts ...options.ApiOption) coreiface.CoreAPI {
+	api := &CoreAPI{
+		nctx: n.Context(),
+
+		identity:   n.Identity,
+		privateKey: n.PrivateKey,
+
+		repo:       n.Repo,
+		blockstore: n.Blockstore,
+		baseBlocks: n.BaseBlocks,
+		blocks:     n.Blocks,
+		dag:        n.DAG,
+		pinning:    n.Pinning,
+
+		peerstore:       n.Peerstore,
+		peerHost:        n.PeerHost,
+		namesys:         n.Namesys,
+		recordValidator: n.RecordValidator,
+		exchange:        n.Exchange,
+
+		routing: n.Routing,
+		pubSub:  n.PubSub,
+
+		checkRouting: func(allowOffline bool) error {
+			if !n.OnlineMode() {
+				if !allowOffline {
+					return coreiface.ErrOffline
+				}
+				return n.SetupOfflineRouting()
+			}
+			return nil
+		},
+
+		isPublishAllowed: func() error {
+			if n.Mounts.Ipns != nil && n.Mounts.Ipns.IsActive() {
+				return errors.New("cannot manually publish while IPNS is mounted")
+			}
+			return nil
+		},
+	}
+
 	return api
 }
 
@@ -89,6 +167,10 @@ func (api *CoreAPI) PubSub() coreiface.PubSubAPI {
 
 // getSession returns new api backed by the same node with a read-only session DAG
 func (api *CoreAPI) getSession(ctx context.Context) *CoreAPI {
-	ng := dag.NewReadOnlyDagService(dag.NewSession(ctx, api.dag))
-	return &CoreAPI{api.node, ng}
+	sesApi := *api
+
+	//TODO: we may want to apply this to other things too
+	sesApi.dag = dag.NewReadOnlyDagService(dag.NewSession(ctx, api.dag))
+
+	return &sesApi
 }

core/coreapi/pubsub.go
@@ -7,12 +7,13 @@ import (
 	"sync"
 	"time"
 
-	core "github.com/ipfs/go-ipfs/core"
 	coreiface "github.com/ipfs/go-ipfs/core/coreapi/interface"
 	caopts "github.com/ipfs/go-ipfs/core/coreapi/interface/options"
 
 	pstore "gx/ipfs/QmQAGG1zxfePqj2t7bLxyN8AFccZ889DDR9Gn8kVLDrGZo/go-libp2p-peerstore"
 	cid "gx/ipfs/QmR8BauakNcBa3RbE4nbQu76PDiJgoQgz8AJdhJuiU4TAw/go-cid"
+	routing "gx/ipfs/QmZBH87CAPFHcc7cYmBqeSQ98zQ3SX9KUxiYgzPmLWNVKz/go-libp2p-routing"
+	p2phost "gx/ipfs/QmahxMNoNuSsgQefo9rkpcfRFmQrMN6Q99aztKXf63K7YJ/go-libp2p-host"
 	pubsub "gx/ipfs/Qmc3BYVGtLs8y3p4uVpARWyo3Xk2oCBFF1AhYUVMPWgwUK/go-libp2p-pubsub"
 	peer "gx/ipfs/QmcqU6QUDSXprb1518vYDGczrTJTyGwLG9eUa5iNX4xUtS/go-libp2p-peer"
 )
@@ -33,7 +34,7 @@ func (api *PubSubAPI) Ls(ctx context.Context) ([]string, error) {
 		return nil, err
 	}
 
-	return api.node.PubSub.GetTopics(), nil
+	return api.pubSub.GetTopics(), nil
 }
 
 func (api *PubSubAPI) Peers(ctx context.Context, opts ...caopts.PubSubPeersOption) ([]peer.ID, error) {
@@ -46,7 +47,7 @@ func (api *PubSubAPI) Peers(ctx context.Context, opts ...caopts.PubSubPeersOptio
 		return nil, err
 	}
 
-	peers := api.node.PubSub.ListPeers(settings.Topic)
+	peers := api.pubSub.ListPeers(settings.Topic)
 	out := make([]peer.ID, len(peers))
 
 	for i, peer := range peers {
@@ -61,7 +62,7 @@ func (api *PubSubAPI) Publish(ctx context.Context, topic string, data []byte) er
 		return err
 	}
 
-	return api.node.PubSub.Publish(topic, data)
+	return api.pubSub.Publish(topic, data)
 }
 
 func (api *PubSubAPI) Subscribe(ctx context.Context, topic string, opts ...caopts.PubSubSubscribeOption) (coreiface.PubSubSubscription, error) {
@@ -71,12 +72,12 @@ func (api *PubSubAPI) Subscribe(ctx context.Context, topic string, opts ...caopt
 		return nil, err
 	}
 
-	sub, err := api.node.PubSub.Subscribe(topic)
+	sub, err := api.pubSub.Subscribe(topic)
 	if err != nil {
 		return nil, err
 	}
 
-	pubctx, cancel := context.WithCancel(api.node.Context())
+	pubctx, cancel := context.WithCancel(api.nctx)
 
 	if options.Discover {
 		go func() {
@@ -86,18 +87,18 @@ func (api *PubSubAPI) Subscribe(ctx context.Context, topic string, opts ...caopt
 				return
 			}
 
-			connectToPubSubPeers(pubctx, api.node, blk.Path().Cid())
+			connectToPubSubPeers(pubctx, api.routing, api.peerHost, blk.Path().Cid())
 		}()
 	}
 
 	return &pubSubSubscription{cancel, sub}, nil
 }
 
-func connectToPubSubPeers(ctx context.Context, n *core.IpfsNode, cid cid.Cid) {
+func connectToPubSubPeers(ctx context.Context, r routing.IpfsRouting, ph p2phost.Host, cid cid.Cid) {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	provs := n.Routing.FindProvidersAsync(ctx, cid, 10)
+	provs := r.FindProvidersAsync(ctx, cid, 10)
 	var wg sync.WaitGroup
 	for p := range provs {
 		wg.Add(1)
@@ -105,7 +106,7 @@ func connectToPubSubPeers(ctx context.Context, n *core.IpfsNode, cid cid.Cid) {
 			defer wg.Done()
 			ctx, cancel := context.WithTimeout(ctx, time.Second*10)
 			defer cancel()
-			err := n.PeerHost.Connect(ctx, pi)
+			err := ph.Connect(ctx, pi)
 			if err != nil {
 				log.Info("pubsub discover: ", err)
 				return
@@ -118,11 +119,11 @@ func connectToPubSubPeers(ctx context.Context, n *core.IpfsNode, cid cid.Cid) {
 }
 
 func (api *PubSubAPI) checkNode() error {
-	if !api.node.OnlineMode() {
-		return coreiface.ErrOffline
+	if err := api.checkRouting(false); err != nil {
+		return err
 	}
 
-	if api.node.PubSub == nil {
+	if api.pubSub == nil {
 		return errors.New("experimental pubsub feature not enabled. Run daemon with --enable-pubsub-experiment to use.")
 	}
 

ipfs/kubo

core/coreapi/coreapi.go
@@ -15,25 +15,103 @@ package coreapi
 
 import (
 	"context"
+	"errors"
 
 	core "github.com/ipfs/go-ipfs/core"
 	coreiface "github.com/ipfs/go-ipfs/core/coreapi/interface"
+	options "github.com/ipfs/go-ipfs/core/coreapi/interface/options"
+	namesys "github.com/ipfs/go-ipfs/namesys"
+	pin "github.com/ipfs/go-ipfs/pin"
+	repo "github.com/ipfs/go-ipfs/repo"
 
+	ci "gx/ipfs/QmNiJiXwWE3kRhZrC5ej3kSjWHm337pYfhjLGSCDNKJP2s/go-libp2p-crypto"
+	exchange "gx/ipfs/QmP2g3VxmC7g7fyRJDj1VJ72KHZbJ9UW24YjSWEj1XTb4H/go-ipfs-exchange-interface"
+	bserv "gx/ipfs/QmPoh3SrQzFBWtdGK6qmHDV4EanKR6kYPj4DD3J2NLoEmZ/go-blockservice"
+	routing "gx/ipfs/QmRASJXJUFygM5qU4YrH7k7jD6S4Hg8nJmgqJ4bYJvLatd/go-libp2p-routing"
+	blockstore "gx/ipfs/QmS2aqUZLJp8kF1ihE5rvDGE5LvmKDPnx32w9Z1BW9xLV5/go-ipfs-blockstore"
+	peer "gx/ipfs/QmY5Grm8pJdiSSVsYxx4uNRgweY72EmYwuSDbRnbFok3iY/go-libp2p-peer"
+	pstore "gx/ipfs/QmZ9zH2FnLcxv1xyzFeUpDUeo55xEhZQHgveZijcxr7TLj/go-libp2p-peerstore"
+	pubsub "gx/ipfs/QmaqGyUhWLsJbVo1QAujSu13mxNjFJ98Kt2VWGSnShGE1Q/go-libp2p-pubsub"
 	ipld "gx/ipfs/QmcKKBwfz6FyQdHR2jsXrrF6XeSBXYL86anmWNewpFpoF5/go-ipld-format"
 	logging "gx/ipfs/QmcuXC5cxs79ro2cUuHs4HQ2bkDLJUYokwL8aivcX6HW3C/go-log"
 	dag "gx/ipfs/QmdV35UHnL1FM52baPkeUo6u7Fxm2CRUkPTLRPxeF8a4Ap/go-merkledag"
+	record "gx/ipfs/QmfARXVCzpwFXQdepAJZuqyNDgV9doEsMnVCo1ssmuSe1U/go-libp2p-record"
+	p2phost "gx/ipfs/QmfD51tKgJiTMnW9JEiDiPwsCY4mqUoxkhKhBfyW12spTC/go-libp2p-host"
 )
 
 var log = logging.Logger("core/coreapi")
 
 type CoreAPI struct {
-	node *core.IpfsNode
-	dag  ipld.DAGService
+	nctx context.Context
+
+	identity   peer.ID //TODO: check mutable structs
+	privateKey ci.PrivKey
+
+	repo       repo.Repo
+	blockstore blockstore.GCBlockstore
+	baseBlocks blockstore.Blockstore
+	blocks     bserv.BlockService
+	dag        ipld.DAGService
+	pinning    pin.Pinner
+
+	peerstore       pstore.Peerstore
+	peerHost        p2phost.Host
+	namesys         namesys.NameSystem
+	recordValidator record.Validator
+	exchange        exchange.Interface
+
+	routing routing.IpfsRouting
+	pubSub  *pubsub.PubSub
+
+	checkRouting func(bool) error
+
+	// TODO: this can be generalized to all functions when we implement some
+	// api based security mechanism
+	isPublishAllowed func() error
 }
 
 // NewCoreAPI creates new instance of IPFS CoreAPI backed by go-ipfs Node.
-func NewCoreAPI(n *core.IpfsNode) coreiface.CoreAPI {
-	api := &CoreAPI{n, n.DAG}
+func NewCoreAPI(n *core.IpfsNode, opts ...options.ApiOption) coreiface.CoreAPI {
+	api := &CoreAPI{
+		nctx: n.Context(),
+
+		identity:   n.Identity,
+		privateKey: n.PrivateKey,
+
+		repo:       n.Repo,
+		blockstore: n.Blockstore,
+		baseBlocks: n.BaseBlocks,
+		blocks:     n.Blocks,
+		dag:        n.DAG,
+		pinning:    n.Pinning,
+
+		peerstore:       n.Peerstore,
+		peerHost:        n.PeerHost,
+		namesys:         n.Namesys,
+		recordValidator: n.RecordValidator,
+		exchange:        n.Exchange,
+
+		routing: n.Routing,
+		pubSub:  n.PubSub,
+
+		checkRouting: func(allowOffline bool) error {
+			if !n.OnlineMode() {
+				if !allowOffline {
+					return coreiface.ErrOffline
+				}
+				return n.SetupOfflineRouting()
+			}
+			return nil
+		},
+
+		isPublishAllowed: func() error {
+			if n.Mounts.Ipns != nil && n.Mounts.Ipns.IsActive() {
+				return errors.New("cannot manually publish while IPNS is mounted")
+			}
+			return nil
+		},
+	}
+
 	return api
 }
 
@@ -89,6 +167,10 @@ func (api *CoreAPI) PubSub() coreiface.PubSubAPI {
 
 // getSession returns new api backed by the same node with a read-only session DAG
 func (api *CoreAPI) getSession(ctx context.Context) *CoreAPI {
-	ng := dag.NewReadOnlyDagService(dag.NewSession(ctx, api.dag))
-	return &CoreAPI{api.node, ng}
+	sesApi := *api
+
+	//TODO: we may want to apply this to other things too
+	sesApi.dag = dag.NewReadOnlyDagService(dag.NewSession(ctx, api.dag))
+
+	return &sesApi
 }

core/coreapi/pubsub.go
@@ -7,14 +7,15 @@ import (
 	"sync"
 	"time"
 
-	core "github.com/ipfs/go-ipfs/core"
 	coreiface "github.com/ipfs/go-ipfs/core/coreapi/interface"
 	caopts "github.com/ipfs/go-ipfs/core/coreapi/interface/options"
 
 	cid "gx/ipfs/QmR8BauakNcBa3RbE4nbQu76PDiJgoQgz8AJdhJuiU4TAw/go-cid"
+	routing "gx/ipfs/QmRASJXJUFygM5qU4YrH7k7jD6S4Hg8nJmgqJ4bYJvLatd/go-libp2p-routing"
 	peer "gx/ipfs/QmY5Grm8pJdiSSVsYxx4uNRgweY72EmYwuSDbRnbFok3iY/go-libp2p-peer"
 	pstore "gx/ipfs/QmZ9zH2FnLcxv1xyzFeUpDUeo55xEhZQHgveZijcxr7TLj/go-libp2p-peerstore"
 	pubsub "gx/ipfs/QmaqGyUhWLsJbVo1QAujSu13mxNjFJ98Kt2VWGSnShGE1Q/go-libp2p-pubsub"
+	p2phost "gx/ipfs/QmfD51tKgJiTMnW9JEiDiPwsCY4mqUoxkhKhBfyW12spTC/go-libp2p-host"
 )
 
 type PubSubAPI CoreAPI
@@ -33,7 +34,7 @@ func (api *PubSubAPI) Ls(ctx context.Context) ([]string, error) {
 		return nil, err
 	}
 
-	return api.node.PubSub.GetTopics(), nil
+	return api.pubSub.GetTopics(), nil
 }
 
 func (api *PubSubAPI) Peers(ctx context.Context, opts ...caopts.PubSubPeersOption) ([]peer.ID, error) {
@@ -46,7 +47,7 @@ func (api *PubSubAPI) Peers(ctx context.Context, opts ...caopts.PubSubPeersOptio
 		return nil, err
 	}
 
-	peers := api.node.PubSub.ListPeers(settings.Topic)
+	peers := api.pubSub.ListPeers(settings.Topic)
 	out := make([]peer.ID, len(peers))
 
 	for i, peer := range peers {
@@ -61,7 +62,7 @@ func (api *PubSubAPI) Publish(ctx context.Context, topic string, data []byte) er
 		return err
 	}
 
-	return api.node.PubSub.Publish(topic, data)
+	return api.pubSub.Publish(topic, data)
 }
 
 func (api *PubSubAPI) Subscribe(ctx context.Context, topic string, opts ...caopts.PubSubSubscribeOption) (coreiface.PubSubSubscription, error) {
@@ -71,12 +72,12 @@ func (api *PubSubAPI) Subscribe(ctx context.Context, topic string, opts ...caopt
 		return nil, err
 	}
 
-	sub, err := api.node.PubSub.Subscribe(topic)
+	sub, err := api.pubSub.Subscribe(topic)
 	if err != nil {
 		return nil, err
 	}
 
-	pubctx, cancel := context.WithCancel(api.node.Context())
+	pubctx, cancel := context.WithCancel(api.nctx)
 
 	if options.Discover {
 		go func() {
@@ -86,18 +87,18 @@ func (api *PubSubAPI) Subscribe(ctx context.Context, topic string, opts ...caopt
 				return
 			}
 
-			connectToPubSubPeers(pubctx, api.node, blk.Path().Cid())
+			connectToPubSubPeers(pubctx, api.routing, api.peerHost, blk.Path().Cid())
 		}()
 	}
 
 	return &pubSubSubscription{cancel, sub}, nil
 }
 
-func connectToPubSubPeers(ctx context.Context, n *core.IpfsNode, cid cid.Cid) {
+func connectToPubSubPeers(ctx context.Context, r routing.IpfsRouting, ph p2phost.Host, cid cid.Cid) {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	provs := n.Routing.FindProvidersAsync(ctx, cid, 10)
+	provs := r.FindProvidersAsync(ctx, cid, 10)
 	var wg sync.WaitGroup
 	for p := range provs {
 		wg.Add(1)
@@ -105,7 +106,7 @@ func connectToPubSubPeers(ctx context.Context, n *core.IpfsNode, cid cid.Cid) {
 			defer wg.Done()
 			ctx, cancel := context.WithTimeout(ctx, time.Second*10)
 			defer cancel()
-			err := n.PeerHost.Connect(ctx, pi)
+			err := ph.Connect(ctx, pi)
 			if err != nil {
 				log.Info("pubsub discover: ", err)
 				return
@@ -118,11 +119,11 @@ func connectToPubSubPeers(ctx context.Context, n *core.IpfsNode, cid cid.Cid) {
 }
 
 func (api *PubSubAPI) checkNode() error {
-	if !api.node.OnlineMode() {
-		return coreiface.ErrOffline
+	if err := api.checkRouting(false); err != nil {
+		return err
 	}
 
-	if api.node.PubSub == nil {
+	if api.pubSub == nil {
 		return errors.New("experimental pubsub feature not enabled. Run daemon with --enable-pubsub-experiment to use.")
 	}
 