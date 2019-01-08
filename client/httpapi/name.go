f638bae3a960a74d284503c30086f0bbffbe73c0
Łukasz Magiera <magik6k@gmail.com>
2019-01-08 02:09:17 +0100

    implement .Name
    This commit was moved from ipfs/go-ipfs-http-client@e19e5f54e48d0909796add6d57e3e74e84eba85e

--- client/httpapi/name.go
+++ client/httpapi/name.go
@@ -2,26 +2,80 @@ package httpapi
 
 import (
 	"context"
+	"fmt"
+	"github.com/ipfs/go-ipfs/namesys/opts"
 
 	"github.com/ipfs/go-ipfs/core/coreapi/interface"
-	"github.com/ipfs/go-ipfs/core/coreapi/interface/options"
+	caopts "github.com/ipfs/go-ipfs/core/coreapi/interface/options"
 )
 
 type NameAPI HttpApi
 
-func (api *NameAPI) Publish(ctx context.Context, p iface.Path, opts ...options.NamePublishOption) (iface.IpnsEntry, error) {
+type ipnsEntry struct {
+	JName  string `json:"Name"`
+	JValue string `json:"Value"`
+}
+
+func (e *ipnsEntry) valid() (iface.Path, error) {
+	return iface.ParsePath(e.JValue)
+}
+
+func (e *ipnsEntry) Name() string {
+	return e.JName
+}
+
+func (e *ipnsEntry) Value() iface.Path {
+	p, _ := e.valid()
+	return p
+}
+
+func (api *NameAPI) Publish(ctx context.Context, p iface.Path, opts ...caopts.NamePublishOption) (iface.IpnsEntry, error) {
+	options, err := caopts.NamePublishOptions(opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	req := api.core().request("name/publish", p.String()).
+		Option("key", options.Key).
+		Option("allow-offline", options.AllowOffline).
+		Option("lifetime", options.ValidTime.String()).
+		Option("resolve", false)
+
+	if options.TTL != nil {
+		req.Option("ttl", options.TTL.String())
+	}
+
+	var out ipnsEntry
+	if err := req.Exec(ctx, &out); err != nil {
+		return nil, err
+	}
+	if _, err := out.valid(); err != nil {
+		return nil, err
+	}
+
+	return &out, nil
+}
+
+func (api *NameAPI) Search(ctx context.Context, name string, opts ...caopts.NameResolveOption) (<-chan iface.IpnsResult, error) {
 	return nil, ErrNotImplemented
 }
 
-func (api *NameAPI) Search(ctx context.Context, name string, opts ...options.NameResolveOption) (<-chan iface.IpnsResult, error) {
-	return nil, ErrNotImplemented
-}
+func (api *NameAPI) Resolve(ctx context.Context, name string, opts ...caopts.NameResolveOption) (iface.Path, error) {
+	options, err := caopts.NameResolveOptions(opts...)
+	if err != nil {
+		return nil, err
+	}
 
-func (api *NameAPI) Resolve(ctx context.Context, name string, opts ...options.NameResolveOption) (iface.Path, error) {
-	// TODO: options!
+	ropts := nsopts.ProcessOpts(options.ResolveOpts)
+	if ropts.Depth != nsopts.DefaultDepthLimit && ropts.Depth != 1 {
+		return nil, fmt.Errorf("Name.Resolve: depth other than 1 or %d not supported", nsopts.DefaultDepthLimit)
+	}
 
-	req := api.core().request("name/resolve")
-	req.Arguments(name)
+	req := api.core().request("name/resolve", name).
+		Option("nocache", !options.Cache).
+		Option("recursive", ropts.Depth != 1).
+		Option("dht-record-count", ropts.DhtRecordCount).
+		Option("dht-timeout", ropts.DhtTimeout.String())
 
 	var out struct{ Path string }
 	if err := req.Exec(ctx, &out); err != nil {
