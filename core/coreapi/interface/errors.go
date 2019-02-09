65579081d98c56ce11932cdbe1492925dbaee36d
Łukasz Magiera <magik6k@gmail.com>
2019-02-09 01:07:46 +0100

    coreapi: extract interface
    License: MIT
    Signed-off-by: Łukasz Magiera <magik6k@gmail.com>

--- core/coreapi/interface/errors.go
+++
@@ -1,9 +0,0 @@
-package iface
-
-import "errors"
-
-var (
-	ErrIsDir   = errors.New("this dag node is a directory")
-	ErrNotFile = errors.New("this dag node is not a regular file")
-	ErrOffline = errors.New("this action must be run in online mode, try running 'ipfs daemon' first")
-)
