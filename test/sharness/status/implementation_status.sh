4259bb800c14356ff96048239baff37a373e5011
Christian Couder <chriscool@tuxfamily.org>
2019-03-04 20:14:55 +0100

    markdown_status: move code to implementation_status.sh
    License: MIT
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- test/sharness/status/implementation_status.sh
+++ test/sharness/status/implementation_status.sh
@@ -69,4 +69,59 @@ do
 		die "could not substitute '$tst' in '$TMP_STATUS'"
 done < "$PARSED_DOT_PROVE"
 
-cat "$TMP_STATUS"
+log "Reduce number of ipfs lines"
+TMP_TXT_STATUS_1="$TMPDIR/text_impl_status_1"
+cp "$TMP_STATUS" "$TMP_TXT_STATUS_1" ||
+	die "could not cp '$TMP_STATUS' into '$TMP_TXT_STATUS_1'"
+perl -pi -e 'BEGIN{undef $/;} s/ipfs (\w+) ([\w-]+ )*--([\w-]+)\nipfs \w+ ([\w-]+ )*-(\w)/ipfs $1 $2-$5|--$3/smg' "$TMP_TXT_STATUS_1" ||
+	die "could not reduce number of ipfs lines in '$TMP_TXT_STATUS_1'"
+TMP_TXT_STATUS_2="$TMPDIR/text_impl_status_2"
+cp "$TMP_TXT_STATUS_1" "$TMP_TXT_STATUS_2" ||
+	die "could not cp '$TMP_TXT_STATUS_1' into '$TMP_TXT_STATUS_2'"
+perl -pi -e 'BEGIN{undef $/;} s/ipfs (\w+) ([\w-]+ )*-(\w)\nipfs \w+ ([\w-]+ )*-([\w-]+)/ipfs $1 $2-$3|--$5/smg' "$TMP_TXT_STATUS_2" ||
+	die "could not reduce number of ipfs lines in '$TMP_TXT_STATUS_2'"
+TMP_TXT_STATUS_3="$TMPDIR/text_impl_status_3"
+cp "$TMP_TXT_STATUS_2" "$TMP_TXT_STATUS_3" ||
+	die "could not cp '$TMP_TXT_STATUS_2' into '$TMP_TXT_STATUS_3'"
+perl -pi -e 'BEGIN{undef $/;} s/ipfs (\w+) ([\w-]+ )*--([\w-]+)\nipfs \w+ ([\w-]+ )*--([\w-]+)/ipfs $1 $2-$5|--$3/smg' "$TMP_TXT_STATUS_3" ||
+	die "could not reduce number of ipfs lines in '$TMP_TXT_STATUS_3'"
+
+log "Reduce number of test lines"
+TMP_TXT_STATUS_4="$TMPDIR/text_impl_status_4"
+perl -ne '
+
+BEGIN {
+	sub print_tests {
+		if (%t) {
+			foreach my $k (sort keys %t) {
+				$sum = 0;
+				map { $sum += $_; } @{$n{$k}};
+				print "$k: " . $sum . " ";
+			}
+			print "\n";
+			%t = ();
+			%n = ();
+		} else {
+			print "???\n";
+		}
+	}
+}
+
+if (m/^\s+((\d+) ((GOOD|BAD):)?(t\d+))$/) {
+	push(@{$n{$4 ? $4 : "XXX"}}, $2);
+	push(@{$t{$4 ? $4 : "XXX"}}, $5);
+	$started = 1;
+} elsif (m/^\s*$/ and $started) {
+	print_tests();
+	print;
+} else {
+	print unless (m/^\s*$/);
+}
+
+END {
+	print_tests();
+}
+
+' "$TMP_TXT_STATUS_3"  > "$TMP_TXT_STATUS_4"
+
+cat "$TMP_TXT_STATUS_4"
--- test/sharness/status/markdown_status.sh
+++ test/sharness/status/markdown_status.sh
@@ -34,67 +34,9 @@ TXT_STATUS='results/status.txt'
 test -f "$TXT_STATUS" ||
 	die "could not find '$TXT_STATUS'"
 
-log "Reduce number of ipfs lines"
-TMP_MD_STATUS_1="$TMPDIR/markdown_impl_status_1"
-cp "$TXT_STATUS" "$TMP_MD_STATUS_1" ||
-	die "could not cp '$TXT_STATUS' into '$TMP_MD_STATUS_1'"
-perl -pi -e 'BEGIN{undef $/;} s/ipfs (\w+) ([\w-]+ )*--([\w-]+)\nipfs \w+ ([\w-]+ )*-(\w)/ipfs $1 $2-$5|--$3/smg' "$TMP_MD_STATUS_1" ||
-	die "could not reduce number of ipfs lines in '$TMP_MD_STATUS_1'"
-TMP_MD_STATUS_2="$TMPDIR/markdown_impl_status_2"
-cp "$TMP_MD_STATUS_1" "$TMP_MD_STATUS_2" ||
-	die "could not cp '$TMP_MD_STATUS_1' into '$TMP_MD_STATUS_2'"
-perl -pi -e 'BEGIN{undef $/;} s/ipfs (\w+) ([\w-]+ )*-(\w)\nipfs \w+ ([\w-]+ )*-([\w-]+)/ipfs $1 $2-$3|--$5/smg' "$TMP_MD_STATUS_2" ||
-	die "could not reduce number of ipfs lines in '$TMP_MD_STATUS_2'"
-TMP_MD_STATUS_3="$TMPDIR/markdown_impl_status_3"
-cp "$TMP_MD_STATUS_2" "$TMP_MD_STATUS_3" ||
-	die "could not cp '$TMP_MD_STATUS_2' into '$TMP_MD_STATUS_3'"
-perl -pi -e 'BEGIN{undef $/;} s/ipfs (\w+) ([\w-]+ )*--([\w-]+)\nipfs \w+ ([\w-]+ )*--([\w-]+)/ipfs $1 $2-$5|--$3/smg' "$TMP_MD_STATUS_3" ||
-	die "could not reduce number of ipfs lines in '$TMP_MD_STATUS_3'"
-
-#cat "$TMP_MD_STATUS_3"
-#exit
-
-log "Reduce number of test lines"
-TMP_MD_STATUS_4="$TMPDIR/markdown_impl_status_4"
-perl -ne '
-
-BEGIN {
-	sub print_tests {
-		if (%t) {
-			foreach my $k (sort keys %t) {
-				$sum = 0;
-				map { $sum += $_; } @{$n{$k}};
-				print "$k: " . $sum . " ";
-			}
-			print "\n";
-			%t = ();
-			%n = ();
-		} else {
-			print "???\n";
-		}
-	}
-}
-
-if (m/^\s+((\d+) ((GOOD|BAD):)?(t\d+))$/) {
-	push(@{$n{$4 ? $4 : "XXX"}}, $2);
-	push(@{$t{$4 ? $4 : "XXX"}}, $5);
-	$started = 1;
-} elsif (m/^\s*$/ and $started) {
-	print_tests();
-	print;
-} else {
-	print unless (m/^\s*$/);
-}
-
-END {
-	print_tests();
-}
-
-' "$TMP_MD_STATUS_3"  > "$TMP_MD_STATUS_4"
-
 log "Generate markdown impl status"
-TMP_MD_STATUS_5="$TMPDIR/markdown_impl_status_5"
-cat <<EOF >"$TMP_MD_STATUS_5"
+MD_STATUS="$TMPDIR/markdown_impl_status"
+cat <<EOF >"$MD_STATUS"
 # IPFS Implementation Status
 
 > Legend: :green_apple: Done &nbsp; :lemon: In Progress &nbsp; :tomato: Missing &nbsp; :chestnut: Not planned
@@ -117,9 +59,9 @@ do
 		#echo "empty line: $line"
 		printf "\n"
 	fi
-done <"$TMP_MD_STATUS_4" >>"$TMP_MD_STATUS_5"
+done <"$TXT_STATUS" >>"$MD_STATUS"
 
-printf "\n" >>"$TMP_MD_STATUS_5"
+printf "\n" >>"$MD_STATUS"
 
 # Output
-cat "$TMP_MD_STATUS_5"
+cat "$MD_STATUS"
