88030561c784ee4c2c05afea4e57e7f64560d0b0
Jakub Sztandera <kubuxu@protonmail.ch>
2019-03-04 22:26:07 +0100

    gomod: fix import path
    License: MIT
    Signed-off-by: Jakub Sztandera <kubuxu@protonmail.ch>

--- test/dependencies/dependencies.go
+++ test/dependencies/dependencies.go
@@ -3,11 +3,10 @@
 package tools
 
 import (
-	_ "gotest.tools/gotestsum"
-	_ "github.com/multiformats/go-multihash/multihash"
-	_ "github.com/jbenet/go-random-files/random-files"
-	_ "github.com/ipfs/hang-fds"
 	_ "github.com/ipfs/go-cidutil/cid-fmt"
-	_ "go-build,github.com/jbenet/go-random/random"
+	_ "github.com/ipfs/hang-fds"
+	_ "github.com/jbenet/go-random-files/random-files"
+	_ "github.com/jbenet/go-random/random"
+	_ "github.com/multiformats/go-multihash/multihash"
+	_ "gotest.tools/gotestsum"
 )
-
