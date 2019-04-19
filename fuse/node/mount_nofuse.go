a3bdfd68de2b7307c25915566f1a0a35c3cf294a
Steven Allen <steven@stebalien.com>
2019-04-18 18:10:29 -0700

    fix macos build when fuse is enabled
    Instead of trying to export the `errNeedFuseVersion` type, just use string
    matching. It's good enough for testing and the alternative was annoying.
    
    License: MIT
    Signed-off-by: Steven Allen <steven@stebalien.com>

--- fuse/node/mount_nofuse.go
+++ fuse/node/mount_nofuse.go
@@ -8,8 +8,6 @@ import (
 	core "github.com/ipfs/go-ipfs/core"
 )
 
-type errNeedFuseVersion error // used in tests, needed in OSX
-
 func Mount(node *core.IpfsNode, fsdir, nsdir string) error {
 	return errors.New("not compiled in")
 }
