b57956c5fb53f49e18c968291e16feee29892048
Steven Allen <steven@stebalien.com>
2019-05-01 15:21:29 -0700

    core: fix autonat construction
    License: MIT
    Signed-off-by: Steven Allen <steven@stebalien.com>

--- core/node/helpers.go
+++ core/node/helpers.go
@@ -43,6 +43,13 @@ func maybeProvide(opt interface{}, enable bool) fx.Option {
 	return fx.Options()
 }
 
+func maybeInvoke(opt interface{}, enable bool) fx.Option {
+	if enable {
+		return fx.Invoke(opt)
+	}
+	return fx.Options()
+}
+
 // baseProcess creates a goprocess which is closed when the lifecycle signals it to stop
 func baseProcess(lc fx.Lifecycle) goprocess.Process {
 	p := goprocess.WithParent(goprocess.Background())
