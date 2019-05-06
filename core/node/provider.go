55676a0b86193aa71e91ad3cedeef1d3863df0d1
Michael Avila <davidmichaelavila@gmail.com>
2019-05-06 11:20:43 -0700

    Remove Ctor from names
    License: MIT
    Signed-off-by: Michael Avila <davidmichaelavila@gmail.com>

--- core/node/provider.go
+++ core/node/provider.go
@@ -24,20 +24,20 @@ func ProviderQueue(mctx helpers.MetricsCtx, lc fx.Lifecycle, repo repo.Repo) (*q
 	return q.NewQueue(helpers.LifecycleCtx(mctx, lc), "provider-v1", repo.Datastore())
 }
 
-// SimpleProviderCtor creates new record provider
-func SimpleProviderCtor(mctx helpers.MetricsCtx, lc fx.Lifecycle, queue *q.Queue, rt routing.IpfsRouting) provider.Provider {
+// SimpleProvider creates new record provider
+func SimpleProvider(mctx helpers.MetricsCtx, lc fx.Lifecycle, queue *q.Queue, rt routing.IpfsRouting) provider.Provider {
 	return simple.NewProvider(helpers.LifecycleCtx(mctx, lc), queue, rt)
 }
 
-// SimpleReproviderCtor creates new reprovider
-func SimpleReproviderCtor(reproviderInterval time.Duration) func(helpers.MetricsCtx, fx.Lifecycle, routing.IpfsRouting, simple.KeyChanFunc) (provider.Reprovider, error) {
+// SimpleReprovider creates new reprovider
+func SimpleReprovider(reproviderInterval time.Duration) func(helpers.MetricsCtx, fx.Lifecycle, routing.IpfsRouting, simple.KeyChanFunc) (provider.Reprovider, error) {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, rt routing.IpfsRouting, keyProvider simple.KeyChanFunc) (provider.Reprovider, error) {
 		return simple.NewReprovider(helpers.LifecycleCtx(mctx, lc), reproviderInterval, rt, keyProvider), nil
 	}
 }
 
-// SimpleProviderSysCtor creates new provider system
-func SimpleProviderSysCtor(lc fx.Lifecycle, p provider.Provider, r provider.Reprovider) provider.System {
+// SimpleProviderSys creates new provider system
+func SimpleProviderSys(lc fx.Lifecycle, p provider.Provider, r provider.Reprovider) provider.System {
 	sys := provider.NewSystem(p, r)
 	lc.Append(fx.Hook{
 		OnStart: func(ctx context.Context) error {
@@ -51,20 +51,20 @@ func SimpleProviderSysCtor(lc fx.Lifecycle, p provider.Provider, r provider.Repr
 	return sys
 }
 
-// SimpleOfflineProviderSysCtor creates a new offline provider system
-func SimpleOfflineProviderSysCtor(p provider.Provider, r provider.Reprovider) provider.System {
+// SimpleOfflineProviderSys creates a new offline provider system
+func SimpleOfflineProviderSys(p provider.Provider, r provider.Reprovider) provider.System {
 	return provider.NewSystem(p, r)
 }
 
 // STRATEGIC
 
-// StrategicProviderSysCtor creates new provider system
-func StrategicProviderSysCtor() provider.System {
+// StrategicProviderSys creates new provider system
+func StrategicProviderSys() provider.System {
 	return provider.NewOfflineProvider()
 }
 
-// StrategicOfflineProviderSysCtor creates a new offline provider system
-func StrategicOfflineProviderSysCtor() provider.System {
+// StrategicOfflineProviderSys creates a new offline provider system
+func StrategicOfflineProviderSys() provider.System {
 	return provider.NewOfflineProvider()
 }
 
@@ -73,24 +73,24 @@ func StrategicOfflineProviderSysCtor() provider.System {
 // OnlineProviders groups units managing provider routing records online
 func OnlineProviders(useStrategicProviding bool, reprovideStrategy string, reprovideInterval string) fx.Option {
 	if useStrategicProviding {
-		return fx.Provide(StrategicProviderSysCtor)
+		return fx.Provide(StrategicProviderSys)
 	}
 
 	return fx.Options(
 		SimpleProviders(reprovideStrategy, reprovideInterval),
-		fx.Provide(SimpleProviderSysCtor),
+		fx.Provide(SimpleProviderSys),
 	)
 }
 
 // OfflineProviders groups units managing provider routing records offline
 func OfflineProviders(useStrategicProviding bool, reprovideStrategy string, reprovideInterval string) fx.Option {
 	if useStrategicProviding {
-		return fx.Provide(StrategicOfflineProviderSysCtor)
+		return fx.Provide(StrategicOfflineProviderSys)
 	}
 
 	return fx.Options(
 		SimpleProviders(reprovideStrategy, reprovideInterval),
-		fx.Provide(SimpleOfflineProviderSysCtor),
+		fx.Provide(SimpleOfflineProviderSys),
 	)
 }
 
@@ -122,8 +122,8 @@ func SimpleProviders(reprovideStrategy string, reprovideInterval string) fx.Opti
 
 	return fx.Options(
 		fx.Provide(ProviderQueue),
-		fx.Provide(SimpleProviderCtor),
+		fx.Provide(SimpleProvider),
 		keyProvider,
-		fx.Provide(SimpleReproviderCtor(reproviderInterval)),
+		fx.Provide(SimpleReprovider(reproviderInterval)),
 	)
 }
