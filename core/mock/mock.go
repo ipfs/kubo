854d45f3ae99c097dc8c993d1efb5ca62562cf2f
Łukasz Magiera <magik6k@gmail.com>
2019-05-20 16:06:33 +0200

    core/mock: remove unused code

--- core/mock/mock.go
+++ core/mock/mock.go
@@ -4,19 +4,13 @@ import (
 	"context"
 	libp2p2 "github.com/ipfs/go-ipfs/core/node/libp2p"
 
-	"github.com/ipfs/go-ipfs/commands"
 	"github.com/ipfs/go-ipfs/core"
-	"github.com/ipfs/go-ipfs/repo"
 
-	"github.com/ipfs/go-datastore"
-	syncds "github.com/ipfs/go-datastore/sync"
-	config "github.com/ipfs/go-ipfs-config"
 	"github.com/libp2p/go-libp2p"
 	host "github.com/libp2p/go-libp2p-host"
 	peer "github.com/libp2p/go-libp2p-peer"
 	pstore "github.com/libp2p/go-libp2p-peerstore"
 	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
-	"github.com/libp2p/go-testutil"
 )
 
 // NewMockNode constructs an IpfsNode for use in tests.
@@ -34,41 +28,4 @@ func MockHostOption(mn mocknet.Mocknet) libp2p2.HostOption {
 	return func(ctx context.Context, id peer.ID, ps pstore.Peerstore, _ ...libp2p.Option) (host.Host, error) {
 		return mn.AddPeerWithPeerstore(id, ps)
 	}
-}
-
-func MockCmdsCtx() (commands.Context, error) {
-	// Generate Identity
-	ident, err := testutil.RandIdentity()
-	if err != nil {
-		return commands.Context{}, err
-	}
-	p := ident.ID()
-
-	conf := config.Config{
-		Identity: config.Identity{
-			PeerID: p.String(),
-		},
-	}
-
-	r := &repo.Mock{
-		D: syncds.MutexWrap(datastore.NewMapDatastore()),
-		C: conf,
-	}
-
-	node, err := core.NewNode(context.Background(), &core.BuildCfg{
-		Repo: r,
-	})
-	if err != nil {
-		return commands.Context{}, err
-	}
-
-	return commands.Context{
-		RepoPath: "/tmp/.mockipfsconfig",
-		LoadConfig: func(path string) (*config.Config, error) {
-			return &conf, nil
-		},
-		ConstructNode: func() (*core.IpfsNode, error) {
-			return node, nil
-		},
-	}, nil
 }
