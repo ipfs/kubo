1f4d8c7243039855f5c62f043168815d057777f2
Raúl Kripalani <raul@protocol.ai>
2019-05-28 17:09:34 +0100

    migrate to go-libp2p-core.
    This commit was moved from ipfs/interface-go-ipfs-core@2cc0c497f2b0b14a4ac22989a3861c1a2b0038c4
    
    This commit was moved from ipfs/boxo@1f2b5ca04f4cb9d5fe9b11d4c58edb91bab8b2a1

--- core/coreiface/dht.go
+++ core/coreiface/dht.go
@@ -6,8 +6,7 @@ import (
 
 	"github.com/ipfs/interface-go-ipfs-core/options"
 
-	"github.com/libp2p/go-libp2p-peer"
-	pstore "github.com/libp2p/go-libp2p-peerstore"
+	"github.com/libp2p/go-libp2p-core/peer"
 )
 
 // DhtAPI specifies the interface to the DHT
@@ -16,11 +15,11 @@ import (
 type DhtAPI interface {
 	// FindPeer queries the DHT for all of the multiaddresses associated with a
 	// Peer ID
-	FindPeer(context.Context, peer.ID) (pstore.PeerInfo, error)
+	FindPeer(context.Context, peer.ID) (peer.AddrInfo, error)
 
 	// FindProviders finds peers in the DHT who can provide a specific value
 	// given a key.
-	FindProviders(context.Context, path.Path, ...options.DhtFindProvidersOption) (<-chan pstore.PeerInfo, error)
+	FindProviders(context.Context, path.Path, ...options.DhtFindProvidersOption) (<-chan peer.AddrInfo, error)
 
 	// Provide announces to the network that you are providing given values
 	Provide(context.Context, path.Path, ...options.DhtProvideOption) error
