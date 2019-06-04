d633cd24d1fad6ef55be18946e6267ed6ef5f53c
Łukasz Magiera <magik6k@gmail.com>
2019-06-04 17:09:18 +0200

    core: Remove old constructor
    License: MIT
    Signed-off-by: Łukasz Magiera <magik6k@gmail.com>

--- core/node/helpers.go
+++ core/node/helpers.go
@@ -35,28 +35,3 @@ func (lp *lcProcess) Append(f goprocess.ProcessFunc) {
 		},
 	})
 }
-
-func maybeProvide(opt interface{}, enable bool) fx.Option {
-	if enable {
-		return fx.Provide(opt)
-	}
-	return fx.Options()
-}
-
-func maybeInvoke(opt interface{}, enable bool) fx.Option {
-	if enable {
-		return fx.Invoke(opt)
-	}
-	return fx.Options()
-}
-
-// baseProcess creates a goprocess which is closed when the lifecycle signals it to stop
-func baseProcess(lc fx.Lifecycle) goprocess.Process {
-	p := goprocess.WithParent(goprocess.Background())
-	lc.Append(fx.Hook{
-		OnStop: func(_ context.Context) error {
-			return p.Close()
-		},
-	})
-	return p
-}
