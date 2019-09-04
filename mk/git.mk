aba6c3d2f8aba08584d90866f106f4f98d324e26
Steven Allen <steven@stebalien.com>
2019-09-04 13:10:31 -0700

    build: fix build when we don't have a full git tree
    When building with docker, we don't have a _full_ git repo.

--- mk/git.mk
+++ mk/git.mk
@@ -1,2 +1,4 @@
-git-hash:=$(shell git describe --always --match=NeVeRmAtCh --dirty 2>/dev/null)
-
+# First try to "describe" the state. This tells us if the state is dirty.
+# If that fails (e.g., we're building a docker image and have an empty objects
+# directory), assume the source isn't dirty and build anyways.
+git-hash:=$(shell git describe --always --match=NeVeRmAtCh --dirty 2>/dev/null || git rev-parse --short HEAD 2>/dev/null)
