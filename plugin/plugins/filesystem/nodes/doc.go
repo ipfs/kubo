3ee383a375b17072183df728100b82e032bd0461
Dominic Della Valle <ddvpublic@gmail.com>
2019-09-09 18:42:41 -0400

    doc: add a table of the root index mapping

--- plugin/plugins/filesystem/nodes/doc.go
+++ plugin/plugins/filesystem/nodes/doc.go
@@ -4,6 +4,12 @@ file systems implementations and wrappers.
 
 The default RootIndex provided by RootAttacher, is a file system of itself
 which relays request to various IPFS subsystems.
-Utilizing the subsystem implementations itself, in the same way a client program would.
+Attaching to and utilizing P9.File implementations themselves, in the same way a client program could use them independently.
+
+The current mapping looks like this:
+ - mountpoint - associated filesystem
+ - /          - points back to itself, returning the root index
+ - /ipfs      - PinFS
+ - /ipfs/*    - IPFS
 */
 package fsnodes
