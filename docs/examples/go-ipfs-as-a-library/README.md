be6140e46b2ec4546a78885a8869a891977bf3c7
David Dias <daviddias.p@gmail.com>
2019-10-17 10:36:18 +0200

    readme notes

--- docs/examples/go-ipfs-as-a-library/README.md
+++ docs/examples/go-ipfs-as-a-library/README.md
@@ -2,4 +2,27 @@
 
 > This tutorial is the sister of the [js-ipfs IPFS 101 tutorial](https://github.com/ipfs/js-ipfs/tree/master/examples/ipfs-101).
 
-In this tutorial, we go through spawning an IPFS node, adding a file and cat'ing that file CID that is found locally and then another one that exists on the IPFS network.
+By the end of this Tutorial, you will learn how to:
+
+- Spawn an IPFS node that runs in process (no separate daemon process)
+- How to create an IPFS Repo
+- How to add files & directories to IPFS
+- How to retrieve those files and directories using cat and get
+- How to connect to other nodes in the Network
+- How to retrieve a file that only exists on the Network
+- The difference between a node in DHT Client mode and Full DHT mode.
+
+All of this using only golang!
+
+You will need:
+- golang installed on your machine. See how at https://golang.org/doc/install
+- git installed on your machine so that go can download the repo and the necessary dependencies. See how at https://git-scm.com/downloads
+
+## Getting started
+
+Download go-ipfs and jump into the example folder
+
+```
+> go get -u github.com/ipfs/go-ipfs
+cd $GOPATH/src/github.com/ipfs/go-ipfs/docs/examples/go-ipfs-as-a-library
+```
--- docs/examples/go-ipfs-as-a-library/main.go
+++ docs/examples/go-ipfs-as-a-library/main.go
@@ -194,12 +194,12 @@ func main() {
 	bootstrapNodes := []string{
 		"/ip4/104.131.131.82/tcp/4001/ipfs/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ",
 		"/ip4/104.236.179.241/tcp/4001/ipfs/QmSoLPppuBtQSGwKDZT2M73ULpjvfd3aZ6ha4oFGL1KrGM",
-		"/ip4/128.199.219.111/tcp/4001/ipfs/QmSoLSafTMBsPKadTEgaXctDQVcqN88CNLHXMkTNwMKPnu",
-		"/ip4/104.236.76.40/tcp/4001/ipfs/QmSoLV4Bbm51jM9C4gDYZQ9Cy3U6aXMJDAbzgu2fzaDs64",
-		"/ip4/178.62.158.247/tcp/4001/ipfs/QmSoLer265NRgSp2LA3dPaeykiS1J6DifTC88f5uVQKNAd",
 		"/ip6/2604:a880:1:20::203:d001/tcp/4001/ipfs/QmSoLPppuBtQSGwKDZT2M73ULpjvfd3aZ6ha4oFGL1KrGM",
+		"/ip4/128.199.219.111/tcp/4001/ipfs/QmSoLSafTMBsPKadTEgaXctDQVcqN88CNLHXMkTNwMKPnu",
 		"/ip6/2400:6180:0:d0::151:6001/tcp/4001/ipfs/QmSoLSafTMBsPKadTEgaXctDQVcqN88CNLHXMkTNwMKPnu",
+		"/ip4/104.236.76.40/tcp/4001/ipfs/QmSoLV4Bbm51jM9C4gDYZQ9Cy3U6aXMJDAbzgu2fzaDs64",
 		"/ip6/2604:a880:800:10::4a:5001/tcp/4001/ipfs/QmSoLV4Bbm51jM9C4gDYZQ9Cy3U6aXMJDAbzgu2fzaDs64",
+		"/ip4/178.62.158.247/tcp/4001/ipfs/QmSoLer265NRgSp2LA3dPaeykiS1J6DifTC88f5uVQKNAd",
 		"/ip6/2a03:b0c0:0:1010::23:1001/tcp/4001/ipfs/QmSoLer265NRgSp2LA3dPaeykiS1J6DifTC88f5uVQKNAd",
 
 		// Disabling dnsaddrs until https://github.com/ipfs/go-ipfs/issues/6714 is resolved
