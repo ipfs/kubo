a596c970c9163fe1bffad4d1a45ccee9efc189e5
Henrique Dias <hacdias@gmail.com>
2019-12-11 17:25:46 +0100

    Update main.go

--- docs/examples/go-ipfs-as-a-library/main.go
+++ docs/examples/go-ipfs-as-a-library/main.go
@@ -57,6 +57,10 @@ func createTempRepo(ctx context.Context) (string, error) {
 	if err != nil {
 		return "", err
 	}
+	
+	// Add your custom configuration to the repository config
+	// For example, you can enable sharding with:
+	// cfg.Experimental.ShardingEnabled = true
 
 	// Create the repo with the config
 	err = fsrepo.Init(repoPath, cfg)
