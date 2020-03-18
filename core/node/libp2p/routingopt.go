70fe64581d63375512533965b2d2af61263296d7
Steven Allen <steven@stebalien.com>
2020-03-18 11:14:55 -0700

    feat(dht): update to cypress DHT in backwards compatibility mode
    This upgrades to the next DHT version, but in backwards compatibility mode.

--- core/node/libp2p/routingopt.go
+++ core/node/libp2p/routingopt.go
@@ -8,26 +8,42 @@ import (
 	host "github.com/libp2p/go-libp2p-core/host"
 	routing "github.com/libp2p/go-libp2p-core/routing"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
-	dhtopts "github.com/libp2p/go-libp2p-kad-dht/opts"
 	record "github.com/libp2p/go-libp2p-record"
 )
+
+var baseDhtOptions = []dht.Option{
+	dht.Concurrency(10),
+	// TODO: Enable this once we have the dual-DHTs. At the moment, this
+	// will break DHTs on private IP addresses (VPNs, etc.).
+	//dht.RoutingTableFilter(dht.PublicRoutingTableFilter),
+	//dht.QueryFilter(dht.PublicQueryFilter),
+}
 
 type RoutingOption func(context.Context, host.Host, datastore.Batching, record.Validator) (routing.Routing, error)
 
 func constructDHTRouting(ctx context.Context, host host.Host, dstore datastore.Batching, validator record.Validator) (routing.Routing, error) {
 	return dht.New(
 		ctx, host,
-		dhtopts.Datastore(dstore),
-		dhtopts.Validator(validator),
+		append([]dht.Option{
+			// TODO: switch to "auto" when we add dual-dht support.
+			// Unfortunately, we can't set this to auto yet or
+			// nobody will become a server when everyone is running
+			// on a private network.
+			dht.Mode(dht.ModeServer),
+			dht.Datastore(dstore),
+			dht.Validator(validator),
+		}, baseDhtOptions...)...,
 	)
 }
 
 func constructClientDHTRouting(ctx context.Context, host host.Host, dstore datastore.Batching, validator record.Validator) (routing.Routing, error) {
 	return dht.New(
 		ctx, host,
-		dhtopts.Client(true),
-		dhtopts.Datastore(dstore),
-		dhtopts.Validator(validator),
+		append([]dht.Option{
+			dht.Mode(dht.ModeClient),
+			dht.Datastore(dstore),
+			dht.Validator(validator),
+		}, baseDhtOptions...)...,
 	)
 }
 
--- go.mod
+++ go.mod
@@ -65,8 +65,8 @@ require (
 	github.com/libp2p/go-libp2p-core v0.5.1
 	github.com/libp2p/go-libp2p-discovery v0.3.0
 	github.com/libp2p/go-libp2p-http v0.1.5
-	github.com/libp2p/go-libp2p-kad-dht v0.5.2
-	github.com/libp2p/go-libp2p-kbucket v0.2.3
+	github.com/libp2p/go-libp2p-kad-dht v0.6.1
+	github.com/libp2p/go-libp2p-kbucket v0.3.3
 	github.com/libp2p/go-libp2p-loggables v0.1.0
 	github.com/libp2p/go-libp2p-mplex v0.2.3
 	github.com/libp2p/go-libp2p-peerstore v0.2.3
@@ -74,7 +74,7 @@ require (
 	github.com/libp2p/go-libp2p-pubsub-router v0.2.1
 	github.com/libp2p/go-libp2p-quic-transport v0.2.4-0.20200228032821-6d8b0e775c52
 	github.com/libp2p/go-libp2p-record v0.1.2
-	github.com/libp2p/go-libp2p-routing-helpers v0.1.0
+	github.com/libp2p/go-libp2p-routing-helpers v0.2.0
 	github.com/libp2p/go-libp2p-secio v0.2.2
 	github.com/libp2p/go-libp2p-swarm v0.2.3
 	github.com/libp2p/go-libp2p-testing v0.1.1
