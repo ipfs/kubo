46935e58b49fc3915508e555311ee29c99e5b139
Peter Rabbitson <ribasushi@protocol.ai>
2020-03-28 10:23:54 +0100

    Fix test emitting garbage on STDIN, breaking JUNIT parsing
    I have no idea how this same test worked on the export branch...

--- test/sharness/t0054-dag-car-import-export.sh
+++ test/sharness/t0054-dag-car-import-export.sh
@@ -122,7 +122,7 @@ test_expect_success "basic offline export of 'getting started' dag works" '
 
 echo "Error: merkledag: not found (currently offline, perhaps retry after attaching to the network)" > offline_fetch_error_expected
 test_expect_success "basic offline export of nonexistent cid" '
-  ! ipfs dag export QmYwAPJXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX 2> offline_fetch_error_actual
+  ! ipfs dag export QmYwAPJXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX 2> offline_fetch_error_actual >/dev/null
 '
 test_expect_success "correct error" '
   test_cmp offline_fetch_error_expected offline_fetch_error_actual
