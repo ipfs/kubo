7b9225113ff63e4fba08c81c6372a52566181914
Steven Allen <steven@stebalien.com>
2020-04-06 17:32:07 -0700

    go-ipfs-config: doc: add a bit of documentation.

--- config/routing.go
+++ config/routing.go
@@ -3,9 +3,11 @@ package config
 // Routing defines configuration options for libp2p routing
 type Routing struct {
 	// Type sets default daemon routing mode.
+	//
+	// Can be one of "dht", "dhtclient", "dhtserver", "none", or unset.
 	Type string
 
-	// PrivateType sets the routing mode for private networks. If unset,
-	// Type will be used.
+	// PrivateType sets the routing mode for private networks. Can take the
+	// same values as Type and defaults to Type if unset.
 	PrivateType string
 }
