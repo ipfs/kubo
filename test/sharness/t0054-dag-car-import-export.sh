edc27793fb746b4a1c1a9f5e475838b2509bce27
Peter Rabbitson <ribasushi@protocol.ai>
2020-04-10 14:46:17 +0200

    Extra cross-iptb-network testcases

--- test/sharness/t0054-dag-car-import-export.sh
+++ test/sharness/t0054-dag-car-import-export.sh
@@ -9,6 +9,7 @@ export -f ipfsi
 set -o pipefail
 
 tar -C ../t0054-dag-car-import-export-data/ --strip-components=1 -Jxf ../t0054-dag-car-import-export-data/test_dataset_car_v0.tar.xz
+tab=$'\t'
 
 reset_blockstore() {
   node=$1
@@ -43,9 +44,17 @@ run_online_imp_exp_tests() {
   reset_blockstore 0
   reset_blockstore 1
 
-  echo -e "Pinned root\tbafkqaaa\tsuccess" > basic_import_expected
-  echo -e "Pinned root\tbafy2bzaceaxm23epjsmh75yvzcecsrbavlmkcxnva66bkdebdcnyw3bjrc74u\tsuccess" >> basic_import_expected
-  echo -e "Pinned root\tbafy2bzaced4ueelaegfs5fqu4tzsh6ywbbpfk3cxppupmxfdhbpbhzawfw5oy\tsuccess" >> basic_import_expected
+  cat > basic_import_expected <<EOE
+Pinned root${tab}bafkqaaa${tab}success
+Pinned root${tab}bafy2bzaceaxm23epjsmh75yvzcecsrbavlmkcxnva66bkdebdcnyw3bjrc74u${tab}success
+Pinned root${tab}bafy2bzaced4ueelaegfs5fqu4tzsh6ywbbpfk3cxppupmxfdhbpbhzawfw5oy${tab}success
+EOE
+
+  cat >naked_root_import_json_expected <<EOE
+{"Root":{"Cid":{"/":"bafy2bzaceaxm23epjsmh75yvzcecsrbavlmkcxnva66bkdebdcnyw3bjrc74u"},"PinErrorMsg":""}}
+{"Root":{"Cid":{"/":"bafy2bzaced4ueelaegfs5fqu4tzsh6ywbbpfk3cxppupmxfdhbpbhzawfw5oy"},"PinErrorMsg":""}}
+EOE
+
 
   touch spin.gc
   test_expect_success "basic import" '
@@ -62,6 +71,33 @@ run_online_imp_exp_tests() {
   '
   test_expect_success "basic import output as expected" '
     test_cmp basic_import_expected basic_import_actual
+  '
+
+  test_expect_success "basic fetch+export 1" '
+    ipfsi 1 dag export bafy2bzaced4ueelaegfs5fqu4tzsh6ywbbpfk3cxppupmxfdhbpbhzawfw5oy > reexported_testnet_128.car
+  '
+  test_expect_success "export of shuffled testnet export identical to canonical original" '
+    test_cmp reexported_testnet_128.car ../t0054-dag-car-import-export-data/lotus_testnet_export_128.car
+  '
+
+  test_expect_success "basic fetch+export 2" '
+    ipfsi 1 dag export bafy2bzaceaxm23epjsmh75yvzcecsrbavlmkcxnva66bkdebdcnyw3bjrc74u > reexported_devnet_genesis.car
+  '
+  test_expect_success "export of shuffled devnet export identical to canonical original" '
+    test_cmp reexported_devnet_genesis.car ../t0054-dag-car-import-export-data/lotus_devnet_genesis.car
+  '
+
+  test_expect_success "pinlist on node1 still empty" '
+    [[ -z "$( ipfsi 1 pin ls )" ]]
+  '
+
+  test_expect_success "import/pin naked roots only, relying on local blockstore having all the data" '
+    ipfsi 1 dag import --enc=json ../t0054-dag-car-import-export-data/combined_naked_roots_genesis_and_128.car \
+      | sort > naked_import_result_json_actual
+  '
+
+  test_expect_success "naked import output as expected" '
+    test_cmp  naked_root_import_json_expected naked_import_result_json_actual
   '
 
   reset_blockstore 0
@@ -135,16 +171,16 @@ test_expect_success "correct error" '
 '
 
 
-cat >multiroot_import_expected <<EOE
+cat >multiroot_import_json_expected <<EOE
 {"Root":{"Cid":{"/":"bafy2bzaceb55n7uxyfaelplulk3ev2xz7gnq6crncf3ahnvu46hqqmpucizcw"},"PinErrorMsg":""}}
 {"Root":{"Cid":{"/":"bafy2bzacebedrc4n2ac6cqdkhs7lmj5e4xiif3gu7nmoborihajxn3fav3vdq"},"PinErrorMsg":""}}
 {"Root":{"Cid":{"/":"bafy2bzacede2hsme6hparlbr4g2x6pylj43olp4uihwjq3plqdjyrdhrv7cp4"},"PinErrorMsg":""}}
 EOE
 test_expect_success "multiroot import works" '
-  ipfs dag import --enc=json ../t0054-dag-car-import-export-data/lotus_testnet_export_256_multiroot.car | sort > multiroot_import_actual
+  ipfs dag import --enc=json ../t0054-dag-car-import-export-data/lotus_testnet_export_256_multiroot.car | sort > multiroot_import_json_actual
 '
 test_expect_success "multiroot import expected output" '
-  test_cmp multiroot_import_expected multiroot_import_actual
+  test_cmp multiroot_import_json_expected multiroot_import_json_actual
 '
 
 
@@ -159,16 +195,12 @@ test_expect_success "expected silence on --pin-roots=false" '
 '
 
 
-cat >naked_root_import_expected <<EOE
-{"Root":{"Cid":{"/":"bafy2bzaceaxm23epjsmh75yvzcecsrbavlmkcxnva66bkdebdcnyw3bjrc74u"},"PinErrorMsg":""}}
-{"Root":{"Cid":{"/":"bafy2bzaced4ueelaegfs5fqu4tzsh6ywbbpfk3cxppupmxfdhbpbhzawfw5oy"},"PinErrorMsg":""}}
-EOE
 test_expect_success "naked root import works" '
   ipfs dag import --enc=json ../t0054-dag-car-import-export-data/combined_naked_roots_genesis_and_128.car \
-  | sort > naked_root_import_actual
+  | sort > naked_root_import_json_actual
 '
 test_expect_success "naked root import expected output" '
-   test_cmp naked_root_import_expected naked_root_import_actual
+   test_cmp naked_root_import_json_expected naked_root_import_json_actual
 '
 
 test_done
