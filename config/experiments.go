e69fcd9d01416aacd704d224298229d6a194ac10
Steven Allen <steven@stebalien.com>
2020-05-25 23:06:42 -0700

    go-ipfs-config: feat: add an option for security transport experiments
    We should have a more permanent way to configure security transports, but
    experimental flags are a quick and unstable way to do this without making any
    promises.

--- config/experiments.go
+++ config/experiments.go
@@ -8,4 +8,11 @@ type Experiments struct {
 	Libp2pStreamMounting bool
 	P2pHttpProxy         bool
 	StrategicProviding   bool
+
+	// OverrideSecurityTransports overrides the set of available security
+	// transports when non-empty. This option should eventually migrate some
+	// place more stable.
+	//
+	// Default: ["tls", "secio", "noise"].
+	OverrideSecurityTransports []string `json:",omitempty"`
 }
