e61a2632f56778fdfc6cda66789fafdb0c940ce3
Petar Maymounkov <petarm@gmail.com>
2020-07-10 11:54:25 -0700

    more sharness

--- test/sharness/t0165-keystore.sh
+++ test/sharness/t0165-keystore.sh
@@ -12,36 +12,36 @@ test_init_ipfs
 
 test_key_cmd() {
   test_expect_success "create a new rsa key" '
-    rsahash=$(ipfs key gen foobarsa --type=rsa --size=2048)
+    rsahash=$(ipfs key gen -f=b58mh foobarsa --type=rsa --size=2048)
   '
 
   test_expect_success "create a new ed25519 key" '
-    edhash=$(ipfs key gen bazed --type=ed25519)
+    edhash=$(ipfs key gen -f=b58mh bazed --type=ed25519)
   '
 
   test_expect_success "both keys show up in list output" '
     echo bazed > list_exp &&
     echo foobarsa >> list_exp &&
     echo self >> list_exp
-    ipfs key list | sort > list_out &&
+    ipfs key list -f=b58mh | sort > list_out &&
     test_cmp list_exp list_out
   '
 
   test_expect_success "key hashes show up in long list output" '
-    ipfs key list -l | grep $edhash > /dev/null &&
-    ipfs key list -l | grep $rsahash > /dev/null
+    ipfs key list -f=b58mh -l | grep $edhash > /dev/null &&
+    ipfs key list -f=b58mh -l | grep $rsahash > /dev/null
   '
 
   test_expect_success "key list -l contains self key with peerID" '
     PeerID="$(ipfs config Identity.PeerID)"
-    ipfs key list -l | grep "$PeerID\s\+self"
+    ipfs key list -f=b58mh -l | grep "$PeerID\s\+self"
   '
 
   test_expect_success "key rm remove a key" '
     ipfs key rm foobarsa
     echo bazed > list_exp &&
     echo self >> list_exp
-    ipfs key list | sort > list_out &&
+    ipfs key list -f=b58mh | sort > list_out &&
     test_cmp list_exp list_out
   '
 
@@ -54,12 +54,12 @@ test_key_cmd() {
     ipfs key rename bazed fooed
     echo fooed > list_exp &&
     echo self >> list_exp
-    ipfs key list | sort > list_out &&
+    ipfs key list -f=b58mh | sort > list_out &&
     test_cmp list_exp list_out
   '
 
   test_expect_success "key rename rename key output succeeds" '
-    key_content=$(ipfs key gen key1 --type=rsa --size=2048) &&
+    key_content=$(ipfs key gen -f=b58mh key1 --type=rsa --size=2048) &&
     ipfs key rename key1 key2 >rs &&
     echo "Key $key_content renamed to key2" >expect &&
     test_cmp rs expect
