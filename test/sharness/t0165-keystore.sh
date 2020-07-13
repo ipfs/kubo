b28a7b4908696e0fca2d4e540d7983020daedd2e
Petar Maymounkov <petarm@gmail.com>
2020-07-13 08:17:47 -0700

    add sharness tests for key gen format

--- test/sharness/t0165-keystore.sh
+++ test/sharness/t0165-keystore.sh
@@ -11,6 +11,31 @@ test_description="Test keystore commands"
 test_init_ipfs
 
 test_key_cmd() {
+# test key output format
+test_expect_success "create an RSA key and test B58MH multihash output" '
+PEERID=$(ipfs key gen -f=b58mh --type=rsa --size=2048 key_rsa) &&
+test_check_rsa2048_b58mh_peerid $PEERID
+'
+
+test_expect_success "test RSA key B36CID multihash format" '
+PEERID=$(ipfs key list -f=b36cid -l | grep key_rsa | head -n 1 | cut -d " " -f1) &&
+test_check_rsa2048_b36cid_peerid $PEERID &&
+ipfs key rm key_rsa
+'
+
+test_expect_success "create an ED25519 key and test multihash output" '
+PEERID=$(ipfs key gen -f=b36cid --type=ed25519 key_ed25519) &&
+test_check_ed25519_b36cid_peerid $PEERID
+'
+
+test_expect_success "test ED25519 key B36CID multihash format" '
+PEERID=$(ipfs key list -f=b36cid -l | grep key_ed25519 | head -n 1 | cut -d " " -f1) &&
+test_check_ed25519_b36cid_peerid $PEERID &&
+ipfs key rm key_ed25519
+'
+# end of format test
+
+
   test_expect_success "create a new rsa key" '
     rsahash=$(ipfs key gen -f=b58mh foobarsa --type=rsa --size=2048)
   '
