af1994bf71570c076a058b018189b5bd2f6b5a3b
Petar Maymounkov <petarm@gmail.com>
2020-09-18 13:04:23 -0700

    Update core/commands/remotepin.go
    Co-authored-by: Marcin Rataj <lidel@lidel.org>

--- core/commands/remotepin.go
+++ core/commands/remotepin.go
@@ -123,7 +123,7 @@ Returns a list of objects that are pinned to a remote pinning service.
 
 	Arguments: []cmds.Argument{},
 	Options: []cmds.Option{
-		cmds.StringOption(pinNameOptionName, "An optional name for the pin to be listed."),
+		cmds.StringOption(pinNameOptionName, "Return pins objects with names that contain provided value (case-insensitive, partial or full match)."),
 		cmds.StringsOption(pinCIDsOptionName, "Return only pin objects for the specified CID(s); optional, comma separated."),
 	},
 	Run: func(req *cmds.Request, res cmds.ResponseEmitter, env cmds.Environment) error {
