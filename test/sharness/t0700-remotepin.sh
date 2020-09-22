c8f07434cabee931023a7a00f86e9aa96a4ffaf3
Petar Maymounkov <petarm@gmail.com>
2020-09-22 14:48:21 -0700

    sharness ready for a CI spin

--- test/sharness/t0700-remotepin.sh
+++ test/sharness/t0700-remotepin.sh
@@ -5,25 +5,22 @@ test_description="Test ipfs remote pinning operations"
 . lib/test-lib.sh
 
 test_remote_pins() {
-  LS_ARGS="$1"
-  BASE=$2
+  BASE=$1
   if [ -n "$BASE" ]; then
     BASE_ARGS="--cid-base=$BASE"
   fi
 
   test_expect_success "create some hashes using base $BASE" '
-    HASH_A=$(echo "A" | ipfs add $BASE_ARGS -q --pin=false) &&
-    HASH_B=$(echo "B" | ipfs add $BASE_ARGS -q --pin=false) &&
-    HASH_C=$(echo "C" | ipfs add $BASE_ARGS -q --pin=false)
+    HASH_A=$(echo "A" | ipfs add $BASE_ARGS -q --pin=false)
   '
   test_expect_success "'ipfs pin remote add'" '
     ID_A=$(ipfs pin remote add --enc=json $BASE_ARGS --name=name_a $HASH_A | jq --raw-output .ID)
   '
 
   test_expect_success "'ipfs pin remote ls' for existing pins by name" '
-    FOUND_ID_A=$(ipfs pin remote ls --enc=json --name=name_a | jq --raw-output .ID) &&
-    echo $ID_A > expected
-    echo $FOUND_ID_A > actual
+    FOUND_ID_A=$(ipfs pin remote ls --enc=json --name=name_a --cid=$HASH_A | jq --raw-output .ID | grep $ID_A) &&
+    echo $ID_A > expected &&
+    echo $FOUND_ID_A > actual &&
     test_cmp expected actual
   '
 
@@ -32,25 +29,21 @@ test_remote_pins() {
   '
 
   test_expect_success "'ipfs pin remote ls' for deleted pin" '
-    FOUND_ID_A=$(ipfs pin remote ls --enc=json --name=name_a | jq --raw-output .ID) &&
-    echo "" > expected
-    echo $FOUND_ID_A > actual
-    test_cmp expected actual
+    ipfs pin remote ls --enc=json --name=name_a | jq --raw-output .ID > list
+    test_expect_code 1 grep $ID_A list
   '
 }
 
 test_init_ipfs
 
-# create user on pinning service
-# IPFS_REMOTE_PIN_SERVICE=XXX
-# IPFS_REMOTE_PIN_KEY=$(curl -X POST $IPFS_REMOTE_PIN_SERVICE/users -d email=sharness@ipfs.io | jq --raw-output '.access_token')
-# returns {"email":"sharness@ipfs.io","access_token":"b79694866904060dc21a4ddcced4f8f1"}
+if [ -z "$IPFS_REMOTE_PIN_SERVICE" && -z "$IPFS_REMOTE_PIN_KEY" ]; then
+        # create user on pinning service
+        IPFS_REMOTE_PIN_SERVICE=localhost:5000
+        IPFS_REMOTE_PIN_KEY=$(curl -X POST $IPFS_REMOTE_PIN_SERVICE/users -d email=sharness@ipfs.io | jq --raw-output '.access_token')
+fi
 
-IPFS_REMOTE_PIN_SERVICE=http://163.172.146.161:5000/api/v1
-IPFS_REMOTE_PIN_KEY="b79694866904060dc21a4ddcced4f8f1"
+test_remote_pins ""
 
-test_remote_pins "" ""
-
-test_kill_ipfs_daemon
+# test_kill_ipfs_daemon
 
 test_done
