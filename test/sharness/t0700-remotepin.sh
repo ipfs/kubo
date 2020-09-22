c58e2960353adc8c0f7cbe25fdaf7c929dbd3e9a
Petar Maymounkov <petarm@gmail.com>
2020-09-22 10:00:44 -0700

    ok

--- test/sharness/t0700-remotepin.sh
+++ test/sharness/t0700-remotepin.sh
@@ -5,9 +5,8 @@ test_description="Test ipfs remote pinning operations"
 . lib/test-lib.sh
 
 test_remote_pins() {
-  PIN_ARGS="$1"
-  LS_ARGS="$2"
-  BASE=$3
+  LS_ARGS="$1"
+  BASE=$2
   if [ -n "$BASE" ]; then
     BASE_ARGS="--cid-base=$BASE"
   fi
@@ -17,30 +16,26 @@ test_remote_pins() {
     HASH_B=$(echo "B" | ipfs add $BASE_ARGS -q --pin=false) &&
     HASH_C=$(echo "C" | ipfs add $BASE_ARGS -q --pin=false)
   '
-  test_expect_success "'ipfs pin remote add $PIN_ARGS'" '
-    ID_A=$(ipfs pin remote add --enc=json $PIN_ARGS $BASE_ARGS --name=name_a $HASH_A | jq --raw-output .ID)
+  test_expect_success "'ipfs pin remote add'" '
+    ID_A=$(ipfs pin remote add --enc=json $BASE_ARGS --name=name_a $HASH_A | jq --raw-output .ID)
   '
 
-  test_expect_success "'ipfs pin remote ls $PIN_ARGS' for existing pins by name" '
-    FOUND_ID_A=$(ipfs pin remote ls --enc=json --name=name_a) | jq --raw-output .ID &&
-    if [ $ID_A = $FOUND_ID_A ]; then
-        return 0
-    else
-        return 1
-    fi
+  test_expect_success "'ipfs pin remote ls' for existing pins by name" '
+    FOUND_ID_A=$(ipfs pin remote ls --enc=json --name=name_a | jq --raw-output .ID) &&
+    echo $ID_A > expected
+    echo $FOUND_ID_A > actual
+    test_cmp expected actual
   '
 
-  test_expect_success "'ipfs pin remote rm $PIN_ARGS' an existing pin by ID" '
-    ipfs pin remote rm --enc=json $ID_A) | jq --raw-output .ID
+  test_expect_success "'ipfs pin remote rm' an existing pin by ID" '
+    ipfs pin remote rm --enc=json $ID_A
   '
 
-  test_expect_success "'ipfs pin remote ls $PIN_ARGS' for deleted pin" '
-    FOUND_ID_A=$(ipfs pin remote ls --enc=json --name=name_a) | jq --raw-output .ID &&
-    if [ "" = $FOUND_ID_A ]; then
-        return 0
-    else
-        return 1
-    fi
+  test_expect_success "'ipfs pin remote ls' for deleted pin" '
+    FOUND_ID_A=$(ipfs pin remote ls --enc=json --name=name_a | jq --raw-output .ID) &&
+    echo "" > expected
+    echo $FOUND_ID_A > actual
+    test_cmp expected actual
   '
 }
 
@@ -54,7 +49,7 @@ test_init_ipfs
 IPFS_REMOTE_PIN_SERVICE=http://163.172.146.161:5000/api/v1
 IPFS_REMOTE_PIN_KEY="b79694866904060dc21a4ddcced4f8f1"
 
-test_remote_pins "" "" ""
+test_remote_pins "" ""
 
 test_kill_ipfs_daemon
 
