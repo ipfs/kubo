2017e76335c13958014b78a69fb75cd24a0053b3
Petar Maymounkov <petarm@gmail.com>
2020-10-05 14:07:23 -0700

    first draft of integration with circleci

--- run_sharness.sh
+++ run_sharness.sh
@@ -7,8 +7,11 @@ cd $SRC_DIR
 
 make -O -j 10 coverage/sharness_tests.coverprofile test/sharness/test-results/sharness.xml TEST_GENERATE_JUNIT=1 CONTINUE_ON_S_FAILURE=1
 
-bash <(curl -s https://codecov.io/bash) -cF sharness -X search -f coverage/sharness_tests.coverprofile
-
+# export results
 mv test/sharness/test-results/sharness.xml /tmp/circleci-test-results/sharness
 
+# make sure we fail if there are test failures
 find test/sharness/test-results -name 't*-*.sh.*.counts' | test/sharness/lib/sharness/aggregate-results.sh | grep 'failed\s*0'
+
+# upload coverage
+bash <(curl -s https://codecov.io/bash) -cF sharness -X search -f coverage/sharness_tests.coverprofile
