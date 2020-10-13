f119b62d02bd88ed8bb87680a92c0a2b526025df
Petar Maymounkov <petarm@gmail.com>
2020-10-13 16:26:55 -0700

    make -j2

--- run_sharness.sh
+++ run_sharness.sh
@@ -4,4 +4,4 @@ set -xe
 SRC_DIR=/go-ipfs
 cd $SRC_DIR
 
-make -O -j 5 coverage/sharness_tests.coverprofile test/sharness/test-results/sharness.xml TEST_GENERATE_JUNIT=1 CONTINUE_ON_S_FAILURE=1
+make -O -j 2 coverage/sharness_tests.coverprofile test/sharness/test-results/sharness.xml TEST_GENERATE_JUNIT=1 CONTINUE_ON_S_FAILURE=1
