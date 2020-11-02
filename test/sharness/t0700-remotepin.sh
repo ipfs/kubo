e8d2d6147ff66e09885522d920467d7c692990cf
Petar Maymounkov <petarm@gmail.com>
2020-11-02 14:33:05 -0800

    use remote pin service config in test; demote circle ci machine

--- test/sharness/t0700-remotepin.sh
+++ test/sharness/t0700-remotepin.sh
@@ -9,8 +9,8 @@ test_init_ipfs
 # create user on pinning service
 test_expect_success "creating test user on remote pinning service" '
         echo CI host IP address ${CI_HOST_IP} &&
-        export IPFS_REMOTE_PIN_SERVICE=http://${CI_HOST_IP}:5000/api/v1 &&
-        export IPFS_REMOTE_PIN_KEY=$(curl -X POST $IPFS_REMOTE_PIN_SERVICE/users -d email=sharness@ipfs.io | jq --raw-output .access_token)
+        export TEST_PIN_SVC=http://${CI_HOST_IP}:5000/api/v1 &&
+        ipfs pin remote service add test_pin_svc $TEST_PIN_SVC $(curl -X POST $TEST_PIN_SVC/users -d email=sharness@ipfs.io | jq --raw-output .access_token)
 '
 
 test_remote_pins() {
@@ -19,9 +19,6 @@ test_remote_pins() {
     BASE_ARGS="--cid-base=$BASE"
   fi
 
-  echo Using IPFS_REMOTE_PIN_SERVICE=$IPFS_REMOTE_PIN_SERVICE
-  echo Using IPFS_REMOTE_PIN_KEY=$IPFS_REMOTE_PIN_KEY
-
   test_expect_success "create some hashes using base $BASE" '
     export HASH_A=$(echo "A" | ipfs add $BASE_ARGS -q --pin=false) &&
     export HASH_B=$(echo "B" | ipfs add $BASE_ARGS -q --pin=false) &&
@@ -29,18 +26,18 @@ test_remote_pins() {
   '
 
   test_expect_success "check connection to pinning service" '
-    ipfs pin remote ls --enc=json
+    ipfs pin remote ls --service=test_pin_svc --enc=json
   '
 
   test_expect_success "'ipfs pin remote add'" '
-    export ID_A=$(ipfs pin remote add --enc=json $BASE_ARGS --name=name_a $HASH_A | jq --raw-output .RequestID) &&
-    export ID_B=$(ipfs pin remote add --enc=json $BASE_ARGS --name=name_b $HASH_B | jq --raw-output .RequestID) &&
-    export ID_C=$(ipfs pin remote add --enc=json $BASE_ARGS --name=name_c $HASH_C | jq --raw-output .RequestID) &&
+    export ID_A=$(ipfs pin remote add --service=test_pin_svc --enc=json $BASE_ARGS --name=name_a $HASH_A | jq --raw-output .RequestID) &&
+    export ID_B=$(ipfs pin remote add --service=test_pin_svc --enc=json $BASE_ARGS --name=name_b $HASH_B | jq --raw-output .RequestID) &&
+    export ID_C=$(ipfs pin remote add --service=test_pin_svc --enc=json $BASE_ARGS --name=name_c $HASH_C | jq --raw-output .RequestID) &&
     sleep 3 # provide time for the pinning service to download the file
   '
 
   test_expect_success "'ipfs pin remote ls' for existing pins by ID" '
-    FOUND_ID_A=$(ipfs pin remote ls --enc=json --cid=$HASH_A | jq --raw-output .RequestID | grep $ID_A) &&
+    FOUND_ID_A=$(ipfs pin remote ls --service=test_pin_svc --enc=json --cid=$HASH_A | jq --raw-output .RequestID | grep $ID_A) &&
     echo ID_A=$ID_A FOUND_ID_A=$FOUND_ID_A &&
     echo $ID_A > expected &&
     echo $FOUND_ID_A > actual &&
@@ -48,7 +45,7 @@ test_remote_pins() {
   '
 
   test_expect_success "'ipfs pin remote ls' for existing pins by name" '
-    FOUND_ID_A=$(ipfs pin remote ls --enc=json --name=name_a | jq --raw-output .RequestID | grep $ID_A) &&
+    FOUND_ID_A=$(ipfs pin remote ls --service=test_pin_svc --enc=json --name=name_a | jq --raw-output .RequestID | grep $ID_A) &&
     echo ID_A=$ID_A FOUND_ID_A=$FOUND_ID_A &&
     echo $ID_A > expected &&
     echo $FOUND_ID_A > actual &&
@@ -56,7 +53,7 @@ test_remote_pins() {
   '
 
   test_expect_success "'ipfs pin remote ls' for existing pins by status" '
-    FOUND_ID_A=$(ipfs pin remote ls --enc=json --status=pinned | jq --raw-output .RequestID | grep $ID_A) &&
+    FOUND_ID_A=$(ipfs pin remote ls --service=test_pin_svc --enc=json --status=pinned | jq --raw-output .RequestID | grep $ID_A) &&
     echo ID_A=$ID_A FOUND_ID_A=$FOUND_ID_A &&
     echo $ID_A > expected &&
     echo $FOUND_ID_A > actual &&
@@ -64,19 +61,19 @@ test_remote_pins() {
   '
 
   test_expect_success "'ipfs pin remote rm' an existing pin by ID" '
-    ipfs pin remote rm --enc=json $ID_A
+    ipfs pin remote rm --service=test_pin_svc --enc=json $ID_A
   '
 
   test_expect_success "'ipfs pin remote rm' an existing pin by name" '
-    ipfs pin remote rm --enc=json --name=name_b
+    ipfs pin remote rm --service=test_pin_svc --enc=json --name=name_b
   '
 
   test_expect_success "'ipfs pin remote rm' an existing pin by status" '
-    ipfs pin remote rm --enc=json --status=pinned
+    ipfs pin remote rm --service=test_pin_svc --enc=json --status=pinned
   '
 
   test_expect_success "'ipfs pin remote ls' for deleted pin" '
-    ipfs pin remote ls --enc=json --name=name_a | jq --raw-output .RequestID > list
+    ipfs pin remote ls --service=test_pin_svc --enc=json --name=name_a | jq --raw-output .RequestID > list
     test_expect_code 1 grep $ID_A list
   '
 }
