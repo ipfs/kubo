f264de41392227173da5ec44d929831d37d60161
vyzo <vyzo@hackzen.org>
2021-04-13 17:53:47 +0300

    go-ipfs-config: update comments
    Co-authored-by: Marcin Rataj <lidel@lidel.org>

--- config/dns.go
+++ config/dns.go
@@ -2,6 +2,14 @@ package config
 
 // DNSConfig specifies DNS resolution rules using custom resolvers
 type DNSConfig struct {
-	// CustomResolvers is a map of FQDNs to URLs for custom DNS resolution.
+	// Resolvers is a map of FQDNs to URLs for custom DNS resolution. 
+	// URLs starting with `https://` indicate DoH endpoints.
+	// Support for other resolver types can be added in the future.
+	// https://en.wikipedia.org/wiki/Fully_qualified_domain_name
+	// https://en.wikipedia.org/wiki/DNS_over_HTTPS 
+	// 
+	// Example:
+	// - Custom resolver for ENS:          `eth.` → `https://eth.link/dns-query`
+	// - Override the default OS resolver: `.`    → `https://doh.applied-privacy.net/query`
 	Resolvers map[string]string
 }
