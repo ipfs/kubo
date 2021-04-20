dbb5fa78a59e09addf7368eff6aaefbbbf08c741
gammazero <gammazero@users.noreply.github.com>
2021-04-20 00:29:38 -0700

    go-ipfs-config: Fix comment

--- config/migration.go
+++ config/migration.go
@@ -9,9 +9,9 @@ var DefaultMigrationDownloadSources = []string{"HTTPS", "IPFS"}
 // Migration configures how migrations are downloaded and if the downloads are
 // added to IPFS locally
 type Migration struct {
-	// Sources in order of preference where "HTTPS" means our gateways and
-	// "IPFS" means over IPFS. Any other values are interpretes as hostnames
-	// for custom gateways. An empty list means "do the default thing"
+	// Sources in order of preference, where "IPFS" means use IPFS and "HTTPS"
+	// means use default gateways. Any other values are interpreted as
+	// hostnames for custom gateways. Empty list means "use default sources".
 	DownloadSources []string
 	// Whether or not to keep the migration after downloading it.
 	// Options are "discard", "cache", "pin".  Empty string for default.
