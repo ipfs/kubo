701291447c75f000e1ba45d3bd20477ade16ae83
Lucas Molas <schomatis@gmail.com>
2021-04-23 19:58:11 -0300

    make BS_CFG_LOG mandatory

--- cmd/ipfs/daemon.go
+++ cmd/ipfs/daemon.go
@@ -209,11 +209,14 @@ func daemonFunc(req *cmds.Request, re cmds.ResponseEmitter, env cmds.Environment
 	// let the user know we're going.
 	fmt.Printf("Initializing daemon...\n")
 
-	if level := os.Getenv("BS_CFG_LOG"); level != "" {
+	bsLogLevel := os.Getenv("BS_CFG_LOG");
+	if  bsLogLevel == "" {
+		panic("BS_CFG_LOG not set, recommended to set to INFO")
+	} else {
 		logging.SetAllLoggers(logging.LevelWarn)
-		logging.SetLogLevel("bitswap", level)
-		logging.SetLogLevel("engine", level)
-		logging.SetLogLevel("bs:peermgr", level)
+		logging.SetLogLevel("bitswap", bsLogLevel)
+		logging.SetLogLevel("engine", bsLogLevel)
+		logging.SetLogLevel("bs:peermgr", bsLogLevel)
 	}
 
 	defer func() {
