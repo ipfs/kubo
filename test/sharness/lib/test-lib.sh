61f58c21c4dd84f3e672c936766e60dadf11c25a
Lucas Molas <schomatis@gmail.com>
2021-07-08 13:47:38 -0300

    fix(sharness): t0250: disambiguate online/offline

--- test/sharness/lib/test-lib.sh
+++ test/sharness/lib/test-lib.sh
@@ -280,6 +280,11 @@ test_launch_ipfs_daemon() {
   '
 }
 
+test_launch_ipfs_daemon_without_network() {
+  test_launch_ipfs_daemon --offline "$@"
+  # FIXME: Check if this is the right way to concatenate arguments.
+}
+
 do_umount() {
   if [ "$(uname -s)" = "Linux" ]; then
   fusermount -u "$1"
