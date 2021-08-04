65767f1c939da3ef3165bbc3fa4471b1314690c5
Petar Maymounkov <petarm@gmail.com>
2021-08-04 08:26:42 -0400

    fix example go.sum; make common example folder; spell out complete source of second example

--- docs/examples/go-ipfs-with-experimental/main.go
+++ docs/examples/go-ipfs-with-experimental/main.go
@@ -57,6 +57,19 @@ func createTempRepo() (string, error) {
 		return "", err
 	}
 
+	// https://github.com/ipfs/go-ipfs/blob/master/docs/experimental-features.md#ipfs-filestore
+	cfg.Experimental.FilestoreEnabled = true
+	// https://github.com/ipfs/go-ipfs/blob/master/docs/experimental-features.md#ipfs-urlstore
+	cfg.Experimental.UrlstoreEnabled = true
+	// https://github.com/ipfs/go-ipfs/blob/master/docs/experimental-features.md#directory-sharding--hamt
+	cfg.Experimental.ShardingEnabled = true
+	// https://github.com/ipfs/go-ipfs/blob/master/docs/experimental-features.md#ipfs-p2p
+	cfg.Experimental.Libp2pStreamMounting = true
+	// https://github.com/ipfs/go-ipfs/blob/master/docs/experimental-features.md#p2p-http-proxy
+	cfg.Experimental.P2pHttpProxy = true
+	// https://github.com/ipfs/go-ipfs/blob/master/docs/experimental-features.md#strategic-providing
+	cfg.Experimental.StrategicProviding = true
+
 	// Create the repo with the config
 	err = fsrepo.Init(repoPath, cfg)
 	if err != nil {
