983e3c63823b6d00fc0c75741f1026d1f5010833
Petar Maymounkov <petarm@gmail.com>
2021-08-25 09:46:55 +0200

    expose unlimited stack trace on http api

---
+++ cmd/ipfs/debug.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"runtime"
+)
+
+func init() {
+	http.HandleFunc("/debug/stack",
+		func(w http.ResponseWriter, _ *http.Request) {
+			writeGoroutineStacks(w)
+		},
+	)
+}
+
+func writeGoroutineStacks(w io.Writer) error {
+	buf := make([]byte, 1<<20)
+	for i := 0; ; i++ {
+		n := runtime.Stack(buf, true)
+		if n < len(buf) {
+			buf = buf[:n]
+			break
+		}
+		// if len(buf) >= 64<<20 {
+		// 	// Filled 64 MB - stop there.
+		// 	break
+		// }
+		buf = make([]byte, 2*len(buf))
+	}
+	_, err := w.Write(buf)
+	return err
+}
