64e69194dae7a95c2f013fe5d7cb5211520905d4
Petar Maymounkov <petarm@gmail.com>
2021-08-26 07:22:50 +0200

    add all goroutines to ipfs diag profile

--- core/commands/profile.go
+++ core/commands/profile.go
@@ -121,6 +121,24 @@ However, it could reveal:
 	},
 }
 
+func writeAllGoroutineStacks(w io.Writer) error {
+	buf := make([]byte, 1<<20)
+	for i := 0; ; i++ {
+		n := runtime.Stack(buf, true)
+		if n < len(buf) {
+			buf = buf[:n]
+			break
+		}
+		// if len(buf) >= 64<<20 {
+		// 	// Filled 64 MB - stop there.
+		// 	break
+		// }
+		buf = make([]byte, 2*len(buf))
+	}
+	_, err := w.Write(buf)
+	return err
+}
+
 func writeProfiles(ctx context.Context, cpuProfileTime time.Duration, w io.Writer) error {
 	archive := zip.NewWriter(w)
 
@@ -142,6 +160,17 @@ func writeProfiles(ctx context.Context, cpuProfileTime time.Duration, w io.Write
 		name: "heap",
 		file: "heap.pprof",
 	}}
+
+	{
+		out, err := archive.Create("goroutine-all.stacks")
+		if err != nil {
+			return err
+		}
+		err = writeAllGoroutineStacks(out)
+		if err != nil {
+			return err
+		}
+	}
 
 	for _, profile := range profiles {
 		prof := pprof.Lookup(profile.name)
