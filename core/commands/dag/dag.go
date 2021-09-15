2e8b0bfee780e357f7045c4c763b8878f11271db
Adin Schmahmann <adin.schmahmann@gmail.com>
2021-09-15 15:16:14 -0400

    change names of ipfs dag put flags to make changes clearer
    (cherry picked from commit 29206790564e6678986773557e2c7251e3ec0ac0)

--- core/commands/dag/dag.go
+++ core/commands/dag/dag.go
@@ -84,8 +84,8 @@ into an object of the specified format.
 		cmds.FileArg("object data", true, true, "The object to put").EnableStdin(),
 	},
 	Options: []cmds.Option{
-		cmds.StringOption("format", "f", "Format that the object will be added as.").WithDefault("dag-cbor"),
-		cmds.StringOption("input-enc", "Format that the input object will be.").WithDefault("dag-json"),
+		cmds.StringOption("store-codec", "s", "Codec that the stored object will be encoded with").WithDefault("dag-cbor"),
+		cmds.StringOption("input-codec", "i", "Codec that the input object is encoded in").WithDefault("dag-json"),
 		cmds.BoolOption("pin", "Pin this object when adding."),
 		cmds.StringOption("hash", "Hash function to use").WithDefault("sha2-256"),
 	},
