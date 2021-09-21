47ac6fecc7c384ac8873b525da4e687704c6ebc8
Lucas Molas <schomatis@gmail.com>
2021-09-21 15:31:08 -0300

    feat(cli): add daemon option --agent-version-suffix (#8419)
    * feat(cli): add daemon option --agent-version-suffix
    * fix sharness test when commit is empty (release)
    
    (cherry picked from commit 3a84352f1811b22a17530ddb6a2da4f13fbfeef5)

--- test/sharness/t0026-id.sh
+++ test/sharness/t0026-id.sh
@@ -7,6 +7,8 @@ test_description="Test to make sure our identity information looks sane"
 test_init_ipfs
 
 test_id_compute_agent() {
+    local AGENT_SUFFIX
+    AGENT_SUFFIX=$1
     AGENT_VERSION="$(ipfs version --number)" || return 1
     AGENT_COMMIT="$(ipfs version --number --commit)" || return 1
     if test "$AGENT_COMMIT" = "$AGENT_VERSION"; then
@@ -14,7 +16,14 @@ test_id_compute_agent() {
     else
         AGENT_COMMIT="${AGENT_COMMIT##$AGENT_VERSION-}"
     fi
-    echo "go-ipfs/$AGENT_VERSION/$AGENT_COMMIT"
+    AGENT_VERSION="go-ipfs/$AGENT_VERSION/$AGENT_COMMIT"
+    if test -n "$AGENT_SUFFIX"; then
+      if test -n "$AGENT_COMMIT"; then
+        AGENT_VERSION="$AGENT_VERSION/"
+      fi
+      AGENT_VERSION="$AGENT_VERSION$AGENT_SUFFIX"
+    fi
+    echo "$AGENT_VERSION"
 }
 
 test_expect_success "checking AgentVersion" '
@@ -22,6 +31,16 @@ test_expect_success "checking AgentVersion" '
   ipfs id -f "<aver>\n" > actual-agent-version &&
   test_cmp expected-agent-version actual-agent-version
 '
+
+test_launch_ipfs_daemon_without_network --agent-version-suffix=test-suffix
+
+test_expect_success "checking AgentVersion with suffix (daemon running)" '
+  test_id_compute_agent test-suffix > expected-agent-version &&
+  ipfs id -f "<aver>\n" > actual-agent-version &&
+  test_cmp expected-agent-version actual-agent-version
+'
+
+test_kill_ipfs_daemon
 
 test_expect_success "checking ProtocolVersion" '
   echo "ipfs/0.1.0" > expected-protocol-version &&
