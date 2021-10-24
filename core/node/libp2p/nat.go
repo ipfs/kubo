4eb380e6a627adb050c6a4b0bb2a7f5668d2ccf8
Marten Seemann <martenseemann@gmail.com>
2021-10-24 14:49:40 +0200

    use the new optional duration configuration value

--- core/node/libp2p/nat.go
+++ core/node/libp2p/nat.go
@@ -3,7 +3,7 @@ package libp2p
 import (
 	"time"
 
-	"github.com/ipfs/go-ipfs-config"
+	config "github.com/ipfs/go-ipfs-config"
 	"github.com/libp2p/go-libp2p"
 )
 
@@ -13,14 +13,12 @@ func AutoNATService(throttle *config.AutoNATThrottleConfig) func() Libp2pOpts {
 	return func() (opts Libp2pOpts) {
 		opts.Opts = append(opts.Opts, libp2p.EnableNATService())
 		if throttle != nil {
-			global := throttle.GlobalLimit
-			peer := throttle.PeerLimit
-			interval := time.Duration(throttle.Interval)
-			if interval == 0 {
-				interval = time.Minute
-			}
 			opts.Opts = append(opts.Opts,
-				libp2p.AutoNATServiceRateLimit(global, peer, interval),
+				libp2p.AutoNATServiceRateLimit(
+					throttle.GlobalLimit,
+					throttle.PeerLimit,
+					throttle.Interval.WithDefault(time.Minute),
+				),
 			)
 		}
 		return opts
