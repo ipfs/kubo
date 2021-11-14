2d54d32dabe4a75769794ac963a3b426fbedeb23
Marten Seemann <martenseemann@gmail.com>
2021-11-14 11:35:10 +0400

    enable hole punching

--- core/node/libp2p/relay.go
+++ core/node/libp2p/relay.go
@@ -70,3 +70,12 @@ func AutoRelay(addDefaultRelays bool) func() (opts Libp2pOpts, err error) {
 		return
 	}
 }
+
+func HolePunching(flag config.Flag, hasRelayClient bool) func() (opts Libp2pOpts, err error) {
+	return func() (opts Libp2pOpts, err error) {
+		if flag.WithDefault(false) hasRelayClient {
+			opts.Opts = append(opts.Opts, libp2p.EnableHolePunching())
+		}
+		return
+	}
+}
