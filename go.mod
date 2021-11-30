ecd2628a16f8ef33f53aed371e45750463c13c74
Mohsin Zaidi <2236875+smrz2001@users.noreply.github.com>
2021-11-30 13:58:15 -0500

    Integrate go-dag-jose plugin (#8569)
    * feat(dag-jose): integrate `go-dag-jose` plugin
    * test(dag-jose): sharness test for dag-jose plugin
    
    Co-authored-by: Eric Myhre <hash@exultant.us>

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ require (
 	bazil.org/fuse v0.0.0-20200117225306-7b5117fecadc
 	contrib.go.opencensus.io/exporter/prometheus v0.3.0
 	github.com/blang/semver/v4 v4.0.0
+	github.com/ceramicnetwork/go-dag-jose v0.1.0
 	github.com/cheggaaa/pb v1.0.29
 	github.com/coreos/go-systemd/v22 v22.3.2
 	github.com/dustin/go-humanize v1.0.0
@@ -60,7 +61,7 @@ require (
 	github.com/ipfs/tar-utils v0.0.1
 	github.com/ipld/go-car v0.3.2
 	github.com/ipld/go-codec-dagpb v1.3.0
-	github.com/ipld/go-ipld-prime v0.12.3
+	github.com/ipld/go-ipld-prime v0.14.1
 	github.com/jbenet/go-random v0.0.0-20190219211222-123a90aedc0c
 	github.com/jbenet/go-temp-err-catcher v0.1.0
 	github.com/jbenet/goprocess v0.1.4
---
+++ plugin/plugins/dagjose/dagjose.go
@@ -0,0 +1,36 @@
+package dagjose
+
+import (
+	"github.com/ipfs/go-ipfs/plugin"
+
+	"github.com/ceramicnetwork/go-dag-jose/dagjose"
+	"github.com/ipld/go-ipld-prime/multicodec"
+	mc "github.com/multiformats/go-multicodec"
+)
+
+// Plugins is exported list of plugins that will be loaded
+var Plugins = []plugin.Plugin{
+	&dagjosePlugin{},
+}
+
+type dagjosePlugin struct{}
+
+var _ plugin.PluginIPLD = (*dagjosePlugin)(nil)
+
+func (*dagjosePlugin) Name() string {
+	return "ipld-codec-dagjose"
+}
+
+func (*dagjosePlugin) Version() string {
+	return "0.0.1"
+}
+
+func (*dagjosePlugin) Init(_ *plugin.Environment) error {
+	return nil
+}
+
+func (*dagjosePlugin) Register(reg multicodec.Registry) error {
+	reg.RegisterEncoder(uint64(mc.DagJose), dagjose.Encode)
+	reg.RegisterDecoder(uint64(mc.DagJose), dagjose.Decode)
+	return nil
+}
