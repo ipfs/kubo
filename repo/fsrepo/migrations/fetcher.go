7fe56507b51beacc174d813c8b4b9265cb5286ea
Adin Schmahmann <adin.schmahmann@gmail.com>
2021-12-13 13:16:23 -0500

    feat: bump repo version to 12 and update CID of the distributions

--- repo/fsrepo/migrations/fetcher.go
+++ repo/fsrepo/migrations/fetcher.go
@@ -10,7 +10,7 @@ import (
 
 const (
 	// Current distribution to fetch migrations from
-	CurrentIpfsDist = "/ipfs/QmP7tLxzhLU1KauTRX3jkVkF93pCv4skcceyUYMhf4AKJR" // fs-repo-migrations v2.0.2
+	CurrentIpfsDist = "/ipfs/QmPweMoUxWFt1MSpYwLWsHB1GBcyYYDKPnANdERMY4U6hK" // fs-repo-11-to-12 v1.0.1
 	// Latest distribution path.  Default for fetchers.
 	LatestIpfsDist = "/ipns/dist.ipfs.io"
 
