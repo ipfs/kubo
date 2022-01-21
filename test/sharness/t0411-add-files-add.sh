846c4fa3544014f07362a5fc7c67bf815660f321
Cory Schwartz <cory@protocol.ai>
2022-01-21 09:47:56 -0800

    no tabs

--- test/sharness/t0411-add-files-add.sh
+++ test/sharness/t0411-add-files-add.sh
@@ -13,23 +13,23 @@ test_launch_ipfs_daemon
 # Verify CIDS are the same
 test_expect_success "files add CID matches add CID" '
   DIR="/test-${RANDOM}"
-	ipfs files mkdir $DIR
+  ipfs files mkdir $DIR
 
   echo "hi" > f1
-	ADDCID=$(ipfs add -q f1)
-	FILESADDCID=$(ipfs files add -q $DIR/ f1)
-	test $ADDCID = $FILESADDCID
+  ADDCID=$(ipfs add -q f1)
+  FILESADDCID=$(ipfs files add -q $DIR/ f1)
+  test $ADDCID = $FILESADDCID
 '
 
 # verify files add does not result in a pin.
 test_expect_code 1 '
   DIR="/test-${RANDOM}"
-	ipfs files mkdir $DIR
+  ipfs files mkdir $DIR
 
-	echo "${RANDOM}" > f2
-	CID=$(ipfs files add -q $DIR/ f2)
+  echo "${RANDOM}" > f2
+  CID=$(ipfs files add -q $DIR/ f2)
 
-	ipfs pin ls | grep $CID
+  ipfs pin ls | grep $CID
 '
 test_kill_ipfs_daemon
 
