17d00ce8cfff23d58ff7c647e204db26777a2681
Marcin Rataj <lidel@lidel.org>
2022-03-09 18:26:48 +0100

    test: t0118-gateway-car/carv1-basic.car
    This test uses official CARv1 fixture from
    https://ipld.io/specs/transport/car/fixture/carv1-basic/
    
    The CAR has two dag-cbor roots, and we use one of them, which represents
    a nice DAG with both dag-cbor, dag-pb and raw blocks

--- test/sharness/t0118-gateway-car.sh
+++ test/sharness/t0118-gateway-car.sh
@@ -11,15 +11,27 @@ test_launch_ipfs_daemon_without_network
 # but if we have a small file that fits into a single block, and export its CID
 # we will get a CAR that is a deterministic array of bytes.
 
-test_expect_success "Create a deterministic CAR for testing" '
-  mkdir -p subdir &&
-  echo "hello application/vnd.ipld.car" > subdir/ascii.txt &&
-  ROOT_DIR_CID=$(ipfs add -Qrw --cid-version 1 subdir) &&
-  FILE_CID=$(ipfs resolve -r /ipfs/$ROOT_DIR_CID/subdir/ascii.txt | cut -d "/" -f3) &&
-  ipfs dag export $ROOT_DIR_CID > test-dag.car &&
-  ipfs dag export $FILE_CID > deterministic.car &&
-  purge_blockstore
-'
+    test_expect_success "Create a deterministic CAR for testing" '
+    mkdir -p subdir &&
+    echo "hello application/vnd.ipld.car" > subdir/ascii.txt &&
+    ROOT_DIR_CID=$(ipfs add -Qrw --cid-version 1 subdir) &&
+    FILE_CID=$(ipfs resolve -r /ipfs/$ROOT_DIR_CID/subdir/ascii.txt | cut -d "/" -f3) &&
+    ipfs dag export $ROOT_DIR_CID > test-dag.car &&
+    ipfs dag export $FILE_CID > deterministic.car &&
+    purge_blockstore
+    '
+
+# GET a reference DAG with dag-cbor+dag-pb+raw blocks as CAR
+
+    # This test uses official CARv1 fixture from https://ipld.io/specs/transport/car/fixture/carv1-basic/
+    test_expect_success "GET for application/vnd.ipld.car with dag-cbor root returns a CARv1 stream with full DAG" '
+    ipfs dag import ../t0118-gateway-car/carv1-basic.car &&
+    DAG_CBOR_CID=bafyreihyrpefhacm6kkp4ql6j6udakdit7g3dmkzfriqfykhjw6cad5lrm &&
+    curl -sX GET -H "Accept: application/vnd.ipld.car" "http://127.0.0.1:$GWAY_PORT/ipfs/$DAG_CBOR_CID" -o gateway-dag-cbor.car &&
+    purge_blockstore &&
+    ipfs dag import gateway-dag-cbor.car &&
+    ipfs dag stat --offline $DAG_CBOR_CID
+    '
 
 # GET unixfs file as CAR
 # (by using a single file we ensure deterministic result that can be compared byte-for-byte)
