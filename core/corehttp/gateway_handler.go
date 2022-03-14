cbb962852f914dc682887ea86c227e8f546c880c
Michael Muré <batolettre@gmail.com>
2022-03-14 18:12:49 +0100

    fix: incorrect logging

--- core/corehttp/gateway_handler.go
+++ core/corehttp/gateway_handler.go
@@ -854,7 +854,7 @@ func webError(w http.ResponseWriter, message string, err error, defaultCode int)
 func webErrorWithCode(w http.ResponseWriter, message string, err error, code int) {
 	http.Error(w, fmt.Sprintf("%s: %s", message, err), code)
 	if code >= 500 {
-		log.Warnf("server error: %s: %s", err)
+		log.Warnf("server error: %s: %s", message, err)
 	}
 }
 
