5b43672a8733525ad9fab9be03d14d983f3f9914
Marcin Rataj <lidel@lidel.org>
2022-03-16 01:16:55 +0100

    fix: car response etag matching
    https://github.com/ipfs/go-ipfs/pull/8758#discussion_r827503815
    lidel needs some sleep

--- core/corehttp/gateway_handler_car.go
+++ core/corehttp/gateway_handler_car.go
@@ -23,8 +23,8 @@ func (i *gatewayHandler) serveCar(w http.ResponseWriter, r *http.Request, rootCi
 
 	// Weak Etag W/ because we can't guarantee byte-for-byte identical  responses
 	// (CAR is streamed, and in theory, blocks may arrive from datastore in non-deterministic order)
-	etag := getEtag(r, rootCid)
-	w.Header().Set("Etag", `W/`+etag)
+	etag := `W/` + getEtag(r, rootCid)
+	w.Header().Set("Etag", etag)
 
 	// Finish early if Etag match
 	if r.Header.Get("If-None-Match") == etag {
