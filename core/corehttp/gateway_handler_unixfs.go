60b38090b6e11b700932867dc5470b17e01de6b0
Marcin Rataj <lidel@lidel.org>
2022-04-04 17:13:48 +0200

    feat(gw): tracing spans per response type (#8841)
    Adds more visibility into how long generic lookup takes vs producing specific response type.

--- core/corehttp/gateway_handler_unixfs.go
+++ core/corehttp/gateway_handler_unixfs.go
@@ -7,13 +7,18 @@ import (
 	"time"
 
 	files "github.com/ipfs/go-ipfs-files"
+	"github.com/ipfs/go-ipfs/tracing"
 	ipath "github.com/ipfs/interface-go-ipfs-core/path"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
 )
 
 func (i *gatewayHandler) serveUnixFs(w http.ResponseWriter, r *http.Request, resolvedPath ipath.Resolved, contentPath ipath.Path, begin time.Time, logger *zap.SugaredLogger) {
+	ctx, span := tracing.Span(r.Context(), "Gateway", "ServeUnixFs", trace.WithAttributes(attribute.String("path", resolvedPath.String())))
+	defer span.End()
 	// Handling UnixFS
-	dr, err := i.api.Unixfs().Get(r.Context(), resolvedPath)
+	dr, err := i.api.Unixfs().Get(ctx, resolvedPath)
 	if err != nil {
 		webError(w, "ipfs cat "+html.EscapeString(contentPath.String()), err, http.StatusNotFound)
 		return
@@ -23,7 +28,7 @@ func (i *gatewayHandler) serveUnixFs(w http.ResponseWriter, r *http.Request, res
 	// Handling Unixfs file
 	if f, ok := dr.(files.File); ok {
 		logger.Debugw("serving unixfs file", "path", contentPath)
-		i.serveFile(w, r, contentPath, resolvedPath.Cid(), f, begin)
+		i.serveFile(w, r, resolvedPath, contentPath, f, begin)
 		return
 	}
 
