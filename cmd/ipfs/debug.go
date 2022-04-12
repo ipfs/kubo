bb68a685253f77fdf49475cb963ee1995c93657f
Gus Eggert <gus@gus.dev>
2022-04-12 11:58:03 -0400

    feat: port collect-profiles.sh to 'ipfs diag profile' (#8786)
    * feat: add block profiling to collect-profiles.sh
    
    * feat: add more profiles to 'ipfs diag profile'
    
    This adds mutex and block profiles, and brings the command up-to-par
    with 'collect-profiles.sh', so that we can remove it.
    
    Profiles are also now collected concurrently, which improves the
    runtime from (profile_time * num_profiles) to just (profile_time).
    
    Note that this has a backwards-incompatible change, removing
    --cpu-profile-time in favor of the more general --profile-time, which
    covers all sampling profiles.
    
    * docs(cli): ipfs diag profile
    
    * add CLI flag to select specific diag collectors
    
    Co-authored-by: Marcin Rataj <lidel@lidel.org>

--- cmd/ipfs/debug.go
+++ cmd/ipfs/debug.go
@@ -3,13 +3,13 @@ package main
 import (
 	"net/http"
 
-	"github.com/ipfs/go-ipfs/core/commands"
+	"github.com/ipfs/go-ipfs/profile"
 )
 
 func init() {
 	http.HandleFunc("/debug/stack",
 		func(w http.ResponseWriter, _ *http.Request) {
-			_ = commands.WriteAllGoroutineStacks(w)
+			_ = profile.WriteAllGoroutineStacks(w)
 		},
 	)
 }
