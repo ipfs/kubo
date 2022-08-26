026fd885164e8ede5fbfaf7262fe688ef8ce0494
Winterhuman <86165318+Winterhuman@users.noreply.github.com>
2022-08-26 15:30:11 +0000

    Update DHTClient description

--- cmd/ipfs/daemon.go
+++ cmd/ipfs/daemon.go
@@ -142,14 +142,15 @@ environment variable:
 
 Routing
 
-IPFS by default will use a DHT for content routing. There is a highly
-experimental alternative that operates the DHT in a 'client only' mode that
-can be enabled by running the daemon as:
+IPFS by default will use a DHT for content routing. There is an alternative
+that operates the DHT in a 'client only' mode that can be enabled by
+running the daemon as:
 
   ipfs daemon --routing=dhtclient
 
-This will later be transitioned into a config option once it gets out of the
-'experimental' stage.
+Or you can set routing to dhtclient in the config:
+
+  ipfs config Routing.Type dhtclient
 
 DEPRECATION NOTICE
 
