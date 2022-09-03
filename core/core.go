82fbb84384d04be3433deb993af37f927432198c
Jorropo <jorropo.pgm@gmail.com>
2022-09-03 15:47:39 +0200

    fmt: applies go1.19 fmt

--- core/core.go
+++ core/core.go
@@ -5,7 +5,7 @@ Packages underneath core/ provide a (relatively) stable, low-level API
 to carry out most IPFS-related tasks.  For more details on the other
 interfaces and how core/... fits into the bigger IPFS picture, see:
 
-  $ godoc github.com/ipfs/go-ipfs
+	$ godoc github.com/ipfs/go-ipfs
 */
 package core
 
