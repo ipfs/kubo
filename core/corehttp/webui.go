9cc5cd02e212671331df1c16d3e4f604ddf1223c
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-09-09 19:54:35 -0700

    Update webui.go

--- core/corehttp/webui.go
+++ core/corehttp/webui.go
@@ -6,7 +6,7 @@ const WebUIPath = "/ipfs/bafybeidb5eryh72zajiokdggzo7yct2d6hhcflncji5im2y5w26uuy
 // this is a list of all past webUI paths.
 var WebUIPaths = []string{
 	WebUIPath,
-	"/ipfs/bafybeibozpulxtpv5nhfa2ue3dcjx23ndh3gwr5vwllk7ptoyfwnfjjr4q"
+	"/ipfs/bafybeibozpulxtpv5nhfa2ue3dcjx23ndh3gwr5vwllk7ptoyfwnfjjr4q",
 	"/ipfs/bafybeiednzu62vskme5wpoj4bjjikeg3xovfpp4t7vxk5ty2jxdi4mv4bu",
 	"/ipfs/bafybeihcyruaeza7uyjd6ugicbcrqumejf6uf353e5etdkhotqffwtguva",
 	"/ipfs/bafybeiflkjt66aetfgcrgvv75izymd5kc47g6luepqmfq6zsf5w6ueth6y",
