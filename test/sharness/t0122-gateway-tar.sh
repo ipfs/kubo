56798ed4a8cb53366e55ec5fa03a9745805d7844
Henrique Dias <hacdias@gmail.com>
2022-10-06 12:20:09 +0200

    tests: test with relative paths inside and outside directory

--- test/sharness/t0122-gateway-tar.sh
+++ test/sharness/t0122-gateway-tar.sh
@@ -8,6 +8,8 @@ test_init_ipfs
 test_launch_ipfs_daemon_without_network
 
 FOO_HASH="bafybeiczqj6w5tggtshvlyevr24drgrboffuepe2lxeojsnwmfw4tpttzu"
+OUTSIDE_ROOT_HASH="bafybeicaj7kvxpcv4neaqzwhrqqmdstu4dhrwfpknrgebq6nzcecfucvyu"
+INSIDE_ROOT_HASH="bafybeibfevfxlvxp5vxobr5oapczpf7resxnleb7tkqmdorc4gl5cdva3y"
 
 test_expect_success "Add directory to test with" '
   ipfs dag import ../t0122-gateway-tar-data/foo.car > import_output &&
@@ -34,8 +36,23 @@ test_expect_success "GET TAR with 'Accept: application/x-tar' has expected Conte
   grep "< Content-Type: application/x-tar" curl_output_filename
 '
 
-# outside-root.car --> bafybeicaj7kvxpcv4neaqzwhrqqmdstu4dhrwfpknrgebq6nzcecfucvyu
-# inside-root.car --> bafybeibfevfxlvxp5vxobr5oapczpf7resxnleb7tkqmdorc4gl5cdva3y
+test_expect_success "Add directories with relative paths to test with" '
+  ipfs dag import ../t0122-gateway-tar-data/outside-root.car > import_output &&
+  grep -q $OUTSIDE_ROOT_HASH import_output &&
+  ipfs dag import ../t0122-gateway-tar-data/inside-root.car > import_output &&
+  grep -q $INSIDE_ROOT_HASH import_output
+'
+
+test_expect_success "GET TAR with relative paths outside root fails" '
+  curl -i "http://127.0.0.1:$GWAY_PORT/ipfs/$OUTSIDE_ROOT_HASH?format=tar" > curl_output_filename &&
+  grep -q "Trailer: X-Stream-Error" curl_output_filename &&
+  grep -q "X-Stream-Error:" curl_output_filename
+'
+
+test_expect_success "GET TAR with relative paths inside root works" '
+  curl -i "http://127.0.0.1:$GWAY_PORT/ipfs/$INSIDE_ROOT_HASH?format=tar" > curl_output_filename &&
+  ! grep -q "X-Stream-Error:" curl_output_filename
+'
 
 test_kill_ipfs_daemon
 
