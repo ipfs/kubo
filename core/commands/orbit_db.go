93dc9949e9c20f5e656bac990a8702f077b273a6
mar1n3r0 <m.kirkov@gmail.com>
2022-10-19 17:51:10 +0300

    transform query output to slice

--- core/commands/orbit_db.go
+++ core/commands/orbit_db.go
@@ -322,9 +322,11 @@ var OrbitQueryDocsCmd = &cmds.Command{
 
 		defer db.Close()
 
-		q, err := store.Query(req.Context, func(e interface{}) (bool, error) {
+		var results []interface{}
+		_, err = store.Query(req.Context, func(e interface{}) (bool, error) {
 			issue := e.(map[string]interface{})
 			if issue[key] == query {
+				results = append(results, issue)
 				return true, nil
 			}
 			return false, nil
@@ -334,7 +336,7 @@ var OrbitQueryDocsCmd = &cmds.Command{
 			return err
 		}
 
-		if err := res.Emit(&q); err != nil {
+		if err := res.Emit(&results); err != nil {
 			return err
 		}
 
