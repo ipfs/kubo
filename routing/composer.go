a3913d9f909edf5c1611130e445a6facc2c0624f
Antonio Navarro Perez <antnavper@gmail.com>
2022-12-01 16:15:22 +0100

    Add logs to the routing system
    Signed-off-by: Antonio Navarro Perez <antnavper@gmail.com>

--- routing/composer.go
+++ routing/composer.go
@@ -23,52 +23,99 @@ type Composer struct {
 }
 
 func (c *Composer) Provide(ctx context.Context, cid cid.Cid, provide bool) error {
-	return c.ProvideRouter.Provide(ctx, cid, provide)
+	log.Debug("composer: calling provide: ", cid)
+	err := c.ProvideRouter.Provide(ctx, cid, provide)
+	if err != nil {
+		log.Debug("composer: calling provide: ", cid, " error: ", err)
+
+	}
+
+	return err
 }
 
 func (c *Composer) ProvideMany(ctx context.Context, keys []multihash.Multihash) error {
+	log.Debug("composer: calling provide many: ", len(keys))
 	pmr, ok := c.ProvideRouter.(routinghelpers.ProvideManyRouter)
 	if !ok {
+		log.Debug("composer: provide many is not implemented on the actual router")
 		return nil
 	}
 
-	return pmr.ProvideMany(ctx, keys)
+	err := pmr.ProvideMany(ctx, keys)
+	if err != nil {
+		log.Debug("composer: calling provide many error: ", err)
+	}
+
+	return err
 }
 
 func (c *Composer) Ready() bool {
+	log.Debug("composer: calling ready")
 	pmr, ok := c.ProvideRouter.(routinghelpers.ProvideManyRouter)
 	if !ok {
-		return false
+		return true
 	}
 
-	return pmr.Ready()
+	ready := pmr.Ready()
+
+	log.Debug("composer: calling ready result: ", ready)
+
+	return ready
 }
 
 func (c *Composer) FindProvidersAsync(ctx context.Context, cid cid.Cid, count int) <-chan peer.AddrInfo {
+	log.Debug("composer: calling findProvidersAsync: ", cid)
 	return c.FindProvidersRouter.FindProvidersAsync(ctx, cid, count)
 }
 
 func (c *Composer) FindPeer(ctx context.Context, pid peer.ID) (peer.AddrInfo, error) {
-	return c.FindPeersRouter.FindPeer(ctx, pid)
+	log.Debug("composer: calling findPeer: ", pid)
+	addr, err := c.FindPeersRouter.FindPeer(ctx, pid)
+	if err != nil {
+		log.Debug("composer: calling findPeer error: ", pid, addr.String(), err)
+	}
+	return addr, err
 }
 
 func (c *Composer) PutValue(ctx context.Context, key string, val []byte, opts ...routing.Option) error {
-	return c.PutValueRouter.PutValue(ctx, key, val, opts...)
+	log.Debug("composer: calling putValue: ", key, len(val))
+	err := c.PutValueRouter.PutValue(ctx, key, val, opts...)
+	if err != nil {
+		log.Debug("composer: calling putValue error: ", key, len(val), err)
+	}
+
+	return err
 }
 
 func (c *Composer) GetValue(ctx context.Context, key string, opts ...routing.Option) ([]byte, error) {
-	return c.GetValueRouter.GetValue(ctx, key, opts...)
+	log.Debug("composer: calling getValue: ", key)
+	val, err := c.GetValueRouter.GetValue(ctx, key, opts...)
+	if err != nil {
+		log.Debug("composer: calling getValue error: ", key, len(val), err)
+	}
+
+	return val, err
 }
 
 func (c *Composer) SearchValue(ctx context.Context, key string, opts ...routing.Option) (<-chan []byte, error) {
-	return c.GetValueRouter.SearchValue(ctx, key, opts...)
+	log.Debug("composer: calling searchValue: ", key)
+	ch, err := c.GetValueRouter.SearchValue(ctx, key, opts...)
+	if err != nil {
+		log.Debug("composer: calling searchValue error: ", key, err)
+	}
+	return ch, err
 }
 
 func (c *Composer) Bootstrap(ctx context.Context) error {
+	log.Debug("composer: calling bootstrap")
 	errfp := c.FindPeersRouter.Bootstrap(ctx)
 	errfps := c.FindProvidersRouter.Bootstrap(ctx)
 	errgv := c.GetValueRouter.Bootstrap(ctx)
 	errpv := c.PutValueRouter.Bootstrap(ctx)
 	errp := c.ProvideRouter.Bootstrap(ctx)
-	return multierror.Append(errfp, errfps, errgv, errpv, errp)
+	err := multierror.Append(errfp, errfps, errgv, errpv, errp)
+	if err != nil {
+		log.Debug("composer: calling bootstrap error: ", err)
+	}
+	return err
 }
