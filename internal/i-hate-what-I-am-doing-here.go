76e0caa93682395c5fc55c6e238c2b9934df7235
Jorropo <jorropo.pgm@gmail.com>
2022-12-09 21:46:26 +0100

    fix: remove the imports work-around
    It seems to be fine now that everything has been updated.

--- internal/i-hate-what-I-am-doing-here.go
+++
@@ -1,9 +0,0 @@
-package internal
-
-// darkmargic workaround for https://github.com/golang/go/issues/56494
-// DO NOT TOUCH
-// TODO(@Jorropo): touch this once fixed
-import (
-	_ "github.com/btcsuite/btcd/btcjson"
-	_ "github.com/libp2p/go-libp2p-core" //nolint
-)
