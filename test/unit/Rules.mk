579175f81d400000881af5701c06351373df3fb8
Gus Eggert <gus@gus.dev>
2022-12-12 09:17:56 -0500

    feat: add basic CLI tests using Go Test
    This is intended as a replacement for sharness. These are vanilla Go
    tests which can be run in your IDE for quick iteration on end-to-end
    CLI tests.
    
    This also removes IPTB by duplicating its functionality in the test
    harness. This isn't a big deal...IPTB's complexity is mostly around
    the fact that its state needs to be saved to disk in between `iptb`
    command invocations, and that it uses Go plugins to inject
    functionality, neither of which are relevant here.
    
    If we merge this, we'll have to live with bifurcated tests for a while
    until they are all migrated. I'd recommend we self-enforce a rule
    that, if we need to touch a sharness test, we migrate it and one more
    test over to Go tests first. Then eventually we will have migrated
    everything.

--- test/unit/Rules.mk
+++ test/unit/Rules.mk
@@ -2,7 +2,7 @@ include mk/header.mk
 
 CLEAN += $(d)/gotest.json $(d)/gotest.junit.xml
 
-$(d)/gotest.junit.xml: clean test/bin/gotestsum coverage/unit_tests.coverprofile
+$(d)/gotest.junit.xml: test/bin/gotestsum coverage/unit_tests.coverprofile
 	gotestsum --no-color --junitfile $@ --raw-command cat $(@D)/gotest.json
 
 include mk/footer.mk
