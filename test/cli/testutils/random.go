430a65ad5f78dc2aed0f54946b2c5922fa4b1ee1
Gus Eggert <gus@gus.dev>
2022-12-12 09:16:55 -0500

    feat: port pins CLI test

---
+++ test/cli/testutils/random.go
@@ -0,0 +1,12 @@
+package testutils
+
+import "crypto/rand"
+
+func RandomBytes(n int) []byte {
+	bytes := make([]byte, n)
+	_, err := rand.Read(bytes)
+	if err != nil {
+		panic(err)
+	}
+	return bytes
+}
