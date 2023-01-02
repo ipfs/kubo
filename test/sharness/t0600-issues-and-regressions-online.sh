648f765afa429fd35302fd168c2a9559ae621276
Marcin Rataj <lidel@lidel.org>
2023-01-02 14:51:02 +0100

    fix(test): stabilize flaky provider tests

--- test/sharness/t0600-issues-and-regressions-online.sh
+++ test/sharness/t0600-issues-and-regressions-online.sh
@@ -34,15 +34,15 @@ test_expect_success "metrics work" '
 '
 
 test_expect_success "pin add api looks right - #3753" '
-  HASH=$(echo "foo" | ipfs add -q) &&
+  HASH=$(date +"%FT%T.%N%z" | ipfs add -q) &&
   curl -X POST "http://$API_ADDR/api/v0/pin/add/$HASH" > pinadd_out &&
-  echo "{\"Pins\":[\"QmYNmQKp6SuaVrpgWRsPTgCQCnpxUYGq76YEKBXuj2N4H6\"]}" > pinadd_exp &&
+  echo "{\"Pins\":[\"$HASH\"]}" > pinadd_exp &&
   test_cmp pinadd_out pinadd_exp
 '
 
 test_expect_success "pin add api looks right - #3753" '
   curl -X POST "http://$API_ADDR/api/v0/pin/rm/$HASH" > pinrm_out &&
-  echo "{\"Pins\":[\"QmYNmQKp6SuaVrpgWRsPTgCQCnpxUYGq76YEKBXuj2N4H6\"]}" > pinrm_exp &&
+  echo "{\"Pins\":[\"$HASH\"]}" > pinrm_exp &&
   test_cmp pinrm_out pinrm_exp
 '
 
