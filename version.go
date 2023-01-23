816904386bf6888140edd2d55fdfca6b99300f69
galargh <piotr.galar@gmail.com>
2023-01-23 13:41:02 +0100

    chore: update version.go

--- version.go
+++ version.go
@@ -11,7 +11,7 @@ import (
 var CurrentCommit string
 
 // CurrentVersionNumber is the current application's version literal
-const CurrentVersionNumber = "0.18.0-rc2"
+const CurrentVersionNumber = "0.18.0"
 
 const ApiVersion = "/kubo/" + CurrentVersionNumber + "/" //nolint
 
