b3cc938630bc7607f86afed9ba4c1cdfda07e828
Henrique Dias <hacdias@gmail.com>
2023-01-24 23:44:55 +0100

    feat: add namesys publish options (#94)
    * feat: add namesys publish options
    * feat: export DefaultIPNSRecordEOL
    * feat: export DefaultIPNSRecordTTL
    
    This commit was moved from ipfs/interface-go-ipfs-core@468dea4bb45aec6ddce2a6225334dcc062d6e752
    
    This commit was moved from ipfs/boxo@bcb9190c2bdbc0ad4823a1b1785fb87ec8014723

--- core/coreiface/options/namesys/opts.go
+++ core/coreiface/options/namesys/opts.go
@@ -13,6 +13,15 @@ const (
 	// trust resolution to eventually complete and can't put an upper
 	// limit on how many steps it will take.
 	UnlimitedDepth = 0
+
+	// DefaultIPNSRecordTTL specifies the time that the record can be cached
+	// before checking if its validity again.
+	DefaultIPNSRecordTTL = time.Minute
+
+	// DefaultIPNSRecordEOL specifies the time that the network will cache IPNS
+	// records after being published. Records should be re-published before this
+	// interval expires. We use the same default expiration as the DHT.
+	DefaultIPNSRecordEOL = 48 * time.Hour
 )
 
 // ResolveOpts specifies options for resolving an IPNS path
@@ -67,6 +76,46 @@ func DhtTimeout(timeout time.Duration) ResolveOpt {
 // ProcessOpts converts an array of ResolveOpt into a ResolveOpts object
 func ProcessOpts(opts []ResolveOpt) ResolveOpts {
 	rsopts := DefaultResolveOpts()
+	for _, option := range opts {
+		option(&rsopts)
+	}
+	return rsopts
+}
+
+// PublishOptions specifies options for publishing an IPNS record.
+type PublishOptions struct {
+	EOL time.Time
+	TTL time.Duration
+}
+
+// DefaultPublishOptions returns the default options for publishing an IPNS record.
+func DefaultPublishOptions() PublishOptions {
+	return PublishOptions{
+		EOL: time.Now().Add(DefaultIPNSRecordEOL),
+		TTL: DefaultIPNSRecordTTL,
+	}
+}
+
+// PublishOption is used to set an option for PublishOpts.
+type PublishOption func(*PublishOptions)
+
+// PublishWithEOL sets an EOL.
+func PublishWithEOL(eol time.Time) PublishOption {
+	return func(o *PublishOptions) {
+		o.EOL = eol
+	}
+}
+
+// PublishWithEOL sets a TTL.
+func PublishWithTTL(ttl time.Duration) PublishOption {
+	return func(o *PublishOptions) {
+		o.TTL = ttl
+	}
+}
+
+// ProcessPublishOptions converts an array of PublishOpt into a PublishOpts object.
+func ProcessPublishOptions(opts []PublishOption) PublishOptions {
+	rsopts := DefaultPublishOptions()
 	for _, option := range opts {
 		option(&rsopts)
 	}
