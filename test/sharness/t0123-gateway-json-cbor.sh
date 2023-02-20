7f3c0baa5d009fd1eb6e8ae58ebc4eaef017bca7
Laurent Senta <laurent@singulargarden.com>
2023-02-20 16:24:57 +0100

    test: use car fixtures in sharness t0123

--- test/sharness/t0123-gateway-json-cbor.sh
+++ test/sharness/t0123-gateway-json-cbor.sh
@@ -7,22 +7,15 @@ test_description="Test HTTP Gateway DAG-JSON (application/vnd.ipld.dag-json) and
 test_init_ipfs
 test_launch_ipfs_daemon_without_network
 
+# Import test case
+# See the static fixtures in ./t0123-gateway-json-cbor/
 test_expect_success "Add the test directory" '
-  mkdir -p rootDir/ipfs &&
-  mkdir -p rootDir/ipns &&
-  mkdir -p rootDir/api &&
-  mkdir -p rootDir/ą/ę &&
-  echo "{ \"test\": \"i am a plain json file\" }" > rootDir/ą/ę/t.json &&
-  echo "I am a txt file on path with utf8" > rootDir/ą/ę/file-źł.txt &&
-  echo "I am a txt file in confusing /api dir" > rootDir/api/file.txt &&
-  echo "I am a txt file in confusing /ipfs dir" > rootDir/ipfs/file.txt &&
-  echo "I am a txt file in confusing /ipns dir" > rootDir/ipns/file.txt &&
-  DIR_CID=$(ipfs add -Qr --cid-version 1 rootDir) &&
-  FILE_JSON_CID=$(ipfs files stat --enc=json /ipfs/$DIR_CID/ą/ę/t.json | jq -r .Hash) &&
-  FILE_CID=$(ipfs files stat --enc=json /ipfs/$DIR_CID/ą/ę/file-źł.txt | jq -r .Hash) &&
-  FILE_SIZE=$(ipfs files stat --enc=json /ipfs/$DIR_CID/ą/ę/file-źł.txt | jq -r .Size)
-  echo "$FILE_CID / $FILE_SIZE"
+  ipfs dag import ../t0123-gateway-json-cbor/fixtures.car
 '
+DIR_CID=bafybeiafyvqlazbbbtjnn6how5d6h6l6rxbqc4qgpbmteaiskjrffmyy4a # ./rootDir
+FILE_JSON_CID=bafkreibrppizs3g7axs2jdlnjua6vgpmltv7k72l7v7sa6mmht6mne3qqe # ./rootDir/ą/ę/t.json
+FILE_CID=bafkreialihlqnf5uwo4byh4n3cmwlntwqzxxs2fg5vanqdi3d7tb2l5xkm # ./rootDir/ą/ę/file-źł.txt
+FILE_SIZE=34
 
 ## Quick regression check for JSON stored on UnixFS:
 ## it has nothing to do with DAG-JSON and JSON codecs,
