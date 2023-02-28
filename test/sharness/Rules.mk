da3596ed1a4de8e4c3d0e5e1795673624cfcb4e7
galargh <piotr.galar@gmail.com>
2023-02-28 13:00:23 +0100

    ci: replace junit html generation with gh action

--- test/sharness/Rules.mk
+++ test/sharness/Rules.mk
@@ -47,21 +47,6 @@ $(d)/test-results/sharness.xml: $(T_$(d))
 	@(cd $(@D)/.. && ./lib/test-aggregate-junit-reports.sh)
 .PHONY: $(d)/test-results/sharness.xml
 
-$(d)/download-saxon:
-	@echo "*** $@ ***"
-	@(cd $(@D) && ./lib/download-saxon.sh)
-.PHONY: $(d)/download-saxon
-
-$(d)/test-results/sharness-html: $(d)/test-results/sharness.xml $(d)/download-saxon
-	@echo "*** $@ ***"
-	@(cd $(@D)/.. && ./lib/test-generate-junit-html.sh frames)
-.PHONY: $(d)/test-results/sharness-html
-
-$(d)/test-results/sharness.html: $(d)/test-results/sharness.xml $(d)/download-saxon
-	@echo "*** $@ ***"
-	@(cd $(@D)/.. && ./lib/test-generate-junit-html.sh no-frames)
-.PHONY: $(d)/test-results/sharness.html
-
 $(d)/clean-test-results:
 	rm -rf $(@D)/test-results
 .PHONY: $(d)/clean-test-results
