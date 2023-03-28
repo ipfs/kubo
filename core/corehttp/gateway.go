1e3b6c98579cf549f4042e4fa76ec9d155f82bf3
Jorropo <jorropo.pgm@gmail.com>
2023-03-28 21:52:36 +0200

    feat: add tracing to the commands client

--- core/corehttp/gateway.go
+++ core/corehttp/gateway.go
@@ -48,6 +48,8 @@ func GatewayOption(paths ...string) ServeOption {
 		}
 
 		gw := gateway.NewHandler(gwConfig, gwAPI)
+		// TODO: Add otelhttp.WithPropagators(tracing.Propagator()) option to
+		// propagate traces through the gateway once we test this feature.
 		gw = otelhttp.NewHandler(gw, "Gateway.Request")
 
 		// By default, our HTTP handler is the gateway handler.
