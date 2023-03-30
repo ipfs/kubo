e89cce63fd1428c40031edb30b407cbb468abb08
Arthur Gavazza <32915690+arthurgavazza@users.noreply.github.com>
2023-03-30 01:34:57 -0300

    feat: add identify option to swarm peers command
    Fixes #9578

---
+++ test/cli/harness/peering.go
@@ -0,0 +1,37 @@
+package harness
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+
+	"github.com/ipfs/kubo/config"
+)
+
+type Peering struct {
+	From int
+	To   int
+}
+
+func newRandPort() int {
+	n := rand.Int()
+	return 3000 + (n % 1000)
+}
+
+func CreatePeerNodes(t *testing.T, n int, peerings []Peering) (*Harness, Nodes) {
+	h := NewT(t)
+	nodes := h.NewNodes(n).Init()
+	nodes.ForEachPar(func(node *Node) {
+		node.UpdateConfig(func(cfg *config.Config) {
+			cfg.Routing.Type = config.NewOptionalString("none")
+			cfg.Addresses.Swarm = []string{fmt.Sprintf("/ip4/127.0.0.1/tcp/%d", newRandPort())}
+		})
+
+	})
+
+	for _, peering := range peerings {
+		nodes[peering.From].PeerWith(nodes[peering.To])
+	}
+
+	return h, nodes
+}
