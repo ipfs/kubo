ced348366c13c841c93ec5734732e5789cba4cbb
Amir Mohammad Fakhimi <79265203+AmirMohammadFakhimi@users.noreply.github.com>
2023-08-17 18:13:27 +0330

    feat: add zsh completions (#10040)
    Co-authored-by: Henrique Dias <hacdias@gmail.com>

--- test/cli/completion_test.go
+++ test/cli/completion_test.go
@@ -29,3 +29,29 @@ func TestBashCompletion(t *testing.T) {
 		assert.NoError(t, res.Err)
 	})
 }
+
+func TestZshCompletion(t *testing.T) {
+	t.Parallel()
+	h := harness.NewT(t)
+	node := h.NewNode()
+
+	res := node.IPFS("commands", "completion", "zsh")
+
+	length := len(res.Stdout.String())
+	if length < 100 {
+		t.Fatalf("expected a long Bash completion file, but got one of length %d", length)
+	}
+
+	t.Run("completion file can be loaded in bash", func(t *testing.T) {
+		RequiresLinux(t)
+
+		completionFile := h.WriteToTemp(res.Stdout.String())
+		res = h.Runner.Run(harness.RunRequest{
+			Path: "zsh",
+			Args: []string{"-c", fmt.Sprintf("autoload -Uz compinit && compinit && source %s && echo -E $_comps[ipfs]", completionFile)},
+		})
+
+		assert.NoError(t, res.Err)
+		assert.NotEmpty(t, res.Stdout.String())
+	})
+}
