8954a7951b77be2e7cbf2b1b8f2adaf333c09124
gammazero <gammazero@users.noreply.github.com>
2023-09-28 18:10:51 -0700

    Migrate peering to ipfs/boxo
    The peering package is being used outside kubo, so should be relocated to boxo.
    
    See https://github.com/ipfs/boxo/pull/473

--- core/node/peering.go
+++ core/node/peering.go
@@ -3,7 +3,7 @@ package node
 import (
 	"context"
 
-	"github.com/ipfs/kubo/peering"
+	"github.com/ipfs/boxo/peering"
 	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/peer"
 	"go.uber.org/fx"
