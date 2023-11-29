dec956ee262a7c5adffd49ecb23675ff2868dc71
Henrique Dias <hacdias@gmail.com>
2023-11-29 13:17:53 +0100

    refactor: use assert and require in key tests

--- core/coreiface/tests/key.go
+++ core/coreiface/tests/key.go
@@ -10,6 +10,7 @@ import (
 	opt "github.com/ipfs/kubo/core/coreiface/options"
 	"github.com/libp2p/go-libp2p/core/peer"
 	mbase "github.com/multiformats/go-multibase"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -43,145 +44,83 @@ func (tp *TestSuite) TestKey(t *testing.T) {
 func (tp *TestSuite) TestListSelf(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
 	api, err := tp.makeAPI(t, ctx)
-	if err != nil {
-		t.Fatal(err)
-		return
-	}
+	require.NoError(t, err)
 
 	self, err := api.Key().Self(ctx)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	keys, err := api.Key().List(ctx)
-	if err != nil {
-		t.Fatalf("failed to list keys: %s", err)
-		return
-	}
-
-	if len(keys) != 1 {
-		t.Fatalf("there should be 1 key (self), got %d", len(keys))
-		return
-	}
-
-	if keys[0].Name() != "self" {
-		t.Errorf("expected the key to be called 'self', got '%s'", keys[0].Name())
-	}
-
-	if keys[0].Path().String() != "/ipns/"+iface.FormatKeyID(self.ID()) {
-		t.Errorf("expected the key to have path '/ipns/%s', got '%s'", iface.FormatKeyID(self.ID()), keys[0].Path().String())
-	}
+	require.NoError(t, err)
+	require.Len(t, keys, 1)
+	assert.Equal(t, "self", keys[0].Name())
+	assert.Equal(t, "/ipns/"+iface.FormatKeyID(self.ID()), keys[0].Path().String())
 }
 
 func (tp *TestSuite) TestRenameSelf(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
 	api, err := tp.makeAPI(t, ctx)
-	if err != nil {
-		t.Fatal(err)
-		return
-	}
+	require.NoError(t, err)
 
 	_, _, err = api.Key().Rename(ctx, "self", "foo")
-	if err == nil {
-		t.Error("expected error to not be nil")
-	} else {
-		if !strings.Contains(err.Error(), "cannot rename key with name 'self'") {
-			t.Fatalf("expected error 'cannot rename key with name 'self'', got '%s'", err.Error())
-		}
-	}
+	require.ErrorContains(t, err, "cannot rename key with name 'self'")
 
 	_, _, err = api.Key().Rename(ctx, "self", "foo", opt.Key.Force(true))
-	if err == nil {
-		t.Error("expected error to not be nil")
-	} else {
-		if !strings.Contains(err.Error(), "cannot rename key with name 'self'") {
-			t.Fatalf("expected error 'cannot rename key with name 'self'', got '%s'", err.Error())
-		}
-	}
+	require.ErrorContains(t, err, "cannot rename key with name 'self'")
 }
 
 func (tp *TestSuite) TestRemoveSelf(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
 	api, err := tp.makeAPI(t, ctx)
-	if err != nil {
-		t.Fatal(err)
-		return
-	}
+	require.NoError(t, err)
 
 	_, err = api.Key().Remove(ctx, "self")
-	if err == nil {
-		t.Error("expected error to not be nil")
-	} else {
-		if !strings.Contains(err.Error(), "cannot remove key with name 'self'") {
-			t.Fatalf("expected error 'cannot remove key with name 'self'', got '%s'", err.Error())
-		}
-	}
+	require.ErrorContains(t, err, "cannot remove key with name 'self'")
 }
 
 func (tp *TestSuite) TestGenerate(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
 	api, err := tp.makeAPI(t, ctx)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	k, err := api.Key().Generate(ctx, "foo")
-	if err != nil {
-		t.Fatal(err)
-		return
-	}
-
-	if k.Name() != "foo" {
-		t.Errorf("expected the key to be called 'foo', got '%s'", k.Name())
-	}
+	require.NoError(t, err)
+	require.Equal(t, "foo", k.Name())
 
 	verifyIPNSPath(t, k.Path().String())
 }
 
-func verifyIPNSPath(t *testing.T, p string) bool {
+func verifyIPNSPath(t *testing.T, p string) {
 	t.Helper()
-	if !strings.HasPrefix(p, "/ipns/") {
-		t.Errorf("path %q does not look like an IPNS path", p)
-		return false
-	}
+
+	require.True(t, strings.HasPrefix(p, "/ipns/"))
+
 	k := p[len("/ipns/"):]
 	c, err := cid.Decode(k)
-	if err != nil {
-		t.Errorf("failed to decode IPNS key %q (%v)", k, err)
-		return false
-	}
+	require.NoError(t, err)
+
 	b36, err := c.StringOfBase(mbase.Base36)
-	if err != nil {
-		t.Fatalf("cid cannot format itself in b36")
-		return false
-	}
-	if b36 != k {
-		t.Errorf("IPNS key is not base36")
-	}
-	return true
+	require.NoError(t, err)
+	require.Equal(t, k, b36)
 }
 
 func (tp *TestSuite) TestGenerateSize(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
 	api, err := tp.makeAPI(t, ctx)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	k, err := api.Key().Generate(ctx, "foo", opt.Key.Size(2048))
-	if err != nil {
-		t.Fatal(err)
-		return
-	}
-
-	if k.Name() != "foo" {
-		t.Errorf("expected the key to be called 'foo', got '%s'", k.Name())
-	}
+	require.NoError(t, err)
+	require.Equal(t, "foo", k.Name())
 
 	verifyIPNSPath(t, k.Path().String())
 }
@@ -193,93 +132,47 @@ func (tp *TestSuite) TestGenerateType(t *testing.T) {
 	defer cancel()
 
 	api, err := tp.makeAPI(t, ctx)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	k, err := api.Key().Generate(ctx, "bar", opt.Key.Type(opt.Ed25519Key))
-	if err != nil {
-		t.Fatal(err)
-		return
-	}
-
-	if k.Name() != "bar" {
-		t.Errorf("expected the key to be called 'foo', got '%s'", k.Name())
-	}
-
+	require.NoError(t, err)
+	require.Equal(t, "bar", k.Name())
 	// Expected to be an inlined identity hash.
-	if !strings.HasPrefix(k.Path().String(), "/ipns/12") {
-		t.Errorf("expected the key to be prefixed with '/ipns/12', got '%s'", k.Path().String())
-	}
+	require.True(t, strings.HasPrefix(k.Path().String(), "/ipns/12"))
 }
 
 func (tp *TestSuite) TestGenerateExisting(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
 	api, err := tp.makeAPI(t, ctx)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	_, err = api.Key().Generate(ctx, "foo")
-	if err != nil {
-		t.Fatal(err)
-		return
-	}
+	require.NoError(t, err)
 
 	_, err = api.Key().Generate(ctx, "foo")
-	if err == nil {
-		t.Error("expected error to not be nil")
-	} else {
-		if !strings.Contains(err.Error(), "key with name 'foo' already exists") {
-			t.Fatalf("expected error 'key with name 'foo' already exists', got '%s'", err.Error())
-		}
-	}
+	require.ErrorContains(t, err, "key with name 'foo' already exists")
 
 	_, err = api.Key().Generate(ctx, "self")
-	if err == nil {
-		t.Error("expected error to not be nil")
-	} else {
-		if !strings.Contains(err.Error(), "cannot create key with name 'self'") {
-			t.Fatalf("expected error 'cannot create key with name 'self'', got '%s'", err.Error())
-		}
-	}
+	require.ErrorContains(t, err, "cannot create key with name 'self'")
 }
 
 func (tp *TestSuite) TestList(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
 	api, err := tp.makeAPI(t, ctx)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	_, err = api.Key().Generate(ctx, "foo")
-	if err != nil {
-		t.Fatal(err)
-		return
-	}
+	require.NoError(t, err)
 
 	l, err := api.Key().List(ctx)
-	if err != nil {
-		t.Fatal(err)
-		return
-	}
-
-	if len(l) != 2 {
-		t.Fatalf("expected to get 2 keys, got %d", len(l))
-		return
-	}
-
-	if l[0].Name() != "self" {
-		t.Fatalf("expected key 0 to be called 'self', got '%s'", l[0].Name())
-		return
-	}
-
-	if l[1].Name() != "foo" {
-		t.Fatalf("expected key 1 to be called 'foo', got '%s'", l[1].Name())
-		return
-	}
+	require.NoError(t, err)
+	require.Len(t, l, 2)
+	require.Equal(t, "self", l[0].Name())
+	require.Equal(t, "foo", l[1].Name())
 
 	verifyIPNSPath(t, l[0].Path().String())
 	verifyIPNSPath(t, l[1].Path().String())
@@ -288,256 +181,138 @@ func (tp *TestSuite) TestList(t *testing.T) {
 func (tp *TestSuite) TestRename(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
 	api, err := tp.makeAPI(t, ctx)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	_, err = api.Key().Generate(ctx, "foo")
-	if err != nil {
-		t.Fatal(err)
-		return
-	}
+	require.NoError(t, err)
 
 	k, overwrote, err := api.Key().Rename(ctx, "foo", "bar")
-	if err != nil {
-		t.Fatal(err)
-		return
-	}
-
-	if overwrote {
-		t.Error("overwrote should be false")
-	}
-
-	if k.Name() != "bar" {
-		t.Errorf("returned key should be called 'bar', got '%s'", k.Name())
-	}
+	require.NoError(t, err)
+	assert.False(t, overwrote)
+	assert.Equal(t, "bar", k.Name())
 }
 
 func (tp *TestSuite) TestRenameToSelf(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
 	api, err := tp.makeAPI(t, ctx)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	_, err = api.Key().Generate(ctx, "foo")
-	if err != nil {
-		t.Fatal(err)
-		return
-	}
+	require.NoError(t, err)
 
 	_, _, err = api.Key().Rename(ctx, "foo", "self")
-	if err == nil {
-		t.Error("expected error to not be nil")
-	} else {
-		if !strings.Contains(err.Error(), "cannot overwrite key with name 'self'") {
-			t.Fatalf("expected error 'cannot overwrite key with name 'self'', got '%s'", err.Error())
-		}
-	}
+	require.ErrorContains(t, err, "cannot overwrite key with name 'self'")
 }
 
 func (tp *TestSuite) TestRenameToSelfForce(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
 	api, err := tp.makeAPI(t, ctx)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	_, err = api.Key().Generate(ctx, "foo")
-	if err != nil {
-		t.Fatal(err)
-		return
-	}
+	require.NoError(t, err)
 
 	_, _, err = api.Key().Rename(ctx, "foo", "self", opt.Key.Force(true))
-	if err == nil {
-		t.Error("expected error to not be nil")
-	} else {
-		if !strings.Contains(err.Error(), "cannot overwrite key with name 'self'") {
-			t.Fatalf("expected error 'cannot overwrite key with name 'self'', got '%s'", err.Error())
-		}
-	}
+	require.ErrorContains(t, err, "cannot overwrite key with name 'self'")
 }
 
 func (tp *TestSuite) TestRenameOverwriteNoForce(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
 	api, err := tp.makeAPI(t, ctx)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	_, err = api.Key().Generate(ctx, "foo")
-	if err != nil {
-		t.Fatal(err)
-		return
-	}
+	require.NoError(t, err)
 
 	_, err = api.Key().Generate(ctx, "bar")
-	if err != nil {
-		t.Fatal(err)
-		return
-	}
+	require.NoError(t, err)
 
 	_, _, err = api.Key().Rename(ctx, "foo", "bar")
-	if err == nil {
-		t.Error("expected error to not be nil")
-	} else {
-		if !strings.Contains(err.Error(), "key by that name already exists, refusing to overwrite") {
-			t.Fatalf("expected error 'key by that name already exists, refusing to overwrite', got '%s'", err.Error())
-		}
-	}
+	require.ErrorContains(t, err, "key by that name already exists, refusing to overwrite")
 }
 
 func (tp *TestSuite) TestRenameOverwrite(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
 	api, err := tp.makeAPI(t, ctx)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	kfoo, err := api.Key().Generate(ctx, "foo")
-	if err != nil {
-		t.Fatal(err)
-		return
-	}
+	require.NoError(t, err)
 
 	_, err = api.Key().Generate(ctx, "bar")
-	if err != nil {
-		t.Fatal(err)
-		return
-	}
+	require.NoError(t, err)
 
 	k, overwrote, err := api.Key().Rename(ctx, "foo", "bar", opt.Key.Force(true))
-	if err != nil {
-		t.Fatal(err)
-		return
-	}
-
-	if !overwrote {
-		t.Error("overwrote should be true")
-	}
-
-	if k.Name() != "bar" {
-		t.Errorf("returned key should be called 'bar', got '%s'", k.Name())
-	}
-
-	if k.Path().String() != kfoo.Path().String() {
-		t.Errorf("k and kfoo should have equal paths, '%s'!='%s'", k.Path().String(), kfoo.Path().String())
-	}
+	require.NoError(t, err)
+	require.True(t, overwrote)
+	assert.Equal(t, "bar", k.Name())
+	assert.Equal(t, kfoo.Path().String(), k.Path().String())
 }
 
 func (tp *TestSuite) TestRenameSameNameNoForce(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
 	api, err := tp.makeAPI(t, ctx)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	_, err = api.Key().Generate(ctx, "foo")
-	if err != nil {
-		t.Fatal(err)
-		return
-	}
+	require.NoError(t, err)
 
 	k, overwrote, err := api.Key().Rename(ctx, "foo", "foo")
-	if err != nil {
-		t.Fatal(err)
-		return
-	}
-
-	if overwrote {
-		t.Error("overwrote should be false")
-	}
-
-	if k.Name() != "foo" {
-		t.Errorf("returned key should be called 'foo', got '%s'", k.Name())
-	}
+	require.NoError(t, err)
+	assert.False(t, overwrote)
+	assert.Equal(t, "foo", k.Name())
 }
 
 func (tp *TestSuite) TestRenameSameName(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
 	api, err := tp.makeAPI(t, ctx)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	_, err = api.Key().Generate(ctx, "foo")
-	if err != nil {
-		t.Fatal(err)
-		return
-	}
+	require.NoError(t, err)
 
 	k, overwrote, err := api.Key().Rename(ctx, "foo", "foo", opt.Key.Force(true))
-	if err != nil {
-		t.Fatal(err)
-		return
-	}
-
-	if overwrote {
-		t.Error("overwrote should be false")
-	}
-
-	if k.Name() != "foo" {
-		t.Errorf("returned key should be called 'foo', got '%s'", k.Name())
-	}
+	require.NoError(t, err)
+	assert.False(t, overwrote)
+	assert.Equal(t, "foo", k.Name())
 }
 
 func (tp *TestSuite) TestRemove(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
 	api, err := tp.makeAPI(t, ctx)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	k, err := api.Key().Generate(ctx, "foo")
-	if err != nil {
-		t.Fatal(err)
-		return
-	}
+	require.NoError(t, err)
 
 	l, err := api.Key().List(ctx)
-	if err != nil {
-		t.Fatal(err)
-		return
-	}
-
-	if len(l) != 2 {
-		t.Fatalf("expected to get 2 keys, got %d", len(l))
-		return
-	}
+	require.NoError(t, err)
+	require.Len(t, l, 2)
 
 	p, err := api.Key().Remove(ctx, "foo")
-	if err != nil {
-		t.Fatal(err)
-		return
-	}
-
-	if k.Path().String() != p.Path().String() {
-		t.Errorf("k and p should have equal paths, '%s'!='%s'", k.Path().String(), p.Path().String())
-	}
+	require.NoError(t, err)
+	assert.Equal(t, p.Path().String(), k.Path().String())
 
 	l, err = api.Key().List(ctx)
-	if err != nil {
-		t.Fatal(err)
-		return
-	}
-
-	if len(l) != 1 {
-		t.Fatalf("expected to get 1 key, got %d", len(l))
-		return
-	}
-
-	if l[0].Name() != "self" {
-		t.Errorf("expected the key to be called 'self', got '%s'", l[0].Name())
-	}
+	require.NoError(t, err)
+	require.Len(t, l, 1)
+	assert.Equal(t, "self", l[0].Name())
 }
 
 func (tp *TestSuite) TestSign(t *testing.T) {
