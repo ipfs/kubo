7b05b5dd33eebd18d0325202d8f2dc312e74dc5a
Marcin Rataj <lidel@lidel.org>
2023-12-01 01:21:36 +0100

    docs(cli): fix spelling
    this fixes lint errors in RPC docs that were raised by vole in
    https://github.com/ipfs/ipfs-docs/pull/1772
    so we don't get them in kubo 0.25

--- core/commands/resolve.go
+++ core/commands/resolve.go
@@ -70,7 +70,7 @@ Resolve the value of an IPFS DAG path:
 	Options: []cmds.Option{
 		cmds.BoolOption(resolveRecursiveOptionName, "r", "Resolve until the result is an IPFS name.").WithDefault(true),
 		cmds.IntOption(resolveDhtRecordCountOptionName, "dhtrc", "Number of records to request for DHT resolution."),
-		cmds.StringOption(resolveDhtTimeoutOptionName, "dhtt", "Max time to collect values during DHT resolution eg \"30s\". Pass 0 for no timeout."),
+		cmds.StringOption(resolveDhtTimeoutOptionName, "dhtt", "Max time to collect values during DHT resolution e.g. \"30s\". Pass 0 for no timeout."),
 	},
 	Run: func(req *cmds.Request, res cmds.ResponseEmitter, env cmds.Environment) error {
 		api, err := cmdenv.GetApi(env, req)
