75dc0c63cb771f8aef783d5fa67e5e26f20ec63b
Marcin Rataj <lidel@lidel.org>
2024-11-08 19:36:44 +0100

    feat(bootstrap): /dnsaddr/va1.bootstrap.libp2p.io
    this adds
    https://github.com/libp2p/js-libp2p-amino-dht-bootstrapper
    instance at /dnsaddr/va1.bootstrap.libp2p.io
    to ensure Kubo does not depend on single language stack
    for initial bootstrap

--- config/bootstrap_peers.go
+++ config/bootstrap_peers.go
@@ -19,6 +19,9 @@ var DefaultBootstrapAddresses = []string{
 	"/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
 	"/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
 	"/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt",
+
+	"/dnsaddr/va1.bootstrap.libp2p.io/p2p/12D3KooWKnDdG3iXw9eTFijk3EWSunZcFi54Zka4wmtqtt6rPxc8", // js-libp2p-amino-dht-bootstrapper
+
 	"/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ",         // mars.i.ipfs.io
 	"/ip4/104.131.131.82/udp/4001/quic-v1/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ", // mars.i.ipfs.io
 }
