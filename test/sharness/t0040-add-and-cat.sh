3af71554d9d0b301941e023c532cf0f3fde583d5
youyyytrok <d.ionov.beats@gmail.com>
2025-01-15 13:21:13 +0100

    Fix CID's t0040-add-and-cat.sh

--- test/sharness/t0040-add-and-cat.sh
+++ test/sharness/t0040-add-and-cat.sh
@@ -887,12 +887,12 @@ test_expect_success "'ipfs add -rn' succeeds" '
 '
 
 test_expect_success "'ipfs add -rn' output looks good" '
-  MOONS="QmVKvomp91nMih5j6hYBA8KjbiaYvEetU2Q7KvtZkLe9nQ" &&
+  MOONS="QmbGoaQZm8kjYfCiN1aBsgwhqfUBGDYTrDb91Mz7Dvq81B" &&
   EUROPA="Qmbjg7zWdqdMaK2BucPncJQDxiALExph5k3NkQv5RHpccu" &&
   JUPITER="QmS5mZddhFPLWFX3w6FzAy9QxyYkaxvUpsWCtZ3r7jub9J" &&
   SATURN="QmaMagZT4rTE7Nonw8KGSK4oe1bh533yhZrCo1HihSG8FK" &&
   TITAN="QmZzppb9WHn552rmRqpPfgU5FEiHH6gDwi3MrB9cTdPwdb" &&
-  MERCURY="QmUJjVtnN8YEeYcS8VmUeWffTWhnMQAkk5DzZdKnPhqUdK" &&
+  MERCURY="QmRsTB5CpEUvDUpDgHCzb3VftZ139zrk9zs5ZcgYh9TMPJ" &&
   echo "added $EUROPA moons/jupiter/europa.txt" >expected &&
   echo "added $MERCURY moons/mercury.txt" >>expected &&
   echo "added $TITAN moons/saturn/titan.txt" >>expected &&
