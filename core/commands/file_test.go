8e6796f2a0ee8dba4d8a19d1e2e0923be534e062
Abhinav Prakash <abhinav.prakash319@gmail.com>
2025-02-16 19:03:41 +0530

    fix::> Proper test env ready
    Signed-off-by: Abhinav Prakash <abhinav.prakash319@gmail.com>

--- core/commands/file_test.go
+++ core/commands/file_test.go
@@ -1,71 +1,126 @@
 package commands
 
 import (
-	"bytes"
 	"context"
-	"fmt"
+	"io"
+	"strings"
 	"testing"
 
 	cmds "github.com/ipfs/go-ipfs-cmds"
-	"github.com/ipfs/kubo/core"
-	"github.com/stretchr/testify/require"
+	coremock "github.com/ipfs/kubo/core/mock"
 )
 
-type testenv struct {
-	ctx  context.Context
-	node *core.IpfsNode
-}
+// type testenv struct {
+// 	ctx     context.Context
+// 	node    *core.IpfsNode
+// 	reqchan chan<- string
+// }
 
-type writeCloser struct {
-	*bytes.Buffer
-}
+// type writeCloser struct {
+// 	*bytes.Buffer
+// }
 
-func (w writeCloser) Close() error { return nil }
+// func (w writeCloser) Close() error { return nil }
 
-func createTestEnv(t *testing.T) cmds.Environment {
-	// Create a new IPFS node for testing
-	ctx := context.Background()
-	node, err := core.NewNode(ctx, &core.BuildCfg{
-		Online: false,
-		Repo:   nil,
-	})
-	require.NoError(t, err)
+// func createTestEnv(t *testing.T, ctx context.Context) cmds.Environment {
+// 	ds := sync.MutexWrap(datastore.NewMapDatastore())
+// 	node, err := core.NewNode(ctx, &core.BuildCfg{
+// 		Online: false,
+// 		Repo: &repo.Mock{
+// 			C: config.Config{
+// 				Identity: config.Identity{
+// 					PeerID: "QmTFauExutTsy4XP6JbMFcw2Wa9645HJt2bTqL6qYDCKfe",
+// 				},
+// 			},
+// 			D: ds,
+// 		},
+// 	})
+// 	require.NoError(t, err)
 
-	return &testenv{
-		ctx:  ctx,
-		node: node,
+// 	return &testenv{
+// 		ctx:  ctx,
+// 		node: node,
+// 	}
+
+// }
+
+// func TestCopyCBORtoMFS(t *testing.T) {
+// 	// mock environment creation
+// 	ctx := context.Background()
+// 	env := createTestEnv(t, ctx)
+
+// 	cborCid := "bafyreigbtj4x7ip5legnfznufuopl4sg4knzc2cof6duas4b3q2fy6swua"
+
+// 	req := &cmds.Request{
+// 		Context: ctx,
+// 		Arguments: []string{
+// 			"/ipfs/" + cborCid,
+// 			"/test-cbor",
+// 		},
+// 		Options: map[string]interface{}{
+// 			filesFlushOptionName: true,
+// 		},
+// 	}
+
+// 	// mock response emitter
+// 	w := writeCloser{new(bytes.Buffer)}
+// 	require.NoError(t, err, "creating response emitter should not fail")
+
+// 	err = filesCpCmd.Run(req, res, env)
+// 	if err != nil {
+// 		t.Logf("Error during file copy: %v", err) // Print actual error
+// 		fmt.Println("Error:", err)                // Alternative direct print
+// 	}
+
+// 	require.Error(t, err, "copying dag-cbor should fail")
+// 	require.Contains(t, err.Error(), "must be a UnixFS node or raw data")
+// 	// return fmt.Errorf("cp: source must be a UnixFS node or raw data")
+// }
+
+func TestFilesCp_DagCborNodeFails(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// _, err := coremock.NewMockNode()
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
+
+	cmdCtx, err := coremock.MockCmdsCtx()
+	if err != nil {
+		t.Fatal(err)
 	}
-}
 
-func TestCopyCBORtoMFS(t *testing.T) {
-	// mock environment creation
-	env := createTestEnv(t)
+	// env := cmdenv.Environment{
+	// 	Node:    node,
+	// 	CoreAPI: coreapi.NewCoreAPI(node),
+	// }
 
-	cborCid := "bafyreigbtj4x7ip5legnfznufuopl4sg4knzc2cof6duas4b3q2fy6swua"
-
+	cborCID := "bafyreigbtj4x7ip5legnfznufuopl4sg4knzc2cof6duas4b3q2fy6swua"
 	req := &cmds.Request{
-		Context: context.Background(),
+		Context: ctx,
 		Arguments: []string{
-			"/ipfs/" + cborCid,
-			"/test-cbor",
+			"/ipfs/" + cborCID,
+			"/test-destination",
 		},
 		Options: map[string]interface{}{
-			filesFlushOptionName: true,
+			"force": false,
 		},
 	}
 
-	// mock response emitter
-	w := writeCloser{new(bytes.Buffer)}
-	res, err := cmds.NewWriterResponseEmitter(w, req)
-	require.NoError(t, err, "creating response emitter should not fail")
-
-	err = filesCpCmd.Run(req, res, env)
+	_, pw := io.Pipe()
+	res, err := cmds.NewWriterResponseEmitter(pw, req)
 	if err != nil {
-		t.Logf("Error during file copy: %v", err) // Print actual error
-		fmt.Println("Error:", err)                // Alternative direct print
+		t.Fatal(err)
+	}
+	err = filesCpCmd.Run(req, res, &cmdCtx)
+
+	if err == nil {
+		t.Fatal("expected error but got nil")
 	}
 
-	require.Error(t, err, "copying dag-cbor should fail")
-	// require.Contains(t, err.Error(), "must be a UnixFS node or raw data")
-	// return fmt.Errorf("cp: source must be a UnixFS node or raw data")
+	expectedErr := "cp: source must be a UnixFS node or raw data"
+	if !strings.Contains(err.Error(), expectedErr) {
+		t.Errorf("got error %q, want %q", err.Error(), expectedErr)
+	}
 }
