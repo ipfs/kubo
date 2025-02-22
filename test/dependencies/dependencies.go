96215c5cfa2fba686e96b65685ac2629643a39cd
Andrew Gillis <11790789+gammazero@users.noreply.github.com>
2025-02-21 15:11:58 -1000

    Update to new go-test (#10729)

--- test/dependencies/dependencies.go
+++ test/dependencies/dependencies.go
@@ -7,7 +7,7 @@ import (
 	_ "github.com/Kubuxu/gocovmerge"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/ipfs/go-cidutil/cid-fmt"
-	_ "github.com/ipfs/go-test/random/files/random-files"
+	_ "github.com/ipfs/go-test/cli/random-files"
 	_ "github.com/ipfs/hang-fds"
 	_ "github.com/jbenet/go-random/random"
 	_ "github.com/multiformats/go-multihash/multihash"
