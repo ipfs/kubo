30bdc1549fb2f4140d1997cf09c89b3b3f9f4e1e
Marcin Rataj <lidel@lidel.org>
2025-04-30 14:33:27 +0200

    fix(config): skip unset Provide.Strategy
    we dont do that for Reprovider.Strategy which is more important,
    this should reduce user confusion

--- config/provider.go
+++ config/provider.go
@@ -4,7 +4,9 @@ const (
 	DefaultProviderWorkerCount = 64
 )
 
+// Provider configuration describes how NEW CIDs are announced the moment they are created.
+// For periodical reprovide configuration, see Reprovider.*
 type Provider struct {
-	Strategy    string           // Which keys to announce
+	Strategy    *OptionalString  `json:",omitempty"` // Unused, you are likely looking for Reprovider.Strategy instead
 	WorkerCount *OptionalInteger `json:",omitempty"` // Number of concurrent provides allowed, 0 means unlimited
 }
