5d605bc47273e1068f5e6217974c0f467da6b588
Hector Sanjuan <code@hector.link>
2025-09-05 15:28:46 +0200

    corehttp/routing: wire GetClosestPeers()
    This allows Kubo to respond to the GetClosestPeers() http routing v1 endpoint
    as spec'ed here: https://github.com/ipfs/specs/pull/476
    
    It is based on work from https://github.com/ipfs/boxo/pull/1021
    
    We let IpfsNode implmement the contentRouter.Client interface with the new
    method.  We use our DHTs to get the closest peers. We try to respect the
    count/closerThan options here.  We then trigger FindPeers lookups to fill-in
    information about the peers (addresses) and return the result.
    
    Tests are missing and will come up once discussions around the spec and the
    boxo pr have settled.

--- core/corehttp/routing.go
+++ core/corehttp/routing.go
@@ -96,6 +96,57 @@ func (r *contentRouter) PutIPNS(ctx context.Context, name ipns.Name, record *ipn
 	return r.n.Routing.PutValue(ctx, string(name.RoutingKey()), raw)
 }
 
+func (r *contentRouter) GetClosestPeers(ctx context.Context, pid peer.ID) (iter.ResultIter[*types.PeerRecord], error) {
+	// Per the spec, if the peer ID is empty, we should use self.
+	if pid == "" {
+		pid = r.n.Identity
+	}
+
+	peers, err := r.n.DHT.WAN.GetClosestPeers(ctx, string(pid))
+	if err != nil {
+		return nil, err
+	}
+
+	lanPeers, err := r.n.DHT.LAN.GetClosestPeers(ctx, string(pid))
+	if err != nil {
+		return nil, err
+	}
+	peers = append(peers, lanPeers...)
+
+	// We have some DHT-closest peers. Find addresses for them.  We can
+	// use any routers for that, we can find records for DHT peers on
+	// non-DHT routers with whatever protocols. FIXME: right? right??
+	var records []*types.PeerRecord
+	for _, p := range peers {
+		record := types.PeerRecord{
+			ID:     &p,
+			Schema: types.SchemaPeer,
+			// we dont seem to care about protocol/extra infos
+			// FIXME: should FindPeers care? That info seems to
+			// not cross the FindPeer API.
+		}
+		// FindPeers will an iterator with a single item because
+		// that's how it's implemented above. Treat it as if it
+		// returned several records for a peer anyways. And merge them into the one above.
+		peerIter, err := r.FindPeers(ctx, p, -1)
+		if err != nil {
+			continue
+		}
+		defer peerIter.Close()
+
+		for peerIter.Next() {
+			val := peerIter.Val()
+			if val.Err != nil {
+				continue
+			}
+			record.Addrs = append(record.Addrs, val.Val.Addrs...)
+		}
+		records = append(records, &record)
+	}
+
+	return iter.ToResultIter(iter.FromSlice(records)), nil
+}
+
 type peerChanIter struct {
 	ch     <-chan peer.AddrInfo
 	cancel context.CancelFunc
