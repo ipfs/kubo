edc2cadc85a17ba30c5c1fd5b48c7813bf953e56
Andrew Gillis <11790789+gammazero@users.noreply.github.com>
2025-12-19 17:08:22 -0800

    output stdout and stderr on example test failure (#11119)

--- docs/examples/kubo-as-a-library/main_test.go
+++ docs/examples/kubo-as-a-library/main_test.go
@@ -9,7 +9,11 @@ import (
 func TestExample(t *testing.T) {
 	out, err := exec.Command("go", "run", "main.go").Output()
 	if err != nil {
-		t.Fatalf("running example (%v)", err)
+		var stderr string
+		if xe, ok := err.(*exec.ExitError); ok {
+			stderr = string(xe.Stderr)
+		}
+		t.Fatalf("running example (%v): %s\n%s", err, string(out), stderr)
 	}
 	if !strings.Contains(string(out), "All done!") {
 		t.Errorf("example did not run successfully")
